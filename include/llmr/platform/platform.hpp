0b95c1064dae7ff4449858e4ea63d3750fe6f05c
Konstantin Käfer <mail@kkaefer.com>
2014-01-27 17:53:52 +0100

    add platform function comments

--- include/llmr/platform/platform.hpp
+++ include/llmr/platform/platform.hpp
@@ -9,8 +9,16 @@ class Tile;
 
 namespace platform {
 
+// Restarts painting. This could for example trigger the event loop of the
+// controlling application.
 void restart(void *obj);
+
+// Download a tile. Obtain the z/x/y from the tile shared_ptr.
+// You should perform the download and parsing of the tile in a separate thread!
+// Then, call map.tileLoaded(tile); or map.tileFailed(tile); in the main thread.
 void request(void *obj, std::shared_ptr<Tile> tile);
+
+// Returns a relative timestamp in seconds. This value must be monotonic.
 double time();
 
 }
