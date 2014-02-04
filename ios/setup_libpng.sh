f754db4c7a77d2991d77742409150796f1e97018
Dane Springmeyer <dane@dbsgeo.com>
2014-02-04 13:29:52 -0800

    fix include directory, remove tabs, get latest from github

--- ios/setup_libpng.sh
+++ ios/setup_libpng.sh
@@ -4,14 +4,15 @@ if [ ! -d 'mapnik-packaging' ]; then
   git clone --depth=0 https://github.com/mapnik/mapnik-packaging.git
 fi
 cd mapnik-packaging/osx/
+git pull
 export CXX11=true
 (source iPhoneSimulator.sh; ./scripts/build_png.sh; \
- source MacOSX.sh; 			./scripts/build_png.sh; \
- source iPhoneOS.sh; 		./scripts/build_png.sh; \
- source iPhoneOSs.sh;  		./scripts/build_png.sh; \
- source iPhoneOS64.sh;  	./scripts/build_png.sh; \
+ source MacOSX.sh;          ./scripts/build_png.sh; \
+ source iPhoneOS.sh;        ./scripts/build_png.sh; \
+ source iPhoneOSs.sh;       ./scripts/build_png.sh; \
+ source iPhoneOS64.sh;      ./scripts/build_png.sh; \
  ./scripts/make_universal.sh)
 export UNIVERSAL_LIBS=`pwd`/out/build-cpp11-libcpp-universal/
-export PNG_INCLUDES=`pwd`/out/build-cpp11-libcpp-i386/
+export PNG_INCLUDES=`pwd`/out/build-cpp11-libcpp-i386/include
 cd ../../
 ./configure --png-libpath=${UNIVERSAL_LIBS} --png-includes=${PNG_INCLUDES}
