06e23543cb60834fb7342c9bcbaf32b548c29d75
Konstantin Käfer <mail@kkaefer.com>
2014-02-19 14:48:34 +0100

    don't use degenerate vertices

--- src/shader/point.vertex.glsl
+++ src/shader/point.vertex.glsl
@@ -4,7 +4,6 @@ uniform mat4 u_matrix;
 uniform float u_size;
 
 void main() {
-    float z = step(32767.0, a_pos.x) * 2.0;
-    gl_Position = u_matrix * vec4(a_pos, z, 1);
+    gl_Position = u_matrix * vec4(a_pos, 0, 1);
     gl_PointSize = u_size;
 }
