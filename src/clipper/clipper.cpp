ae3bd04171a671a877c3240ed505096186c280cc
Konstantin Käfer <mail@kkaefer.com>
2014-02-27 16:07:17 +0100

    fix include

--- src/clipper/clipper.cpp
+++ src/clipper/clipper.cpp
@@ -38,7 +38,7 @@
 *                                                                              *
 *******************************************************************************/
 
-#include "clipper.hpp"
+#include <clipper/clipper.hpp>
 #include <cmath>
 #include <vector>
 #include <algorithm>
