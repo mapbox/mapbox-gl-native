60229ed7db4e58bf9b6c8fdf36fc3e20ae656351
Konstantin Käfer <mail@kkaefer.com>
2014-03-11 18:42:01 +0100

    add color parser

---
+++ include/llmr/util/color.hpp
@@ -0,0 +1,45 @@
+// (c) Dean McNamee <dean@gmail.com>, 2012.
+// C++ port by Konstantin Käfer <mail@kkaefer.com, 2014.
+//
+// https://github.com/deanm/css-color-parser-js
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+#ifndef LLMR_UTIL_COLOR
+#define LLMR_UTIL_COLOR
+
+#include <string>
+
+namespace llmr {
+namespace util {
+
+struct CSSColor {
+    inline CSSColor() {}
+    inline CSSColor(unsigned char r, unsigned char g, unsigned char b, float a)
+        : r(r), g(g), b(b), a(a) {}
+    unsigned char r = 0, g = 0, b = 0;
+    float a = 1.0f;
+};
+
+CSSColor parseCSSColor(std::string str);
+
+}
+}
+
+#endif
+
