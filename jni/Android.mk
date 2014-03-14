574ee140941f4f633207251326ba8e4ec38a10e0
Tom MacWright <tom@macwright.org>
2014-03-13 21:55:08 -0700

    Update png build script

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE    := libpng
+LOCAL_SRC_FILES := libpng16.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
 
 NDK_TOOLCHAIN_VERSION=clang3.3
 
@@ -13,14 +18,16 @@ CXXFLAGS = -v -Wall -Wextra -std=c++11 -Wno-unused-parameter -latomic
 LOCAL_LDLIBS := -llog -lGLESv2 -latomic
 LOCAL_LDFLAGS += -DLINKER_DEBUG=1 -v
 
-LOCAL_STATIC_LIBRARIES := png
+LOCAL_STATIC_LIBRARIES := libpng
 
-# LOCAL_SRC_FILES := libpng.a
+LOCAL_SRC_FILES    += libpng.a
 LOCAL_SRC_FILES    += request.cpp
 LOCAL_SRC_FILES    += settings.cpp
 LOCAL_SRC_FILES    += llmr.cpp
 LOCAL_SRC_FILES    += $(wildcard src/*/*.cpp)
 LOCAL_SRC_FILES    += $(wildcard src/libtess2/*.c)
 
-
 include $(BUILD_SHARED_LIBRARY)
+
+NDK_MODULE_PATH=lib
+# $(call import-module,libpng)
--- libpng.sh
+++ libpng.sh
@@ -1,11 +1,20 @@
 mkdir -p libpngtmp
 
+echo "setting platform ${PLATFORM_PREFIX}"
 cd libpngtmp
 
 
-wget "http://prdownloads.sourceforge.net/libpng/libpng-1.6.9.tar.gz?download"
-tar xzvf "libpng-1.6.9.tar.gz?download"
+# wget "http://prdownloads.sourceforge.net/libpng/libpng-1.6.9.tar.gz?download" -O libpng-1.6.9.tar.gz
+# tar xzvf "libpng-1.6.9.tar.gz"
 cd libpng-1.6.9
-./configure --host=arm-linux-androideabi --prefix=$PLATFORM_PREFIX
+./configure \
+    --host=arm-linux-androideabi \
+    CC=arm-linux-androideabi-clang3.3 \
+    STRIP=arm-linux-androideabi-strip \
+    --prefix=$PLATFORM_PREFIX \
+    AR=arm-linux-androideabi-ar \
+    RANLIB=arm-linux-androideabi-ranlib
 make
 make install
+cd ../../
+cp ./android-ndk-r9c/platforms/android-19/lib/libpng* jni
