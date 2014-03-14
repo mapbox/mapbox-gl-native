da221a58fdea013025d28e7d2869173d133362e4
Tom MacWright <tom@macwright.org>
2014-03-13 21:57:09 -0700

    Porting over other necessary tweaks

--- src/platform/gl.cpp
+++ src/platform/gl.cpp
@@ -18,6 +18,10 @@ void _CHECK_GL_ERROR(const char *cmd, const char *file, int line) {
         }
 
         std::cerr << "GL_" << error.c_str() << " - " << file << ":" << line << std::endl;
+
+        #ifdef __ANDROID__
+        #else
         exit(1);
+        #endif
     }
 }
