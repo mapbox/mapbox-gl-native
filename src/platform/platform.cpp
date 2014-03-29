1915c39eeee26d0f39a6fd0a3df1ce3d4be070b9
Justin R. Miller <incanus@codesorcery.net>
2014-03-29 10:01:37 -0700

    change sprite URL to more reliable endpoint

--- src/platform/platform.cpp
+++ src/platform/platform.cpp
@@ -2,7 +2,7 @@
 
 const char *llmr::kTileVectorURL = "http://a.gl-api-us-east-1.tilestream.net/v3/mapbox.mapbox-streets-v4/%d/%d/%d.gl.pbf";
 const char *llmr::kTileRasterURL = "https://a.tiles.mapbox.com/v3/justin.map-pgygbwdm/%d/%d/%d%s.png256";
-const char *llmr::kSpriteURL = "https://dl.dropboxusercontent.com/u/575564/sprite";
+const char *llmr::kSpriteURL = "http://mapbox-kkaefer.s3.amazonaws.com/static/sprite";
 
 const int32_t llmr::kTileVectorMaxZoom = 14;
 const int32_t llmr::kTileRasterMaxZoom = 21;
