a09fdc82b1922f94b4ec3e2d397269cdf325f90e
Konstantin Käfer <mail@kkaefer.com>
2014-04-08 15:02:12 -0400

    update the mac os x http implementation to the new api

--- macosx/main.mm
+++ macosx/main.mm
@@ -7,7 +7,6 @@
 
 #include <cstdio>
 
-
 int main() {
     llmr::Settings_MacOSX settings;
     llmr::Map map(settings);
@@ -17,50 +16,48 @@ int main() {
     return 0;
 }
 
-
-
 namespace llmr {
 namespace platform {
 
 class Request {
 public:
-    uint64_t identifier;
+    Request(NSURLSessionDataTask *task, const std::string &original_url)
+        : task(task), original_url(original_url) {}
+    NSURLSessionDataTask *task;
     std::string original_url;
 };
 
-Request *request_http(std::string url, std::function<void(Response *)> fn) {
-    fprintf(stderr, "[request_http request thread: %lx]\n", uv_thread_self());
+std::shared_ptr<Request> request_http(const std::string &url, std::function<void(Response *)> fn) {
+    __block std::shared_ptr<Request> req;
     NSURLSessionDataTask *task = [[NSURLSession sharedSession]
                                   dataTaskWithURL:[NSURL URLWithString:@(url.c_str())]
                                   completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        Response *res = new Response();
+        // Make sure we clear the shared_ptr to resolve the circular reference. We're referencing
+        // this shared_ptr by value so that the object stays around until this completion handler is
+        // invoked.
+        req.reset();
+
+        Response res;
 
         if (!error && [response isKindOfClass:[NSHTTPURLResponse class]]) {
-            res->code = [(NSHTTPURLResponse *)response statusCode];
-            res->body = { (const char *)[data bytes], [data length] };
+            res.code = [(NSHTTPURLResponse *)response statusCode];
+            res.body = {(const char *)[data bytes], [data length]};
         }
 
-        fn(res);
+        fn(&res);
     }];
+
+    req = std::make_shared<Request>(task, url);
 
     [task resume];
 
-    return new Request({
-        task.taskIdentifier,
-        url
-    });;
+    return req;
 }
 
-void cancel_request_http(Request *request) {
-    [[NSURLSession sharedSession]
-     getTasksWithCompletionHandler:^(NSArray *dataTasks, NSArray *uploadTasks, NSArray *downloadTasks) {
-        for (NSURLSessionDownloadTask *task in downloadTasks) {
-            if (task.taskIdentifier == request->identifier) {
-                return [task cancel];
-            }
-        }
-    }];
+void cancel_request_http(const std::shared_ptr<Request> &req) {
+    if (req) {
+        [req->task cancel];
+    }
 }
-
 }
 }
