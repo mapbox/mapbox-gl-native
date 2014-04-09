189b8951456ad1cd381f23516e70eff3fb72c359
Konstantin Käfer <mail@kkaefer.com>
2014-04-09 18:21:15 -0400

    build libuv

--- setup-libraries.sh
+++ setup-libraries.sh
@@ -6,29 +6,34 @@ if [ ! -d 'mapnik-packaging' ]; then
 fi
 
 cd mapnik-packaging/osx/
-git pull
+# git pull
 
 export CXX11=true
 
 source iPhoneOS.sh
     if [ ! -f out/build-cpp11-libcpp-armv7/lib/libpng.a ] ; then ./scripts/build_png.sh ; fi
+    if [ ! -f out/build-cpp11-libcpp-armv7/lib/libuv.a ] ; then ./scripts/build_uv.sh ; fi
     echo '     ...done'
 
 source iPhoneOSs.sh
     if [ ! -f out/build-cpp11-libcpp-armv7s/lib/libpng.a ] ; then ./scripts/build_png.sh ; fi
+    if [ ! -f out/build-cpp11-libcpp-armv7s/lib/libuv.a ] ; then ./scripts/build_uv.sh ; fi
     echo '     ...done'
 
 source iPhoneOS64.sh
     if [ ! -f out/build-cpp11-libcpp-arm64/lib/libpng.a ] ; then ./scripts/build_png.sh ; fi
+    if [ ! -f out/build-cpp11-libcpp-arm64/lib/libuv.a ] ; then ./scripts/build_uv.sh ; fi
     echo '     ...done'
 
 source iPhoneSimulator.sh
     if [ ! -f out/build-cpp11-libcpp-i386/lib/libpng.a ] ; then ./scripts/build_png.sh ; fi
+    if [ ! -f out/build-cpp11-libcpp-i386/lib/libuv.a ] ; then ./scripts/build_uv.sh ; fi
     echo '     ...done'
 
 source MacOSX.sh
     if [ ! -f out/build-cpp11-libcpp-x86_64/lib/libpng.a ] ; then ./scripts/build_png.sh ; fi
     if [ ! -f out/build-cpp11-libcpp-x86_64/lib/libglfw3.a ] ; then ./scripts/build_glfw.sh ; fi
+    if [ ! -f out/build-cpp11-libcpp-x86_64/lib/libuv.a ] ; then ./scripts/build_uv.sh ; fi
     if [ ! -f out/build-cpp11-libcpp-x86_64/lib/libssl.a ] ; then ./scripts/build_openssl.sh ; fi
     if [ ! -f out/build-cpp11-libcpp-x86_64/lib/libcurl.a ] ; then ./scripts/build_curl.sh ; fi
     echo '     ...done'
