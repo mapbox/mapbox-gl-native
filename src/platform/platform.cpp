0b347a4f3ca41efe89b1b01c56bb8b6fcd680c15
Konstantin Käfer <mail@kkaefer.com>
2014-04-11 10:20:08 -0400

    use satellite tiles

--- src/platform/platform.cpp
+++ src/platform/platform.cpp
@@ -1,7 +1,7 @@
 #include <llmr/platform/platform.hpp>
 
 const char *llmr::kTileVectorURL = "http://a.gl-api-us-east-1.tilestream.net/v3/mapbox.mapbox-streets-v4/%d/%d/%d.gl.pbf";
-const char *llmr::kTileRasterURL = "https://a.tiles.mapbox.com/v3/justin.map-pgygbwdm/%d/%d/%d%s.png256";
+const char *llmr::kTileRasterURL = "http://a.tiles.mapbox.com/v3/kkaefer.holbci77/%d/%d/%d.png";
 const char *llmr::kSpriteURL = "http://mapbox-kkaefer.s3.amazonaws.com/static/sprite";
 
 const int32_t llmr::kTileVectorMaxZoom = 14;
