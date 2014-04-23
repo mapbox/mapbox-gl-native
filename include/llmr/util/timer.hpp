39d79f4fdc6f0fc3974269f5e23f3cc819ced43f
Konstantin Käfer <mail@kkaefer.com>
2014-04-23 17:17:23 +0200

    add timer

---
+++ include/llmr/util/timer.hpp
@@ -0,0 +1,29 @@
+#ifndef LLMR_UTIL_TIMER
+#define LLMR_UTIL_TIMER
+
+#include <iostream>
+#include <string>
+#include <chrono>
+
+namespace llmr {
+namespace util {
+
+class timer {
+    using hr = std::chrono::high_resolution_clock;
+public:
+    timer(const std::string &name) : name(name), start(hr::now()) {}
+    ~timer() {
+        using namespace std::chrono;
+        std::cerr << name << ": "
+                  << (double)duration_cast<microseconds>(hr::now() - start).count() / 1000
+                  << "ms" << std::endl;
+    }
+
+private:
+    const std::string name;
+    const hr::time_point start;
+};
+}
+}
+
+#endif
