6ed5c0ddf3d940b8fb6c4517984b25e0e5d9d002
Konstantin Käfer <mail@kkaefer.com>
2014-05-26 15:35:08 +0200

    use sigma 1 for blur by default

--- src/shader/gaussian.fragment.glsl
+++ src/shader/gaussian.fragment.glsl
@@ -1,14 +1,12 @@
 uniform sampler2D u_image;
 
-varying vec2 v_coords[5];
+varying vec2 v_coords[3];
 
 void main()
 {
     vec4 sum = vec4(0.0);
-    sum += texture2D(u_image, v_coords[0]) * 0.204164;
-    sum += texture2D(u_image, v_coords[1]) * 0.304005;
-    sum += texture2D(u_image, v_coords[2]) * 0.304005;
-    sum += texture2D(u_image, v_coords[3]) * 0.093913;
-    sum += texture2D(u_image, v_coords[4]) * 0.093913;
+    sum += texture2D(u_image, v_coords[0]) * 0.40261994689424746;
+    sum += texture2D(u_image, v_coords[1]) * 0.2986900265528763;
+    sum += texture2D(u_image, v_coords[2]) * 0.2986900265528763;
     gl_FragColor = sum;
 }
--- src/shader/gaussian.vertex.glsl
+++ src/shader/gaussian.vertex.glsl
@@ -3,15 +3,13 @@ attribute vec2 a_pos;
 uniform mat4 u_matrix;
 uniform vec2 u_offset;
 
-varying vec2 v_coords[5];
+varying vec2 v_coords[3];
 
 void main() {
     gl_Position = u_matrix * vec4(a_pos, 0, 1);
 
     vec2 tex = (gl_Position.xy + 1.0) / 2.0;
     v_coords[0] = tex;
-    v_coords[1] = tex + u_offset * 1.407333;
-    v_coords[2] = tex - u_offset * 1.407333;
-    v_coords[3] = tex + u_offset * 3.294215;
-    v_coords[4] = tex - u_offset * 3.294215;
+    v_coords[1] = tex + u_offset * 1.1824255238063563;
+    v_coords[2] = tex - u_offset * 1.1824255238063563;
 }
