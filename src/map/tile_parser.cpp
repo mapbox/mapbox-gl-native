83b3b0bb6a2cc557deebf1cd59486c3fe0e1e063
Konstantin Käfer <mail@kkaefer.com>
2014-05-28 22:18:18 +0200

    create a copy

--- src/map/tile_parser.cpp
+++ src/map/tile_parser.cpp
@@ -205,7 +205,8 @@ std::unique_ptr<Bucket> TileParser::createTextBucket(const VectorTileLayer& laye
         glyphStore.waitForGlyphRanges(bucket_desc.geometry.font, ranges);
     }
 
-    const FontStack &fontStack = glyphStore.getFontStack(bucket_desc.geometry.font);
+    // Create a copy!
+    const FontStack fontStack = glyphStore.getFontStack(bucket_desc.geometry.font);
 
     // Shape and place all labels.
     {
