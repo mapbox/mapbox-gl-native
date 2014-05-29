e0dacd465b3d3643d47e4ad06e9898df7210acda
Dane Springmeyer <dane@mapbox.com>
2014-05-29 10:46:32 -0700

    [build] chmod +x scripts + only upgrade gcc on ubuntu precise (12.04)

--- scripts/install_build_deps_linux.sh
+++ scripts/install_build_deps_linux.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-sudo apt-get update -qq
+if [[ `lsb_release -r` =~ "12.04" ]]; then
+    sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+fi
+sudo apt-get update -y
 sudo apt-get -y install gcc-4.8 g++-4.8
-sudo apt-get -y install pkg-config nodejs cmake automake libtool xutils-dev
+sudo apt-get -y install pkg-config nodejs cmake automake libtool xutils-dev git
 sudo apt-get -y install libxi-dev libglu1-mesa-dev x11proto-randr-dev x11proto-xext-dev libxrandr-dev x11proto-xf86vidmode-dev libxxf86vm-dev libxcursor-dev
--- scripts/install_build_deps_osx.sh
+++ scripts/install_build_deps_osx.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-brew install autoconf automake libtool makedepend cmake pkg-config node
+brew install autoconf automake libtool makedepend cmake pkg-config node git
