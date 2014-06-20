8a36f25983953c1cf7cb8044f872f116e8fbdb53
Leith Bade <leith@leithalweapon.geek.nz>
2014-06-20 16:17:56 +1000

    New Android project

---
+++ android/src/com/mapbox/mapboxgl/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mapbox.mapboxgl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
