8140827f285c8c30d5ea84c096ca6f92f060381f
Justin R. Miller <incanus@codesorcery.net>
2014-06-20 11:20:11 -0700

    refs #168: no longer need zooms

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -172,7 +172,6 @@ void Map::setup() {
                            painter,
                            kVectorTileURL,
                            Source::Type::vector,
-                           {{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }},
                            512,
                            0,
                            14,
