1d291f5bdb0ae5712b3c9391c21584fd965f9579
Leith Bade <leith@leithalweapon.geek.nz>
2014-06-22 23:21:54 +1000

    Properly check EGL configs

--- android/jni/NativeMapView.cpp
+++ android/jni/NativeMapView.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include <memory>
+
 #include <GLES2/gl2.h>
 
 #include <llmr/platform/platform.hpp>
@@ -90,8 +92,10 @@ bool NativeMapView::initializeContext(ANativeWindow* window) {
 
 	const EGLint config_attribs[] = {
 		EGL_CONFIG_CAVEAT, EGL_NONE,
+		EGL_CONFORMANT, EGL_OPENGL_ES2_BIT,
 		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
 		EGL_COLOR_BUFFER_TYPE, EGL_RGB_BUFFER,
+		EGL_BUFFER_SIZE, 32, // Ensure we get 32bit color buffer on Tegra, 24 bit will be sorted first without it (slow software mode)
 		EGL_RED_SIZE, 8,
 		EGL_GREEN_SIZE, 8,
 		EGL_BLUE_SIZE, 8,
@@ -99,10 +103,9 @@ bool NativeMapView::initializeContext(ANativeWindow* window) {
 		EGL_STENCIL_SIZE, 8,
 		EGL_NONE
 	};
-	EGLConfig config;
 	EGLint num_configs;
-	if (!eglChooseConfig(display, config_attribs, &config, 1, &num_configs)) {
-		ERROR("eglChooseConfig() returned error %d", eglGetError());
+	if (!eglChooseConfig(display, config_attribs, nullptr, 0, &num_configs)) {
+		ERROR("eglChooseConfig(NULL) returned error %d", eglGetError());
 		terminateContext();
 		return false;
 	}
@@ -112,8 +115,23 @@ bool NativeMapView::initializeContext(ANativeWindow* window) {
 		return false;
 	}
 
+	const std::unique_ptr<EGLConfig[]> configs(new EGLConfig[num_configs]);
+	if (!eglChooseConfig(display, config_attribs, configs.get(), num_configs, &num_configs)) {
+		ERROR("eglChooseConfig() returned error %d", eglGetError());
+		terminateContext();
+		return false;
+	}
+
+	int chosen_config = chooseConfig(configs.get(), num_configs);
+	if (chosen_config < 0) {
+		ERROR("No config chosen");
+		terminateContext();
+		return false;
+	}
+	DEBUG("Chosen config is %d", chosen_config);
+
 	EGLint format;
-	if (!eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format)) {
+	if (!eglGetConfigAttrib(display, configs[chosen_config], EGL_NATIVE_VISUAL_ID, &format)) {
 		ERROR("eglGetConfigAttrib() returned error %d", eglGetError());
 		terminateContext();
 		return false;
@@ -125,7 +143,7 @@ bool NativeMapView::initializeContext(ANativeWindow* window) {
 	const EGLint surface_attribs[] = {
 		EGL_NONE
 	};
-	surface = eglCreateWindowSurface(display, config, window, surface_attribs);
+	surface = eglCreateWindowSurface(display, configs[chosen_config], window, surface_attribs);
 	if (surface == EGL_NO_SURFACE) {
 		ERROR("eglCreateWindowSurface() returned error %d", eglGetError());
 		terminateContext();
@@ -144,7 +162,7 @@ bool NativeMapView::initializeContext(ANativeWindow* window) {
 		EGL_CONTEXT_CLIENT_VERSION, 2,
 		EGL_NONE
 	};
-	context = eglCreateContext(display, config, EGL_NO_CONTEXT, context_attribs);
+	context = eglCreateContext(display, configs[chosen_config], EGL_NO_CONTEXT, context_attribs);
 	if (context == EGL_NO_CONTEXT) {
 		ERROR("eglCreateContext() returned error %d", eglGetError());
 		terminateContext();
@@ -177,6 +195,105 @@ bool NativeMapView::initializeContext(ANativeWindow* window) {
 	map->start();
 
 	return true;
+}
+
+int NativeMapView::chooseConfig(const EGLConfig configs[], EGLint num_configs) {
+	INFO("Found %d configs", num_configs);
+
+	int chosen_config = -1;
+	for (int i = 0; i < num_configs; i++) {
+		INFO("Config %d:", i);
+
+		EGLint bits, red, green, blue, alpha, alpha_mask, depth, stencil, sample_buffers, samples;
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_BUFFER_SIZE, &bits)) {
+			ERROR("eglGetConfigAttrib(EGL_BUFFER_SIZE) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_RED_SIZE, &red)) {
+			ERROR("eglGetConfigAttrib(EGL_RED_SIZE) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_GREEN_SIZE, &green)) {
+			ERROR("eglGetConfigAttrib(EGL_GREEN_SIZE) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_BLUE_SIZE, &blue)) {
+			ERROR("eglGetConfigAttrib(EGL_BLUE_SIZE) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_ALPHA_SIZE, &alpha)) {
+			ERROR("eglGetConfigAttrib(EGL_ALPHA_SIZE) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_ALPHA_MASK_SIZE, &alpha_mask)) {
+			ERROR("eglGetConfigAttrib(EGL_ALPHA_MASK_SIZE) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_DEPTH_SIZE, &depth)) {
+			ERROR("eglGetConfigAttrib(EGL_DEPTH_SIZE) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_STENCIL_SIZE, &stencil)) {
+			ERROR("eglGetConfigAttrib(EGL_STENCIL_SIZE) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_SAMPLE_BUFFERS, &sample_buffers)) {
+			ERROR("eglGetConfigAttrib(EGL_SAMPLE_BUFFERS) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		if (!eglGetConfigAttrib(display, configs[i], EGL_SAMPLES, &samples)) {
+			ERROR("eglGetConfigAttrib(EGL_SAMPLES) returned error %d", eglGetError());
+			terminateContext();
+			return false;
+		}
+
+		INFO("Color: %d", bits);
+		INFO("Red: %d", red);
+		INFO("Green: %d", green);
+		INFO("Blue: %d", blue);
+		INFO("Alpha: %d", alpha);
+		INFO("Alpha mask: %d", alpha_mask);
+		INFO("Depth: %d", depth);
+		INFO("Stencil: %d", stencil);
+		INFO("Sample buffers: %d", sample_buffers);
+		INFO("Samples: %d", samples);
+
+		bool config_ok = true;
+		config_ok &= bits == 32;
+		config_ok &= red == 8;
+		config_ok &= green == 8;
+		config_ok &= blue == 8;
+		config_ok &= (alpha == 0) || (alpha == 8); // Can be either 0 for RGBX or 8 for RGBA but we don't care either way
+		config_ok &= depth == 16;
+		config_ok &= stencil == 8;
+		config_ok &= sample_buffers == 0;
+		config_ok &= samples == 0;
+
+		if (config_ok) { // Choose the last matching config, that way we get RGBX if possible (since it is sorted highest to lowest bits)
+			chosen_config = i;
+		}
+	}
+
+	return chosen_config;
 }
 
 void NativeMapView::terminateContext() {
