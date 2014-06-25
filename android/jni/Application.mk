4ddaf6e5c062d4f5a86c3e80727db483a3fa79db
Leith Bade <leith@leithalweapon.geek.nz>
2014-06-26 01:12:56 +1000

    Experiment with optimised flags

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,7 +1,14 @@
 APP_MODULES := NativeMapView
-APP_ABI := armeabi
+APP_ABI := armeabi-v7a-hard
 APP_PLATFORM := android-19
 APP_STL := c++_static
+# Can increase to vfpv4 in clang 3.4
+APP_CFLAGS := -O3 -mfpu=vfpv3
 APP_CPPFLAGS := -fexceptions -std=c++11
 
+TARGET_CFLAGS += -mhard-float -D_NDK_MATH_NO_SOFTFP=1
+TARGET_LDFLAGS += -Wl,--no-warn-mismatch -lm_hard
+
 NDK_TOOLCHAIN_VERSION := clang
+
+#http://blog.algolia.com/android-ndk-how-to-reduce-libs-size/
