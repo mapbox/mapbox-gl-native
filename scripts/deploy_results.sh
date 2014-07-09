37bd80dd9d9a9c0e90aef411f7d494e18a02860e
Konstantin Käfer <mail@kkaefer.com>
2014-07-09 13:10:07 -0700

    add html result table

--- scripts/deploy_results.sh
+++ scripts/deploy_results.sh
@@ -3,10 +3,9 @@
 set -e
 set -o pipefail
 
-aws s3 cp test/fixtures/styles/ s3://mapbox-gl-testing/headless/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER/ --acl public-read --recursive --exclude "*" --include "*.png"
+node scripts/
 
-cd test/fixtures/styles
-for FILE in *.png ; do
-    echo http://mapbox-gl-testing.s3.amazonaws.com/headless/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER/$FILE
-done
-cd ../../..
+aws s3 cp test/fixtures/styles/ s3://mapbox-gl-testing/headless/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER/ --acl public-read --recursive --exclude "*" --include "*.actual.png" --include "*.html"
+
+
+echo http://mapbox-gl-testing.s3.amazonaws.com/headless/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER/index.html
