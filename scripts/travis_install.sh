0148942eebbaf32e477c4dd0df9e751762924b1d
Justin R. Miller <incanus@codesorcery.net>
2014-08-12 16:37:00 -0700

    prettify xcodebuild output

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -5,9 +5,14 @@
 #
 make setup
 
-#
-# we're only using a test suite on Linux
-#
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+    #
+    # we're need the test suite on Linux
+    #
     npm install git+https://github.com/mapbox/mapbox-gl-test-suite.git
+elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+    #
+    # let's pretty it up on OS X/iOS
+    #
+    gem install xcpretty --no-rdoc --no-ri --no-document --quiet
 fi
