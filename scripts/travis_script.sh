e0ea2d115bd80a1d666186c911f5378958c54010
Justin R. Miller <incanus@codesorcery.net>
2014-08-15 10:38:31 -0700

    forgot argument

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -27,5 +27,5 @@ elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
     git clone --depth 1 https://github.com/mapbox/mapbox-gl-cocoa ios/mapbox-gl-cocoa
     make iproj-cli
     xcodebuild clean -project ./build/ios/mapbox-gl-cocoa/app/mapboxgl-app.xcodeproj
-    xcodebuild ./build/ios/mapbox-gl-cocoa/app/mapboxgl-app.xcodeproj
+    xcodebuild -project ./build/ios/mapbox-gl-cocoa/app/mapboxgl-app.xcodeproj
 fi
