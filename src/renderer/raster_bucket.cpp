8c3675755e2b0692599ac887178ba0ef8689e89c
Konstantin Käfer <mail@kkaefer.com>
2014-08-15 13:39:49 +0200

    base static vertexbuffer on buffer object

--- src/renderer/raster_bucket.cpp
+++ src/renderer/raster_bucket.cpp
@@ -17,14 +17,14 @@ bool RasterBucket::setImage(const std::string &data) {
     return raster.load(data);
 }
 
-void RasterBucket::drawRaster(RasterShader& shader, VertexBuffer &vertices, VertexArrayObject &array) {
+void RasterBucket::drawRaster(RasterShader& shader, StaticVertexBuffer &vertices, VertexArrayObject &array) {
     raster.bind(true);
     shader.setImage(0);
     array.bind(shader, vertices, BUFFER_OFFSET(0));
     glDrawArrays(GL_TRIANGLES, 0, (GLsizei)vertices.index());
 }
 
-void RasterBucket::drawRaster(RasterShader& shader, VertexBuffer &vertices, VertexArrayObject &array, GLuint texture) {
+void RasterBucket::drawRaster(RasterShader& shader, StaticVertexBuffer &vertices, VertexArrayObject &array, GLuint texture) {
     raster.bind(texture);
     shader.setImage(0);
     array.bind(shader, vertices, BUFFER_OFFSET(0));
