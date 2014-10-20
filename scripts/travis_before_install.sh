6e51c1037afa1a32c6a893e5706aaa6f16b1b1c4
Mike Morris <michael.patrick.morris@gmail.com>
2014-10-20 14:55:21 -0400

    add tsan to mesa deps

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -24,7 +24,8 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     mapbox_time "install_mesa_deps" \
     sudo apt-get install -y libpthread-stubs0-dev \
                             xserver-xorg-dev x11proto-xinerama-dev libx11-xcb-dev \
-                            libxcb-glx0-dev libxrender-dev llvm-3.4 mesa-utils && \
+                            libxcb-glx0-dev libxrender-dev llvm-3.4 libtsan0 \
+                            mesa-utils && \
     sudo apt-get build-dep -y libgl1-mesa-dri libxcb-glx0-dev
 
     mapbox_time "build_mesa" \
