59d0be10760d22b279142422a47451a60ae06634
Konstantin Käfer <mail@kkaefer.com>
2014-10-21 06:57:44 -0700

    use ppa for more recent boost

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -10,11 +10,12 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     if [[ `lsb_release -r` =~ "12.04" ]]; then
         sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
     fi
+    sudo add-apt-repository --yes ppa:boost-latest/ppa
     sudo apt-get update -y
     sudo apt-get -y install gcc-4.8 g++-4.8
     sudo apt-get -y install git build-essential zlib1g-dev automake \
                             libtool xutils-dev make cmake pkg-config python-pip \
-                            libboost-dev libboost-regex-dev libcurl4-openssl-dev \
+                            libboost1.55-dev libboost-regex1.55-dev libcurl4-openssl-dev \
                             libpng-dev libsqlite3-dev
     sudo apt-get -y install libxi-dev libglu1-mesa-dev x11proto-randr-dev \
                             x11proto-xext-dev libxrandr-dev \
