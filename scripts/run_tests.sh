3b397c56ed2b851b07bca3a7c7d38cb0e7cb53f3
Konstantin Käfer <mail@kkaefer.com>
2014-10-23 03:30:05 -0700

    print core dumps on application crashes

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,8 +3,24 @@
 set -e
 set -o pipefail
 
+# allow writing core files
+ulimit -c unlimited -S
+
 cd build/${BUILDTYPE:-Release}
 
 for TEST in ./test_* ; do
-    ${TEST}
+    RESULT=0
+    ${TEST} || RESULT=$?
+
+    # output core dump if we got one
+    for DUMP in $(find ./ -maxdepth 1 -name 'core*' -print); do
+        gdb ${TEST} ${DUMP} -ex "thread apply all bt" -ex "set pagination 0" -batch
+        rm -rf ${DUMP}
+    done
+
+    # now we should present travis with the original
+    # error code so the run cleanly stops
+    if [[ ${RESULT} != 0 ]]; then
+        exit $RESULT
+    fi
 done
