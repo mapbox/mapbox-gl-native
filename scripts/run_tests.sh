959d879f05da0e40270782e823435ca6946707ad
Konstantin Käfer <mail@kkaefer.com>
2014-10-23 06:16:13 -0700

    show more debug information

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,20 +2,21 @@
 
 set -e
 set -o pipefail
-
-
+c
 cd build/${BUILDTYPE:-Release}
 
 for TEST in ./test_* ; do
     # allow writing core files
     ulimit -c unlimited -S
+    ulimit -c
+    cat /proc/sys/kernel/core_pattern
+    sysctl kernel.core_pattern
 
     RESULT=0
     ${TEST} || RESULT=$?
 
     if [[ ${RESULT} != 0 ]]; then
         echo "The program crashed with exit code ${RESULT}. We're now trying to output the core dump."
-        ls -la
     fi
 
     # output core dump if we got one
