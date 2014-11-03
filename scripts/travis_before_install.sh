6eafca787453915ef6b9e897fd2673f81047ad84
Dane Springmeyer <dane@mapbox.com>
2014-11-02 21:09:44 -0500

    disable more of mapbox_time

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -34,18 +34,18 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     sudo add-apt-repository --yes ppa:boost-latest/ppa
 
     #mapbox_time "apt_update" \
-    sudo apt-get update -y || true
+    sudo apt-get update -y
 
-    mapbox_time "install_gcc" \
+    #mapbox_time "install_gcc" \
     sudo apt-get -y install gcc-4.8 g++-4.8
 
-    mapbox_time "install_build_tools" \
+    #mapbox_time "install_build_tools" \
     sudo apt-get -y install git build-essential zlib1g-dev automake gdb \
                             libtool xutils-dev make cmake pkg-config python-pip \
                             libboost1.55-dev libcurl4-openssl-dev \
                             libpng-dev libsqlite3-dev
 
-    mapbox_time "install_opengl" \
+    #mapbox_time "install_opengl" \
     sudo apt-get -y install mesa-utils libxi-dev x11proto-randr-dev \
                             x11proto-xext-dev libxrandr-dev \
                             x11proto-xf86vidmode-dev libxxf86vm-dev \
@@ -57,7 +57,7 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     #mapbox_time "install_mesa" \
     MASON_PLATFORM= mason install mesa 10.3.1
 
-    mapbox_time "install_awscli" \
+    #mapbox_time "install_awscli" \
     sudo pip install awscli
 elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
     #
