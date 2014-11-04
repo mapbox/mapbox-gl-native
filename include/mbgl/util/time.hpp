75889d812402ac2b75d891576a1c37cb43c8eaf3
Konstantin Käfer <mail@kkaefer.com>
2014-11-04 06:44:50 -0800

    replace strftime_l with our custom formatter

--- include/mbgl/util/time.hpp
+++ include/mbgl/util/time.hpp
@@ -1,7 +1,9 @@
 #ifndef MBGL_UTIL_TIME
 #define MBGL_UTIL_TIME
 
+#include <string>
 #include <cstdint>
+#include <ctime>
 
 namespace mbgl {
 
@@ -14,6 +16,10 @@ namespace util {
 // no relation to the current time, but is continuously increasing, so
 // comparisons between different timestamps produce meaningful values.
 timestamp now();
+
+
+// Returns the RFC1123 formatted date. E.g. "Tue, 04 Nov 2014 02:13:24 GMT"
+std::string rfc1123(std::time_t time);
 
 }
 
