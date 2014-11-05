b1bfbbcc8f1b462c0980a1c214601ebe40b57a7c
Leith Bade <leith@leithalweapon.geek.nz>
2014-11-06 08:58:57 +1100

    Fix aapt issue

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -41,9 +41,8 @@ if [[ "${MASON_PLATFORM}" == "android" ]]; then
 
     echo "debug: installing SDK"
     #mapbox_time "installing SDK" \
+    sudo apt-get -y install lib32stdc++6 lib32z1
     echo y | ./android-sdk-linux/tools/android update sdk -u -a -t tools,platform-tools,build-tools-21.1.0,android-19
-
-    find find $PWD/android-sdk-linux/
 fi
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
