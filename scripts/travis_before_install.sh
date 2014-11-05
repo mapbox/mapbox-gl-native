f353b5ba55fdb2ad0a8924e2e06262433ff26356
Leith Bade <leith@leithalweapon.geek.nz>
2014-11-06 07:44:22 +1100

    Add find for travis debug

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -42,6 +42,8 @@ if [[ "${MASON_PLATFORM}" == "android" ]]; then
     echo "debug: installing SDK"
     #mapbox_time "installing SDK" \
     echo y | ./android-sdk-linux/tools/android update sdk -u -a -t tools,platform-tools,build-tools-21.1.0,android-19
+
+    find
 fi
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
