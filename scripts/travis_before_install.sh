b5021ea5fc7cd057bb02773bba9e395e46760214
Dane Springmeyer <dane@mapbox.com>
2014-11-04 23:10:13 -0500

    debug prints around failing apt-get install

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -33,31 +33,29 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     fi
     sudo add-apt-repository --yes ppa:boost-latest/ppa
 
-    #mapbox_time "apt_update" \
+    echo "apt_update"
     sudo apt-get update -y
 
-    #mapbox_time "install_gcc" \
+    echo "install_gcc"
     sudo apt-get -y install gcc-4.8 g++-4.8
 
-    #mapbox_time "install_build_tools" \
+    echo "install_build_tools"
     sudo apt-get -y install git build-essential zlib1g-dev automake gdb \
                             libtool xutils-dev make cmake pkg-config python-pip \
                             libboost1.55-dev libcurl4-openssl-dev \
                             libpng-dev libsqlite3-dev
 
-    #mapbox_time "install_opengl" \
+    echo "install_opengl"
     sudo apt-get -y install mesa-utils libxi-dev x11proto-randr-dev \
                             x11proto-xext-dev libxrandr-dev \
                             x11proto-xf86vidmode-dev libxxf86vm-dev \
                             libxcursor-dev libxinerama-dev \
                             llvm-3.4 # required for mesa
 
-
-    # TODO - figure out how to unset MASON_PLATFORM for just this command
-    #mapbox_time "install_mesa" \
+    echo "install_mesa"
     MASON_PLATFORM= mason install mesa 10.3.1
 
-    #mapbox_time "install_awscli" \
+    echo "install_awscli"
     sudo pip install awscli
 elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
     #
