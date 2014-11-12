0f4bd9a33237ee4a7ad03b283394c9e4ff682426
Justin R. Miller <incanus@codesorcery.net>
2014-11-11 16:21:07 -0800

    add dev icon burning

---
+++ update_icons.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+export PATH=$HOME/bin:/usr/local/bin:/usr/bin:/bin
+
+if [[ -z `which iconoblast` ]]; then
+    echo "Requires https://github.com/mapbox/iconoblast!"
+    exit 1
+fi
+
+commit=`git rev-parse --short HEAD`
+branch=`git rev-parse --abbrev-ref HEAD`
+repo=`git remote show origin | grep 'Fetch URL' | sed -e 's/.*github\.com:mapbox\///' -e 's/\.git$//' -e 's/^mapbox-//'`
+
+pwd=`pwd`
+cd android/java/app/src/main/res
+for folder in `find . -type d -name drawable-\* -maxdepth 1`
+do
+    cd ${folder}
+    cp icon.png icon_burned.png
+    iconoblast icon_burned.png $commit $branch $repo
+    cd ..
+done
+cd ${pwd}
