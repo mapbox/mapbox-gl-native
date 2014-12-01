8f60624becbca685369925794d377d401652f761
Leith Bade <leith@mapbox.com>
2014-12-02 00:52:43 +1100

    Fix import problem

--- include/mbgl/platform/gl.hpp
+++ include/mbgl/platform/gl.hpp
@@ -8,11 +8,6 @@
     #if TARGET_OS_IPHONE
         #include <OpenGLES/ES2/gl.h>
         #include <OpenGLES/ES2/glext.h>
-        // #define glGenVertexArrays glGenVertexArraysOES
-        // #define glBindVertexArray glBindVertexArrayOES
-        // #define glDeleteVertexArrays glDeleteVertexArraysOES
-        // #define GL_ARB_vertex_array_object 1
-        // #define GL_DEPTH24_STENCIL8 GL_DEPTH24_STENCIL8_OES
     #elif TARGET_IPHONE_SIMULATOR
         #include <OpenGLES/ES2/gl.h>
         #include <OpenGLES/ES2/glext.h>
@@ -26,7 +21,6 @@
     #include <GLES2/gl2.h>
     #include <GLES2/gl2ext.h>
 #else
-    #define GL_GLEXT_PROTOTYPES
     #include <GL/gl.h>
     #include <GL/glext.h>
 #endif
