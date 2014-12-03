3058cd2d6091426ffc36141a1f0c742f08bc42c7
Leith Bade <leith@mapbox.com>
2014-12-03 20:43:36 +1100

    Remove old TODO

--- platform/android/cache_database_data.cpp
+++ platform/android/cache_database_data.cpp
@@ -6,7 +6,6 @@ namespace platform {
 
 // Returns the path to the default cache database on this system.
 std::string defaultCacheDatabase() {
-// TODO need to use get cache dir from java
     return mbgl::android::cache_path + "/mbgl-cache.db";
 }
 
