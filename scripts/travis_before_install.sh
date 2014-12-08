8b1a2679e6171edeac8cbdec45c0349853eaaa7e
Leith Bade <leith@mapbox.com>
2014-12-09 09:13:09 +1100

    Dump running services.

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -31,6 +31,10 @@ if [[ "${MASON_PLATFORM}" == "android" ]]; then
 
         echo "inserting access token"
         sed -i "s/access token goes here/${ANDROID_KEY}/g" android/java/app/src/main/java/com/mapbox/mapboxgl/app/MapFragment.java
+        
+        service --status-all
+        
+        ps -a
 
 elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
 
