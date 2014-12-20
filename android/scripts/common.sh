42ebe442149e0fb2c6d1c061b2856eb4f5633c56
Leith Bade <leith@mapbox.com>
2014-12-20 18:37:50 +1300

    Use access key

--- android/scripts/common.sh
+++ android/scripts/common.sh
@@ -5,6 +5,13 @@ set -o pipefail
 
 NAME=$1
 
+
+export CC=clang CXX=clang++
+export CXXFLAGS="-Wno-unknown-warning-option -Wno-unused-local-typedefs -Wno-unknown-pragmas ${CXXFLAGS}";
+
 export MASON_PLATFORM=android
+
+mkdir ./android/java/app/src/main/res/raw/
+echo "${ANDROID_KEY}" >> ./android/java/app/src/main/res/raw/token.txt
 
 make android -j$JOBS BUILDTYPE=${BUILDTYPE}
