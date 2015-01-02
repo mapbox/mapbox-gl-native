b35b6e359e64274e95109b3d06ab99b4af807784
Leith Bade <leith@mapbox.com>
2015-01-02 14:01:58 +1300

    Change to ami with jq

--- android/scripts/run-build.sh
+++ android/scripts/run-build.sh
@@ -6,7 +6,7 @@ set -o pipefail
 cwd=$(pwd)
 
 region="us-east-1"
-region_ami="ami-bec4aed6"
+region_ami="ami-56d1bb3e"
 sleep=10
 instance_name="android-gl-build-$TRAVIS_REPO_SLUG-$TRAVIS_JOB_NUMBER"
 echo $ami_name
