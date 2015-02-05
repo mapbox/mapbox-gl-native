e1a3b9ff8ea06a31e09c4e7aaad48901959af84a
Justin R. Miller <incanus@codesorcery.net>
2015-02-04 17:05:57 -0800

    fixes #782: ignore 64->32 conversion warnings for Boost

--- src/mbgl/text/collision.hpp
+++ src/mbgl/text/collision.hpp
@@ -7,6 +7,7 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-variable"
 #pragma GCC diagnostic ignored "-Wshadow"
+#pragma GCC diagnostic ignored "-Wshorten-64-to-32"
 #ifdef __clang__
 #pragma GCC diagnostic ignored "-Wdeprecated-register"
 #else
