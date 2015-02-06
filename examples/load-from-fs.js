f8a7a4425d1c97e2ff5dce554d390698bc0e389c
Konstantin Käfer <mail@kkaefer.com>
2015-02-06 12:12:51 +0100

    add example script

---
+++ examples/load-from-fs.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+/* jshint node: true, unused: false */
+'use strict';
+
+var mbgl = require('..');
+var fs = require('fs');
+var path = require('path');
+
+var base = path.join(path.dirname(process.mainModule.filename), '../test');
+
+var fileSource = new mbgl.FileSource();
+fileSource.request = function(req) {
+    fs.readFile(path.join(base, req.url), function(err, data) {
+        req.respond(err, { data: data });
+    });
+};
+
+fileSource.cancel = function(req) {
+};
+
+var map = new mbgl.Map(fileSource);
+map.load(require('../test/fixtures/style.json'));
+map.render({}, function(err, image) {
+    if (err) throw err;
+    fs.writeFileSync('image.png', image);
+    console.warn('Written image.png');
+});
