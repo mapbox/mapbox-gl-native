025654a101eada83c23178ae39e48a0fe495a6f5
Konstantin Käfer <mail@kkaefer.com>
2015-02-13 14:51:01 -0800

    throw exception when shader compilation fails

---
+++ include/mbgl/util/exception.hpp
@@ -0,0 +1,27 @@
+#ifndef MBGL_UTIL_EXCEPTION
+#define MBGL_UTIL_EXCEPTION
+
+#include <stdexcept>
+
+namespace mbgl {
+namespace util {
+
+struct Exception : std::runtime_error {
+    inline Exception(const char *msg) : std::runtime_error(msg) {}
+    inline Exception(const std::string &msg) : std::runtime_error(msg) {}
+};
+
+struct MisuseException : Exception {
+    inline MisuseException(const char *msg) : Exception(msg) {}
+    inline MisuseException(const std::string &msg) : Exception(msg) {}
+};
+
+struct ShaderException : Exception {
+    inline ShaderException(const char *msg) : Exception(msg) {}
+    inline ShaderException(const std::string &msg) : Exception(msg) {}
+};
+
+}
+}
+
+#endif
