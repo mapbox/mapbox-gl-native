63dfba065dfdd68e4f9cfd536565744ed64399ce
Konstantin Käfer <mail@kkaefer.com>
2015-02-13 11:52:20 -0800

    add tap-spec to format output

--- test/js/file_source.test.js
+++ test/js/file_source.test.js
@@ -3,6 +3,12 @@
 /* jshint node: true */
 
 var test = require('tape');
+var tapSpec = require('tap-spec');
+
+test.createStream()
+  .pipe(tapSpec())
+  .pipe(process.stdout);
+
 var mbgl = require('../..');
 
 test('FileSource', function(t) {
--- test/render.test.js
+++ test/render.test.js
@@ -3,6 +3,12 @@
 /* jshint node:true */
 
 var test = require('tape');
+var tapSpec = require('tap-spec');
+
+test.createStream()
+  .pipe(tapSpec())
+  .pipe(process.stdout);
+
 var mbgl = require('..');
 var fs = require('fs');
 var path = require('path');
