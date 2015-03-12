d63281697056ad22a70a7c663bf09e55cbb5a4e0
Konstantin Käfer <mail@kkaefer.com>
2015-03-12 10:53:00 +0100

    only upload the artifact we're building
    fixes #982

--- android/scripts/common.sh
+++ android/scripts/common.sh
@@ -4,16 +4,37 @@ set -e
 set -o pipefail
 
 NAME=$1
-export JOBS=`nproc`
+
+case `uname -s` in
+    'Darwin') export JOBS=`sysctl -n hw.ncpu` ;;
+    'Linux')  export JOBS=`nproc` ;;
+    *)        export JOBS=1 ;;
+esac
+
 
 export CC=clang CXX=clang++
 export MASON_PLATFORM=android
 
-mkdir ./android/java/MapboxGLAndroidSDKTestApp/src/main/res/raw/
+mkdir -p ./android/java/MapboxGLAndroidSDKTestApp/src/main/res/raw/
 echo "${MAPBOX_ACCESS_TOKEN}" >> ./android/java/MapboxGLAndroidSDKTestApp/src/main/res/raw/token.txt
 
 make android -j$JOBS BUILDTYPE=$BUILDTYPE JOBS=$JOBS
 
-aws s3 cp ./build/android-${ANDROID_ABI}/${BUILDTYPE}/lib.target/libmapbox-gl.so s3://mapbox/mapbox-gl-native/android/build/${NAME}/libmapbox-gl.so
-aws s3 cp ./android/java/MapboxGLAndroidSDKTestApp/build/outputs/apk/MapboxGLAndroidSDKTestApp-debug.apk s3://mapbox/mapbox-gl-native/android/build/${NAME}/MapboxGLAndroidSDKTestApp-debug.apk
-aws s3 cp ./android/java/MapboxGLAndroidSDKTestApp/build/outputs/apk/MapboxGLAndroidSDKTestApp-release-unsigned.apk s3://mapbox/mapbox-gl-native/android/build/${NAME}/MapboxGLAndroidSDKTestApp-release-unsigned.apk
+S3_PREFIX=s3://mapbox/mapbox-gl-native/android/build/${NAME}
+APK_OUTPUTS=./android/java/MapboxGLAndroidSDKTestApp/build/outputs/apk
+
+# Upload the shared object.
+aws s3 cp \
+    ./build/android-${ANDROID_ABI}/${BUILDTYPE}/lib.target/libmapbox-gl.so \
+    ${S3_PREFIX}/libmapbox-gl.so
+
+# Upload either the debug or the release build
+if [ ${BUILDTYPE} == "Debug" ] ; then
+    aws s3 cp \
+        ${APK_OUTPUTS}/MapboxGLAndroidSDKTestApp-debug.apk \
+        ${S3_PREFIX}/MapboxGLAndroidSDKTestApp-debug.apk
+elif [ ${BUILDTYPE} == "Release" ] ; then
+    aws s3 cp \
+        ${APK_OUTPUTS}/MapboxGLAndroidSDKTestApp-release-unsigned.apk \
+        ${S3_PREFIX}/MapboxGLAndroidSDKTestApp-release-unsigned.apk
+fi
