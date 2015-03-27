83f083c7b5854db25168240a4ad7a313f2765f05
Justin R. Miller <incanus@codesorcery.net>
2015-03-26 21:27:56 -0700

    fixes #1116: metrics stability & performance refactor

--- include/mbgl/ios/MGLMapboxEvents.h
+++ include/mbgl/ios/MGLMapboxEvents.h
@@ -1,26 +1,43 @@
-//
-//  MapboxEvents.h
-//  MapboxEvents
-//
-//  Created by Brad Leege on 3/5/15.
-//  Copyright (c) 2015 Mapbox. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+
+extern NSString *const MGLEventMapLoad;
+extern NSString *const MGLEventMapTap;
+extern NSString *const MGLEventMapSingleTap;
+extern NSString *const MGLEventMapDoubleTap;
+extern NSString *const MGLEventMapTwoFingerSingleTap;
+extern NSString *const MGLEventMapQuickZoom;
+extern NSString *const MGLEventMapPanStart;
+extern NSString *const MGLEventMapPanEnd;
+extern NSString *const MGLEventMapPinchStart;
+extern NSString *const MGLEventMapRotateStart;
+extern NSString *const MGLEventMapLocation;
 
 @interface MGLMapboxEvents : NSObject
 
-@property (atomic) NSInteger flushAt;
-@property (atomic) NSInteger flushAfter;
-@property (atomic) NSString *api;
-@property (atomic) NSString *token;
-@property (atomic) NSString *appName;
-@property (atomic) NSString *appVersion;
+// You must call these methods from the main thread.
+//
++ (void) setToken:(NSString *)token;
++ (void) setAppName:(NSString *)appName;
++ (void) setAppVersion:(NSString *)appVersion;
 
-+ (id)sharedManager;
+// You can call this method from any thread. Significant work will
+// be dispatched to a low-priority background queue and all
+// resulting calls are guaranteed threadsafe.
+//
+// Events or attributes passed could be accessed on non-main threads,
+// so you must not reference UI elements from within any arguments.
+// Copy any values needed first or create dedicated methods in this
+// class for threadsafe access to UIKit classes.
+//
++ (void) pushEvent:(NSString *)event withAttributes:(NSDictionary *)attributeDictionary;
 
-- (void) pushEvent:(NSString *)event withAttributes:(NSDictionary *)attributeDictionary;
+// You can call these methods from any thread.
+//
++ (NSString *) checkEmailEnabled;
++ (BOOL) checkPushEnabled;
 
-- (void) flush;
+// You can call this method from any thread.
+//
++ (void) flush;
 
 @end
--- include/mbgl/ios/MGLMetricsLocationManager.h
+++ include/mbgl/ios/MGLMetricsLocationManager.h
@@ -1,22 +1,9 @@
-//
-//  MBLocationManager.h
-//  Hermes
-//
-//  Created by Brad Leege on 3/8/15.
-//  Copyright (c) 2015 Mapbox. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
-#import "CoreLocation/CoreLocation.h"
 
-@interface MGLMetricsLocationManager : NSObject <CLLocationManagerDelegate>
+@interface MGLMetricsLocationManager : NSObject
 
-+ (id)sharedManager;
-
-- (BOOL) isAuthorizedStatusDetermined;
-- (void) requestAlwaysAuthorization;
-
-- (void) startUpdatingLocation;
-- (void) stopUpdatingLocation;
+// This method can be called from any thread.
+//
++ (instancetype)sharedManager;
 
 @end
