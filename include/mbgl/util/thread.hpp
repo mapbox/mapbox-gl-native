278e6bd556865aa8cad688d4c671d19a1f5e6871
Konstantin Käfer <mail@kkaefer.com>
2015-04-13 12:58:47 +0200

    use std::tuple<> to forward parameters into the Thread<> object
    works around GCC bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=47226

--- include/mbgl/util/thread.hpp
+++ include/mbgl/util/thread.hpp
@@ -4,6 +4,21 @@
 #include <future>
 #include <thread>
 
+namespace {
+
+template <::std::size_t...>
+struct index_sequence {};
+
+template <::std::size_t N, ::std::size_t... I>
+struct integer_sequence : integer_sequence<N - 1, N - 1, I...> {};
+
+template <::std::size_t... I>
+struct integer_sequence<0, I...> {
+    using type = index_sequence<I...>;
+};
+
+}
+
 namespace mbgl {
 namespace util {
 
@@ -30,6 +45,15 @@ public:
     inline operator Object*() const { return &object; }
 
 private:
+    template <typename P, std::size_t... I>
+    void run(std::promise<Object&>& promise, P&& params, index_sequence<I...>) {
+        Object context(std::get<I>(std::forward<P>(params))...);
+        promise.set_value(context);
+        context.start();
+        joinable.get_future().get();
+    }
+
+private:
     std::thread thread;
     std::promise<void> joinable;
     Object& object;
@@ -38,16 +62,16 @@ private:
 template <class Object>
 template <class... Args>
 Thread<Object>::Thread(Args&&... args)
-    : object([&]() -> Object& {
+    : object([&](std::tuple<Args...>&& params) -> Object& {
+          // Note: We're using std::tuple<> to store the arguments because GCC 4.9 has a bug
+          // when expanding parameters packs captured in lambdas.
           std::promise<Object&> promise;
+          constexpr auto seq = typename integer_sequence<sizeof...(Args)>::type();
           thread = std::thread([&] {
-              Object context(::std::forward<Args>(args)...);
-              promise.set_value(context);
-              context.start();
-              joinable.get_future().get();
+              run(promise, std::move(params), seq);
           });
           return promise.get_future().get();
-      }()) {
+      }(std::move(std::forward_as_tuple(::std::forward<Args>(args)...)))) {
 }
 
 template <class Object>
