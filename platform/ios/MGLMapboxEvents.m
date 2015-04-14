21685caa2a9e29f4f23a73679eb3f79d27029cf2
Brad Leege <bleege@gmail.com>
2015-04-14 18:03:59 -0500

    #1268 - Adding horizontalAccuracy to location event

--- platform/ios/MGLMapboxEvents.m
+++ platform/ios/MGLMapboxEvents.m
@@ -24,6 +24,7 @@ NSString *const MGLEventKeyZoomLevel = @"zoom";
 NSString *const MGLEventKeySpeed = @"speed";
 NSString *const MGLEventKeyCourse = @"course";
 NSString *const MGLEventKeyAltitude = @"altitude";
+NSString *const MGLEventKeyHorizontalAccuracy = @"horizontalAccuracy";
 NSString *const MGLEventKeyVerticalAccuracy = @"verticalAccuracy";
 NSString *const MGLEventKeyPushEnabled = @"enabled.push";
 NSString *const MGLEventKeyEmailEnabled = @"enabled.email";
