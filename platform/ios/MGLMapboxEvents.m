28d573fc234233505da84349f09d8e13ca6799af
Brad Leege <bleege@gmail.com>
2015-05-01 20:39:19 -0400

    #1396 - Creating alternate data server url infrastructure

--- platform/ios/MGLMapboxEvents.m
+++ platform/ios/MGLMapboxEvents.m
@@ -82,6 +82,7 @@ NSString *const MGLEventGestureRotateStart = @"Rotation";
 @property (atomic) NSURLSession *session;
 @property (atomic) NSData *digicertCert;
 @property (atomic) NSData *geoTrustCert;
+@property (atomic) NSString *alternateDataServerURL;
 
 
 // The paused state tracker is only ever accessed from the main thread.
@@ -247,6 +248,14 @@ NSString *const MGLEventGestureRotateStart = @"Rotation";
     MGLAssertIsMainThread();
     [MGLMapboxEvents sharedManager].token = token;
 }
+
+// Must be called from the main thread.
+//
++ (void) setAlternateDataServerURL:(NSString *)alternateDataServerURL {
+    MGLAssertIsMainThread();
+    [MGLMapboxEvents sharedManager].alternateDataServerURL = alternateDataServerURL;
+}
+
 
 // Must be called from the main thread.
 //
