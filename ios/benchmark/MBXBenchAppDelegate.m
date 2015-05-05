24083c582c7308025ed05285ad9e82696c70658a
Konstantin Käfer <mail@kkaefer.com>
2015-05-05 11:38:27 +0200

    [iOS] add benchmarking application

---
+++ ios/benchmark/MBXBenchAppDelegate.m
@@ -0,0 +1,15 @@
+#import "MBXBenchAppDelegate.h"
+#import "MBXBenchViewController.h"
+
+@implementation MBXBenchAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [MBXBenchViewController new];
+    [self.window makeKeyAndVisible];
+
+    return YES;
+}
+
+@end
---
+++ ios/benchmark/MBXBenchViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface MBXBenchViewController : UIViewController
+
+@end
