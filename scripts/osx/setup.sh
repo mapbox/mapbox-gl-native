8e15732954dfc5635490d42fff7142e793a5208c
Mike Morris <michael.patrick.morris@gmail.com>
2015-05-07 16:08:10 -0400

    drop unnecessary mason PATH and MASON_DIR exports from setup.sh

--- scripts/osx/setup.sh
+++ scripts/osx/setup.sh
@@ -3,8 +3,5 @@
 set -e
 set -o pipefail
 
-# Add Mason to PATH
-export PATH="`pwd`/.mason:${PATH}" MASON_DIR="`pwd`/.mason"
-
 # Set the core file limit to unlimited so a core file is generated upon crash
 ulimit -c unlimited -S
