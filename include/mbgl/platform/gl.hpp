0819628fea8ae7498b4c9130dec8b3e367cba6e5
Thiago Marcos P. Santos <thiago@mapbox.com>
2015-05-08 15:28:43 +0300

    Fix Linux GCC Debug build
    Compiler was complaining about shadowed variable after
    macro expansion.

--- include/mbgl/platform/gl.hpp
+++ include/mbgl/platform/gl.hpp
@@ -39,7 +39,7 @@ struct Error : ::std::runtime_error {
 void checkError(const char *cmd, const char *file, int line);
 
 #if defined(DEBUG)
-#define MBGL_CHECK_ERROR(cmd) ([&]() { struct _ { inline ~_() { ::mbgl::gl::checkError(#cmd, __FILE__, __LINE__); } } _; return cmd; }())
+#define MBGL_CHECK_ERROR(cmd) ([&]() { struct __MBGL_C_E { inline ~__MBGL_C_E() { ::mbgl::gl::checkError(#cmd, __FILE__, __LINE__); } } __MBGL_C_E; return cmd; }())
 #else
 #define MBGL_CHECK_ERROR(cmd) (cmd)
 #endif
