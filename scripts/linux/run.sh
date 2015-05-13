83484dc849e0e585c6f45a910432eb14b9bfa9c7
Konstantin Käfer <mail@kkaefer.com>
2015-05-13 15:29:54 +0200

    move result deployment to after_failure script

--- scripts/linux/run.sh
+++ scripts/linux/run.sh
@@ -35,18 +35,3 @@ make test-* BUILDTYPE=${BUILDTYPE}
 
 mapbox_time "compare_results" \
 ./scripts/compare_images.sh
-
-################################################################################
-# Deploy
-################################################################################
-
-if [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] ; then
-    # Install and add awscli to PATH for uploading the results
-    mapbox_time "install_awscli" \
-    pip install --user awscli
-    export PATH="`python -m site --user-base`/bin:${PATH}"
-
-    mapbox_time_start "deploy_results"
-    (cd ./test/suite/ && ./bin/deploy_results.sh)
-    mapbox_time_finish
-fi
