0ad76c95d20f7ef7b7ef712d8edb8c54219880e7
Minh Nguyễn <mxn@1ec5.org>
2015-05-13 22:33:03 -0700

    Obsoleted +setMapboxMetricsEnabledSettingShownInApp:
    Made `+[MGLAccountManager setMapboxMetricsEnabledSettingShownInApp:]` unavailable, with a message explaining what to do instead. Removed a commented-out call to that method. Only the environment for an access token if one hasn’t already been set in Info.plist.

--- ios/app/MBXAppDelegate.m
+++ ios/app/MBXAppDelegate.m
@@ -8,24 +8,22 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Set Access Token
-    NSString *accessToken = [[NSProcessInfo processInfo] environment][@"MAPBOX_ACCESS_TOKEN"];
-    if (accessToken) {
-        // Store to preferences so that we can launch the app later on without having to specify
-        // token.
-        [[NSUserDefaults standardUserDefaults] setObject:accessToken forKey:@"access_token"];
-    } else {
-        // Try to retrieve from preferences, maybe we've stored them there previously and can reuse
-        // the token.
-        accessToken = [[NSUserDefaults standardUserDefaults] objectForKey:@"access_token"];
+    // Set access token, unless MGLAccountManager already read it in from Info.plist.
+    if ( ! [MGLAccountManager accessToken]) {
+        NSString *accessToken = [[NSProcessInfo processInfo] environment][@"MAPBOX_ACCESS_TOKEN"];
+        if (accessToken) {
+            // Store to preferences so that we can launch the app later on without having to specify
+            // token.
+            [[NSUserDefaults standardUserDefaults] setObject:accessToken forKey:@"access_token"];
+        } else {
+            // Try to retrieve from preferences, maybe we've stored them there previously and can reuse
+            // the token.
+            accessToken = [[NSUserDefaults standardUserDefaults] objectForKey:@"access_token"];
+        }
+        if ( ! accessToken) NSLog(@"No access token set. Mapbox vector tiles won't work.");
+
+        [MGLAccountManager setAccessToken:accessToken];
     }
-    if ( ! accessToken) NSLog(@"No access token set. Mapbox vector tiles won't work.");
-
-    // Signal To SDK That Opt Out Is In App UI
-//    [MGLAccountManager setMapboxMetricsEnabledSettingShownInApp:YES];
-
-    // Start Mapbox GL SDK
-    [MGLAccountManager setAccessToken:accessToken];
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[MBXViewController new]];
