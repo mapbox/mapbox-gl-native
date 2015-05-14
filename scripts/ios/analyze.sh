6cde7b1c82a1c494b2ef1359c9d09d54643dba4b
Minh Nguyễn <mxn@1ec5.org>
2015-05-14 16:47:27 -0700

    Skip analysis on clipper.cpp and mesh.c
    Due to memononen/libtess2#19 and angusj/polyclipping#134.

--- scripts/ios/analyze.sh
+++ scripts/ios/analyze.sh
@@ -4,6 +4,12 @@ set -e
 set -o pipefail
 set -u
 
+# Disable static analyzer in third-party libraries with known issues:
+# https://github.com/memononen/libtess2/pull/19
+# https://sourceforge.net/p/polyclipping/bugs/134/
+perl -pi -e 's/(isa = PBXBuildFile; fileRef = [0-9A-F]+ \/\* (clipper\.cpp|mesh\.c) \*\/;) }/$1 settings = {COMPILER_FLAGS = "-Qunused-arguments -Xanalyzer -analyzer-disable-all-checks"; }; }/g' \
+    ./build/ios/mbgl.xcodeproj/project.pbxproj
+
 xcodebuild \
     -sdk iphoneos \
     -project ./build/ios/gyp/ios.xcodeproj \
