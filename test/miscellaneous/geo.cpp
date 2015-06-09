b9809c60794ee73e0e001a7cf65416440cc0f98c
Konstantin Käfer <mail@kkaefer.com>
2015-06-09 17:01:19 -0400

    fix latitude vs. longitude

--- test/miscellaneous/geo.cpp
+++ test/miscellaneous/geo.cpp
@@ -8,20 +8,20 @@ using namespace mbgl;
 TEST(Geo, LatLngFromTileID) {
     for (int i = 0; i < 20; i++) {
         const LatLng ll{ TileID(i, 0, 0, 0) };
-        ASSERT_DOUBLE_EQ(-180, ll.latitude);
-        ASSERT_DOUBLE_EQ(85.051128779806604, ll.longitude);
+        ASSERT_DOUBLE_EQ(-180, ll.longitude);
+        ASSERT_DOUBLE_EQ(85.051128779806604, ll.latitude);
     }
 
     {
         const LatLng ll{ TileID(0, 1, 0, 0) };
-        ASSERT_DOUBLE_EQ(180, ll.latitude);
-        ASSERT_DOUBLE_EQ(85.051128779806604, ll.longitude);
+        ASSERT_DOUBLE_EQ(180, ll.longitude);
+        ASSERT_DOUBLE_EQ(85.051128779806604, ll.latitude);
     }
 
     {
         const LatLng ll{ TileID(0, -1, 0, 0) };
-        ASSERT_DOUBLE_EQ(-540, ll.latitude);
-        ASSERT_DOUBLE_EQ(85.051128779806604, ll.longitude);
+        ASSERT_DOUBLE_EQ(-540, ll.longitude);
+        ASSERT_DOUBLE_EQ(85.051128779806604, ll.latitude);
     }
 }
 
@@ -29,33 +29,33 @@ TEST(Geo, LatLngFromTileID) {
 TEST(Geo, LatLngBoundsFromTileID) {
     {
         const LatLngBounds bounds{ TileID(0, 0, 0, 0) };
-        ASSERT_DOUBLE_EQ(-180, bounds.sw.latitude);
-        ASSERT_DOUBLE_EQ(-85.051128779806604, bounds.sw.longitude);
-        ASSERT_DOUBLE_EQ(180, bounds.ne.latitude);
-        ASSERT_DOUBLE_EQ(85.051128779806604, bounds.ne.longitude);
+        ASSERT_DOUBLE_EQ(-180, bounds.sw.longitude);
+        ASSERT_DOUBLE_EQ(-85.051128779806604, bounds.sw.latitude);
+        ASSERT_DOUBLE_EQ(180, bounds.ne.longitude);
+        ASSERT_DOUBLE_EQ(85.051128779806604, bounds.ne.latitude);
     }
 
     {
         const LatLngBounds bounds{ TileID(1, 0, 1, 0) };
-        ASSERT_DOUBLE_EQ(-180, bounds.sw.latitude);
-        ASSERT_DOUBLE_EQ(-85.051128779806604, bounds.sw.longitude);
-        ASSERT_DOUBLE_EQ(0, bounds.ne.latitude);
+        ASSERT_DOUBLE_EQ(-180, bounds.sw.longitude);
+        ASSERT_DOUBLE_EQ(-85.051128779806604, bounds.sw.latitude);
         ASSERT_DOUBLE_EQ(0, bounds.ne.longitude);
+        ASSERT_DOUBLE_EQ(0, bounds.ne.latitude);
     }
 
     {
         const LatLngBounds bounds{ TileID(1, 1, 1, 0) };
-        ASSERT_DOUBLE_EQ(0, bounds.sw.latitude);
-        ASSERT_DOUBLE_EQ(-85.051128779806604, bounds.sw.longitude);
-        ASSERT_DOUBLE_EQ(180, bounds.ne.latitude);
-        ASSERT_DOUBLE_EQ(0, bounds.ne.longitude);
+        ASSERT_DOUBLE_EQ(0, bounds.sw.longitude);
+        ASSERT_DOUBLE_EQ(-85.051128779806604, bounds.sw.latitude);
+        ASSERT_DOUBLE_EQ(180, bounds.ne.longitude);
+        ASSERT_DOUBLE_EQ(0, bounds.ne.latitude);
     }
 
     {
         const LatLngBounds bounds{ TileID(1, 0, 0, 0) };
-        ASSERT_DOUBLE_EQ(-180, bounds.sw.latitude);
-        ASSERT_DOUBLE_EQ(0, bounds.sw.longitude);
-        ASSERT_DOUBLE_EQ(0, bounds.ne.latitude);
-        ASSERT_DOUBLE_EQ(85.051128779806604, bounds.ne.longitude);
+        ASSERT_DOUBLE_EQ(-180, bounds.sw.longitude);
+        ASSERT_DOUBLE_EQ(0, bounds.sw.latitude);
+        ASSERT_DOUBLE_EQ(0, bounds.ne.longitude);
+        ASSERT_DOUBLE_EQ(85.051128779806604, bounds.ne.latitude);
     }
 }
