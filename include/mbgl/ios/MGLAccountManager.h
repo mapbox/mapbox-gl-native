afb7095ecc1cbe9c3e7a15f3e6e6723db080ade1
Justin R. Miller <incanus@codesorcery.net>
2015-06-19 11:07:24 -0700

    refs #1759: remove header docs for now-unavailable API

--- include/mbgl/ios/MGLAccountManager.h
+++ include/mbgl/ios/MGLAccountManager.h
@@ -21,8 +21,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 /** @name Providing User Metrics Opt-Out */
 
-/** Certain Mapbox plans require the collection of user metrics. If you aren't using a preference switch in an existing or new `Settings.bundle` in your application, set this value to `YES` to indicate that you are providing a metrics opt-out for users within your app's interface directly.
-*   @param showsOptOut Whether your application's interface provides a user opt-out preference. The default value is `NO`, meaning a `Settings.bundle` is expected for providing a user opt-out preference. */
 + (void)setMapboxMetricsEnabledSettingShownInApp:(BOOL)showsOptOut __attribute__((unavailable("Set MGLMapboxMetricsEnabledSettingShownInApp in Info.plist.")));
 
 /** Whether in-app user metrics opt-out is configured. If set to the default value of `NO`, a user opt-out preference is expected in a `Settings.bundle` that shows in the application's section within the system Settings app. */
