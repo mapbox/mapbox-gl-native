938165de174e9dbec523739378122b4f970a2ac0
Thiago Marcos P. Santos <thiago@mapbox.com>
2015-07-14 14:26:40 +0300

    Re-enable the Satellite style on the Linux/OSX app

--- platform/default/default_styles.cpp
+++ platform/default/default_styles.cpp
@@ -7,7 +7,8 @@ const std::vector<std::pair<std::string, std::string>> defaultStyles = {
     { "asset://styles/mapbox-streets-v7.json", "Mapbox Streets" },
     { "asset://styles/emerald-v7.json", "Emerald" },
     { "asset://styles/light-v7.json", "Light" },
-    { "asset://styles/dark-v7.json", "Dark" }
+    { "asset://styles/dark-v7.json", "Dark" },
+    { "asset://styles/satellite-v7.json", "Satellite" }
 };
 
 } // end namespace util
