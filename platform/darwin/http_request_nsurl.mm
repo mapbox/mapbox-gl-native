785b34df2e064b54006617322464a634bb5cd124
Bruno de Oliveira Abinader <bruno@mapbox.com>
2015-07-28 10:44:41 +0300

    Moved ResponseStatus to its own header

--- platform/darwin/http_request_nsurl.mm
+++ platform/darwin/http_request_nsurl.mm
@@ -1,4 +1,5 @@
 #include <mbgl/storage/http_context.hpp>
+#include <mbgl/storage/http_request_base.hpp>
 #include <mbgl/storage/resource.hpp>
 #include <mbgl/storage/response.hpp>
 
@@ -11,36 +12,6 @@
 #include <cassert>
 
 namespace mbgl {
-
-enum class ResponseStatus : uint8_t {
-    // This error probably won't be resolved by retrying anytime soon. We are giving up.
-    PermanentError,
-
-    // This error might be resolved by waiting some time (e.g. server issues).
-    // We are going to do an exponential back-off and will try again in a few seconds.
-    TemporaryError,
-
-    // This error was caused by a temporary error and it is likely that it will be resolved
-    // immediately. We are going to try again right away. This is like the TemporaryError, except
-    // that we will not perform exponential back-off.
-    SingularError,
-
-    // This error might be resolved once the network reachability status changes.
-    // We are going to watch the network status for changes and will retry as soon as the
-    // operating system notifies us of a network status change.
-    ConnectionError,
-
-    // The request was canceled mid-way.
-    Canceled,
-
-    // The request returned data successfully. We retrieved and decoded the data successfully.
-    Successful,
-
-    // The request confirmed that the data wasn't changed. We already have the data.
-    NotModified,
-};
-
-// -------------------------------------------------------------------------------------------------
 
 class HTTPNSURLContext;
 
--- platform/default/http_request_curl.cpp
+++ platform/default/http_request_curl.cpp
@@ -1,4 +1,5 @@
 #include <mbgl/storage/http_context.hpp>
+#include <mbgl/storage/http_request_base.hpp>
 #include <mbgl/storage/resource.hpp>
 #include <mbgl/storage/response.hpp>
 #include <mbgl/util/chrono.hpp>
@@ -33,26 +34,6 @@ void handleError(CURLcode code) {
 }
 
 namespace mbgl {
-
-enum class ResponseStatus : int8_t {
-    // This error probably won't be resolved by retrying anytime soon. We are giving up.
-    PermanentError,
-
-    // This error might be resolved by waiting some time (e.g. server issues).
-    // We are going to do an exponential back-off and will try again in a few seconds.
-    TemporaryError,
-
-    // This error might be resolved once the network reachability status changes.
-    // We are going to watch the network status for changes and will retry as soon as the
-    // operating system notifies us of a network status change.
-    ConnectionError,
-
-    // The request returned data successfully. We retrieved and decoded the data successfully.
-    Successful,
-
-    // The request confirmed that the data wasn't changed. We already have the data.
-    NotModified,
-};
 
 class HTTPRequest;
 
