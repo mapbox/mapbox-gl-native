66f0473bcf585724a0311416466b30694d7754d0
Konstantin Käfer <mail@kkaefer.com>
2015-08-03 15:59:03 +0200

    add MBGL_USE_GLES2 define

--- include/mbgl/util/gl_helper.hpp
+++ include/mbgl/util/gl_helper.hpp
@@ -25,21 +25,21 @@ inline void setBlend(const bool& enabled) {
     enabled ? MBGL_CHECK_ERROR(glEnable(GL_BLEND)) : MBGL_CHECK_ERROR(glDisable(GL_BLEND));
 }
 
-inline std::array<double, 4> getClearColor() {
-    std::array<double, 4> color;
-    MBGL_CHECK_ERROR(glGetDoublev(GL_COLOR_CLEAR_VALUE, color.data()));
+inline std::array<float, 4> getClearColor() {
+    std::array<float, 4> color;
+    MBGL_CHECK_ERROR(glGetFloatv(GL_COLOR_CLEAR_VALUE, color.data()));
     return color;
 }
 
-inline void setClearColor(const std::array<double, 4>& color) {
+inline void setClearColor(const std::array<float, 4>& color) {
     MBGL_CHECK_ERROR(glClearColor(color[0], color[1], color[2], color[3]));
 }
 
 
 inline std::array<GLenum, 2> getBlendFunc() {
     GLint func[2];
-    glGetIntegerv(GL_BLEND_SRC, &func[0]);
-    glGetIntegerv(GL_BLEND_DST, &func[1]);
+    glGetIntegerv(GL_BLEND_SRC_ALPHA, &func[0]);
+    glGetIntegerv(GL_BLEND_DST_ALPHA, &func[1]);
     return {{ static_cast<GLenum>(func[0]), static_cast<GLenum>(func[1]) }};
 }
 
@@ -47,7 +47,7 @@ inline void setBlendFunc(const std::array<GLenum, 2>& func) {
     MBGL_CHECK_ERROR(glBlendFunc(func[0], func[1]));
 }
 
-
+#ifndef GL_ES_VERSION_2_0
 inline std::array<double, 2> getPixelZoom() {
     std::array<double, 2> zoom;
     glGetDoublev(GL_ZOOM_X, &zoom[0]);
@@ -69,17 +69,20 @@ inline std::array<double, 4> getRasterPos() {
 inline void setRasterPos(const std::array<double, 4>& pos) {
     MBGL_CHECK_ERROR(glRasterPos4d(pos[0], pos[1], pos[2], pos[3]));
 }
-
+#endif
 } // end anonymous namespace
 
 namespace mbgl {
 namespace gl {
 
 using PreserveBlend = Preserve<bool, getBlend, setBlend>;
-using PreserveClearColor = Preserve<std::array<double, 4>, getClearColor, setClearColor>;
+using PreserveClearColor = Preserve<std::array<float, 4>, getClearColor, setClearColor>;
 using PreserveBlendFunc = Preserve<std::array<GLenum, 2>, getBlendFunc, setBlendFunc>;
+
+#ifndef GL_ES_VERSION_2_0
 using PreservePixelZoom = Preserve<std::array<double, 2>, getPixelZoom, setPixelZoom>;
 using PreserveRasterPos = Preserve<std::array<double, 4>, getRasterPos, setRasterPos>;
+#endif
 
 }
 }
