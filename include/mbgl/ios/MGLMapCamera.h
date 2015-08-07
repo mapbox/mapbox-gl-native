c6ae9cb243b9520ed53359595b1c324745c2b59e
Minh Nguyễn <mxn@1ec5.org>
2015-08-07 01:00:10 -0700

    MGLMapCamera

--- include/mbgl/ios/MGLMapCamera.h
+++ include/mbgl/ios/MGLMapCamera.h
@@ -1,13 +1,31 @@
-//
-//  MGLMapCamera.h
-//  mbgl
-//
-//  Created by Minh Nguyen on 2015-08-05.
-//
-//
+#import "Mapbox.h"
 
-#import <Foundation/Foundation.h>
+#pragma once
 
-@interface MGLMapCamera : NSObject
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MGLMapCamera : NSObject <NSSecureCoding, NSCopying>
+
+/** Coordinate at the center of the map view. */
+@property (nonatomic) CLLocationCoordinate2D centerCoordinate;
+
+/** Heading measured in degrees clockwise from true north. */
+@property (nonatomic) CLLocationDirection heading;
+
+/** Meters above ground level. */
+@property (nonatomic) CLLocationDistance altitude;
+
+/** Returns a new camera with all properties set to 0. */
++ (instancetype)camera;
+
++ (instancetype)cameraLookingAtCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate
+                              fromEyeCoordinate:(CLLocationCoordinate2D)eyeCoordinate
+                                    eyeAltitude:(CLLocationDistance)eyeAltitude;
+
++ (instancetype)cameraLookingAtCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate
+                                   fromDistance:(CLLocationDistance)distance
+                                        heading:(CLLocationDirection)heading;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- include/mbgl/ios/MGLMapView.h
+++ include/mbgl/ios/MGLMapView.h
@@ -1,4 +1,5 @@
 #import "MGLGeometry.h"
+#import "MGLMapCamera.h"
 
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
@@ -6,6 +7,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @class MGLAnnotationImage;
+@class MGLMapCamera;
 @class MGLUserLocation;
 @class MGLPolyline;
 @class MGLPolygon;
@@ -195,6 +197,10 @@ IB_DESIGNABLE
 
 /** Resets the map pitch to head-on. */
 - (IBAction)resetPitch;
+
+@property (nonatomic, copy) MGLMapCamera *camera;
+
+- (void)setCamera:(MGLMapCamera *)camera animated:(BOOL)animated;
 
 #pragma mark - Converting Map Coordinates
 
