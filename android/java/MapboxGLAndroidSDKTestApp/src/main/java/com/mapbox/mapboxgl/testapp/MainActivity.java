c1d796bbc173ee64aecd4816a1099fe449ec8ad9
Brad Leege <bleege@gmail.com>
2015-08-11 16:16:13 -0500

    #1856 - Added CoordinatorLayout back in to support Snackbar widget

--- android/java/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxgl/testapp/MainActivity.java
+++ android/java/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxgl/testapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -150,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
                 // Toggle GPS position updates
                 toggleGps(!mapView.isMyLocationEnabled());
                 updateMap();
+//                Snackbar.make(v, "Snack time!", Snackbar.LENGTH_LONG).show();
             }
         });
 
