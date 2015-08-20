9a6c6831aba9d4bea29d454972b61aa8b5466578
Mike Morris <michael.patrick.morris@gmail.com>
2015-08-20 16:11:32 -0400

    View::swap -> View::beforeRender View::afterRender
    To be able to resize the framebuffer on the map thread
    in HeadlessView.

--- platform/ios/MGLMapView.mm
+++ platform/ios/MGLMapView.mm
@@ -2942,7 +2942,12 @@ class MBGLView : public mbgl::View
                                   waitUntilDone:NO];
     }
 
-    void swap() override
+    void beforeRender() override
+    {
+        // no-op
+    }
+
+    void afterRender() override
     {
         // no-op
     }
