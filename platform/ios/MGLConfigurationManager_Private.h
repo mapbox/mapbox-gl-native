15b2c60fbe9c8a3b6a880792899e4fb4c9420c01
boundsj <jesse@rebounds.net>
2015-08-21 11:52:45 -0700

    Introduce MGLConfigurationManager
    This commit renames MGLAccountManager to MGLConfigurationManager
    since the name "account" was too specific. In addition, a new
    setting was added to the configuration manager to allow a user
    to specify a desired `activityType` to be used by location manager
    instances in the library. This was added using existing conventions
    so that the value can be specified by the API or in the Info.plist
    in the client application. It's worth noting that, for backwards
    compatibility reasons, MGLAccountManager was kept around
    with deprecation warnings and just serves as a wrapper for
    MGLConfigurationManager.
    
    Finally, a few unrelated changes were made to clean up an unused
    property in the MGLConfigurationManager_Private header and to
    make constants for string values used in MGLConfigurationManager
    (what was MGLAccountManager).

---
+++ platform/ios/MGLConfigurationManager_Private.h
@@ -0,0 +1,8 @@
+#import "MGLConfigurationManager.h"
+
+@interface MGLConfigurationManager (Private)
+
+/** Returns the shared instance of the `MGLConfigurationManager` class. */
++ (instancetype)sharedManager;
+
+@end
