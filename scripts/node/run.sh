53f2b125c2ff583fee62c95d56d988bc330d60d5
Mike Morris <michael.patrick.morris@gmail.com>
2015-08-27 18:09:08 -0400

    merge platform/node/scripts/* into scripts/node/run.sh

--- scripts/node/run.sh
+++ scripts/node/run.sh
@@ -21,11 +21,24 @@ pushd platform/node && npm install --build-from-source && popd
 # Test
 ################################################################################
 
-mapbox_time "checkout_test_suite" \
-git submodule update --init test/suite
+# Travis OS X has no GPU
+if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+    mapbox_time "checkout_test_suite" \
+    git submodule update --init test/suite
 
-mapbox_time "run_tests" \
-pushd platform/node && npm test && popd
+    mapbox_time "run_tests" \
+    pushd platform/node && npm test && popd
 
-mapbox_time "run_render_tests" \
-pushd platform/node && npm run test-suite && popd
+    mapbox_time "run_render_tests" \
+    pushd platform/node && npm run test-suite && popd
+
+    if [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] ; then
+      # Install and add awscli to PATH for uploading the results
+        mapbox_time "install_awscli" \
+        pip install --user awscli
+        export PATH="`python -m site --user-base`/bin:${PATH}"
+
+        mapbox_time "deploy_results" \
+        pushd test/suite && ./bin/deploy_results.sh && popd
+    fi
+fi
