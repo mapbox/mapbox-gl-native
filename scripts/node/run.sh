3d04da40a679dde25025e1d6da1f5cfa427b9ed1
Mike Morris <michael.patrick.morris@gmail.com>
2015-09-01 16:14:38 -0400

    remove pushd/popd from scripts/node/run.sh, update license in package.json

--- scripts/node/run.sh
+++ scripts/node/run.sh
@@ -17,10 +17,8 @@ nvm use $NODE_VERSION
 mapbox_time "checkout_styles" \
 git submodule update --init styles
 
-pushd platform/node
 mapbox_time "compile_program" \
 npm install --build-from-source
-popd
 
 ################################################################################
 # Test
@@ -31,13 +29,11 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     mapbox_time "checkout_test_suite" \
     git submodule update --init test/suite
 
-    pushd platform/node
     mapbox_time "run_tests" \
     npm test
 
     mapbox_time "run_render_tests" \
     npm run test-suite
-    popd
 
     if [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] ; then
       # Install and add awscli to PATH for uploading the results
