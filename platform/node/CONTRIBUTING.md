4694c7b51a4e4e18dcbf2d45cc087f125c581a91
Mike Morris <michael.patrick.morris@gmail.com>
2015-09-01 16:08:46 -0400

    fix example, clean up duplicate files

--- platform/node/CONTRIBUTING.md
+++
@@ -1,1 +0,0 @@
-Please run [`clang-format`](http://clang.llvm.org/docs/ClangFormat.html) and verify that code formatting will remain consistent before submitting a pull request.
--- platform/node/examples/load-from-fs.js
+++ platform/node/examples/load-from-fs.js
@@ -2,19 +2,20 @@
 /* jshint node: true, unused: false */
 'use strict';
 
-var mbgl = require('..');
+var mbgl = require('../../..');
 var fs = require('fs');
 var path = require('path');
 var PNG = require('pngjs').PNG;
 
-var base = path.join(path.dirname(process.mainModule.filename), '../test');
+var base = path.join(__dirname, '../test');
 
 var map = new mbgl.Map({
     request: function(req) {
         fs.readFile(path.join(base, req.url), function(err, data) {
             req.respond(err, { data: data });
         });
-    }
+    },
+    ratio: 1.0
 });
 
 map.load(require('../test/fixtures/style.json'));
