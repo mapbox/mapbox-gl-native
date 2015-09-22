142b59246e13b6801f49d79bd029f57fdd2f7d45
Mike Morris <michael.patrick.morris@gmail.com>
2015-09-22 17:31:00 -0400

    [osx] disable -Wunused-local-typedefs
    Fixes OS X builds with Xcode 7, necessitated by
    https://svn.boost.org/trac/boost/ticket/11240

--- bin/render.cpp
+++ bin/render.cpp
@@ -9,13 +9,17 @@
 #include <mbgl/storage/default_file_source.hpp>
 #include <mbgl/storage/sqlite_cache.hpp>
 
+#pragma clang diagnostic push
 #pragma GCC diagnostic push
-#ifndef __clang__
+#ifdef __clang__
+#pragma clang diagnostic ignored "-Wunused-local-typedefs"
+#else
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #pragma GCC diagnostic ignored "-Wshadow"
 #endif
 #include <boost/program_options.hpp>
 #pragma GCC diagnostic pop
+#pragma clang diagnostic pop
 
 namespace po = boost::program_options;
 
