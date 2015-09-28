f833356321d0770c6b7dab4abbaeea53021bcf2a
Jason Wray <jason@kulturny.com>
2015-09-28 11:34:10 -0400

    [iOS] Fix links in generated docs [skip ci]

--- ios/docs/install_docs.sh
+++ ios/docs/install_docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -z `which appledoc` ]; then
-    echo "Unable to find appledoc. See https://github.com/mapbox/mapbox-gl-native#manually"
+    echo "Unable to find appledoc. See https://github.com/mapbox/mapbox-gl-native/blob/master/INSTALL.md"
     exit 1
 fi
 
