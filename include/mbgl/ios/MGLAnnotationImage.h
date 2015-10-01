7087a4c55adf3fd47f3bd6bac7adf432866edfcd
Justin R. Miller <incanus@codesorcery.net>
2015-10-01 14:27:47 -0700

    [ios] fixes #2491: document MGLAnnotationImage

--- include/mbgl/ios/MGLAnnotationImage.h
+++ include/mbgl/ios/MGLAnnotationImage.h
@@ -4,12 +4,28 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/** The MGLAnnotationImage class is responsible for presenting point-based annotations visually on a map view. Annotation image objects wrap `UIImage` objects and may be recycled later and put into a reuse queue that is maintained by the map view. */
 @interface MGLAnnotationImage : NSObject
 
-@property (nonatomic, readonly) UIImage *image;
-@property (nonatomic, readonly) NSString *reuseIdentifier;
+/** @name Initializing and Preparing the Image Object */
 
+/** Initializes and returns a new annotation image object.
+*   @param image The image to be displayed for the annotation.
+*   @param reuseIdentifier The string that identifies that this annotation image is reusable.
+*   @return The initialized annotation image object or `nil` if there was a problem initializing the object. */
 + (instancetype)annotationImageWithImage:(UIImage *)image reuseIdentifier:(NSString *)reuseIdentifier;
+
+/** @name Getting and Setting Attributes */
+
+/** The image to be displayed for the annotation. */
+@property (nonatomic, readonly) UIImage *image;
+
+/** The string that identifies that this annotation image is reusable. (read-only)
+*
+*   You specify the reuse identifier when you create the image object. You use this type later to retrieve an annotation image object that was created previously but which is currently unused because its annotation is not on screen.
+*
+*   If you define distinctly different types of annotations (with distinctly different annotation images to go with them), you can differentiate between the annotation types by specifying different reuse identifiers for each one. */
+@property (nonatomic, readonly) NSString *reuseIdentifier;
 
 @end
 
