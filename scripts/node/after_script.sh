3a91686a099533de85b47ca42b7f97035f8d4a82
Mike Morris <michael.patrick.morris@gmail.com>
2015-11-05 12:40:58 -0500

    [node] autopublish node-pre-gyp binary
    If $TRAVIS_TAG matches with node-v${PACKAGE_JSON_VERSION}.

--- scripts/node/after_script.sh
+++ scripts/node/after_script.sh
@@ -11,8 +11,9 @@ else
 fi
 
 COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
+PACKAGE_JSON_VERSION=$(node ./scripts/node/package-version.js)
 
-if test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE"; then
+if [[ ${TRAVIS_TAG} == node-v${PACKAGE_JSON_VERSION} ]] || test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE"; then
     source ~/.nvm/nvm.sh
     nvm use $NODE_VERSION
 
