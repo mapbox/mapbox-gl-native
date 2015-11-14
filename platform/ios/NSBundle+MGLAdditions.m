212e7a1e081da35309730e6aa15a5fbe967bfeb4
Justin R. Miller <incanus@codesorcery.net>
2015-11-13 17:22:01 -0800

    provide resource bundle search for premade frameworks

--- platform/ios/NSBundle+MGLAdditions.m
+++ platform/ios/NSBundle+MGLAdditions.m
@@ -8,8 +8,21 @@ void mgl_linkBundleCategory(){}
 
 + (NSString *)mgl_resourceBundlePath
 {
-    NSString *resourceBundlePath = [[NSBundle bundleForClass:[MGLMapView class]] pathForResource:@"Mapbox" ofType:@"bundle"];
+    NSString *resourceBundlePath = nil;
 
+    // check for resource bundle in framework bundle (Fabric, premade framework)
+    //
+    NSString *frameworkBundlePath = [NSString stringWithFormat:@"%@/Mapbox.framework/Mapbox.bundle",
+        [[NSBundle mainBundle] privateFrameworksPath]];
+    if ([NSBundle bundleWithPath:frameworkBundlePath]) resourceBundlePath = frameworkBundlePath;
+
+    // check for resource bundle in app bundle (static library)
+    //
+    if ( ! resourceBundlePath) resourceBundlePath = [[NSBundle bundleForClass:
+        [MGLMapView class]] pathForResource:@"Mapbox" ofType:@"bundle"];
+
+    // fall back to resources directly in app bundle (test app)
+    //
     if ( ! resourceBundlePath) resourceBundlePath = [[NSBundle mainBundle] bundlePath];
 
     return resourceBundlePath;
