435cd9ea9e327ad16e3bf9ee598999eb1bf586aa
Justin R. Miller <incanus@codesorcery.net>
2015-11-24 11:49:12 -0800

    fixes #3113: bring in MGLMapView via +load for Interface Builder

--- platform/ios/MGLCategoryLoader.m
+++ platform/ios/MGLCategoryLoader.m
@@ -4,15 +4,21 @@
 #import "NSProcessInfo+MGLAdditions.h"
 #import "NSString+MGLAdditions.h"
 
-// https://github.com/mapbox/mapbox-gl-native/issues/2966
+#import "MGLMapView.h"
 
 @implementation MGLCategoryLoader
 
 + (void)loadCategories
 {
+    // https://github.com/mapbox/mapbox-gl-native/issues/2966
+    //
     mgl_linkBundleCategory();
     mgl_linkProcessCategory();
     mgl_linkStringCategory();
+
+    // https://github.com/mapbox/mapbox-gl-native/issues/3113
+    //
+    NSString *description = [MGLMapView description];
 }
 
 @end
