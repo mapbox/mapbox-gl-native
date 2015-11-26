5a2704f789958f191d113bbbf79e5c5b9ec59fc0
Maciej Swic <maciej@swic.name>
2015-11-26 13:55:40 +0100

    Fixed indentation

--- src/mbgl/map/transform_state.hpp
+++ src/mbgl/map/transform_state.hpp
@@ -75,12 +75,12 @@ public:
     LatLng coordinateToLatLng(const TileCoordinate&) const;
 
     PrecisionPoint coordinateToPoint(const TileCoordinate&) const;
-	PrecisionPoint latLngToXY(LatLng latlng) const;
+    PrecisionPoint latLngToXY(LatLng latlng) const;
     TileCoordinate pointToCoordinate(const PrecisionPoint&) const;
 
 private:
     void constrain(double& scale, double& x, double& y) const;
-	void userConstrain(double& scale_, double& x_, double& y_) const;
+    void userConstrain(double& scale_, double& x_, double& y_) const;
 
     // Limit the amount of zooming possible on the map.
     double min_scale = std::pow(2, 0);
