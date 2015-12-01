645e9eeb8cc8aedf3667f56ec597f4ea04f09445
Antonio Zugaldia <antonio@mapbox.com>
2015-12-01 16:01:32 -0500

    [android] #2805 - add sample tilt activity to test app

---
+++ android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/TiltActivity.java
@@ -0,0 +1,85 @@
+package com.mapbox.mapboxsdk.testapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.utils.ApiAccess;
+import com.mapbox.mapboxsdk.views.MapView;
+
+public class TiltActivity extends AppCompatActivity {
+
+    private MapView mMapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tilt);
+
+        // Target
+        LatLng nyc = new LatLng(40.73581, -73.99155);
+
+        // Set up the map
+        mMapView = (MapView) findViewById(R.id.tiltMapView);
+        mMapView.setAccessToken(ApiAccess.getToken(this));
+        mMapView.setStyleUrl(Style.MAPBOX_STREETS);
+        mMapView.setCenterCoordinate(nyc);
+        mMapView.setZoomLevel(11);
+        mMapView.onCreate(savedInstanceState);
+
+        /*
+         * Our tilt API follows Google's Android API:
+         * https://developers.google.com/maps/documentation/android-api/views#updating_the_camera_view
+         */
+
+        // Construct a CameraPosition focusing on target and animate the camera to that position.
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(nyc)      // Sets the center of the map to target
+                .zoom(17)         // Sets the zoom
+                .bearing(90)      // Sets the orientation of the camera to east
+                .tilt(30)         // Sets the tilt of the camera to 30 degrees
+                .build();         // Creates a CameraPosition from the builder
+
+        // Triggers tilt
+        //mMapView.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onPause()  {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+}
