f2b22666b09b430d5b11ed65d40d8635387f7ed9
Tobrun <tobrun.van.nuland@gmail.com>
2015-12-10 18:49:40 -0800

    [android] #3246 disable zoom controls

--- android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/MainActivity.java
+++ android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/MainActivity.java
@@ -116,7 +116,6 @@ public class MainActivity extends AppCompatActivity {
 
         mMapView = (MapView) findViewById(R.id.mainMapView);
         mMapView.setAccessToken(ApiAccess.getToken(this));
-        mMapView.setZoomControlsEnabled(true);
         mMapView.onCreate(savedInstanceState);
 
         mMapView.setOnFpsChangedListener(new MyOnFpsChangedListener());
