b98b686b9e38ddd254098d671f0a93c0c54497d8
Justin R. Miller <incanus@codesorcery.net>
2015-12-14 11:23:15 -0800

    use defined type

--- src/mbgl/map/map.cpp
+++ src/mbgl/map/map.cpp
@@ -410,7 +410,7 @@ LatLngBounds Map::getBoundsForAnnotations(const AnnotationIDs& annotations) {
 #pragma mark - Features
 
 FeatureResults Map::featuresAt(const PrecisionPoint point, const uint8_t radius) const {
-    return context->invokeSync<std::vector<std::tuple<std::string, std::string, FeatureProperties>>>(&MapContext::featuresAt, point, radius);
+    return context->invokeSync<FeatureResults>(&MapContext::featuresAt, point, radius);
 }
 
 
