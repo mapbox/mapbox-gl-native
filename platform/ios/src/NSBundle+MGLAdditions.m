26578ea9d5a5e8e208a10c6c59f8bc8d5bca471e
Minh Nguyễn <mxn@1ec5.org>
2015-12-22 15:05:47 -0800

    [ios] Look for resources in static framework bundle
    The library is packaged as a static framework, so there isn’t a separate resource bundle anymore. Instead, look for the static framework inside the Frameworks folder.

--- platform/ios/src/NSBundle+MGLAdditions.m
+++ platform/ios/src/NSBundle+MGLAdditions.m
@@ -8,7 +8,14 @@ void mgl_linkBundleCategory() {}
 
 + (instancetype)mgl_frameworkBundle
 {
-    return [self bundleForClass:[MGLAccountManager class]];
+    NSBundle *bundle = [self bundleForClass:[MGLAccountManager class]];
+    if (![bundle.infoDictionary[@"CFBundlePackageType"] isEqualToString:@"FMWK"]) {
+        // For static frameworks, the bundle is the containing application
+        // bundle but the resources are still in the framework bundle.
+        bundle = [NSBundle bundleWithPath:[bundle.privateFrameworksPath
+                                           stringByAppendingPathComponent:@"Mapbox.framework"]];
+    }
+    return bundle;
 }
 
 @end
