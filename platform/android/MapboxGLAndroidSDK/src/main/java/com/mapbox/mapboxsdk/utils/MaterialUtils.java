729d57fc26cdcc0f066d8d8c511049a8420663f3
Tobrun <tobrun.van.nuland@gmail.com>
2016-01-13 14:24:49 +0100

    [android] #3231 - added javadoc to MaterialUtils

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/utils/MaterialUtils.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/utils/MaterialUtils.java
@@ -7,24 +7,33 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.v4.graphics.drawable.DrawableCompat;
-import android.util.Log;
 import android.util.TypedValue;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.mapbox.mapboxsdk.R;
 
 public class MaterialUtils {
 
+    /**
+     * Returns a color integer associated as primary color from a theme based on a {@link Context}.
+     *
+     * @param context The context used to style the color attributes.
+     * @return The primary color value of current theme in the form 0xAARRGGBB.
+     */
     @ColorInt
     public static int getPrimaryColor(@NonNull Context context) {
         TypedValue typedValue = new TypedValue();
         Resources.Theme theme = context.getTheme();
         theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
-        Log.v("KLEUR",Integer.toHexString(typedValue.data));
         return typedValue.data;
     }
 
+    /**
+     * Returns a color integer associated as primary dark color from a theme based on a {@link Context}.
+     *
+     * @param context The context used to style the color attributes.
+     * @return The primary dark color value of current theme in the form 0xAARRGGBB.
+     */
     @ColorInt
     public static int getPrimaryDarkColor(@NonNull Context context) {
         TypedValue typedValue = new TypedValue();
@@ -33,6 +42,12 @@ public class MaterialUtils {
         return typedValue.data;
     }
 
+    /**
+     * Returns a color integer associated as accent color from a theme based on a {@link Context}.
+     *
+     * @param context The context used to style the color attributes.
+     * @return The accent color value of current theme in the form 0xAARRGGBB.
+     */
     @ColorInt
     public static int getAccentColor(@NonNull Context context) {
         TypedValue typedValue = new TypedValue();
@@ -41,27 +56,44 @@ public class MaterialUtils {
         return typedValue.data;
     }
 
+    /**
+     * Returns a color state list associated with a theme based on a {@link Context}
+     *
+     * @param context The context used to style the color attributes.
+     * @return A ColorStateList object containing the primary color of a theme
+     */
     @NonNull
-    public static ColorStateList getSelector(@NonNull Context context){
+    public static ColorStateList getSelector(@NonNull Context context) {
         return new ColorStateList(
                 new int[][]{
                         new int[]{android.R.attr.state_pressed},
                         new int[]{}
                 },
-                new int[] {
+                new int[]{
                         getPrimaryColor(context),
                         getPrimaryColor(context)
                 }
         );
     }
 
-    public static void setTintList(@NonNull ImageView imageView){
+    /**
+     * Set a primary color tint list to the {@link Drawable} of an {@link ImageView}.
+     *
+     * @param imageView The view to set the default tint list.
+     */
+    public static void setTintList(@NonNull ImageView imageView) {
         Drawable originalDrawable = imageView.getDrawable();
         Drawable wrappedDrawable = DrawableCompat.wrap(originalDrawable);
         DrawableCompat.setTintList(wrappedDrawable, getSelector(imageView.getContext()));
     }
 
-    public static void setTint(@NonNull Context context, @NonNull Drawable drawable){
+    /**
+     * Set a primary color tint to a {@link Drawable}.
+     *
+     * @param context  The context used to style the color attributes.
+     * @param drawable The drawable to tint.
+     */
+    public static void setTint(@NonNull Context context, @NonNull Drawable drawable) {
         Drawable wrappedDrawable = DrawableCompat.wrap(drawable);
         DrawableCompat.setTint(wrappedDrawable, MaterialUtils.getPrimaryColor(context));
     }
