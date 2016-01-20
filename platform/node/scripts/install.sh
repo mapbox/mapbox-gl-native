47ebf0fd59e3ed2b20ae29c0a4291ef8ff162f49
John Firebaugh <john.firebaugh@gmail.com>
2016-01-20 14:03:45 -0800

    [node] Build OS X on Bitrise

--- platform/node/scripts/install.sh
+++ platform/node/scripts/install.sh
@@ -3,13 +3,11 @@
 set -e
 set -o pipefail
 
-mapbox_time "checkout_mason" \
 git submodule update --init .mason
 
 export PATH="`pwd`/.mason:${PATH}" MASON_DIR="`pwd`/.mason"
 
 if [ ${TRAVIS_OS_NAME} == "linux" ]; then
-    mapbox_time "install_mesa" \
     mason install mesa 10.4.3
 fi
 
@@ -19,9 +17,7 @@ fi
 
 source ~/.nvm/nvm.sh
 
-mapbox_time $NODE_VERSION \
 nvm install $NODE_VERSION
-
 nvm alias default $NODE_VERSION
 
 node --version
