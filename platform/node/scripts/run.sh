837dd11f281f8c1c2229d6f1b0eb616b977d10f0
John Firebaugh <john.firebaugh@gmail.com>
2016-01-20 14:03:45 -0800

    [node] Build OS X on Bitrise

--- platform/node/scripts/run.sh
+++ platform/node/scripts/run.sh
@@ -13,19 +13,14 @@ BUILDTYPE=${BUILDTYPE:-Release}
 
 source ~/.nvm/nvm.sh
 nvm use $NODE_VERSION
-
-mapbox_time "compile_program" \
 npm install --build-from-source
 
 ################################################################################
 # Test
 ################################################################################
 
-# Travis OS X has no GPU
+# https://github.com/mapbox/mapbox-gl-native/issues/2150
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-    mapbox_time "run_tests" \
     npm test
-
-    mapbox_time "run_render_tests" \
     npm run test-suite
 fi
