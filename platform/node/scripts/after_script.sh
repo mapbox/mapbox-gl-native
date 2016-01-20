231e0b5de1b5074a2c954bd6e203cc5f0e6a2f46
John Firebaugh <john.firebaugh@gmail.com>
2016-01-20 14:03:45 -0800

    [node] Build OS X on Bitrise

--- platform/node/scripts/after_script.sh
+++ platform/node/scripts/after_script.sh
@@ -10,10 +10,9 @@ else
     otool -L ./lib/mapbox-gl-native.node
 fi
 
-COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
 
-if [[ ${TRAVIS_TAG} == node-v${PACKAGE_JSON_VERSION} ]] || test "${COMMIT_MESSAGE#*'[publish binary]'}" != "$COMMIT_MESSAGE"; then
+if [[ ${TRAVIS_TAG} == node-v${PACKAGE_JSON_VERSION} ]]; then
     source ~/.nvm/nvm.sh
     nvm use $NODE_VERSION
 
