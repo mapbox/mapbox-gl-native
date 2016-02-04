d64ad85d0b942b1e1b94d7f5361251b2dace0ef3
Brad Leege <bleege@gmail.com>
2016-02-04 13:52:02 -0600

    [android] #2421 - Using Constants for Version and Events

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/telemetry/MapboxEvent.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/telemetry/MapboxEvent.java
@@ -3,9 +3,12 @@ package com.mapbox.mapboxsdk.telemetry;
 import java.io.Serializable;
 
 public class MapboxEvent implements Serializable {
-    public static final int version = 1;
+    public static final int VERSION = 1;
     public static final String MGLMapboxEventsUserAgent = "Sirius/1.0/ANDROID MapboxEventsiOS/1.1";
     public static final String MGLMapboxEventsAPIBase = "https://api.tiles.mapbox.com";
+
+    // Event Types
+    public static final String MAPBOXEVENT_LOCATION = "location";
 
     public static final String MGLEventKeyLatitude = "lat";
     public static final String MGLEventKeyLongitude = "lng";
