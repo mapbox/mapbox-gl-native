06a8442072c004423eae1dfed2f8f15275df6f76
Brad Leege <bleege@gmail.com>
2016-02-04 16:04:55 -0600

    [android] #2421 - Adding remaining attributes as constants

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/telemetry/MapboxEvent.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/telemetry/MapboxEvent.java
@@ -22,4 +22,20 @@ public class MapboxEvent implements Serializable {
     public static final String MGLEventKeyCourse = "course";
     public static final String MGLEventKeyAltitude = "altitude";
     public static final String MGLEventKeyHorizontalAccuracy = "horizontalAccuracy";
+
+    public static final String MGLEventKeyPushEnabled = "enabled.push";
+    public static final String MGLEventKeyEmailEnabled = "enabled.email";
+    public static final String MGLEventKeyGestureID = "gesture";
+    public static final String MGLEventKeyArrivalDate = "arrivalDate";
+    public static final String MGLEventKeyDepartureDate = "departureDate";
+
+    public static final String MGLEventGestureSingleTap = "SingleTap";
+    public static final String MGLEventGestureDoubleTap = "DoubleTap";
+    public static final String MGLEventGestureTwoFingerSingleTap = "TwoFingerTap";
+    public static final String MGLEventGestureQuickZoom = "QuickZoom";
+    public static final String MGLEventGesturePanStart = "Pan";
+    public static final String MGLEventGesturePinchStart = "Pinch";
+    public static final String MGLEventGestureRotateStart = "Rotation";
+    public static final String MGLEventGesturePitchStart = "Pitch";
+
 }
