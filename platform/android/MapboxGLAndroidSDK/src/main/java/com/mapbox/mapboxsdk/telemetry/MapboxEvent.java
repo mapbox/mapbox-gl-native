901d5ceb07b36960fa524057cb58c9ae1a248ba4
Brad Leege <bleege@gmail.com>
2016-02-04 16:29:40 -0600

    [android] #2421 - Giving constants a more Java like naming scheme

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/telemetry/MapboxEvent.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/telemetry/MapboxEvent.java
@@ -7,50 +7,50 @@ public class MapboxEvent implements Serializable {
     public static final String MGLMapboxEventsAPIBase = "https://api.tiles.mapbox.com";
 
     // Event Types
-    public static final String MAPBOXEVENT_TURNSTILE = "appUserTurnstile";
-    public static final String MAPBOXEVENT_MAP_LOAD = "map.load";
-    public static final String MAPBOXEVENT_MAP_CLICK = "map.click";
-    public static final String MAPBOXEVENT_MAP_DRAGEND = "map.dragend";
-    public static final String MAPBOXEVENT_LOCATION = "location";
-    public static final String MAPBOXEVENT_VISIT = "visit";
+    public static final String TYPE_TURNSTILE = "appUserTurnstile";
+    public static final String TYPE_MAP_LOAD = "map.load";
+    public static final String TYPE_MAP_CLICK = "map.click";
+    public static final String TYPE_MAP_DRAGEND = "map.dragend";
+    public static final String TYPE_LOCATION = "location";
+    public static final String TYPE_VISIT = "visit";
 
     // Event Keys
-    public static final String MGLEventKeyLatitude = "lat";
-    public static final String MGLEventKeyLongitude = "lng";
-    public static final String MGLEventKeySpeed = "speed";
-    public static final String MGLEventKeyCourse = "course";
-    public static final String MGLEventKeyAltitude = "altitude";
-    public static final String MGLEventKeyHorizontalAccuracy = "horizontalAccuracy";
+    public static final String KEY_LATITUDE = "lat";
+    public static final String KEY_LONGITUDE = "lng";
+    public static final String KEY_SPEED = "speed";
+    public static final String KEY_COURSE = "course";
+    public static final String KEY_ALTITUDE = "altitude";
+    public static final String KEY_HORIZONTAL_ACCURACY = "horizontalAccuracy";
 
-    public static final String MGLEventKeyPushEnabled = "enabled.push";
-    public static final String MGLEventKeyEmailEnabled = "enabled.email";
-    public static final String MGLEventKeyGestureID = "gesture";
-    public static final String MGLEventKeyArrivalDate = "arrivalDate";
-    public static final String MGLEventKeyDepartureDate = "departureDate";
+    public static final String KEY_PUSH_ENABLED = "enabled.push";
+    public static final String KEY_EMAIL_ENABLED = "enabled.email";
+    public static final String KEY_GESTURE_ID = "gesture";
+    public static final String KEY_ARRIVAL_DATE = "arrivalDate";
+    public static final String KEY_DEPARTURE_DATE = "departureDate";
 
-    public static final String MGLEventGestureSingleTap = "SingleTap";
-    public static final String MGLEventGestureDoubleTap = "DoubleTap";
-    public static final String MGLEventGestureTwoFingerSingleTap = "TwoFingerTap";
-    public static final String MGLEventGestureQuickZoom = "QuickZoom";
-    public static final String MGLEventGesturePanStart = "Pan";
-    public static final String MGLEventGesturePinchStart = "Pinch";
-    public static final String MGLEventGestureRotateStart = "Rotation";
-    public static final String MGLEventGesturePitchStart = "Pitch";
+    public static final String GESTURE_SINGLETAP = "SingleTap";
+    public static final String GESTURE_DOUBLETAP = "DoubleTap";
+    public static final String GESTURE_TWO_FINGER_SINGLETAP = "TwoFingerTap";
+    public static final String GESTURE_QUICK_ZOOM = "QuickZoom";
+    public static final String GESTURE_PAN_START = "Pan";
+    public static final String GESTURE_PINCH_START = "Pinch";
+    public static final String GESTURE_ROTATION_START = "Rotation";
+    public static final String GESTURE_PITCH_START = "Pitch";
 
     // Event Attributes
-    public static final int VERSION = 1;
-    public static final String CREATED = "created";
-    public static final String INSTANCE = "instance";
-    public static final String VENDORID = "vendorId";
-    public static final String APPBUNDLEID = "appBundleId";
-    public static final String MODEL = "model";
-    public static final String OPERATINGSYSTEM= "operatingSystem";
-    public static final String ORIENTATION = "orientation";
-    public static final String BATTERYLEVEL = "batteryLevel";
-    public static final String APPLICATIONSTATE = "applicationState";
-    public static final String RESOLUTION = "resolution";
-    public static final String ACCESSIBILITYFONTSCALE = "accessibilityFontScale";
-    public static final String CARRIER = "carrier";
-    public static final String CELLULARNETWORKTYPE = "cellularNetworkType";
-    public static final String WIFI = "wifi";
+    public static final int ATTRIBUTE_VERSION = 1;
+    public static final String ATTRIBUTE_CREATED = "created";
+    public static final String ATTRIBUTE_INSTANCE = "instance";
+    public static final String ATTRIBUTE_VENDOR_ID = "vendorId";
+    public static final String ATTRIBUTE_APP_BUNDLE_ID = "appBundleId";
+    public static final String ATTRIBUTE_MODEL = "model";
+    public static final String ATTRIBUTE_OPERATING_SYSTEM= "operatingSystem";
+    public static final String ATTRIBUTE_ORIENTATION = "orientation";
+    public static final String ATTRIBUTE_BATTERY_LEVEL = "batteryLevel";
+    public static final String ATTRIBUTE_APPLICATION_STATE = "applicationState";
+    public static final String ATTRIBUTE_RESOLUTION = "resolution";
+    public static final String ATTRIBUTE_ACCESSIBILITY_FONT_SCALE = "accessibilityFontScale";
+    public static final String ATTRIBUTE_CARRIER = "carrier";
+    public static final String ATTRIBUTE_CELLULAR_NETWORK_TYPE = "cellularNetworkType";
+    public static final String ATTRIBUTE_WIFI = "wifi";
 }
