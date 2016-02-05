fa210516834feada45d69e218f9c1dce2d2ee6f6
Tobrun <tobrun@mapbox.com>
2016-02-05 10:25:18 +0100

    [android] #3830 added mocked parcel implementation

--- platform/android/MapboxGLAndroidSDKTestApp/src/test/java/com/mapbox/mapboxsdk/geometry/LatLngTest.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/test/java/com/mapbox/mapboxsdk/geometry/LatLngTest.java
@@ -4,10 +4,15 @@ import android.location.Location;
 import android.os.Parcel;
 
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.utils.MockParcel;
 
 import org.junit.Test;
 
+import java.util.Objects;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.mock;
@@ -21,6 +26,24 @@ public class LatLngTest {
     public void testSanity() {
         LatLng latLng = new LatLng(0.0, 0.0);
         assertNotNull("latLng  should not be null", latLng);
+    }
+
+    @Test
+    public void testLatitudeEmptyConstructor() {
+        LatLng latLng = new LatLng();
+        assertEquals("latitude default value", latLng.getLatitude(), 0, DELTA);
+    }
+
+    @Test
+    public void testLongitudeEmptyConstructor() {
+        LatLng latLng = new LatLng();
+        assertEquals("longitude default value", latLng.getLongitude(), 0, DELTA);
+    }
+
+    @Test
+    public void testAltitudeEmptyConstructor() {
+        LatLng latLng1 = new LatLng();
+        assertEquals("altitude default value", latLng1.getAltitude(), 0.0, DELTA);
     }
 
     @Test
@@ -129,6 +152,54 @@ public class LatLngTest {
         assertEquals("string should match",
                 latLng.toString(),
                 "LatLng [longitude=3.4, latitude=1.2, altitude=5.6]");
+    }
+
+    @Test
+    public void testEqualsOther() {
+        double latitude = 1.2;
+        double longitude = 3.4;
+        double altitude = 5.6;
+        LatLng latLng1 = new LatLng(latitude, longitude, altitude);
+        LatLng latLng2 = new LatLng(latitude, longitude, altitude);
+        assertEquals("LatLng should match", latLng1, latLng2);
+    }
+
+    @Test
+    public void testEqualsItself() {
+        LatLng latLng = new LatLng(1,2,3);
+        assertEquals("LatLng should match", latLng, latLng);
+    }
+
+    @Test
+    public void testNotEquals() {
+        LatLng latLng = new LatLng(1, 2);
+        assertNotEquals("LatLng should match", latLng, new Object());
+    }
+
+    @Test
+    public void testParcelable() {
+        LatLng latLng = new LatLng(1, 2, 3);
+        Parcel parcel = MockParcel.obtain();
+        latLng.writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+        LatLng parceledLatLng = LatLng.CREATOR.createFromParcel(parcel);
+        assertEquals("parcel should match initial object", latLng, parceledLatLng);
+    }
+
+    @Test
+    public void testParcelableArray() {
+        LatLng[] latLngs = new LatLng[]{new LatLng(1, 2, 3), new LatLng(1, 2)};
+        Parcel parcel = MockParcel.obtain();
+        parcel.writeParcelableArray(latLngs, 0);
+        parcel.setDataPosition(0);
+        LatLng[] parceledLatLngs = (LatLng[]) parcel.readParcelableArray(LatLng.class.getClassLoader());
+        assertArrayEquals("parcel should match initial object", latLngs, parceledLatLngs);
+    }
+
+    @Test
+    public void testDescribeContents() {
+        LatLng latLng = new LatLng(1.2, 3.4);
+        assertEquals("contents should be 0", 0, latLng.describeContents(), DELTA);
     }
 
 }
