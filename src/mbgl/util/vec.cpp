1ce99d2d7b1921dfac1d302553160184ceac7d3e
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-02-09 13:02:22 +0200

    [core] Explicit instantiate vec{2,3,4}<double>

---
+++ src/mbgl/util/vec.cpp
@@ -0,0 +1,11 @@
+#include <mbgl/util/vec.hpp>
+
+namespace mbgl {
+
+template struct vec2<double>;
+template struct vec2<float>;
+
+template struct vec3<double>;
+template struct vec4<double>;
+
+} // namespace mbgl
