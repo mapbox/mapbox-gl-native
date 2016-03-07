ec7a7460076576ee669c5b100e82d650c13334d7
Romain Quidet <romain.quidet.ext@mappy.com>
2016-03-07 17:34:16 +0100

    remove tests on demo app

--- platform/ios/app/MBXViewController.mm
+++ platform/ios/app/MBXViewController.mm
@@ -175,7 +175,6 @@ static const CLLocationCoordinate2D WorldTourDestinations[] = {
                                                                  : @"Show Custom Style Layer"),
                                                                 @"Print Telemetry Logfile",
                                                                 @"Delete Telemetry Logfile",
-                            @"test",
                                                                 nil];
 
     [sheet showFromBarButtonItem:self.navigationItem.leftBarButtonItem animated:YES];
@@ -326,10 +325,6 @@ static const CLLocationCoordinate2D WorldTourDestinations[] = {
                 NSLog(@"Error deleting telemetry log: %@", error.localizedDescription);
             }
         }
-    }
-    else if (buttonIndex == actionSheet.firstOtherButtonIndex + 16)
-    {
-        self.mapView.contentInset = UIEdgeInsetsMake(64.0, 0.0, 0.0, 50.0);
     }
 }
 
