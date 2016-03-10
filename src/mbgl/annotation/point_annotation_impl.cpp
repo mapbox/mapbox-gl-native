c561ea5d0564193a4fb7cb84fba5a2d008e30540
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-03-10 19:49:09 +0200

    [core] Check for NaNs in mbgl::util::{min,max,clamp}

--- src/mbgl/annotation/point_annotation_impl.cpp
+++ src/mbgl/annotation/point_annotation_impl.cpp
@@ -13,7 +13,7 @@ void PointAnnotationImpl::updateLayer(const TileID& tileID, AnnotationTileLayer&
     featureProperties.emplace("sprite", point.icon.empty() ? std::string("default_marker") : point.icon);
 
     mbgl::ScreenCoordinate projected = point.position.project();
-    projected *= 1 << tileID.z;
+    projected *= std::pow(2, tileID.z);
     projected.x = std::fmod(projected.x, 1);
     projected.y = std::fmod(projected.y, 1);
     projected *= GeometryTileFeature::defaultExtent;
