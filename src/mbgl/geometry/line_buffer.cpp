3cc55a9dacf2c0984ca277500b334c3f44fa60c0
Ansis Brammanis <brammanis@gmail.com>
2016-03-28 16:58:54 -0700

    [core] pack extrude and line distance into unsigned byte
    instead of a signed byte. It's a bit easier to understand.
    
    https://github.com/mapbox/mapbox-gl-js/commit/6c29251b3572ce9f2afae8182b56da6c1b9838b4

--- src/mbgl/geometry/line_buffer.cpp
+++ src/mbgl/geometry/line_buffer.cpp
@@ -13,16 +13,24 @@ GLsizei LineVertexBuffer::add(vertex_type x, vertex_type y, float ex, float ey, 
     coords[0] = (x * 2) | tx;
     coords[1] = (y * 2) | ty;
 
-    int8_t *extrude = static_cast<int8_t *>(data);
-    extrude[4] = ::round(extrudeScale * ex);
-    extrude[5] = ::round(extrudeScale * ey);
+    uint8_t *ubytes = static_cast<uint8_t *>(data);
+    // add 128 to store an byte in an unsigned byte
+    ubytes[4] = ::round(extrudeScale * ex) + 128;
+    ubytes[5] = ::round(extrudeScale * ey) + 128;
+
+    // Encode the -1/0/1 direction value into the first two bits of .z of a_data.
+    // Combine it with the lower 6 bits of `linesofar` (shifted by 2 bites to make
+    // room for the direction value). The upper 8 bits of `linesofar` are placed in
+    // the `w` component. `linesofar` is scaled down by `LINE_DISTANCE_SCALE` so that
+    // we can store longer distances while sacrificing precision.
 
     // Encode the -1/0/1 direction value into .zw coordinates of a_data, which is normally covered
     // by linesofar, so we need to merge them.
     // The z component's first bit, as well as the sign bit is reserved for the direction,
     // so we need to shift the linesofar.
-    extrude[6] = ((dir < 0) ? -1 : 1) * ((dir ? 1 : 0) | static_cast<int8_t>((linesofar << 1) & 0x7F));
-    extrude[7] = ((linesofar >> 6) & 0xFF) - 128;
+
+    ubytes[6] = ((dir == 0 ? 0 : (dir < 0 ? -1 : 1 )) + 1) | ((linesofar & 0x3F) << 2);
+    ubytes[7] = linesofar >> 6;
 
     return idx;
 }
