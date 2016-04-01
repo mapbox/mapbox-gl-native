7d55c33f170142c8b2f4e4b4774a1c0f9820550d
John Firebaugh <john.firebaugh@gmail.com>
2016-04-01 15:44:57 -0700

    [osx] Use a more reliable hack for the getting the .xcscheme file into place

--- platform/osx/scripts/test.sh
+++ platform/osx/scripts/test.sh
@@ -9,17 +9,8 @@ OSX_PROJ_PATH=./build/osx-x86_64/gyp/osx.xcodeproj
 
 export BUILDTYPE=${BUILDTYPE:-Release}
 
-if [[ ! -e "${OSX_PROJ_PATH}/xcshareddata/xcschemes/osxsdk.xcscheme" ]]; then
-    # Generate schemes
-    open -g "${OSX_PROJ_PATH}"
-    sleep 20
-    
-    # Share osxsdk scheme
-    mkdir -pv "${OSX_PROJ_PATH}/xcshareddata/xcschemes/"
-    mv -v \
-        "${OSX_PROJ_PATH}/xcuserdata/${USER}.xcuserdatad/xcschemes/osxsdk.xcscheme" \
-        "${OSX_PROJ_PATH}/xcshareddata/xcschemes/"
-fi
+mkdir -p "${OSX_PROJ_PATH}/xcshareddata/xcschemes"
+cp platform/osx/scripts/osxsdk.xcscheme "${OSX_PROJ_PATH}/xcshareddata/xcschemes/osxsdk.xcscheme"
 
 xcodebuild -verbose \
     -sdk macosx${OSX_SDK_VERSION} \
