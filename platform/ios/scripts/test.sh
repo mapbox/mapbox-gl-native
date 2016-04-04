11f7db293e5488690b87ee0260f910e49e5e0275
John Firebaugh <john.firebaugh@gmail.com>
2016-04-04 11:52:10 -0700

    [ios] Remove KIF-based tests from CI. They just don't work reliably.

--- platform/ios/scripts/test.sh
+++ platform/ios/scripts/test.sh
@@ -4,9 +4,4 @@ set -e
 set -o pipefail
 set -u
 
-xcodebuild \
-    -project ./platform/ios/test/ios-tests.xcodeproj \
-    -scheme 'Mapbox GL Tests' \
-    -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' \
-    test
+# No tests for now. KIF tests are too unreliable.
