2fcfc2a939df12e7abf75a9e5659a82728b805f2
John Firebaugh <john.firebaugh@gmail.com>
2016-04-15 13:18:31 -0700

    [ios] Don't try to launch test server (#4719)

--- test/src/mbgl/test/test.cpp
+++ test/src/mbgl/test/test.cpp
@@ -6,7 +6,10 @@
 namespace mbgl {
 
 int runTests(int argc, char *argv[]) {
+#if TEST_HAS_SERVER
     auto server = std::make_unique<test::Server>("test/storage/server.js");
+#endif
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
