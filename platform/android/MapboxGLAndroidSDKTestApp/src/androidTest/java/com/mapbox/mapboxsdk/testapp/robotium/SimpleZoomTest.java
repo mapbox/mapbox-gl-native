ba9703351d6bfc9bfa9bcf54553d62961e42c723
Tobrun <tobrun@mapbox.com>
2016-04-26 12:25:33 +0200

    [android] #4845 - disable robotium

--- platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/testapp/robotium/SimpleZoomTest.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/testapp/robotium/SimpleZoomTest.java
@@ -6,21 +6,23 @@ import com.mapbox.mapboxsdk.geometry.LatLng;
 
 import org.junit.Test;
 
-public class SimpleZoomTest extends BaseMainActivityTest {
-
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-    }
-
-    @Test
-    public void testZoomIn() {
-        float currentZoomlevel = MapboxConstants.MINIMUM_ZOOM;
-        while (currentZoomlevel < MapboxConstants.MAXIMUM_ZOOM - 5) {
-            mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(47.376271, 8.539116),currentZoomlevel));
-            currentZoomlevel++;
-            solo.sleep(1400);
-        }
-    }
+public class SimpleZoomTest {
+//    extends
+//} BaseMainActivityTest {
+//
+//    @Override
+//    public void setUp() throws Exception {
+//        super.setUp();
+//    }
+//
+//    @Test
+//    public void testZoomIn() {
+//        float currentZoomlevel = MapboxConstants.MINIMUM_ZOOM;
+//        while (currentZoomlevel < MapboxConstants.MAXIMUM_ZOOM - 5) {
+//            mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(47.376271, 8.539116),currentZoomlevel));
+//            currentZoomlevel++;
+//            solo.sleep(1400);
+//        }
+//    }
 
 }
