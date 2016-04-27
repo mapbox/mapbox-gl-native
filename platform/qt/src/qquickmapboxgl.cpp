c7379f188ef33ead6953eb29651b46377aed9c6e
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-04-27 16:10:32 +0300

    [Qt] Flip Qt Quick item vertically if Qt version >= 5.6

--- platform/qt/src/qquickmapboxgl.cpp
+++ platform/qt/src/qquickmapboxgl.cpp
@@ -7,6 +7,10 @@
 QQuickMapboxGL::QQuickMapboxGL(QQuickItem *parent_)
     : QQuickFramebufferObject(parent_)
 {
+#if QT_VERSION >= 0x050600
+    // FIXME: https://github.com/mapbox/mapbox-gl-native/issues/4866
+    setMirrorVertically(true);
+#endif
 }
 
 QQuickMapboxGL::~QQuickMapboxGL()
