833a0a26df3d0beeb9f419f3eac91c8d2e21b437
Jason Wray <jason@mapbox.com>
2016-05-05 18:51:39 -0400

    Limit callouts to point annotations
    Also, include callout properties in object description.

--- platform/darwin/src/MGLPointAnnotation.m
+++ platform/darwin/src/MGLPointAnnotation.m
@@ -3,14 +3,17 @@
 @implementation MGLPointAnnotation
 
 @synthesize coordinate;
+@synthesize canShowCallout;
+@synthesize calloutView;
 
 - (NSString *)description
 {
-    return [NSString stringWithFormat:@"<%@: %p; title = %@; subtitle = %@; coordinate = %f, %f>",
+    return [NSString stringWithFormat:@"<%@: %p; title = %@; subtitle = %@; coordinate = %f, %f; callout: %@ (%@)>",
             NSStringFromClass([self class]), (void *)self,
             self.title ? [NSString stringWithFormat:@"\"%@\"", self.title] : self.title,
             self.subtitle ? [NSString stringWithFormat:@"\"%@\"", self.subtitle] : self.subtitle,
-            coordinate.latitude, coordinate.longitude];
+            coordinate.latitude, coordinate.longitude,
+            canShowCallout ? @"yes" : @"no", calloutView];
 }
 
 @end
--- platform/darwin/src/MGLShape.h
+++ platform/darwin/src/MGLShape.h
@@ -1,10 +1,5 @@
 #import <Foundation/Foundation.h>
 
-#if TARGET_OS_IPHONE
-#import <UIKit/UIKit.h>
-#import "MGLCalloutView.h"
-#endif
-
 #import "MGLAnnotation.h"
 
 #import "MGLTypes.h"
@@ -30,14 +25,6 @@ NS_ASSUME_NONNULL_BEGIN
  `nil`.
  */
 @property (nonatomic, copy, nullable) NSString *subtitle;
-
-#if TARGET_OS_IPHONE
-
-@property (nonatomic) BOOL canShowCallout;
-
-@property (nonatomic, nullable) UIView <MGLCalloutView> *calloutView;
-
-#endif
 
 #if !TARGET_OS_IPHONE
 
