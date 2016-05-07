461cf567cc8a0233aa2cd03801099d361890466c
Jason Wray <jason@mapbox.com>
2016-05-07 01:18:17 -0400

    Expand tests to cover more types

--- test/util/number_conversions.cpp
+++ test/util/number_conversions.cpp
@@ -6,6 +6,12 @@
 using namespace mbgl;
 
 TEST(NumberConversions, number_to_string) {
+    EXPECT_EQ("0", util::toString(0));
+    EXPECT_EQ("10", util::toString(10));
+    EXPECT_EQ("-10", util::toString(-10));
+    EXPECT_EQ("10", util::toString(10.0));
+    EXPECT_EQ("-10", util::toString(-10.0));
+
     EXPECT_EQ("1", util::toString(1));
     EXPECT_EQ("-1", util::toString(-1));
     EXPECT_EQ("32768", util::toString(32768));
@@ -35,4 +41,12 @@ TEST(NumberConversions, number_to_string) {
     EXPECT_EQ("-1.123457", util::toString(-1.123456789));
     EXPECT_EQ("32768.123457", util::toString(32768.123456789));
     EXPECT_EQ("-32768.123457", util::toString(-32768.123456789));
+
+    EXPECT_EQ("3.141593", util::toString(3.1415926535897932385128089594061862044327426701784));
+
+    EXPECT_EQ("0.000053", util::toString((long double)5.32e-5));
+    EXPECT_EQ("0.000000", util::toString((long double)5.32e-40));
+
+    EXPECT_EQ("10", util::toString(unsigned(10)));
+    EXPECT_EQ("10", util::toString(unsigned(10.0)));
 }
