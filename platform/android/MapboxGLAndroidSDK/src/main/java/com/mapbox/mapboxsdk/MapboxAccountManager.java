56cf42e12a6e83f203acf6850cd9a5b210166a5c
Brad Leege <bleege@gmail.com>
2016-05-11 11:01:18 -0500

    [android] #5004 - Initial MapboxAccountManager singleton

---
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/MapboxAccountManager.java
@@ -0,0 +1,38 @@
+package com.mapbox.mapboxsdk;
+
+public class MapboxAccountManager {
+
+    private static MapboxAccountManager mapboxAccountManager = null;
+
+    private final String accessToken;
+
+    /**
+     * MapboxAccountManager should NOT be instantiated directly.
+     * Use MapboxAccountManager.getInstance() instead.
+     * @param accessToken
+     */
+    private MapboxAccountManager(String accessToken) {
+        super();
+        this.accessToken = accessToken;
+    }
+
+    /**
+     * Get an instance of MapboxAccountManager configured with the app's Access Token
+     * @param accessToken Mapbox Access Token.  You can get one on the Mapbox Web site.
+     * @return MapboxAccountManager instance for app
+     */
+    public static MapboxAccountManager getInstance(String accessToken) {
+        if (mapboxAccountManager == null) {
+            mapboxAccountManager = new MapboxAccountManager(accessToken);
+        }
+        return mapboxAccountManager;
+    }
+
+    /**
+     * Access Token for this application
+     * @return Mapbox Access Token
+     */
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
