60bce7014bcad14823a6007b3441bb99f610a34e
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-05-24 19:26:27 +0300

    [build] Restrict C++11 ABI check script for Linux

--- scripts/check-cxx11abi.sh
+++ scripts/check-cxx11abi.sh
@@ -3,6 +3,11 @@
 set -e
 set -o pipefail
 
+if [ ! `uname -s` = 'Linux' ]; then
+    echo ""
+    exit 0
+fi
+
 # check-cxx11abi.dat is a binary just so we can use the loader
 # to take care of finding the libstdc++ which can be tricky.
 LIBSTDCPP=$(ldd $(dirname $0)/check-cxx11abi.dat |grep libstdc++ |cut -d' ' -f3)
