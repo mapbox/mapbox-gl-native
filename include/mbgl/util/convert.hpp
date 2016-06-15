121b4ca9ca0fdf4d46c296ffc0372beca80b1cb6
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-06-15 16:33:14 +0300

    [core] Added mbgl::util::convert helper function
    Converts a given std::array into another using a different (convertible)
    type.

---
+++ include/mbgl/util/convert.hpp
@@ -0,0 +1,17 @@
+#include <array>
+#include <type_traits>
+#include <utility>
+
+namespace mbgl {
+namespace util {
+
+template<typename To, typename From, std::size_t Size,
+         typename = std::enable_if_t<std::is_convertible<From, To>::value>>
+constexpr std::array<To, Size> convert(const std::array<From, Size>&from) {
+    std::array<To, Size> to {};
+    std::copy(std::begin(from), std::end(from), std::begin(to));
+    return to;
+}
+
+} // namespace util
+} // namespace mbgl
