31fc6940c0dd96357cea48d598fd9660c326fe09
Jason Wray <jason@mapbox.com>
2016-07-11 22:58:49 -0400

    [ios] Pass along `animated` property when selecting annotation views

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -3463,8 +3463,8 @@ mbgl::Duration MGLDurationInSeconds(NSTimeInterval duration)
             positioningRect = annotationView.frame;
             
             [annotationView.superview bringSubviewToFront:annotationView];
-            
-            annotationView.selected = YES;
+
+            [annotationView setSelected:YES animated:animated];
         }
     }
     
