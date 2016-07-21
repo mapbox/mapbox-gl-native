57bed88b03e87344712800f8fbe796f81a00fc16
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-07-21 12:48:46 +0300

    [build] Copy 'ca-bundle.crt' for mbgl-glfw target

--- cmake/glfw.cmake
+++ cmake/glfw.cmake
@@ -22,6 +22,13 @@ target_link_libraries(mbgl-glfw
     PRIVATE mbgl-core
 )
 
+add_custom_command(
+    TARGET mbgl-glfw POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy
+            ${CMAKE_SOURCE_DIR}/common/ca-bundle.crt
+            ${CMAKE_CURRENT_BINARY_DIR}/ca-bundle.crt
+)
+
 target_add_mason_package(mbgl-glfw PRIVATE glfw)
 
 mbgl_platform_glfw()
