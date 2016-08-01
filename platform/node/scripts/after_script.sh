ad5a7f8b5496dc39a063b058ee94a41a816fc32c
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-08-01 18:27:04 +0300

    [node] build and publish Node.js binaries with BUILDTYPE=Release
    Skip tests when publishing binaries and skip publishing render test results when those tests don't run.

--- platform/node/scripts/after_script.sh
+++ platform/node/scripts/after_script.sh
@@ -6,17 +6,19 @@ set -o pipefail
 JOB=$1
 TAG=$2
 
-if [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] ; then
-    gzip --stdout node_modules/mapbox-gl-test-suite/render-tests/index.html | \
-        aws s3 cp --acl public-read --content-encoding gzip --content-type text/html \
-            - s3://mapbox/mapbox-gl-native/render-tests/$JOB/index.html
+if [[ ${PUBLISH} ]]; then
+    if [[ "$BUILDTYPE" == "Debug" ]]; then
+        echo "Please run this script in release mode (BUILDTYPE=Release)."
+        exit 1
+    else
+        ./node_modules/.bin/node-pre-gyp package publish info
+    fi
+else
+    if [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] ; then
+        gzip --stdout node_modules/mapbox-gl-test-suite/render-tests/index.html | \
+            aws s3 cp --acl public-read --content-encoding gzip --content-type text/html \
+                - s3://mapbox/mapbox-gl-native/render-tests/$JOB/index.html
 
-    echo http://mapbox.s3.amazonaws.com/mapbox-gl-native/render-tests/$JOB/index.html
-fi
-
-PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
-
-if [[ $TAG == node-v${PACKAGE_JSON_VERSION} ]]; then
-    ./node_modules/.bin/node-pre-gyp package
-    ./node_modules/.bin/node-pre-gyp publish info
+        echo http://mapbox.s3.amazonaws.com/mapbox-gl-native/render-tests/$JOB/index.html
+    fi
 fi
