ab88bb7f09d28aff5441102b6e68af94e7015c8f
Thiago Marcos P. Santos <thiago@mapbox.com>
2016-08-03 19:56:10 +0300

    [Qt] Use QThreadStorage for thread local
    Portable.

---
+++ platform/qt/src/thread_local.cpp
@@ -0,0 +1,43 @@
+#include <mbgl/style/class_dictionary.hpp>
+#include <mbgl/util/run_loop.hpp>
+#include <mbgl/util/thread_local.hpp>
+
+#include <array>
+
+#include <QThreadStorage>
+
+namespace mbgl {
+namespace util {
+
+template <class T>
+class ThreadLocal<T>::Impl {
+public:
+    QThreadStorage<std::array<T*, 1>> local;
+};
+
+template <class T>
+ThreadLocal<T>::ThreadLocal() : impl(std::make_unique<Impl>()) {
+    set(nullptr);
+}
+
+template <class T>
+ThreadLocal<T>::~ThreadLocal() {
+    delete get();
+}
+
+template <class T>
+T* ThreadLocal<T>::get() {
+    return impl->local.localData()[0];
+}
+
+template <class T>
+void ThreadLocal<T>::set(T* ptr) {
+   impl->local.localData()[0] = ptr;
+}
+
+template class ThreadLocal<RunLoop>;
+template class ThreadLocal<int>;
+template class ThreadLocal<style::ClassDictionary>;
+
+} // namespace util
+} // namespace mbgl
