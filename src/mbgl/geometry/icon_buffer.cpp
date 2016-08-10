c75637b936b9e91bcb8aaa42f940afa9a6e48c22
amyleewalton <amylee@mapbox.com>
2016-08-10 10:53:26 -0700

    updated make tests

--- src/mbgl/geometry/icon_buffer.cpp
+++ src/mbgl/geometry/icon_buffer.cpp
@@ -22,11 +22,10 @@ size_t IconVertexBuffer::add(int16_t x, int16_t y, float ox, float oy, int16_t t
     ushorts[5] /* tex */ = ty / 4;
 
     uint8_t *ubytes = static_cast<uint8_t *>(data);
-    // a_data1
+    // a_data2
     ubytes[12] /* labelminzoom */ = labelminzoom * 10;
     ubytes[13] /* labelangle */ = labelangle;
 
-    // a_data2
     ubytes[14] /* minzoom */ = minzoom * 10; // 1/10 zoom levels: z16 == 160.
     ubytes[15] /* maxzoom */ = ::fmin(maxzoom, 25) * 10; // 1/10 zoom levels: z16 == 160.
 
