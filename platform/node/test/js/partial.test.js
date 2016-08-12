2c2fbd083328473c3d9938a18bbaef62abfb0e07
Mike Morris <mikemorris@users.noreply.github.com>
2016-08-12 10:11:57 -0400

    [node] add tests for partial tile states caused by slow resource requests

---
+++ platform/node/test/js/partial.test.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var test = require('tape');
+var mbgl = require('../../../../lib/mapbox-gl-native');
+var fs = require('fs');
+var path = require('path');
+var glyphs = require('../fixtures/glyphs.json');
+var sprite = require('../fixtures/sprite.json');
+
+mbgl.on('message', function(msg) {
+    console.log('%s (%s): %s', msg.severity, msg.class, msg.text);
+});
+
+test('Partial tiles with slow resources', function(t) {
+    var options = {
+        request: function(req, callback) {
+            function readFile() {
+                fs.readFile(path.join(__dirname, '..', req.url), function(err, data) {
+                    callback(err, { data: data });
+                });
+            }
+
+            switch (req.kind) {
+                case 4:
+                case 5:
+                case 6:
+                    setTimeout(readFile, 1000);
+                    break;
+                default:
+                   readFile(); 
+            }
+        },
+        ratio: 1
+    };
+
+    t.test('Slow glyphs', function(t) {
+        var map = new mbgl.Map(options);
+        map.load(glyphs);
+
+        map.render(glyphs, function(err, pixels) {
+            t.error(err);
+            t.end();
+        });
+    });
+
+    t.test('Slow sprite', function(t) {
+        var map = new mbgl.Map(options);
+        map.load(sprite);
+
+        map.render(sprite, function(err, pixels) {
+            t.error(err);
+            t.end();
+        });
+    });
+
+    t.end();
+});
