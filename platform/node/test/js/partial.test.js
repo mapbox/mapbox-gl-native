b3a4d9c7d0dfee38a354a3be2efea0a3fc9ae130
Mike Morris <mikemorris@users.noreply.github.com>
2016-08-12 11:19:47 -0400

    [node] Add test for slow vector tiles

--- platform/node/test/js/partial.test.js
+++ platform/node/test/js/partial.test.js
@@ -4,11 +4,41 @@ var test = require('tape');
 var mbgl = require('../../../../lib/mapbox-gl-native');
 var fs = require('fs');
 var path = require('path');
+var style = require('../fixtures/style.json');
 var glyphs = require('../fixtures/glyphs.json');
 var sprite = require('../fixtures/sprite.json');
 
 mbgl.on('message', function(msg) {
     console.log('%s (%s): %s', msg.severity, msg.class, msg.text);
+});
+
+test('Slow tiles', function(t) {
+    var options = {
+        request: function(req, callback) {
+            function readFile() {
+                fs.readFile(path.join(__dirname, '..', req.url), function(err, data) {
+                    callback(err, { data: data });
+                });
+            }
+
+            switch (req.kind) {
+                case 3:
+                    setTimeout(readFile, 1000);
+                    break;
+                default:
+                   readFile(); 
+            }
+        },
+        ratio: 1
+    };
+
+    var map = new mbgl.Map(options);
+    map.load(style);
+
+    map.render({}, function(err, pixels) {
+        t.error(err);
+        t.end();
+    });
 });
 
 test('Partial tiles with slow resources', function(t) {
