11fe1ae7935e2450ec108e9e6d1243aec8579554
Mike Morris <mikemorris@users.noreply.github.com>
2016-08-24 14:39:15 -0400

    add GL_CONTEXT_LOST case to checkError
    and reorder according to hex value
    https://www.opengl.org/wiki/OpenGL_Error#Testing_for_errors

--- src/mbgl/gl/gl.cpp
+++ src/mbgl/gl/gl.cpp
@@ -69,13 +69,16 @@ void checkError(const char *cmd, const char *file, int line) {
             case GL_INVALID_ENUM: error = "INVALID_ENUM"; break;
             case GL_INVALID_VALUE: error = "INVALID_VALUE"; break;
             case GL_INVALID_OPERATION: error = "INVALID_OPERATION"; break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION:  error = "INVALID_FRAMEBUFFER_OPERATION";  break;
-            case GL_OUT_OF_MEMORY: error = "OUT_OF_MEMORY"; break;
+#ifdef GL_STACK_OVERFLOW
+            case GL_STACK_OVERFLOW:  error = "STACK_OVERFLOW";  break;
+#endif
 #ifdef GL_STACK_UNDERFLOW
             case GL_STACK_UNDERFLOW:  error = "STACK_UNDERFLOW";  break;
 #endif
-#ifdef GL_STACK_OVERFLOW
-            case GL_STACK_OVERFLOW:  error = "STACK_OVERFLOW";  break;
+            case GL_OUT_OF_MEMORY: error = "OUT_OF_MEMORY"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION:  error = "INVALID_FRAMEBUFFER_OPERATION";  break;
+#ifdef GL_CONTEXT_LOST
+            case GL_CONTEXT_LOST:  error = "CONTEXT_LOST";  break;
 #endif
             default: error = "(unknown)"; break;
         }
