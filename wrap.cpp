4d4ff07eba7bd3f2c71071e49e0415183db6de6f
Tobrun <tobrun.van.nuland@gmail.com>
2016-09-08 10:01:54 -0700

    use double instead of integer

--- wrap.cpp
+++ wrap.cpp
@@ -6,21 +6,21 @@
 using namespace mbgl;
 
 TEST(Math, WrapHigherValue) {
-    ASSERT_DOUBLE_EQ(4, util::wrap(16, 0, 12));
+    ASSERT_DOUBLE_EQ(4.0, util::wrap(16.0, 0.0, 12.0));
 }
 
 TEST(Math, WrapLowerValue) {
-    ASSERT_DOUBLE_EQ(9, util::wrap(-3, 0, 12));
+    ASSERT_DOUBLE_EQ(9.0, util::wrap(-3.0, 0.0, 12.0));
 }
 
 TEST(Math, WrapInRangeValue) {
-    ASSERT_DOUBLE_EQ(4, util::wrap(4, 0, 12));
+    ASSERT_DOUBLE_EQ(4.0, util::wrap(4.0, 0.0, 12.0));
 }
 
 TEST(Math, WrapMaxValue) {
-    ASSERT_DOUBLE_EQ(0, util::wrap(12, 0, 12));
+    ASSERT_DOUBLE_EQ(0.0, util::wrap(12.0, 0.0, 12.0));
 }
 
 TEST(Math, WrapMinValue) {
-    ASSERT_DOUBLE_EQ(0, util::wrap(0, 0, 12));
+    ASSERT_DOUBLE_EQ(0.0, util::wrap(0.0, 0.0, 12.0));
 }
