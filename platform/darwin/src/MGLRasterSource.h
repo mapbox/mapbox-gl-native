f7a44275ac94e9017e734a85b11ffa8e5a44b5a5
Jesse Bounds <jesse@rebounds.net>
2016-09-22 16:19:36 -0700

    [ios, macos] Use integer to represent tile size in raster source

--- platform/darwin/src/MGLRasterSource.h
+++ platform/darwin/src/MGLRasterSource.h
@@ -8,7 +8,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface MGLRasterSource : MGLSource
 
 @property (nonatomic, readonly, copy) NSURL *URL;
-@property (nonatomic, readonly, assign) CGFloat tileSize;
+@property (nonatomic, readonly, assign) NSUInteger tileSize;
 @property (nonatomic, readonly, nullable) MGLTileSet *tileSet;
 
 - (instancetype)initWithSourceIdentifier:(NSString *)sourceIdentifier URL:(NSURL *)url tileSize:(CGFloat)tileSize;
