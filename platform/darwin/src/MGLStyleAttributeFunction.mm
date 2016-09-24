0c8a3feffa94a9594bcaa77fdc18d11a3c482ff5
Minh Nguyễn <mxn@1ec5.org>
2016-09-24 11:12:21 -0700

    [ios, macos] Replaced MGLStyleAttributeValue with MGLStyleValue
    MGLStyleValue is an umbrella class serving as a variant container for MGLStyleConstantValue and MGLStyleFunction. These classes use lightweight generics to indicate the underlying type. A templated C++ class concisely converts between mbgl::style::PropertyValue and MGLStyleValue.

--- platform/darwin/src/MGLStyleAttributeFunction.mm
+++ platform/darwin/src/MGLStyleAttributeFunction.mm
@@ -4,6 +4,68 @@
 #import "MGLStyleAttributeValue_Private.h"
 #import "MGLStyleAttributeFunction_Private.h"
 
+@implementation MGLStyleValue
+
++ (instancetype)valueWithRawValue:(id)rawValue {
+    return [MGLStyleConstantValue valueWithRawValue:rawValue];
+}
+
++ (instancetype)valueWithBase:(CGFloat)base stops:(NSDictionary *)stops {
+    return [MGLStyleFunction functionWithBase:base stops:stops];
+}
+
++ (instancetype)valueWithStops:(NSDictionary *)stops {
+    return [MGLStyleFunction functionWithStops:stops];
+}
+
+@end
+
+@implementation MGLStyleConstantValue
+
++ (instancetype)valueWithRawValue:(id)rawValue {
+    return [[self alloc] initWithRawValue:rawValue];
+}
+
+- (instancetype)initWithRawValue:(id)rawValue {
+    if (self = [super init]) {
+        _rawValue = rawValue;
+    }
+    return self;
+}
+
+@end
+
+@implementation MGLStyleFunction
+
++ (instancetype)functionWithBase:(CGFloat)base stops:(NSDictionary *)stops {
+    return [[self alloc] initWithBase:base stops:stops];
+}
+
++ (instancetype)functionWithStops:(NSDictionary *)stops {
+    return [[self alloc] initWithBase:1 stops:stops];
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _base = 1;
+    }
+    return self;
+}
+
+- (instancetype)initWithBase:(CGFloat)base stops:(NSDictionary *)stops {
+    if (self = [self init]) {
+        _base = base;
+        _stops = stops;
+    }
+    return self;
+}
+
+@end
+
+
+
+
+
 @interface MGLStyleAttributeFunction() <MGLStyleAttributeValue_Private>
 @end
 
