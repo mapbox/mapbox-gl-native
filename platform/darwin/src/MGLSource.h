520c011124d4a961b51421ae162849ad28df9318
Justin R. Miller <incanus@codesorcery.net>
2016-09-26 16:29:43 -0700

    add MGLSource base docs

--- platform/darwin/src/MGLSource.h
+++ platform/darwin/src/MGLSource.h
@@ -1,5 +1,10 @@
 #import <Foundation/Foundation.h>
 
+/**
+ A source supplies data to be shown on the map. Sources don't contain styling details like color or width. Use style layers to give visual representation to sources. 
+ 
+ You should use the concrete subclasses of MGLSource to create vector, raster, GeoJSON, and other source types.
+ */
 @interface MGLSource : NSObject
 
 @property (nonatomic, copy) NSString *sourceIdentifier;
