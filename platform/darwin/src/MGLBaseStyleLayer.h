a1f8264f2c84c3f5f11787032e99846e3a63b546
Justin R. Miller <incanus@codesorcery.net>
2016-09-26 15:58:46 -0700

    fixes #5959: MGLBaseStyleLayer visible docs & update style docs

--- platform/darwin/src/MGLBaseStyleLayer.h
+++ platform/darwin/src/MGLBaseStyleLayer.h
@@ -4,17 +4,24 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ The base style layer class from which all other layer types inherit. 
+ Style layers allow runtime customization of all map styling properties. 
+ */
 @interface MGLBaseStyleLayer : NSObject
 
+/**
+ The display of the layer. A value of `NO` hides the layer.
+ */
 @property (nonatomic, assign, getter=isVisible) BOOL visible;
 
 /**
- The maximum zoom level on which the layer gets parsed and appears on.
+ The maximum zoom level at which the layer gets parsed and appears.
  */
 @property (nonatomic, assign) float maximumZoomLevel;
 
 /**
- The minimum zoom level on which the layer gets parsed and appears on.
+ The minimum zoom level at which the layer gets parsed and appears.
  */
 @property (nonatomic, assign) float minimumZoomLevel;
 
