3f3fc7b7723698e44427e2a14a2f4906832800bf
Konstantin Käfer <mail@kkaefer.com>
2016-09-28 11:45:33 +0200

    [test] add .test.cpp suffix to test case files

--- cmake/test-files.cmake
+++ cmake/test-files.cmake
@@ -2,44 +2,44 @@
 
 set(MBGL_TEST_FILES
     # actor
-    test/actor/actor.cpp
-    test/actor/actor_ref.cpp
+    test/actor/actor.test.cpp
+    test/actor/actor_ref.test.cpp
 
     # algorithm
-    test/algorithm/covered_by_children.cpp
-    test/algorithm/generate_clip_ids.cpp
+    test/algorithm/covered_by_children.test.cpp
+    test/algorithm/generate_clip_ids.test.cpp
     test/algorithm/mock.hpp
-    test/algorithm/update_renderables.cpp
+    test/algorithm/update_renderables.test.cpp
 
     # api
-    test/api/annotations.cpp
-    test/api/api_misuse.cpp
-    test/api/custom_layer.cpp
-    test/api/render_missing.cpp
-    test/api/repeated_render.cpp
+    test/api/annotations.test.cpp
+    test/api/api_misuse.test.cpp
+    test/api/custom_layer.test.cpp
+    test/api/render_missing.test.cpp
+    test/api/repeated_render.test.cpp
 
     # geometry
-    test/geometry/binpack.cpp
+    test/geometry/binpack.test.cpp
 
     # gl
-    test/gl/object.cpp
+    test/gl/object.test.cpp
 
     # include/mbgl
     test/include/mbgl/test.hpp
 
     # map
-    test/map/map.cpp
-    test/map/transform.cpp
+    test/map/map.test.cpp
+    test/map/transform.test.cpp
 
     # math
-    test/math/clamp.cpp
-    test/math/minmax.cpp
-    test/math/wrap.cpp
+    test/math/clamp.test.cpp
+    test/math/minmax.test.cpp
+    test/math/wrap.test.cpp
 
     # sprite
-    test/sprite/sprite_atlas.cpp
-    test/sprite/sprite_image.cpp
-    test/sprite/sprite_parser.cpp
+    test/sprite/sprite_atlas.test.cpp
+    test/sprite/sprite_image.test.cpp
+    test/sprite/sprite_parser.test.cpp
 
     # src
     test/src/main.cpp
@@ -58,53 +58,53 @@ set(MBGL_TEST_FILES
     test/src/mbgl/test/util.hpp
 
     # storage
-    test/storage/asset_file_source.cpp
-    test/storage/default_file_source.cpp
-    test/storage/headers.cpp
-    test/storage/http_file_source.cpp
-    test/storage/offline.cpp
-    test/storage/offline_database.cpp
-    test/storage/offline_download.cpp
-    test/storage/online_file_source.cpp
-    test/storage/resource.cpp
+    test/storage/asset_file_source.test.cpp
+    test/storage/default_file_source.test.cpp
+    test/storage/headers.test.cpp
+    test/storage/http_file_source.test.cpp
+    test/storage/offline.test.cpp
+    test/storage/offline_database.test.cpp
+    test/storage/offline_download.test.cpp
+    test/storage/online_file_source.test.cpp
+    test/storage/resource.test.cpp
 
     # style/conversion
-    test/style/conversion/geojson_options.cpp
+    test/style/conversion/geojson_options.test.cpp
 
     # style
-    test/style/filter.cpp
-    test/style/functions.cpp
-    test/style/source.cpp
-    test/style/style.cpp
-    test/style/style_layer.cpp
-    test/style/style_parser.cpp
-    test/style/tile_source.cpp
+    test/style/filter.test.cpp
+    test/style/functions.test.cpp
+    test/style/source.test.cpp
+    test/style/style.test.cpp
+    test/style/style_layer.test.cpp
+    test/style/style_parser.test.cpp
+    test/style/tile_source.test.cpp
 
     # text
-    test/text/glyph_atlas.cpp
-    test/text/quads.cpp
+    test/text/glyph_atlas.test.cpp
+    test/text/quads.test.cpp
 
     # tile
-    test/tile/geometry_tile_data.cpp
-    test/tile/tile_id.cpp
+    test/tile/geometry_tile_data.test.cpp
+    test/tile/tile_id.test.cpp
 
     # util
-    test/util/async_task.cpp
-    test/util/geo.cpp
-    test/util/http_timeout.cpp
-    test/util/image.cpp
-    test/util/mapbox.cpp
-    test/util/memory.cpp
-    test/util/merge_lines.cpp
-    test/util/number_conversions.cpp
-    test/util/offscreen_texture.cpp
-    test/util/projection.cpp
-    test/util/run_loop.cpp
-    test/util/text_conversions.cpp
-    test/util/thread.cpp
-    test/util/thread_local.cpp
-    test/util/tile_cover.cpp
-    test/util/timer.cpp
-    test/util/token.cpp
-    test/util/work_queue.cpp
+    test/util/async_task.test.cpp
+    test/util/geo.test.cpp
+    test/util/http_timeout.test.cpp
+    test/util/image.test.cpp
+    test/util/mapbox.test.cpp
+    test/util/memory.test.cpp
+    test/util/merge_lines.test.cpp
+    test/util/number_conversions.test.cpp
+    test/util/offscreen_texture.test.cpp
+    test/util/projection.test.cpp
+    test/util/run_loop.test.cpp
+    test/util/text_conversions.test.cpp
+    test/util/thread.test.cpp
+    test/util/thread_local.test.cpp
+    test/util/tile_cover.test.cpp
+    test/util/timer.test.cpp
+    test/util/token.test.cpp
+    test/util/work_queue.test.cpp
 )
