5891c7028dbc6ae6decb5a3b059fd48f4e700d44
John Firebaugh <john.firebaugh@gmail.com>
2016-10-10 10:57:57 -0700

    Update mapbox-gl-shaders to 98a56d

--- src/mbgl/shader/fill_outline_pattern_shader.cpp
+++ src/mbgl/shader/fill_outline_pattern_shader.cpp
@@ -1,14 +1,14 @@
 #include <mbgl/shader/fill_outline_pattern_shader.hpp>
-#include <mbgl/shader/outlinepattern.vertex.hpp>
-#include <mbgl/shader/outlinepattern.fragment.hpp>
+#include <mbgl/shader/fill_outline_pattern.vertex.hpp>
+#include <mbgl/shader/fill_outline_pattern.fragment.hpp>
 #include <mbgl/shader/fill_vertex.hpp>
 
 namespace mbgl {
 
 FillOutlinePatternShader::FillOutlinePatternShader(gl::Context& context, Defines defines)
-    : Shader(shaders::outlinepattern::name,
-             shaders::outlinepattern::vertex,
-             shaders::outlinepattern::fragment,
+    : Shader(shaders::fill_outline_pattern::name,
+             shaders::fill_outline_pattern::vertex,
+             shaders::fill_outline_pattern::fragment,
              context, defines) {
 }
 
--- src/mbgl/shader/symbol_icon_shader.cpp
+++ src/mbgl/shader/symbol_icon_shader.cpp
@@ -1,14 +1,14 @@
 #include <mbgl/shader/symbol_icon_shader.hpp>
-#include <mbgl/shader/icon.vertex.hpp>
-#include <mbgl/shader/icon.fragment.hpp>
+#include <mbgl/shader/symbol_icon.vertex.hpp>
+#include <mbgl/shader/symbol_icon.fragment.hpp>
 #include <mbgl/shader/symbol_vertex.hpp>
 
 namespace mbgl {
 
 SymbolIconShader::SymbolIconShader(gl::Context& context, Defines defines)
-    : Shader(shaders::icon::name,
-             shaders::icon::vertex,
-             shaders::icon::fragment,
+    : Shader(shaders::symbol_icon::name,
+             shaders::symbol_icon::vertex,
+             shaders::symbol_icon::fragment,
              context, defines) {
 }
 
