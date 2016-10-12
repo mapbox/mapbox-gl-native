f6ebb15cee5b40975d3d97a06e8345320cef8972
Mike Morris <mikemorris@users.noreply.github.com>
2016-10-12 17:28:22 -0400

    [core] [node] pass thread pool impl to Map constructor
    Adds NodeThreadPool implementation using AsyncQueue to call
    Nan::AsyncQueueWorker from main thread

---
+++ platform/node/src/node_thread_pool.cpp
@@ -0,0 +1,39 @@
+#include "node_thread_pool.hpp"
+#include "util/async_queue.hpp"
+
+#include <mbgl/actor/mailbox.hpp>
+
+namespace node_mbgl {
+
+NodeThreadPool::NodeThreadPool()
+    : queue(new util::AsyncQueue<std::weak_ptr<mbgl::Mailbox>>(uv_default_loop(), [this](std::weak_ptr<mbgl::Mailbox> mailbox) {
+        NodeThread* worker = new NodeThread(mailbox);
+        Nan::AsyncQueueWorker(worker);
+    })) {
+    // Don't keep the event loop alive.
+    queue->unref();
+}
+
+NodeThreadPool::~NodeThreadPool() {
+    queue->stop();
+}
+
+void NodeThreadPool::schedule(std::weak_ptr<mbgl::Mailbox> mailbox) {
+    queue->send(std::move(mailbox));
+}
+
+NodeThreadPool::NodeThread::NodeThread(std::weak_ptr<mbgl::Mailbox> mailbox_)
+    : AsyncWorker(nullptr),
+    mailbox(std::move(mailbox_)) {};
+
+void NodeThreadPool::NodeThread::Execute() {
+    if (auto locked = mailbox.lock()) {
+        locked->receive();
+    }
+}
+
+void NodeThreadPool::NodeThread::WorkComplete() {
+    // no-op to avoid calling nullptr callback
+}
+
+} // namespace node_mbgl
