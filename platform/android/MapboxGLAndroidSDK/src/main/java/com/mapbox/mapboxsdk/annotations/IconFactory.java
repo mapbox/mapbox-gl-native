1898df606dd6ca3c8badcaa40cb17c2bce307055
Tobrun <tobrun.van.nuland@gmail.com>
2016-10-13 17:04:02 +0200

    [android] - add transparent image for MarkerViews ghosting SymbolAnnotations (#6675)
    * [android] - add transparent image to core for MarkerView ghosting SymbolAnnotations. Removes logging messages: `Can't find sprite ... `. Cleanup MapView.
    
    * [android] - use alpha_8 config to only allocate one byte, rename id for better readability.

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/annotations/IconFactory.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/annotations/IconFactory.java
@@ -24,11 +24,14 @@ import java.io.InputStream;
 
 /**
  * Factory for creating {@link Icon} objects.
+ *
  * @see Icon
  */
 public final class IconFactory {
 
     private static final String ICON_ID_PREFIX = "com.mapbox.icons.icon_";
+    public static final Bitmap ICON_MARKERVIEW_BITMAP = Bitmap.createBitmap(1, 1, Bitmap.Config.ALPHA_8);
+    public static final String ICON_MARKERVIEW_ID = ICON_ID_PREFIX + "marker_view";
 
     private Context mContext;
     private static IconFactory sInstance;
