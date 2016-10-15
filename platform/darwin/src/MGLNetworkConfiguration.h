3ec5510b486e611d4ec55f0c0ccff84891be35db
Justin R. Miller <incanus@codesorcery.net>
2016-10-14 17:12:49 -0700

    remove public setter

--- platform/darwin/src/MGLNetworkConfiguration.h
+++ platform/darwin/src/MGLNetworkConfiguration.h
@@ -4,8 +4,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface MGLNetworkConfiguration : NSObject
 
-+ (void)setAPIBaseURL:(nullable NSURL *)apiBaseURL;
-
 /**
  Returns any custom API base URL in use by instances of MGLMapView and MGLOfflineStorage in the current application.
  */
