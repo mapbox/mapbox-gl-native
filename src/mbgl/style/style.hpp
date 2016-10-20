6789a314d6b7547f311897cbaf952a9c506a9a68
Ivo van Dongen <info@ivovandongen.nl>
2016-10-20 12:02:04 +0300

    [core] reload geojson source on url change

--- src/mbgl/style/style.hpp
+++ src/mbgl/style/style.hpp
@@ -134,6 +134,7 @@ private:
     void onSourceLoaded(Source&) override;
     void onSourceAttributionChanged(Source&, const std::string&) override;
     void onSourceError(Source&, std::exception_ptr) override;
+    void onSourceDescriptionChanged(Source&) override;
     void onTileChanged(Source&, const OverscaledTileID&) override;
     void onTileError(Source&, const OverscaledTileID&, std::exception_ptr) override;
 
