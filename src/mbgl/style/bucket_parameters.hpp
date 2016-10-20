d98e67dc39ba85e28fe5212b289ec0ae647c109f
John Firebaugh <john.firebaugh@gmail.com>
2016-10-20 16:37:15 -0700

    [core] Remove unused BucketParameter members

--- src/mbgl/style/bucket_parameters.hpp
+++ src/mbgl/style/bucket_parameters.hpp
@@ -9,42 +9,26 @@
 
 namespace mbgl {
 
-class TileID;
 class GeometryTileLayer;
 class GeometryTileFeature;
-class GlyphAtlas;
-class CollisionTile;
 class FeatureIndex;
 
 namespace style {
 
 class BucketParameters {
 public:
-    BucketParameters(const OverscaledTileID& tileID_,
-                          const std::atomic<bool>& obsolete_,
-                          uintptr_t tileUID_,
-                          GlyphAtlas& glyphAtlas_,
-                          FeatureIndex& featureIndex_,
-                          const MapMode mode_)
-        : tileID(tileID_),
-          obsolete(obsolete_),
-          tileUID(tileUID_),
-          glyphAtlas(glyphAtlas_),
-          featureIndex(featureIndex_),
-          mode(mode_) {}
+    const OverscaledTileID& tileID;
+    const std::atomic<bool>& obsolete;
+    FeatureIndex& featureIndex;
+    const MapMode mode;
 
     bool cancelled() const {
         return obsolete;
     }
 
-    void eachFilteredFeature(const Filter&, const GeometryTileLayer&, std::function<void (const GeometryTileFeature&, std::size_t index, const std::string& layerName)>);
-
-    const OverscaledTileID& tileID;
-    const std::atomic<bool>& obsolete;
-    uintptr_t tileUID;
-    GlyphAtlas& glyphAtlas;
-    FeatureIndex& featureIndex;
-    const MapMode mode;
+    void eachFilteredFeature(const Filter&,
+                             const GeometryTileLayer&,
+                             std::function<void (const GeometryTileFeature&, std::size_t index, const std::string& layerName)>);
 };
 
 } // namespace style
