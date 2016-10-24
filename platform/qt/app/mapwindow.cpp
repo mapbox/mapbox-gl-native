ba6972336a3632e17ccfc8332f722a7b9a0e281a
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-10-24 18:57:39 +0300

    [qt] Fix macOS Qt demo app with Qt4.x

--- platform/qt/app/mapwindow.cpp
+++ platform/qt/app/mapwindow.cpp
@@ -297,5 +297,15 @@ void MapWindow::resizeGL(int w, int h)
 void MapWindow::paintGL()
 {
     m_frameDraws++;
+
+#if QT_VERSION < 0x050400 && defined(__APPLE__)
+    // XXX GL framebuffer is valid only after first attempt of painting on
+    // older versions of Qt on macOS.
+    // See https://bugreports.qt.io/browse/QTBUG-36802 for details.
+    static bool first = true;
+    if (!first) m_map.render();
+    first = false;
+#else
     m_map.render();
+#endif
 }
