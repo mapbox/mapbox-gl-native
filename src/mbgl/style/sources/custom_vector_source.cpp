a530538307469849ad7049c3c9b74db4422cb04f
Jesse Crocker <jesse@gaiagps.com>
2016-11-07 11:01:45 -0700

    Change indentation to match project standards

--- src/mbgl/style/sources/custom_vector_source.cpp
+++ src/mbgl/style/sources/custom_vector_source.cpp
@@ -2,18 +2,20 @@
 #include <mbgl/style/sources/custom_vector_source_impl.hpp>
 
 namespace mbgl {
-  namespace style {
+namespace style {
     
-    CustomVectorSource::CustomVectorSource(std::string id, GeoJSONOptions options, std::function<void(uint8_t, uint32_t, uint32_t)> fetchTile)
-    : Source(SourceType::Vector, std::make_unique<CustomVectorSource::Impl>(std::move(id), *this,  options, fetchTile)),
-    impl(static_cast<Impl*>(baseImpl.get())) { }
+CustomVectorSource::CustomVectorSource(std::string id, GeoJSONOptions options, std::function<void(uint8_t, uint32_t, uint32_t)> fetchTile)
+      : Source(SourceType::Vector, std::make_unique<CustomVectorSource::Impl>(std::move(id), *this,  options, fetchTile)),
+        impl(static_cast<Impl*>(baseImpl.get())) {
+}
+      
+void CustomVectorSource::setTileData(uint8_t z, uint32_t x, uint32_t y, const mapbox::geojson::geojson& geoJSON) {
+    impl->setTileData(z, x, y, geoJSON);
+}
+      
+void CustomVectorSource::updateTile(uint8_t z, uint32_t x, uint32_t y) {
+    impl->updateTile(z, x, y);
+}
     
-    void CustomVectorSource::setTileData(uint8_t z, uint32_t x, uint32_t y, const mapbox::geojson::geojson& geoJSON) {
-      impl->setTileData(z, x, y, geoJSON);
-    }
-    
-    void CustomVectorSource::updateTile(uint8_t z, uint32_t x, uint32_t y) {
-        impl->updateTile(z, x, y);
-    }
-  } // namespace style
+} // namespace style
 } // namespace mbgl
--- src/mbgl/style/sources/custom_vector_source_impl.cpp
+++ src/mbgl/style/sources/custom_vector_source_impl.cpp
@@ -6,41 +6,40 @@
 #include <supercluster.hpp>
 
 namespace mbgl {
-  namespace style {
+namespace style {
     
-    CustomVectorSource::Impl::Impl(std::string id, Source& base_, GeoJSONOptions options_, std::function<void(uint8_t, uint32_t, uint32_t)> fetchTile_)
+CustomVectorSource::Impl::Impl(std::string id, Source& base_, GeoJSONOptions options_, std::function<void(uint8_t, uint32_t, uint32_t)> fetchTile_)
     : Source::Impl(SourceType::Vector, std::move(id), base_), options(options_), fetchTile(fetchTile_) {
-      loaded = true;
-    }
+    loaded = true;
+}
     
-    Range<uint8_t> CustomVectorSource::Impl::getZoomRange() {
-      return { options.minzoom, options.maxzoom };
-    }
+Range<uint8_t> CustomVectorSource::Impl::getZoomRange() {
+    return { options.minzoom, options.maxzoom };
+}
     
-    uint16_t CustomVectorSource::Impl::getTileSize() const {
-      return options.tileSize;
-    }
+uint16_t CustomVectorSource::Impl::getTileSize() const {
+    return options.tileSize;
+}
     
-    std::unique_ptr<Tile> CustomVectorSource::Impl::createTile(const OverscaledTileID& tileID,
+std::unique_ptr<Tile> CustomVectorSource::Impl::createTile(const OverscaledTileID& tileID,
                                                           const UpdateParameters& parameters) {
-      auto tilePointer = std::make_unique<GeoJSONTile>(tileID, base.getID(), parameters);
-      fetchTile(tileID.canonical.z, tileID.canonical.x, tileID.canonical.y);
-      return std::move(tilePointer);
-    }
+    auto tilePointer = std::make_unique<GeoJSONTile>(tileID, base.getID(), parameters);
+    fetchTile(tileID.canonical.z, tileID.canonical.x, tileID.canonical.y);
+    return std::move(tilePointer);
+}
     
-    void CustomVectorSource::Impl::setTileData(uint8_t z, uint32_t x, uint32_t y, const mapbox::geojson::geojson& geoJSON)
-    {
-      double scale = util::EXTENT / util::tileSize;
-      
-      variant<GeoJSONVTPointer, SuperclusterPointer> geoJSONOrSupercluster;
-      if (!options.cluster) {
+void CustomVectorSource::Impl::setTileData(uint8_t z, uint32_t x, uint32_t y, const mapbox::geojson::geojson& geoJSON) {
+    double scale = util::EXTENT / util::tileSize;
+    
+    variant<GeoJSONVTPointer, SuperclusterPointer> geoJSONOrSupercluster;
+    if (!options.cluster) {
         mapbox::geojsonvt::Options vtOptions;
         vtOptions.maxZoom = options.maxzoom;
         vtOptions.extent = util::EXTENT;
         vtOptions.buffer = std::round(scale * options.buffer);
         vtOptions.tolerance = scale * options.tolerance;
         geoJSONOrSupercluster = std::make_unique<mapbox::geojsonvt::GeoJSONVT>(geoJSON, vtOptions);
-      } else {
+    } else {
         mapbox::supercluster::Options clusterOptions;
         clusterOptions.maxZoom = options.clusterMaxZoom;
         clusterOptions.extent = util::EXTENT;
@@ -49,29 +48,29 @@ namespace mbgl {
         const auto& features = geoJSON.get<mapbox::geometry::feature_collection<double>>();
         geoJSONOrSupercluster =
         std::make_unique<mapbox::supercluster::Supercluster>(features, clusterOptions);
-      }
-      
-      for (auto const &item : tiles) {
-        GeoJSONTile* tile = static_cast<GeoJSONTile*>(item.second.get());
-        if(tile->id.canonical.z == z && tile->id.canonical.x == x && tile->id.canonical.y == y) {
-          if (geoJSONOrSupercluster.is<GeoJSONVTPointer>()) {
-            tile->updateData(geoJSONOrSupercluster.get<GeoJSONVTPointer>()->getTile(z, x, y).features);
-          } else {
-            assert(geoJSONOrSupercluster.is<SuperclusterPointer>());
-            tile->updateData(geoJSONOrSupercluster.get<SuperclusterPointer>()->getTile(z, x, y));
-          }
-        }
-      }
     }
     
-    void CustomVectorSource::Impl::updateTile(uint8_t z, uint32_t x, uint32_t y) {
-        for (auto const &item : tiles) {
-            GeoJSONTile* tile = static_cast<GeoJSONTile*>(item.second.get());
-            if(tile->id.canonical.z == z && tile->id.canonical.x == x && tile->id.canonical.y == y) {
-                fetchTile(z, x, y);
+    for (auto const &item : tiles) {
+        GeoJSONTile* tile = static_cast<GeoJSONTile*>(item.second.get());
+        if(tile->id.canonical.z == z && tile->id.canonical.x == x && tile->id.canonical.y == y) {
+            if (geoJSONOrSupercluster.is<GeoJSONVTPointer>()) {
+                tile->updateData(geoJSONOrSupercluster.get<GeoJSONVTPointer>()->getTile(z, x, y).features);
+            } else {
+                assert(geoJSONOrSupercluster.is<SuperclusterPointer>());
+                tile->updateData(geoJSONOrSupercluster.get<SuperclusterPointer>()->getTile(z, x, y));
             }
         }
     }
+}
     
-  } // namespace style
+void CustomVectorSource::Impl::updateTile(uint8_t z, uint32_t x, uint32_t y) {
+    for (auto const &item : tiles) {
+        GeoJSONTile* tile = static_cast<GeoJSONTile*>(item.second.get());
+        if(tile->id.canonical.z == z && tile->id.canonical.x == x && tile->id.canonical.y == y) {
+            fetchTile(z, x, y);
+        }
+    }
+}
+    
+} // namespace style
 } // namespace mbgl
