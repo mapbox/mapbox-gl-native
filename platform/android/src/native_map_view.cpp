2a5328cb221f56f197726721bb9d0837109fd519
Konstantin Käfer <mail@kkaefer.com>
2016-11-09 12:59:56 +0100

    [android] update viewport size when framebuffer size changed

--- platform/android/src/native_map_view.cpp
+++ platform/android/src/native_map_view.cpp
@@ -197,6 +197,11 @@ void NativeMapView::invalidate() {
 void NativeMapView::render() {
     activate();
 
+    if (framebufferSizeChanged) {
+        getContext().viewport = { 0, 0, getFramebufferSize() };
+        framebufferSizeChanged = false;
+    }
+
     updateViewBinding();
     map->render(*this);
 
@@ -729,6 +734,7 @@ void NativeMapView::resizeView(int w, int h) {
 void NativeMapView::resizeFramebuffer(int w, int h) {
     fbWidth = w;
     fbHeight = h;
+    framebufferSizeChanged = true;
     invalidate();
 }
 
