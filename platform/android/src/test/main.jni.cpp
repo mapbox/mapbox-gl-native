e6cb119e5939038a3a5af4a3b8fc93380d2d52e8
Ivo van Dongen <info@ivovandongen.nl>
2016-11-15 19:56:18 +0100

    [android] fix indentation

--- platform/android/src/test/main.jni.cpp
+++ platform/android/src/test/main.jni.cpp
@@ -8,12 +8,12 @@
 #pragma clang diagnostic ignored "-Wunused-parameter"
 
 void runAllTests(JNIEnv *env, jni::jobject* obj) {
-	std::cout << "Starting tests";
-	mbgl::runTests(0, nullptr);
+    std::cout << "Starting tests";
+    mbgl::runTests(0, nullptr);
 }
 
 extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
-	std::cout << "Loading tests";
+    std::cout << "Loading tests";
 
     mbgl::android::theJVM = vm;
 
@@ -21,12 +21,12 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
         return reinterpret_cast<mbgl::gl::glProc>(eglGetProcAddress(name));
     });
 
-	struct Main { static constexpr auto Name() { return "Main"; } };
-	
-	jni::JNIEnv& env = jni::GetEnv(*vm, jni::jni_version_1_6);
-	
-	#define MAKE_NATIVE_METHOD(name, sig) jni::MakeNativeMethod<decltype(name), name>( #name, sig )
-	jni::RegisterNatives(env, jni::Class<Main>::Find(env), MAKE_NATIVE_METHOD(runAllTests, "()V"));
+    struct Main { static constexpr auto Name() { return "Main"; } };
 
-	return JNI_VERSION_1_6;
+    jni::JNIEnv& env = jni::GetEnv(*vm, jni::jni_version_1_6);
+
+    #define MAKE_NATIVE_METHOD(name, sig) jni::MakeNativeMethod<decltype(name), name>( #name, sig )
+    jni::RegisterNatives(env, jni::Class<Main>::Find(env), MAKE_NATIVE_METHOD(runAllTests, "()V"));
+
+    return JNI_VERSION_1_6;
 }
