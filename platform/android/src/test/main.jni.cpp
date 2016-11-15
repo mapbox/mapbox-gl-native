e89af2f8977403986098de6d1687c9bee1ffd735
Ivo van Dongen <info@ivovandongen.nl>
2016-11-15 19:16:47 +0100

    [android] initialize gl extensions in tests

--- platform/android/src/test/main.jni.cpp
+++ platform/android/src/test/main.jni.cpp
@@ -1,4 +1,7 @@
 #include "../jni.hpp"
+
+#include <EGL/egl.h>
+#include <mbgl/gl/extension.hpp>
 #include <jni/jni.hpp>
 #include <mbgl/test.hpp>
 
@@ -13,6 +16,10 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
 	std::cout << "Loading tests";
 
     mbgl::android::theJVM = vm;
+
+    mbgl::gl::InitializeExtensions([] (const char * name) {
+        return reinterpret_cast<mbgl::gl::glProc>(eglGetProcAddress(name));
+    });
 
 	struct Main { static constexpr auto Name() { return "Main"; } };
 	
