d4fc66af3924805d40576989c1e139ddafcc4670
Chris Loer <chris.loer@gmail.com>
2016-11-16 12:50:57 -0800

    [core] Add minimal line breaking support for RTL text.

--- src/mbgl/text/glyph_set.hpp
+++ src/mbgl/text/glyph_set.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <mbgl/text/bidi.hpp>
 #include <mbgl/text/glyph.hpp>
 #include <mbgl/util/geometry.hpp>
 
@@ -9,12 +10,12 @@ class GlyphSet {
 public:
     void insert(uint32_t id, SDFGlyph&&);
     const std::map<uint32_t, SDFGlyph> &getSDFs() const;
-    const Shaping getShaping(const std::u16string &string, float maxWidth, float lineHeight,
+    const Shaping getShaping(const std::u16string &string, const WritingDirection writingDirection, float maxWidth, float lineHeight,
                              float horizontalAlign, float verticalAlign, float justify,
                              float spacing, const Point<float> &translate) const;
     void lineWrap(Shaping &shaping, float lineHeight, float maxWidth, float horizontalAlign,
                   float verticalAlign, float justify, const Point<float> &translate,
-                  bool useBalancedIdeographicBreaking) const;
+                  bool useBalancedIdeographicBreaking, const WritingDirection writingDirection) const;
 
 private:
     std::map<uint32_t, SDFGlyph> sdfs;
