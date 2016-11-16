c1bddb32459b1834a987a466771a75731746955d
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-11-16 17:20:26 +0100

    [android] Remove mbgl::gl::InitializeExtensions from test JNI_OnLoad

--- platform/android/src/test/main.jni.cpp
+++ platform/android/src/test/main.jni.cpp
@@ -1,11 +1,11 @@
 #include "../jni.hpp"
 
-#include <EGL/egl.h>
-#include <mbgl/gl/extension.hpp>
 #include <jni/jni.hpp>
 #include <mbgl/test.hpp>
 
 #pragma clang diagnostic ignored "-Wunused-parameter"
+
+#define MAKE_NATIVE_METHOD(name, sig) jni::MakeNativeMethod<decltype(name), name>( #name, sig )
 
 void runAllTests(JNIEnv *env, jni::jobject* obj) {
     std::cout << "Starting tests";
@@ -17,15 +17,10 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
 
     mbgl::android::theJVM = vm;
 
-    mbgl::gl::InitializeExtensions([] (const char * name) {
-        return reinterpret_cast<mbgl::gl::glProc>(eglGetProcAddress(name));
-    });
-
     struct Main { static constexpr auto Name() { return "Main"; } };
 
     jni::JNIEnv& env = jni::GetEnv(*vm, jni::jni_version_1_6);
 
-    #define MAKE_NATIVE_METHOD(name, sig) jni::MakeNativeMethod<decltype(name), name>( #name, sig )
     jni::RegisterNatives(env, jni::Class<Main>::Find(env), MAKE_NATIVE_METHOD(runAllTests, "()V"));
 
     return JNI_VERSION_1_6;
