7dafdbaa29071cc5e4da658c121c535e1b84478a
Konstantin Käfer <mail@kkaefer.com>
2016-11-21 16:43:22 +0100

    [build] reduce framework size for Release builds on macOS

--- platform/ios/toolchain.cmake
+++ platform/ios/toolchain.cmake
@@ -113,13 +113,6 @@ set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
 set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-
-# This little macro lets you set any XCode specific property
-macro (set_xcode_property TARGET XCODE_PROPERTY XCODE_VALUE)
-    set_property (TARGET ${TARGET} PROPERTY XCODE_ATTRIBUTE_${XCODE_PROPERTY} ${XCODE_VALUE})
-endmacro (set_xcode_property)
-
-
 # This macro lets you find executable programs on the host system
 macro (find_host_package)
     set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
