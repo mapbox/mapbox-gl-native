fe68b9bc4a2acbc2ef08195bc73c7279e6587131
Pablo Guardiola <guardiola31337@hotmail.com>
2016-11-23 13:28:24 -0500

    Fix incorrect command comment in templates (#7158)
    * [android] - fix incorrect command comment in templates and regenerate classes (#7157)
    
    * [android] - fix incorrect command comment in layer templates and regenerate classes (#7157)

--- platform/android/src/style/conversion/types.hpp
+++ platform/android/src/style/conversion/types.hpp
@@ -1,4 +1,4 @@
-// This file is generated. Edit android/platform/scripts/generate-style-code.js, then run `make style-code-android`.
+// This file is generated. Edit android/platform/scripts/generate-style-code.js, then run `make android-style-code`.
 #pragma once
 
 #include "types_string_values.hpp"
