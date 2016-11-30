6ae19c4ef3963ade9e0035ab9f9a44adfc9a8ebe
Jesse Crocker <jesse@gaiagps.com>
2016-11-30 08:13:02 -0700

    fix typo

--- platform/ios/src/MGLMapView.h
+++ platform/ios/src/MGLMapView.h
@@ -409,7 +409,7 @@ IB_DESIGNABLE
 @property(nonatomic) CGFloat decelerationRate;
 
 /**
- The festure recognizer used to detect single taps on the map.
+ The gesture recognizer used to detect single taps on the map.
  
  Your application can use this to modify the behavior of your own gesture recognizers.
  */
