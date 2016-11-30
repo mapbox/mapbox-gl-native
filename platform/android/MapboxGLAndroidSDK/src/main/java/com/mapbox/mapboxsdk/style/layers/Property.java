13b7b3a92b6e4fd88c78b61904abdf87e4903adf
Pablo Guardiola <guardiola31337@hotmail.com>
2016-11-30 13:54:26 +0100

    [android] - add uithread annotation and regenerate classes (#7156)

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/style/layers/Property.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/style/layers/Property.java
@@ -1,4 +1,4 @@
-// This file is generated. Edit android/platform/scripts/generate-style-code.js, then run `make style-code-android`.
+// This file is generated. Edit android/platform/scripts/generate-style-code.js, then run `make android-style-code`.
 package com.mapbox.mapboxsdk.style.layers;
 
 import android.support.annotation.StringDef;
