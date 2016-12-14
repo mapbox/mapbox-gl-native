f68449aeb8a4d874c15958edfcb02bc1ad7015a2
Chris Loer <chris.loer@gmail.com>
2016-12-14 10:18:37 -0800

    Cleaning up line breaking code.
    * Don't include trailing spacing (as opposed to whitespace) in lineLength
    * Modify BiDi interface to require initial bidi layout and linebreaking to happen in one call.
    * Code style changes suggested by @kkaefer

--- platform/qt/src/bidi.cpp
+++ platform/qt/src/bidi.cpp
@@ -16,15 +16,12 @@ std::u16string applyArabicShaping(const std::u16string& input) {
     return utf16string.toStdU16String();
 }
 
-ProcessedBiDiText::ProcessedBiDiText(BiDi& p_bidi) : bidi(p_bidi) {
-}
-
-void ProcessedBiDiText::mergeParagraphLineBreaks(std::set<int32_t>& lineBreakPoints) {
+void BiDi::mergeParagraphLineBreaks(std::set<int32_t>& lineBreakPoints) {
     lineBreakPoints.insert(bidi.impl->string.length());
 }
 
 std::vector<std::u16string>
-ProcessedBiDiText::applyLineBreaking(std::set<int32_t> lineBreakPoints) {
+BiDi::applyLineBreaking(std::set<int32_t> lineBreakPoints) {
     mergeParagraphLineBreaks(lineBreakPoints);
 
     std::vector<std::u16string> transformedLines;
@@ -44,9 +41,9 @@ BiDi::BiDi() : impl(std::make_unique<BiDiImpl>())
 BiDi::~BiDi() {
 }
 
-ProcessedBiDiText BiDi::processText(const std::u16string& input) {
+std::vector<std::u16string> BiDi::processText(const std::u16string& input, std::set<int32_t> lineBreakPoints) {
     impl->string = QString::fromStdU16String(input);
-    return ProcessedBiDiText(*this);
+    return applyLineBreaking(lineBreakPoints);
 }
 
 std::u16string BiDi::getLine(int32_t start, int32_t end) {
