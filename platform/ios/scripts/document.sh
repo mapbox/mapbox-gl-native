74f39c0120844e5c08feb05907fcc19a653a8b4d
Tom MacWright <tmcw@users.noreply.github.com>
2016-12-15 10:30:45 -0800

    [ios] [osx] Make theme configurable (#7420)
    * Make theme configurable
    
    * [ios][osx] Make jazzy theme configurable

--- platform/ios/scripts/document.sh
+++ platform/ios/scripts/document.sh
@@ -32,6 +32,9 @@ mkdir -p ${OUTPUT}
 
 cp platform/ios/screenshot.png "${OUTPUT}"
 
+DEFAULT_THEME="platform/darwin/docs/theme"
+THEME=${JAZZY_THEME:-$DEFAULT_THEME}
+
 jazzy \
     --config platform/ios/jazzy.yml \
     --sdk iphonesimulator \
@@ -40,7 +43,7 @@ jazzy \
     --readme ${README} \
     --documentation="platform/ios/docs/Info.plist Keys.md" \
     --root-url https://www.mapbox.com/ios-sdk/api/${RELEASE_VERSION}/ \
-    --theme platform/darwin/docs/theme \
+    --theme ${THEME} \
     --output ${OUTPUT}
 # https://github.com/realm/jazzy/issues/411
 find ${OUTPUT} -name *.html -exec \
