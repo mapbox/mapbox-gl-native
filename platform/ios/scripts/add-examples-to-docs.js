b232e3327526e075ed82c8b5cc5e95db55cb6312
Minh Nguyễn <mxn@1ec5.org>
2017-01-01 21:03:58 -0800

    [ios] Refactored example insertion script

--- platform/ios/scripts/add-examples-to-docs.js
+++ platform/ios/scripts/add-examples-to-docs.js
@@ -23,6 +23,69 @@ const examples = fs.readFileSync('platform/ios/test/MGLDocumentationExampleTests
 // 4 (sample code): "let sampleCode: String?"
 const exampleRegex = /func test(\w+)(?:\$(\w+))?\s*\(\)\s*\{[^]*?\n([ \t]+)\/\/#-example-code\n([^]+?)\n\3\/\/#-end-example-code\n/gm;
 
+/**
+ * Returns the one-based line number of a symbol in the file at the given path.
+ */
+function getLineForSymbol(path, className, memberName) {
+  let docStr = execFileSync('sourcekitten', ['doc', '--objc', path, '--', '-x', 'objective-c']).toString().trim();
+  let docJson = JSON.parse(docStr);
+  let fileStructure = _.find(docJson, fileStructure => path in fileStructure)[path];
+  let substructure = fileStructure['key.substructure'];
+  substructure = _.find(substructure, decl => decl['key.name'] === className);
+  if (memberName) {
+    substructure = _.find(substructure['key.substructure'] || [], decl => decl['key.name'] === memberName);
+  }
+  if (!substructure) {
+    console.log(`error: ${className} not found in ${path}`);
+    process.exit(1);
+  }
+  return substructure['key.parsed_scope.start'];
+}
+
+/**
+ * Returns the given source with example code inserted into the documentation
+ * comment for the symbol at the given line.
+ */
+function completeSymbolInSource(src, line, exampleCode) {
+  // Split the file contents right before the symbol declaration (but after its documentation comment).
+  let srcUpToSymbol = src.split('\n', line - 1).join('\n');
+  let srcFromSymbol = src.substr(srcUpToSymbol.length);
+  
+  // Match the documentation comment block that is not followed by the beginning or end of a declaration.
+  let commentMatch = srcUpToSymbol.match(/\/\*\*\s*(?:[^*]|\*(?!\/))+?\s*\*\/[^;{}]*?$/);
+  
+  // Replace the Swift code block with the test method’s contents.
+  let completedComment = commentMatch[0].replace(/^([ \t]*)```swift\n[^]*?```/m, function (m, indentation) {
+    // Apply the original indentation to each line.
+    return ('```swift\n' + exampleCode + '\n```').replace(/^/gm, indentation);
+  });
+  
+  // Splice the modified comment into the overall file contents.
+  srcUpToSymbol = (srcUpToSymbol.substr(0, commentMatch.index) + completedComment +
+                   srcUpToSymbol.substr(commentMatch.index + commentMatch[0].length));
+  return srcUpToSymbol + srcFromSymbol;
+}
+
+/**
+ * Edits the file at the given path to include the given example code.
+ */
+function completeSymbolInFile(path, className, memberName, exampleCode) {
+  // Use SourceKitten to find the class or class member named by the test method.
+  let startLine = getLineForSymbol(path, className, memberName);
+  
+  // Update the documentation comment for the class or class member.
+  let src = fs.readFileSync(path, 'utf8');
+  let newSrc = completeSymbolInSource(src, startLine, exampleCode);
+  
+  // Write out the modified file contents.
+  if (src === newSrc) {
+    console.log('Skipping', path);
+  } else {
+    console.log("Updating", path);
+    fs.writeFileSync(path, newSrc);
+  }
+}
+
 console.log("Installing examples...");
 
 let match;
@@ -37,49 +100,17 @@ while ((match = exampleRegex.exec(examples)) !== null) {
 
   // check if file exists at path
   let path = `platform/darwin/src/${className}.h`;
-  if (!fs.existsSync(path)) {
-    path = `platform/ios/src/${className}.h`;
-  }
-
   if (fs.existsSync(path)) {
-    let src = fs.readFileSync(path, 'utf8');
-    
-    // Use SourceKitten to find the class or class member named by the test method.
-    let docStr = execFileSync('sourcekitten', ['doc', '--objc', path, '--', '-x', 'objective-c']).toString().trim();
-    let docJson = JSON.parse(docStr);
-    let fileStructure = _.find(docJson, fileStructure => path in fileStructure)[path];
-    let substructure = fileStructure['key.substructure'];
-    substructure = _.find(substructure, decl => decl['key.name'] === className);
-    if (memberName) {
-      substructure = _.find(substructure['key.substructure'], decl => decl['key.name'] === memberName);
-    }
-    
-    // Split the file contents right before the symbol declaration (but after its documentation comment).
-    let srcUpToSymbol = src.split('\n', substructure['key.parsed_scope.start'] - 1).join('\n');
-    let srcFromSymbol = src.substr(srcUpToSymbol.length);
-    
-    // Match the documentation comment block that is not followed by the beginning or end of a declaration.
-    let commentMatch = srcUpToSymbol.match(/\/\*\*\s*(?:[^*]|\*(?!\/))+?\s*\*\/[^;{}]*?$/);
-    
-    // Replace the Swift code block with the test method’s contents.
-    let completedComment = commentMatch[0].replace(/^([ \t]*)```swift\n[^]*?```/m, function (m, indentation) {
-      // Apply the original indentation to each line.
-      return ('```swift\n' + exampleCode + '\n```').replace(/^/gm, indentation);
-    });
-    
-    // Splice the modified comment into the overall file contents.
-    srcUpToSymbol = (srcUpToSymbol.substr(0, commentMatch.index) + completedComment +
-                     srcUpToSymbol.substr(commentMatch.index + commentMatch[0].length));
-    let newSrc = srcUpToSymbol + srcFromSymbol;
-    
-    // Write out the modified file contents.
-    if (src === newSrc) {
-      console.log('Skipping', path);
-    } else {
-      console.log("Updating", path);
-      fs.writeFileSync(path, newSrc);
-    }
-  } else {
-    console.log("error: File doesn't exist:", path);
+    completeSymbolInFile(path, className, memberName, exampleCode);
+    continue;
   }
+  
+  path = `platform/ios/src/${className}.h`;
+  if (fs.existsSync(path)) {
+    completeSymbolInFile(path, className, memberName, exampleCode);
+    continue;
+  }
+  
+  console.log("error: File doesn't exist:", path);
+  process.exit(1);
 }
