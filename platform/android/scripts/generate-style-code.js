9c5abc122176a9080f03754e88c0bab4c08a034b
John Firebaugh <john.firebaugh@gmail.com>
2017-01-27 11:46:06 -0800

    [build] Use style-spec from gl-js monorepo

--- platform/android/scripts/generate-style-code.js
+++ platform/android/scripts/generate-style-code.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const ejs = require('ejs');
-const spec = require('mapbox-gl-style-spec').latest;
+const spec = require('../../../mapbox-gl-js/js/style-spec').latest;
 const _ = require('lodash');
 
 require('../../../scripts/style-code');
--- scripts/generate-style-code.js
+++ scripts/generate-style-code.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const ejs = require('ejs');
-const spec = require('mapbox-gl-style-spec').latest;
+const spec = require('../mapbox-gl-js/js/style-spec').latest;
 const colorParser = require('csscolorparser');
 
 require('./style-code');
