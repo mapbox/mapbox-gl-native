13007e10489719755e908f8d79feb583721a5946
Jesse Bounds <jesse@rebounds.net>
2017-02-14 13:09:49 -0800

    [ios, macos] Recreate pending and raw layer ref when layer is removed

--- platform/darwin/src/MGLSymbolStyleLayer.mm
+++ platform/darwin/src/MGLSymbolStyleLayer.mm
@@ -166,13 +166,14 @@ namespace mbgl {
 
 - (void)removeFromMapView:(MGLMapView *)mapView
 {
-    _pendingLayer = nullptr;
-    self.rawLayer = nullptr;
-
     auto removedLayer = mapView.mbglMap->removeLayer(self.identifier.UTF8String);
     if (!removedLayer) {
         return;
     }
+    
+    auto originalLayer = std::make_unique<mbgl::style::SymbolLayer>(self.identifier.UTF8String, self.sourceIdentifier.UTF8String);
+    _pendingLayer = std::move(originalLayer);
+    self.rawLayer = _pendingLayer.get();
 
     mbgl::style::SymbolLayer *layer = dynamic_cast<mbgl::style::SymbolLayer *>(removedLayer.get());
     if (!layer) {
