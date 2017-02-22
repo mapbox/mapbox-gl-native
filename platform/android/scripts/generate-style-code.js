eb997540041c60021ab56032b37cd9ccf98cc9ed
Guardiola31337 <pablo.guardiola@mapbox.com>
2017-02-22 17:02:00 +0100

    [android] fix runtime style template which generated classes that did not conform to checkstyle

--- platform/android/scripts/generate-style-code.js
+++ platform/android/scripts/generate-style-code.js
@@ -176,6 +176,37 @@ global.propertyFactoryMethodDoc = function (property) {
     return doc;
 };
 
+global.wordWrap = function(long_string, max_char){
+  var sum_length_of_words = function(word_array){
+    var out = 0;
+    if (word_array.length != 0){
+      for (var i = 0; i < word_array.length; i++){
+        var word = word_array[i];
+        out = out + word.length;
+      }
+    };
+    return out;
+  }
+
+  var split_out = [[]];
+  var split_string = long_string.split(' ');
+  for (var i = 0; i < split_string.length; i++){
+    var word = split_string[i];
+
+    if ((sum_length_of_words(split_out[split_out.length - 1]) + word.length) > max_char){
+      split_out = split_out.concat([[]]);
+    }
+
+    split_out[split_out.length - 1] = split_out[split_out.length - 1].concat(word);
+  }
+
+  for (var i = 0; i<split_out.length; i++){
+    split_out[i] = split_out[i].join(" ");
+  }
+
+  return split_out;
+};
+
 /**
  * Produces documentation for property value constants
  */
