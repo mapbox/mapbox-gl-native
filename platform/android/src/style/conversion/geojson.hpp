9b39013ce4c6f98590b80ced425e0a3bcdb3f77f
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-02-23 14:48:56 +0200

    [core] Internalize rapidjson.hpp

--- platform/android/src/style/conversion/geojson.hpp
+++ platform/android/src/style/conversion/geojson.hpp
@@ -5,12 +5,8 @@
 #include <mapbox/geojson.hpp>
 #include <mbgl/style/conversion.hpp>
 #include <mbgl/style/conversion/geojson.hpp>
-#include <mbgl/util/rapidjson.hpp>
 #include <mbgl/util/logging.hpp>
 #include <jni/jni.hpp>
-
-#include <sstream>
-#include <string>
 
 namespace mbgl {
 namespace style {
@@ -18,32 +14,14 @@ namespace conversion {
 
 template <>
 optional<GeoJSON> convertGeoJSON(const mbgl::android::Value& value, Error& error) {
-
     // Value should be a string wrapped in an object
     mbgl::android::Value jsonValue = value.get("data");
     if(value.isNull()) {
         error = { "no json data found" };
         return {};
     }
-    std::string jsonString = value.get("data").toString();
 
-    rapidjson::GenericDocument<rapidjson::UTF8<>, rapidjson::CrtAllocator> d;
-    d.Parse(jsonString.c_str());
-
-    if (d.HasParseError()) {
-        std::stringstream message;
-        message << d.GetErrorOffset() << " - " << rapidjson::GetParseError_En(d.GetParseError());
-        error = { message.str() };
-        return {};
-    }
-
-    optional<GeoJSON> geoJSON = conversion::convertGeoJSON<JSValue>(d, error);
-    if (!geoJSON) {
-        error = { error.message };
-        return {};
-    }
-
-    return geoJSON;
+    return convertGeoJSON(value.get("data").toString(), error);
 }
 
 template <>
