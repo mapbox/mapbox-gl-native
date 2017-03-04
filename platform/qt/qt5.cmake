e7a17a747a4525cab2888a1b4bdf309215bd51df
Thiago Marcos P. Santos <thiago@mapbox.com>
2017-03-04 09:06:04 -0800

    [Qt] Remove the QML app
    We are now on Qt 5.9 and this is where the plugin will be developed
    from now on.

--- platform/qt/qt5.cmake
+++ platform/qt/qt5.cmake
@@ -3,7 +3,6 @@ find_package(Qt5Gui      REQUIRED)
 find_package(Qt5Location REQUIRED)
 find_package(Qt5Network  REQUIRED)
 find_package(Qt5OpenGL   REQUIRED)
-find_package(Qt5Quick    REQUIRED)
 find_package(Qt5Widgets  REQUIRED)
 find_package(Qt5Sql      REQUIRED)
 
@@ -21,21 +20,11 @@ set(MBGL_QT_TEST_LIBRARIES
     PRIVATE Qt5::OpenGL
 )
 
-target_sources(qmapboxgl
-    PRIVATE platform/qt/include/qquickmapboxgl.hpp
-    PRIVATE platform/qt/include/qquickmapboxglmapparameter.hpp
-    PRIVATE platform/qt/src/qquickmapboxgl.cpp
-    PRIVATE platform/qt/src/qquickmapboxglmapparameter.cpp
-    PRIVATE platform/qt/src/qquickmapboxglrenderer.cpp
-    PRIVATE platform/qt/src/qquickmapboxglrenderer.hpp
-)
-
 target_link_libraries(qmapboxgl
     PRIVATE mbgl-core
     PRIVATE Qt5::Core
     PRIVATE Qt5::Gui
     PRIVATE Qt5::Location
-    PRIVATE Qt5::Quick
     PRIVATE Qt5::Sql
 )
 
@@ -43,17 +32,4 @@ target_link_libraries(mbgl-qt
     PRIVATE qmapboxgl
     PRIVATE Qt5::OpenGL
     PRIVATE Qt5::Widgets
-)
-
-# QtQuick app
-add_executable(mbgl-qt-qml
-    platform/qt/qmlapp/main.cpp
-    platform/qt/qmlapp/qml.qrc
-    platform/qt/resources/common.qrc
-)
-
-target_link_libraries(mbgl-qt-qml
-    PRIVATE qmapboxgl
-    PRIVATE Qt5::Location
-    PRIVATE Qt5::Quick
 )
