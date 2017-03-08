b05b7dd93a68c589e50990900a3253bf83283f87
Konstantin Käfer <mail@kkaefer.com>
2017-03-08 14:50:49 -0800

    [darwin] Use explicit initialization rather than global static variables for the default RunLoop
    When linking Mapbox GL as a static library, the object file (MGLFoundation.o) containing the global static variable initialization was not linked into the resulting binary since it was not referenced. This resulted in the RunLoop not being initialized in the main thread.
    
    Reverts 03a14ff0003e976a4ded70d284bc80adf54bc6c9

--- platform/darwin/src/MGLOfflineStorage.mm
+++ platform/darwin/src/MGLOfflineStorage.mm
@@ -1,5 +1,6 @@
 #import "MGLOfflineStorage_Private.h"
 
+#import "MGLFoundation_Private.h"
 #import "MGLAccountManager_Private.h"
 #import "MGLGeometry_Private.h"
 #import "MGLNetworkConfiguration.h"
@@ -170,6 +171,8 @@ NSString * const MGLOfflinePackMaximumCountUserInfoKey = MGLOfflinePackUserInfoK
 }
 
 - (instancetype)init {
+    MGLInitializeRunLoop();
+
     if (self = [super init]) {
         NSURL *cacheURL = [[self class] cacheURLIncludingSubdirectory:YES];
         NSString *cachePath = cacheURL.path ?: @"";
