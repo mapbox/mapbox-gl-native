a517323bfc77c33d40a0a43a1fb2f79a6761bbe6
Konstantin Käfer <mail@kkaefer.com>
2017-03-08 11:51:13 -0800

    [build] increase binary size log duration to 60 days

--- scripts/publish_binary_size.sh
+++ scripts/publish_binary_size.sh
@@ -24,9 +24,9 @@ function publish_binary_size {
     local DATE_END=$(date -u +${DATE_FORMAT})
 
     if [ `uname -s` = 'Darwin' ]; then # BSD date
-        local DATE_BEGIN=$(date -jf "${DATE_FORMAT}" -v-30d "${DATE_END}" +"${DATE_FORMAT}")
+        local DATE_BEGIN=$(date -jf "${DATE_FORMAT}" -v-60d "${DATE_END}" +"${DATE_FORMAT}")
     else # GNU date
-        local DATE_BEGIN=$(date --date="${DATE_END} - 30 days" +"${DATE_FORMAT}")
+        local DATE_BEGIN=$(date --date="${DATE_END} - 60 days" +"${DATE_FORMAT}")
     fi
 
     # Download the metrics, gzip, and upload to S3.
