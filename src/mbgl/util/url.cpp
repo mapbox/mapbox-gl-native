fb44c33adaec985011f79d93c82218529a1b64e4
Konstantin Käfer <mail@kkaefer.com>
2017-03-21 14:41:08 +0100

    [core] Remove private header includes from CLI utilities

--- src/mbgl/util/url.cpp
+++ src/mbgl/util/url.cpp
@@ -66,19 +66,6 @@ std::string percentDecode(const std::string& input) {
     return decoded;
 }
 
-// Checks whether the input string contains ://, and the part before it is all alphanumeric ASCII.
-bool isURL(const std::string& input) {
-    auto it = input.begin();
-    // First character has to be alphabetic
-    if (it == input.end() || !isAlphaCharacter(*it++)) return false;
-    // The remaining characters of the scheme can be alphanumeric, or be one of +.-
-    while (it != input.end() && isSchemeCharacter(*it)) ++it;
-    // Check that :// follows
-    return (it != input.end() && *it++ == ':') &&
-           (it != input.end() && *it++ == '/') &&
-           (it != input.end() && *it++ == '/');
-}
-
 URL::URL(const std::string& str)
     : query([&]() -> Segment {
           const auto hashPos = str.find('#');
