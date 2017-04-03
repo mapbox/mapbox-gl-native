9e25f3363b3c429484ade48d5ff8781026adcf89
Eimantas Vaiciunas <eimantas@walkingsmarts.com>
2017-04-03 07:15:40 +0300

    Match curly braces style

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -3531,13 +3531,15 @@ public:
     }
 }
 
-- (nullable NS_ARRAY_OF(id <MGLOverlay>) *)overlays {
+- (nullable NS_ARRAY_OF(id <MGLOverlay>) *)overlays
+{
     if (self.annotations == nil) { return nil; }
 
     NS_MUTABLE_ARRAY_OF(id <MGLOverlay>) *mutableOverlays = [NSMutableArray new];
 
     [self.annotations enumerateObjectsUsingBlock:^(id<MGLAnnotation>  _Nonnull annotation, NSUInteger idx, BOOL * _Nonnull stop) {
-        if ([annotation conformsToProtocol:@protocol(MGLOverlay)]) {
+        if ([annotation conformsToProtocol:@protocol(MGLOverlay)])
+        {
             [mutableOverlays addObject:(id<MGLOverlay>)annotation];
         }
     }];
--- platform/macos/src/MGLMapView.mm
+++ platform/macos/src/MGLMapView.mm
@@ -2413,13 +2413,15 @@ public:
 
 #pragma mark Overlays
 
-- (nullable NS_ARRAY_OF(id <MGLOverlay>) *)overlays {
+- (nullable NS_ARRAY_OF(id <MGLOverlay>) *)overlays
+{
     if (self.annotations == nil) { return nil; }
 
     NS_MUTABLE_ARRAY_OF(id <MGLOverlay>) *mutableOverlays = [NSMutableArray new];
 
     [self.annotations enumerateObjectsUsingBlock:^(id<MGLAnnotation>  _Nonnull annotation, NSUInteger idx, BOOL * _Nonnull stop) {
-        if ([annotation conformsToProtocol:@protocol(MGLOverlay)]) {
+        if ([annotation conformsToProtocol:@protocol(MGLOverlay)])
+        {
             [mutableOverlays addObject:(id<MGLOverlay>)annotation];
         }
     }];
