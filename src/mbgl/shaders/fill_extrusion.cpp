fcdbcda5b5eeb668cc1af68962e20e837fce0788
Lauren Budorick <lauren@mapbox.com>
2017-04-04 14:00:32 -0700

    Rebased shaders

--- src/mbgl/shaders/fill_extrusion.cpp
+++ src/mbgl/shaders/fill_extrusion.cpp
@@ -7,60 +7,6 @@ namespace shaders {
 
 const char* fill_extrusion::name = "fill_extrusion";
 const char* fill_extrusion::vertexSource = R"MBGL_SHADER(
-#ifdef GL_ES
-precision highp float;
-#else
-
-#if !defined(lowp)
-#define lowp
-#endif
-
-#if !defined(mediump)
-#define mediump
-#endif
-
-#if !defined(highp)
-#define highp
-#endif
-
-#endif
-
-float evaluate_zoom_function_1(const vec4 values, const float t) {
-    if (t < 1.0) {
-        return mix(values[0], values[1], t);
-    } else if (t < 2.0) {
-        return mix(values[1], values[2], t - 1.0);
-    } else {
-        return mix(values[2], values[3], t - 2.0);
-    }
-}
-vec4 evaluate_zoom_function_4(const vec4 value0, const vec4 value1, const vec4 value2, const vec4 value3, const float t) {
-    if (t < 1.0) {
-        return mix(value0, value1, t);
-    } else if (t < 2.0) {
-        return mix(value1, value2, t - 1.0);
-    } else {
-        return mix(value2, value3, t - 2.0);
-    }
-}
-
-// The offset depends on how many pixels are between the world origin and the edge of the tile:
-// vec2 offset = mod(pixel_coord, size)
-//
-// At high zoom levels there are a ton of pixels between the world origin and the edge of the tile.
-// The glsl spec only guarantees 16 bits of precision for highp floats. We need more than that.
-//
-// The pixel_coord is passed in as two 16 bit values:
-// pixel_coord_upper = floor(pixel_coord / 2^16)
-// pixel_coord_lower = mod(pixel_coord, 2^16)
-//
-// The offset is calculated in a series of steps that should preserve this precision:
-vec2 get_pattern_pos(const vec2 pixel_coord_upper, const vec2 pixel_coord_lower,
-    const vec2 pattern_size, const float tile_units_to_pixels, const vec2 pos) {
-
-    vec2 offset = mod(mod(mod(pixel_coord_upper, pattern_size) * 256.0, pattern_size) * 256.0 + pixel_coord_lower, pattern_size);
-    return (tile_units_to_pixels * pos + offset) / pattern_size;
-}
 uniform mat4 u_matrix;
 uniform vec3 u_lightcolor;
 uniform lowp vec3 u_lightpos;
@@ -73,23 +19,20 @@ attribute float a_edgedistance;
 varying vec4 v_color;
 
 uniform lowp float a_base_t;
-attribute lowp float a_base_min;
-attribute lowp float a_base_max;
+attribute lowp vec2 a_base;
 varying lowp float base;
 uniform lowp float a_height_t;
-attribute lowp float a_height_min;
-attribute lowp float a_height_max;
+attribute lowp vec2 a_height;
 varying lowp float height;
 
 uniform lowp float a_color_t;
-attribute lowp vec4 a_color_min;
-attribute lowp vec4 a_color_max;
+attribute lowp vec4 a_color;
 varying lowp vec4 color;
 
 void main() {
-    base = mix(a_base_min, a_base_max, a_base_t);
-    height = mix(a_height_min, a_height_max, a_height_t);
-    color = mix(a_color_min, a_color_max, a_color_t);
+    base = unpack_mix_vec2(a_base, a_base_t);
+    height = unpack_mix_vec2(a_height, a_height_t);
+    color = unpack_mix_vec4(a_color, a_color_t);
 
     float ed = a_edgedistance; // use each attrib in order to not trip a VAO assert
     float t = mod(a_normal.x, 2.0);
@@ -129,23 +72,6 @@ void main() {
 
 )MBGL_SHADER";
 const char* fill_extrusion::fragmentSource = R"MBGL_SHADER(
-#ifdef GL_ES
-precision mediump float;
-#else
-
-#if !defined(lowp)
-#define lowp
-#endif
-
-#if !defined(mediump)
-#define mediump
-#endif
-
-#if !defined(highp)
-#define highp
-#endif
-
-#endif
 varying vec4 v_color;
 varying lowp float base;
 varying lowp float height;
--- src/mbgl/shaders/fill_extrusion_pattern.cpp
+++ src/mbgl/shaders/fill_extrusion_pattern.cpp
@@ -7,60 +7,6 @@ namespace shaders {
 
 const char* fill_extrusion_pattern::name = "fill_extrusion_pattern";
 const char* fill_extrusion_pattern::vertexSource = R"MBGL_SHADER(
-#ifdef GL_ES
-precision highp float;
-#else
-
-#if !defined(lowp)
-#define lowp
-#endif
-
-#if !defined(mediump)
-#define mediump
-#endif
-
-#if !defined(highp)
-#define highp
-#endif
-
-#endif
-
-float evaluate_zoom_function_1(const vec4 values, const float t) {
-    if (t < 1.0) {
-        return mix(values[0], values[1], t);
-    } else if (t < 2.0) {
-        return mix(values[1], values[2], t - 1.0);
-    } else {
-        return mix(values[2], values[3], t - 2.0);
-    }
-}
-vec4 evaluate_zoom_function_4(const vec4 value0, const vec4 value1, const vec4 value2, const vec4 value3, const float t) {
-    if (t < 1.0) {
-        return mix(value0, value1, t);
-    } else if (t < 2.0) {
-        return mix(value1, value2, t - 1.0);
-    } else {
-        return mix(value2, value3, t - 2.0);
-    }
-}
-
-// The offset depends on how many pixels are between the world origin and the edge of the tile:
-// vec2 offset = mod(pixel_coord, size)
-//
-// At high zoom levels there are a ton of pixels between the world origin and the edge of the tile.
-// The glsl spec only guarantees 16 bits of precision for highp floats. We need more than that.
-//
-// The pixel_coord is passed in as two 16 bit values:
-// pixel_coord_upper = floor(pixel_coord / 2^16)
-// pixel_coord_lower = mod(pixel_coord, 2^16)
-//
-// The offset is calculated in a series of steps that should preserve this precision:
-vec2 get_pattern_pos(const vec2 pixel_coord_upper, const vec2 pixel_coord_lower,
-    const vec2 pattern_size, const float tile_units_to_pixels, const vec2 pos) {
-
-    vec2 offset = mod(mod(mod(pixel_coord_upper, pattern_size) * 256.0, pattern_size) * 256.0 + pixel_coord_lower, pattern_size);
-    return (tile_units_to_pixels * pos + offset) / pattern_size;
-}
 uniform mat4 u_matrix;
 uniform vec2 u_pattern_size_a;
 uniform vec2 u_pattern_size_b;
@@ -85,17 +31,15 @@ varying vec4 v_lighting;
 varying float v_directional;
 
 uniform lowp float a_base_t;
-attribute lowp float a_base_min;
-attribute lowp float a_base_max;
+attribute lowp vec2 a_base;
 varying lowp float base;
 uniform lowp float a_height_t;
-attribute lowp float a_height_min;
-attribute lowp float a_height_max;
+attribute lowp vec2 a_height;
 varying lowp float height;
 
 void main() {
-    base = mix(a_base_min, a_base_max, a_base_t);
-    height = mix(a_height_min, a_height_max, a_height_t);
+    base = unpack_mix_vec2(a_base, a_base_t);
+    height = unpack_mix_vec2(a_height, a_height_t);
 
     float t = mod(a_normal.x, 2.0);
     float z = t > 0.0 ? height : base;
@@ -122,23 +66,6 @@ void main() {
 
 )MBGL_SHADER";
 const char* fill_extrusion_pattern::fragmentSource = R"MBGL_SHADER(
-#ifdef GL_ES
-precision mediump float;
-#else
-
-#if !defined(lowp)
-#define lowp
-#endif
-
-#if !defined(mediump)
-#define mediump
-#endif
-
-#if !defined(highp)
-#define highp
-#endif
-
-#endif
 uniform vec2 u_pattern_tl_a;
 uniform vec2 u_pattern_br_a;
 uniform vec2 u_pattern_tl_b;
