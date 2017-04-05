6d44ee0f9785a0bc76e3298003901c74dd0bf7d2
Konstantin Käfer <mail@kkaefer.com>
2017-04-05 15:57:14 +0200

    [core] destroy surface before context in EGL backend

--- platform/linux/src/headless_backend_egl.cpp
+++ platform/linux/src/headless_backend_egl.cpp
@@ -31,14 +31,14 @@ struct EGLImpl : public HeadlessBackend::Impl {
     }
 
     ~EGLImpl() {
-        if (!eglDestroyContext(display, glContext)) {
-            throw std::runtime_error("Failed to destroy EGL context.\n");
-        }
         if (glSurface != EGL_NO_SURFACE) {
             if (!eglDestroySurface(display, glSurface)) {
-                throw std::runtime_error("Failed to destroy EGL context.\n");
+                throw std::runtime_error("Failed to destroy EGL surface.\n");
             }
             glSurface = EGL_NO_SURFACE;
+        }
+        if (!eglDestroyContext(display, glContext)) {
+            throw std::runtime_error("Failed to destroy EGL context.\n");
         }
     }
 
