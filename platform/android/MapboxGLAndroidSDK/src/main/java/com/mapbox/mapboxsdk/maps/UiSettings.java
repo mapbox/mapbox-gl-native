df22b614af2cf8d468d5e6fe3fcae129b5b95df0
Pablo Guardiola <guardiola31337@gmail.com>
2017-04-10 12:47:07 +0200

    add compass drawable initialization in ui settings (#8695)

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/UiSettings.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/UiSettings.java
@@ -15,6 +15,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -149,6 +150,9 @@ public final class UiSettings {
       setCompassMargins(tenDp, tenDp, tenDp, tenDp);
     }
     setCompassFadeFacingNorth(options.getCompassFadeFacingNorth());
+    if (options.getCompassImage() == null) {
+      options.compassImage(ResourcesCompat.getDrawable(resources, R.drawable.mapbox_compass_icon, null));
+    }
     setCompassImage(options.getCompassImage());
   }
 
