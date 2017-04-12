aaf135a5ba946cdcf8bd61227e0d871119df2fd8
Jesse Bounds <jesse@rebounds.net>
2017-04-12 15:22:55 -0400

    [ios, macos] Add Swift integration tests for map view delegate

---
+++ platform/darwin/test/MGLSDKTestHelpers.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+class MGLSDKTestHelpers {
+
+    class func checkTestsContainAllMethods(testClass: Swift.AnyClass, in p: Protocol) {
+        let testMethods = self.classMethodDescriptions(testClass)
+        let subjectMethods = self.protocolMethodDescriptions(p)
+
+        for method in subjectMethods {
+            if !testMethods.contains(method) {
+                XCTFail("\(String(describing: testClass)) does not contain \(method) from \(String(describing: p))")
+            }
+        }
+
+        XCTAssert(true)
+    }
+
+}
+
+extension MGLSDKTestHelpers {
+
+    class func protocolMethodDescriptions(_ p: Protocol) -> Set<String> {
+        var methods = Set<String>()
+        var methodCount = UInt32()
+        let methodDescriptionList: UnsafeMutablePointer<objc_method_description>! = protocol_copyMethodDescriptionList(p, false, true, &methodCount)
+        for i in 0..<Int(methodCount) {
+            let description: objc_method_description = methodDescriptionList[i]
+            methods.insert(description.name.description)
+        }
+        free(methodDescriptionList)
+        return methods
+    }
+
+    class func classMethodDescriptions(_ cls: Swift.AnyClass) -> Set<String> {
+        var methods = Set<String>()
+        var methodCount = UInt32()
+        let methodList: UnsafeMutablePointer<Method?>! = class_copyMethodList(cls, &methodCount)
+        for i in 0..<Int(methodCount) {
+            let method = methodList[i]
+            let selector : Selector = method_getName(method)
+            methods.insert(selector.description)
+        }
+        free(methodList)
+        return methods
+    }
+
+}
---
+++ platform/ios/test/MGLMapViewDelegateIntegrationTests.swift
@@ -0,0 +1,82 @@
+import XCTest
+import Mapbox
+
+class MGLMapViewDelegateIntegrationTests: XCTestCase {
+
+    func testCoverage() {
+        MGLSDKTestHelpers.checkTestsContainAllMethods(testClass: MGLMapViewDelegateIntegrationTests.self, in: MGLMapViewDelegate.self)
+    }
+
+}
+
+extension MGLMapViewDelegateIntegrationTests: MGLMapViewDelegate {
+
+    func mapViewRegionIsChanging(_ mapView: MGLMapView) {}
+
+    func mapView(_ mapView: MGLMapView, didChange mode: MGLUserTrackingMode, animated: Bool) {}
+
+    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {}
+
+    func mapViewDidStopLocatingUser(_ mapView: MGLMapView) {}
+
+    func mapViewWillStartLoadingMap(_ mapView: MGLMapView) {}
+
+    func mapViewWillStartLocatingUser(_ mapView: MGLMapView) {}
+
+    func mapViewWillStartRenderingMap(_ mapView: MGLMapView) {}
+
+    func mapViewWillStartRenderingFrame(_ mapView: MGLMapView) {}
+
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {}
+
+    func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {}
+
+    func mapView(_ mapView: MGLMapView, didDeselect annotation: MGLAnnotation) {}
+
+    func mapView(_ mapView: MGLMapView, regionDidChangeAnimated animated: Bool) {}
+
+    func mapView(_ mapView: MGLMapView, regionWillChangeAnimated animated: Bool) {}
+
+    func mapViewDidFailLoadingMap(_ mapView: MGLMapView, withError error: Error) {}
+
+    func mapView(_ mapView: MGLMapView, didUpdate userLocation: MGLUserLocation?) {}
+
+    func mapViewDidFinishRenderingMap(_ mapView: MGLMapView, fullyRendered: Bool) {}
+
+    func mapView(_ mapView: MGLMapView, didFailToLocateUserWithError error: Error) {}
+
+    func mapView(_ mapView: MGLMapView, tapOnCalloutFor annotation: MGLAnnotation) {}
+
+    func mapViewDidFinishRenderingFrame(_ mapView: MGLMapView, fullyRendered: Bool) {}
+
+    func mapView(_ mapView: MGLMapView, didAdd annotationViews: [MGLAnnotationView]) {}
+
+    func mapView(_ mapView: MGLMapView, didSelect annotationView: MGLAnnotationView) {}
+
+    func mapView(_ mapView: MGLMapView, didDeselect annotationView: MGLAnnotationView) {}
+
+    func mapView(_ mapView: MGLMapView, alphaForShapeAnnotation annotation: MGLShape) -> CGFloat { return 0 }
+
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? { return nil }
+
+    func mapView(_ mapView: MGLMapView, imageFor annotation: MGLAnnotation) -> MGLAnnotationImage? { return nil }
+
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool { return false }
+
+    func mapView(_ mapView: MGLMapView, calloutViewFor annotation: MGLAnnotation) -> MGLCalloutView? { return nil }
+
+    func mapView(_ mapView: MGLMapView, strokeColorForShapeAnnotation annotation: MGLShape) -> UIColor { return .black }
+
+    func mapView(_ mapView: MGLMapView, fillColorForPolygonAnnotation annotation: MGLPolygon) -> UIColor { return .black }
+
+    func mapView(_ mapView: MGLMapView, leftCalloutAccessoryViewFor annotation: MGLAnnotation) -> UIView? { return nil }
+
+    func mapView(_ mapView: MGLMapView, lineWidthForPolylineAnnotation annotation: MGLPolyline) -> CGFloat { return 0 }
+
+    func mapView(_ mapView: MGLMapView, rightCalloutAccessoryViewFor annotation: MGLAnnotation) -> UIView? { return nil }
+
+    func mapView(_ mapView: MGLMapView, annotation: MGLAnnotation, calloutAccessoryControlTapped control: UIControl) {}
+
+    func mapView(_ mapView: MGLMapView, shouldChangeFrom oldCamera: MGLMapCamera, to newCamera: MGLMapCamera) -> Bool { return false }
+
+}
