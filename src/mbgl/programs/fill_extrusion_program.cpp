52b4120918a57eec3c85c5b0a2c31909b6b3f8cf
Lauren Budorick <lauren@mapbox.com>
2017-04-12 16:33:18 -0700

    Per review: * Use Light::Evaluated to initialize FillExtrusionUniforms + FillExtrusionPatternUniforms * painter_fill_extrusion.cpp reindentation

--- src/mbgl/programs/fill_extrusion_program.cpp
+++ src/mbgl/programs/fill_extrusion_program.cpp
@@ -3,12 +3,45 @@
 #include <mbgl/style/cross_faded_property_evaluator.hpp>
 #include <mbgl/tile/tile_id.hpp>
 #include <mbgl/map/transform_state.hpp>
+#include <mbgl/util/mat3.hpp>
 
 namespace mbgl {
 
 using namespace style;
 
 static_assert(sizeof(FillExtrusionLayoutVertex) == 12, "expected FillExtrusionLayoutVertex size");
+
+std::array<float, 3> lightColor(const style::Light::Evaluated& light) {
+    const auto color = light.get<LightColor>();
+    return {{ color.r, color.g, color.b }};
+}
+
+std::array<float, 3> lightPosition(const style::Light::Evaluated& light, const TransformState& state) {
+    auto lightPos = light.get<LightPosition>().getCartesian();
+    mat3 lightMat;
+    matrix::identity(lightMat);
+    if (light.get<LightAnchor>() == LightAnchorType::Viewport) {
+        matrix::rotate(lightMat, lightMat, -state.getAngle());
+    }
+    matrix::transformMat3f(lightPos, lightPos, lightMat);
+    return lightPos;
+}
+
+float lightIntensity(const style::Light::Evaluated& light) {
+    return light.get<LightIntensity>();
+}
+
+FillExtrusionUniforms::Values
+FillExtrusionUniforms::values(mat4 matrix,
+                              const TransformState& state,
+                              const style::Light::Evaluated& light) {
+    return FillExtrusionUniforms::Values{
+        uniforms::u_matrix::Value{ matrix },
+        uniforms::u_lightcolor::Value{ lightColor(light) },
+        uniforms::u_lightpos::Value{ lightPosition(light, state) },
+        uniforms::u_lightintensity::Value{ lightIntensity(light) }
+    };
+}
 
 FillExtrusionPatternUniforms::Values
 FillExtrusionPatternUniforms::values(mat4 matrix,
@@ -18,9 +51,7 @@ FillExtrusionPatternUniforms::values(mat4 matrix,
                                      const UnwrappedTileID& tileID,
                                      const TransformState& state,
                                      const float heightFactor,
-                                     const std::array<float, 3>& lightColor,
-                                     const std::array<float, 3>& lightPos,
-                                     const float lightIntensity) {
+                                     const style::Light::Evaluated& light) {
     int32_t tileSizeAtNearestZoom = util::tileSize * state.zoomScale(state.getIntegerZoom() - tileID.canonical.z);
     int32_t pixelX = tileSizeAtNearestZoom * (tileID.canonical.x + tileID.wrap * state.zoomScale(tileID.canonical.z));
     int32_t pixelY = tileSizeAtNearestZoom * tileID.canonical.y;
@@ -41,9 +72,9 @@ FillExtrusionPatternUniforms::values(mat4 matrix,
         uniforms::u_pixel_coord_lower::Value{ std::array<float, 2>{{ float(pixelX & 0xFFFF), float(pixelY & 0xFFFF) }} },
         uniforms::u_tile_units_to_pixels::Value{ 1.0f / tileID.pixelsToTileUnits(1.0f, state.getIntegerZoom()) },
         uniforms::u_height_factor::Value{ heightFactor },
-        uniforms::u_lightcolor::Value{ lightColor },
-        uniforms::u_lightpos::Value{ lightPos },
-        uniforms::u_lightintensity::Value{ lightIntensity },
+        uniforms::u_lightcolor::Value{ lightColor(light) },
+        uniforms::u_lightpos::Value{ lightPosition(light, state) },
+        uniforms::u_lightintensity::Value{ lightIntensity(light) },
     };
 }
 
