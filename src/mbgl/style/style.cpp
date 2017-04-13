681878f0199168eb2e653bf034e6e050ebcc9955
Lauren Budorick <lauren@mapbox.com>
2017-04-13 14:51:12 -0700

    Per review: * Simplify CascadingLightProperty and rename (nothing cascades)

--- src/mbgl/style/style.cpp
+++ src/mbgl/style/style.cpp
@@ -359,8 +359,6 @@ void Style::cascade(const TimePoint& timePoint, MapMode mode) {
     for (const auto& layer : renderLayers) {
         layer->cascade(parameters);
     }
-
-    light.cascade(parameters);
 }
 
 void Style::recalculate(float z, const TimePoint& timePoint, MapMode mode) {
