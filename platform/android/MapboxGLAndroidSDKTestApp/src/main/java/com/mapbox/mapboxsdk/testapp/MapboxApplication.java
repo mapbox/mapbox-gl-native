f27bf49d2ea37ef7f1c70f288a49e5e6e2235cc3
Guardiola31337 <pablo.guardiola@mapbox.com>
2017-05-09 12:40:48 +0200

    fix access token not set error message

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/MapboxApplication.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/MapboxApplication.java
@@ -23,6 +23,10 @@ public class MapboxApplication extends Application {
 
   private static final String LOG_TAG = MapboxApplication.class.getSimpleName();
   private static final String DEFAULT_MAPBOX_ACCESS_TOKEN = "YOUR_MAPBOX_ACCESS_TOKEN_GOES_HERE";
+  private static final String ACCESS_TOKEN_NOT_SET_MESSAGE = "In order to run the Test App you need to set a valid "
+    + "access token. During development, you can set the MAPBOX_ACCESS_TOKEN environment variable for the SDK to "
+    + "automatically include it in the Test App. Otherwise, you can manually include it in the "
+    + "res/values/developer-config.xml file in the MapboxGLAndroidSDKTestApp folder.";
 
   @Override
   public void onCreate() {
@@ -51,7 +55,7 @@ public class MapboxApplication extends Application {
 
     String mapboxAccessToken = TokenUtils.getMapboxAccessToken(getApplicationContext());
     if (TextUtils.isEmpty(mapboxAccessToken) || mapboxAccessToken.equals(DEFAULT_MAPBOX_ACCESS_TOKEN)) {
-      Log.w(LOG_TAG, "Warning: access token isn't set.");
+      Log.w(LOG_TAG, ACCESS_TOKEN_NOT_SET_MESSAGE);
     }
 
     Mapbox.getInstance(getApplicationContext(), mapboxAccessToken);
