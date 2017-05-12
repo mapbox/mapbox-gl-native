85cb04be22e8d996d3d3447a252ed936eb6f3659
Pablo Guardiola <guardiola31337@gmail.com>
2017-05-12 12:34:03 +0200

    [android] Fix gradle developer config creation task with discussed suggestions (#8897)
    * fix gradle developer config creation task with discussed suggestions
    
    * fix access token not set error message

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/MapboxApplication.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/MapboxApplication.java
@@ -2,8 +2,11 @@ package com.mapbox.mapboxsdk.testapp;
 
 import android.app.Application;
 import android.os.StrictMode;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.testapp.utils.TokenUtils;
 import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
@@ -17,6 +20,13 @@ import static timber.log.Timber.DebugTree;
  * </p>
  */
 public class MapboxApplication extends Application {
+
+  private static final String LOG_TAG = MapboxApplication.class.getSimpleName();
+  private static final String DEFAULT_MAPBOX_ACCESS_TOKEN = "YOUR_MAPBOX_ACCESS_TOKEN_GOES_HERE";
+  private static final String ACCESS_TOKEN_NOT_SET_MESSAGE = "In order to run the Test App you need to set a valid "
+    + "access token. During development, you can set the MAPBOX_ACCESS_TOKEN environment variable for the SDK to "
+    + "automatically include it in the Test App. Otherwise, you can manually include it in the "
+    + "res/values/developer-config.xml file in the MapboxGLAndroidSDKTestApp folder.";
 
   @Override
   public void onCreate() {
@@ -43,7 +53,12 @@ public class MapboxApplication extends Application {
       .penaltyDeath()
       .build());
 
-    Mapbox.getInstance(getApplicationContext(), getString(R.string.mapbox_access_token));
+    String mapboxAccessToken = TokenUtils.getMapboxAccessToken(getApplicationContext());
+    if (TextUtils.isEmpty(mapboxAccessToken) || mapboxAccessToken.equals(DEFAULT_MAPBOX_ACCESS_TOKEN)) {
+      Log.w(LOG_TAG, ACCESS_TOKEN_NOT_SET_MESSAGE);
+    }
+
+    Mapbox.getInstance(getApplicationContext(), mapboxAccessToken);
   }
 
   private void initializeLogger() {
