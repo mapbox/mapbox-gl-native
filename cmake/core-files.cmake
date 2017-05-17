313415bf4f44da08ce4ba1ecc682f0b7f1f91165
Chris Loer <chris.loer@gmail.com>
2017-05-17 13:49:48 -0700

    [core] Throttle tiles to redo symbol placement at most once every 300ms. Fixes issue #8435 and prepares for pitch-scaling changes in issue #8967.

--- cmake/core-files.cmake
+++ cmake/core-files.cmake
@@ -583,6 +583,8 @@ set(MBGL_CORE_FILES
     src/mbgl/util/thread_context.cpp
     src/mbgl/util/thread_context.hpp
     src/mbgl/util/thread_local.hpp
+    src/mbgl/util/throttler.cpp
+    src/mbgl/util/throttler.hpp
     src/mbgl/util/tile_coordinate.hpp
     src/mbgl/util/tile_cover.cpp
     src/mbgl/util/tile_cover.hpp
