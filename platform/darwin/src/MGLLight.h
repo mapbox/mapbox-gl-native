a95fef501e46986e40b980d4fc6db4f9e80a0628
Fabian Guerra <fabian.guerra@mapbox.com>
2017-06-12 11:30:53 -0400

    [ios, macos] Fix MGLSphericalPosition.radial misleading type.

--- platform/darwin/src/MGLLight.h
+++ platform/darwin/src/MGLLight.h
@@ -20,7 +20,7 @@ typedef NS_ENUM(NSUInteger, MGLLightAnchor) {
  */
 typedef struct MGLSphericalPosition {
     /** Distance from the center of the base of an object to its light. */
-    CLLocationDistance radial;
+    CGFloat radial;
     /** Position of the light relative to 0° (0° when `MGLLight.anchor` is set to viewport corresponds
      to the top of the viewport, or 0° when `MGLLight.anchor` is set to map corresponds to due north,
      and degrees proceed clockwise). */
