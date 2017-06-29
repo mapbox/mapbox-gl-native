b7befc3cd1beeac838a72d2f593326c93b7b185c
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-06-29 22:40:50 +0300

    [core] Factor timePoint initialization: take 2

--- src/mbgl/map/zoom_history.hpp
+++ src/mbgl/map/zoom_history.hpp
@@ -13,19 +13,20 @@ struct ZoomHistory {
     bool first = true;
 
     bool update(float z, const TimePoint& now) {
+        constexpr TimePoint zero = TimePoint(Duration::zero());
         if (first) {
             first = false;
             lastIntegerZoom = std::floor(z);
-            lastIntegerZoomTime = TimePoint(Duration::zero());
+            lastIntegerZoomTime = zero;
             lastZoom = z;
             return true;
         } else {
             if (std::floor(lastZoom) < std::floor(z)) {
                 lastIntegerZoom = std::floor(z);
-                lastIntegerZoomTime = now;
+                lastIntegerZoomTime = now == Clock::time_point::max() ? zero : now;
             } else if (std::floor(lastZoom) > std::floor(z)) {
                 lastIntegerZoom = std::floor(z + 1);
-                lastIntegerZoomTime = now;
+                lastIntegerZoomTime = now == Clock::time_point::max() ? zero : now;
             }
 
             if (z != lastZoom) {
