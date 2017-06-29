f6e101827b5038b4c0abf5b0d6383b5da06bcfc8
Lauren Budorick <lauren@mapbox.com>
2017-06-29 10:38:59 -0700

    Comment to explain returning on location -1 in AttributeBinding::bind

--- src/mbgl/gl/attribute.cpp
+++ src/mbgl/gl/attribute.cpp
@@ -66,7 +66,11 @@ template <> DataType DataTypeOf<float>    = DataType::Float;
 
 template <class T, std::size_t N>
 void AttributeBinding<T, N>::bind(Context& context, AttributeLocation location, std::size_t vertexOffset) const {
+    // FillProgram will attempt to bind at location -1 because it includes
+    // a fill-outline-color paint property but does not use it or have an
+    // a_outline_color shader attribute - in this case, we have nothing to bind.
     if (location == -1) return;
+
     context.vertexBuffer = vertexBuffer;
     MBGL_CHECK_ERROR(glEnableVertexAttribArray(location));
     MBGL_CHECK_ERROR(glVertexAttribPointer(
