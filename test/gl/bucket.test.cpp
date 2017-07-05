06a0c4ad15a335e1fe8c66886d8b9f75d6e3bdbe
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-07-05 16:31:16 +0300

    [test] Added bucket feature insertion tests

--- test/gl/bucket.test.cpp
+++ test/gl/bucket.test.cpp
@@ -1,4 +1,5 @@
 #include <mbgl/test/util.hpp>
+#include <mbgl/test/stub_geometry_tile_feature.hpp>
 
 #include <mbgl/renderer/buckets/circle_bucket.hpp>
 #include <mbgl/renderer/buckets/fill_bucket.hpp>
@@ -14,20 +15,53 @@
 using namespace mbgl;
 
 TEST(Buckets, CircleBucket) {
+    gl::Context context;
     CircleBucket bucket { { {0, 0, 0}, MapMode::Still, 1.0 }, {} };
     ASSERT_FALSE(bucket.hasData());
+    ASSERT_FALSE(bucket.needsUpload());
+
+    GeometryCollection point { { { 0, 0 } } };
+    bucket.addFeature(StubGeometryTileFeature { {}, FeatureType::Point, point, {} }, point);
+    ASSERT_TRUE(bucket.hasData());
+    ASSERT_TRUE(bucket.needsUpload());
+
+    bucket.upload(context);
+    ASSERT_TRUE(bucket.hasData());
     ASSERT_FALSE(bucket.needsUpload());
 }
 
 TEST(Buckets, FillBucket) {
+    gl::Context context;
     FillBucket bucket { { {0, 0, 0}, MapMode::Still, 1.0 }, {} };
     ASSERT_FALSE(bucket.hasData());
+    ASSERT_FALSE(bucket.needsUpload());
+
+    GeometryCollection polygon { { { 0, 0 }, { 0, 1 }, { 1, 1 } } };
+    bucket.addFeature(StubGeometryTileFeature { {}, FeatureType::Polygon, polygon, {} }, polygon);
+    ASSERT_TRUE(bucket.hasData());
+    ASSERT_TRUE(bucket.needsUpload());
+
+    bucket.upload(context);
     ASSERT_FALSE(bucket.needsUpload());
 }
 
 TEST(Buckets, LineBucket) {
+    gl::Context context;
     LineBucket bucket { { {0, 0, 0}, MapMode::Still, 1.0 }, {}, {} };
     ASSERT_FALSE(bucket.hasData());
+    ASSERT_FALSE(bucket.needsUpload());
+
+    // Ignore invalid feature type.
+    GeometryCollection point { { { 0, 0 } } };
+    bucket.addFeature(StubGeometryTileFeature { {}, FeatureType::Point, point, {} }, point);
+    ASSERT_FALSE(bucket.hasData());
+
+    GeometryCollection line { { { 0, 0 }, { 1, 1 } } };
+    bucket.addFeature(StubGeometryTileFeature { {}, FeatureType::LineString, line, {} }, line);
+    ASSERT_TRUE(bucket.hasData());
+    ASSERT_TRUE(bucket.needsUpload());
+
+    bucket.upload(context);
     ASSERT_FALSE(bucket.needsUpload());
 }
 
@@ -36,11 +70,26 @@ TEST(Buckets, SymbolBucket) {
     bool sdfIcons = false;
     bool iconsNeedLinear = false;
 
+    gl::Context context;
     SymbolBucket bucket { layout, {}, 16.0f, 1.0f, 0, sdfIcons, iconsNeedLinear };
     ASSERT_FALSE(bucket.hasIconData());
     ASSERT_FALSE(bucket.hasTextData());
     ASSERT_FALSE(bucket.hasCollisionBoxData());
     ASSERT_FALSE(bucket.hasData());
+    ASSERT_FALSE(bucket.needsUpload());
+
+    // SymbolBucket::addFeature() is a no-op.
+    GeometryCollection point { { { 0, 0 } } };
+    bucket.addFeature(StubGeometryTileFeature { {}, FeatureType::Point, point, {} }, point);
+    ASSERT_FALSE(bucket.hasData());
+    ASSERT_FALSE(bucket.needsUpload());
+
+    bucket.text.segments.emplace_back(0, 0);
+    ASSERT_TRUE(bucket.hasTextData());
+    ASSERT_TRUE(bucket.hasData());
+    ASSERT_TRUE(bucket.needsUpload());
+
+    bucket.upload(context);
     ASSERT_FALSE(bucket.needsUpload());
 }
 
@@ -48,6 +97,7 @@ TEST(Buckets, RasterBucket) {
     gl::Context context;
     UnassociatedImage rgba({ 1, 1 });
 
+    // RasterBucket::hasData() is always true.
     RasterBucket bucket = { std::move(rgba) };
     ASSERT_TRUE(bucket.hasData());
     ASSERT_TRUE(bucket.needsUpload());
