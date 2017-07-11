3086670651ebc70d7a98c32825783a5c6c8c2702
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-07-11 15:32:51 +0300

    [core] Added MBGL_CONSTEXPR to satisfy GCC 4.9

--- src/mbgl/map/update.hpp
+++ src/mbgl/map/update.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <mbgl/util/traits.hpp>
+#include <mbgl/util/util.hpp>
 
 namespace mbgl {
 
@@ -11,15 +12,15 @@ enum class Update {
     AnnotationData            = 1 << 7
 };
 
-constexpr Update operator|(Update lhs, Update rhs) {
+MBGL_CONSTEXPR Update operator|(Update lhs, Update rhs) {
     return Update(mbgl::underlying_type(lhs) | mbgl::underlying_type(rhs));
 }
 
-constexpr Update& operator|=(Update& lhs, const Update& rhs) {
+MBGL_CONSTEXPR Update& operator|=(Update& lhs, const Update& rhs) {
     return (lhs = lhs | rhs);
 }
 
-constexpr bool operator& (Update lhs, Update rhs) {
+MBGL_CONSTEXPR bool operator& (Update lhs, Update rhs) {
     return mbgl::underlying_type(lhs) & mbgl::underlying_type(rhs);
 }
 
