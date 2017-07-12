fbbc8f2d7ae461c1004a1bc69ff8451f4ca35fc1
John Firebaugh <john.firebaugh@gmail.com>
2017-07-12 13:40:32 -0700

    [tests] Local ignores.json file for ignored integration tests

--- platform/node/test/query.test.js
+++ platform/node/test/query.test.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var suite = require('../../../mapbox-gl-js/test/integration').query;
-var suiteImplementation = require('./suite_implementation');
+const suite = require('../../../mapbox-gl-js/test/integration').query;
+const suiteImplementation = require('./suite_implementation');
+const ignores = require('./ignores.json');
 
-var tests;
+let tests;
 
 if (process.argv[1] === __filename && process.argv.length > 2) {
     tests = process.argv.slice(2);
 }
 
-suite.run('native', {tests: tests}, suiteImplementation);
+suite.run('native', {tests: tests, ignores: ignores}, suiteImplementation);
