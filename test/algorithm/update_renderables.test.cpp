698542cf459e51cd72f7bc46b6979a9d67b33309
Konstantin Käfer <mail@kkaefer.com>
2017-07-17 15:22:09 +0200

    [build] make Xcode use the correct compiler when it is installed in a non-standard location

--- test/algorithm/update_renderables.test.cpp
+++ test/algorithm/update_renderables.test.cpp
@@ -100,8 +100,6 @@ auto createTileDataFn(ActionLog& log, T& dataTiles) {
     };
 }
 
-// Unused template argument to fix Clang crash
-// See https://github.com/mapbox/mapbox-gl-native/pull/9501
 template <typename = int>
 auto retainTileDataFn(ActionLog& log) {
     return [&](auto& tileData, Resource::Necessity necessity) {
@@ -109,8 +107,6 @@ auto retainTileDataFn(ActionLog& log) {
     };
 }
 
-// Unused template argument to fix Clang crash
-// See https://github.com/mapbox/mapbox-gl-native/pull/9501
 template <typename = int>
 auto renderTileFn(ActionLog& log) {
     return [&](const auto& id, auto& tileData) {
