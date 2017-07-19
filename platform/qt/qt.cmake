a3244421a54660a699c669988943035b365e1bb9
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2017-07-19 13:52:21 +0300

    [Qt] Rename files with same name
    QMake is having some issues with files with same names when
    building the Qt Location plugin.

--- platform/qt/qt.cmake
+++ platform/qt/qt.cmake
@@ -46,7 +46,7 @@ set(MBGL_QT_FILES
     PRIVATE platform/qt/src/http_file_source.hpp
     PRIVATE platform/qt/src/http_request.cpp
     PRIVATE platform/qt/src/http_request.hpp
-    PRIVATE platform/qt/src/image.cpp
+    PRIVATE platform/qt/src/qt_image.cpp
     PRIVATE platform/qt/src/run_loop.cpp
     PRIVATE platform/qt/src/run_loop_impl.hpp
     PRIVATE platform/qt/src/sqlite3.cpp
