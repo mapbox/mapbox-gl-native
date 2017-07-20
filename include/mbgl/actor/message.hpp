a9ac314a5d28bec2a26cd6889364f81f811d4cd4
Ivo van Dongen <info@ivovandongen.nl>
2017-07-20 19:17:49 +0300

    [core] implement ask pattern in actor

--- include/mbgl/actor/message.hpp
+++ include/mbgl/actor/message.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <mbgl/util/optional.hpp>
+
+#include <future>
 #include <utility>
 
 namespace mbgl {
@@ -36,12 +39,43 @@ public:
     ArgsTuple argsTuple;
 };
 
+template <class ResultType, class Object, class MemberFn, class ArgsTuple>
+class AskMessageImpl : public Message {
+public:
+    AskMessageImpl(std::promise<ResultType> promise_, Object& object_, MemberFn memberFn_, ArgsTuple argsTuple_)
+        : object(object_),
+          memberFn(memberFn_),
+          argsTuple(std::move(argsTuple_)),
+          promise(std::move(promise_)) {
+    }
+
+    void operator()() override {
+        promise.set_value(ask(std::make_index_sequence<std::tuple_size<ArgsTuple>::value>()));
+    }
+
+    template <std::size_t... I>
+    ResultType ask(std::index_sequence<I...>) {
+        return (object.*memberFn)(std::move(std::get<I>(argsTuple))...);
+    }
+
+    Object& object;
+    MemberFn memberFn;
+    ArgsTuple argsTuple;
+    std::promise<ResultType> promise;
+};
+
 namespace actor {
 
 template <class Object, class MemberFn, class... Args>
 std::unique_ptr<Message> makeMessage(Object& object, MemberFn memberFn, Args&&... args) {
     auto tuple = std::make_tuple(std::forward<Args>(args)...);
     return std::make_unique<MessageImpl<Object, MemberFn, decltype(tuple)>>(object, memberFn, std::move(tuple));
+}
+
+template <class ResultType, class Object, class MemberFn, class... Args>
+std::unique_ptr<Message> makeMessage(std::promise<ResultType>&& promise, Object& object, MemberFn memberFn, Args&&... args) {
+    auto tuple = std::make_tuple(std::forward<Args>(args)...);
+    return std::make_unique<AskMessageImpl<ResultType, Object, MemberFn, decltype(tuple)>>(std::move(promise), object, memberFn, std::move(tuple));
 }
 
 } // namespace actor
