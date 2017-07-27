93ad61dac4b8336bb71593cccc7078cacea0f5f0
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-07-27 17:45:33 +0300

    [core] Added Basic, Bright default styles

--- platform/default/mbgl/util/default_styles.hpp
+++ platform/default/mbgl/util/default_styles.hpp
@@ -17,13 +17,15 @@ constexpr const DefaultStyle streets          = { "mapbox://styles/mapbox/street
 constexpr const DefaultStyle outdoors         = { "mapbox://styles/mapbox/outdoors-v10",          "Outdoors",          10 };
 constexpr const DefaultStyle light            = { "mapbox://styles/mapbox/light-v9",              "Light",              9 };
 constexpr const DefaultStyle dark             = { "mapbox://styles/mapbox/dark-v9",               "Dark",               9 };
+constexpr const DefaultStyle basic            = { "mapbox://styles/mapbox/basic-v9",              "Basic",              9 };
+constexpr const DefaultStyle bright           = { "mapbox://styles/mapbox/bright-v9",             "Bright",             9 };
 constexpr const DefaultStyle satellite        = { "mapbox://styles/mapbox/satellite-v9",          "Satellite",          9 };
 constexpr const DefaultStyle satelliteStreets = { "mapbox://styles/mapbox/satellite-streets-v10", "Satellite Streets", 10 };
 constexpr const DefaultStyle trafficDay       = { "mapbox://styles/mapbox/traffic-day-v2",        "Traffic Day",        2 };
 constexpr const DefaultStyle trafficNight     = { "mapbox://styles/mapbox/traffic-night-v2",      "Traffic Night",      2 };
 
 const DefaultStyle orderedStyles[] = {
-    streets, outdoors, light, dark, satellite, satelliteStreets,
+    streets, outdoors, light, dark, basic, bright, satellite, satelliteStreets,
     trafficDay, trafficNight,
 };
 const size_t numOrderedStyles = sizeof(orderedStyles) / sizeof(DefaultStyle);
