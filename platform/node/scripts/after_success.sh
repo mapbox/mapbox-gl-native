c759c51583976bd3fb3f4c6bd1c3c975389dbd10
Bobby Sudekum <bobby@mapbox.com>
2017-08-01 12:06:02 -0700

    move

--- platform/node/scripts/after_success.sh
+++ platform/node/scripts/after_success.sh
@@ -3,7 +3,12 @@
 set -e
 set -o pipefail
 
-if [[ "${PUBLISH:-}" == true ]]; then
+PACKAGE_JSON_VERSION=`node -e "console.log(require('./package.json').version)"`
+
+echo $PACKAGE_JSON_VERSION
+echo $CIRCLE_TAG
+
+if [[ "${CIRCLE_TAG}" == "node-v${PACKAGE_JSON_VERSION}" ]]; then
     if [[ "${BUILDTYPE}" == "RelWithDebInfo" ]]; then
         ./node_modules/.bin/node-pre-gyp package publish info
     elif [[ "${BUILDTYPE}" == "Debug" ]]; then
@@ -12,6 +17,4 @@ if [[ "${PUBLISH:-}" == true ]]; then
         echo "error: must provide either Debug or RelWithDebInfo for BUILDTYPE"
         exit 1
     fi
-else
-    echo "Not publishing. $PUBLISH $CIRCLE_TAG"
 fi
