9390e81ca2645767e70b9b2e464176121a26b2a2
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-08-28 18:12:19 +0300

    [test] Interpret render tests params: --recycleMap and --shuffle

--- platform/node/test/render.test.js
+++ platform/node/test/render.test.js
@@ -5,9 +5,21 @@ const suiteImplementation = require('./suite_implementation');
 const ignores = require('./ignores.json');
 
 let tests;
+let shuffle = false;
+let recycleMap = false;
 
-if (process.argv[1] === __filename && process.argv.length > 2) {
-    tests = process.argv.slice(2);
+function checkParameter(param) {
+    const index = tests.indexOf(param);
+    if (index === -1)
+        return false;
+    tests.splice(index, 1);
+    return true;
 }
 
-suite.run('native', {tests: tests, ignores: ignores}, suiteImplementation);
+if (process.argv[1] === __filename && process.argv.length > 2) {
+    tests = process.argv.slice(2).filter((value, index, self) => { return self.indexOf(value) === index; });
+    shuffle = checkParameter('--shuffle');
+    recycleMap = checkParameter('--recycle-map');
+}
+
+suite.run('native', {tests, ignores, shuffle, recycleMap}, suiteImplementation);
