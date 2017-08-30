6d474717673530d6e4160fa251bbf226d301f1f1
Ivo van Dongen <info@ivovandongen.nl>
2017-08-30 17:22:28 +0300

    [ios] extend ui image additions with init from premultiplied image

--- platform/ios/src/UIImage+MGLAdditions.mm
+++ platform/ios/src/UIImage+MGLAdditions.mm
@@ -22,6 +22,19 @@
     return self;
 }
 
+- (nullable instancetype)initWithMGLPremultipliedImage:(const mbgl::PremultipliedImage&&)mbglImage
+{
+    CGImageRef image = CGImageFromMGLPremultipliedImage(mbglImage.clone());
+    if (!image) {
+        return nil;
+    }
+
+    self = [self initWithCGImage:image scale:1.0 orientation:UIImageOrientationUp];
+    
+    CGImageRelease(image);
+    return self;
+}
+
 - (std::unique_ptr<mbgl::style::Image>)mgl_styleImageWithIdentifier:(NSString *)identifier {
     BOOL isTemplate = self.renderingMode == UIImageRenderingModeAlwaysTemplate;
     return std::make_unique<mbgl::style::Image>([identifier UTF8String],
