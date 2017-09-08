d608334f9dfd64b23e0ff2a8278be113ca9e7c54
Jesse Crocker <jesse@gaiagps.com>
2017-09-08 14:01:03 -0700

    [darwin] Abstract ShapeSource interface

--- platform/darwin/src/MGLShapeSource_Private.h
+++ platform/darwin/src/MGLShapeSource_Private.h
@@ -12,7 +12,4 @@ namespace mbgl {
 @interface MGLShapeSource (Private)
 @end
 
-MGL_EXPORT
-mbgl::style::GeoJSONOptions MGLGeoJSONOptionsFromDictionary(NS_DICTIONARY_OF(MGLShapeSourceOption, id) *options);
-
 NS_ASSUME_NONNULL_END
