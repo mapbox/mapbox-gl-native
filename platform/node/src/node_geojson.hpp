e9914bf0344b0ef996f831ec2d80e5103df8b949
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-09-11 18:30:10 -0400

    [node] Implement GeoJSON converter

--- platform/node/src/node_geojson.hpp
+++ platform/node/src/node_geojson.hpp
@@ -1,13 +1,15 @@
 #include <mbgl/style/conversion/geojson.hpp>
 
+#include <string>
 namespace mbgl {
 namespace style {
 namespace conversion {
 
 template <>
-optional<GeoJSON> Converter<GeoJSON>::operator()(const v8::Local<v8::Value>&, Error& error) const {
-    error = { "not implemented" };
-    return {};
+optional<GeoJSON> Converter<GeoJSON>::operator()(const v8::Local<v8::Value>& value, Error& error) const {
+    Nan::JSON JSON;
+    std::string string = *Nan::Utf8String(JSON.Stringify(value->ToObject()).ToLocalChecked());
+    return convert<GeoJSON>(string, error);
 }
 
 } // namespace conversion
