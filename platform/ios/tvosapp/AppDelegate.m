0a720aee8cf1f9616de04950a41e4eabde9b5d75
Minh Nguyễn <mxn@1ec5.org>
2017-10-05 23:00:59 -0700

    [tvos] Fixed app installation issues
    Also read access token from environment or user default.

--- platform/ios/tvosapp/AppDelegate.m
+++ platform/ios/tvosapp/AppDelegate.m
@@ -1,43 +1,30 @@
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+#import <Mapbox/Mapbox.h>
 
+static NSString * const MBXMapboxAccessTokenDefaultsKey = @"MBXMapboxAccessToken";
+
+@interface AppDelegate ()
 @end
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // Set access token, unless MGLAccountManager already read it in from Info.plist.
+    if (![MGLAccountManager accessToken]) {
+        NSString *accessToken = [[NSProcessInfo processInfo] environment][@"MAPBOX_ACCESS_TOKEN"];
+        if (accessToken) {
+            // Store to preferences so that we can launch the app later on without having to specify
+            // token.
+            [[NSUserDefaults standardUserDefaults] setObject:accessToken forKey:MBXMapboxAccessTokenDefaultsKey];
+        } else {
+            // Try to retrieve from preferences, maybe we've stored them there previously and can reuse
+            // the token.
+            accessToken = [[NSUserDefaults standardUserDefaults] objectForKey:MBXMapboxAccessTokenDefaultsKey];
+        }
+        [MGLAccountManager setAccessToken:accessToken];
+    }
     return YES;
 }
-
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 
 @end
