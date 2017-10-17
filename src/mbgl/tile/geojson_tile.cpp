aa24540c2f453a3d47bf3518e20c78c5666dab04
danesfeder <dan.nesfeder@mapbox.com>
2017-10-17 10:24:18 -0700

    fix crash when querying GeoJSON tiles

--- src/mbgl/tile/geojson_tile.cpp
+++ src/mbgl/tile/geojson_tile.cpp
@@ -25,19 +25,19 @@ void GeoJSONTile::querySourceFeatures(
     const SourceQueryOptions& options) {
     
     // Ignore the sourceLayer, there is only one
-    auto layer = getData()->getLayer({});
-    
-    if (layer) {
-        auto featureCount = layer->featureCount();
-        for (std::size_t i = 0; i < featureCount; i++) {
-            auto feature = layer->getFeature(i);
-            
-            // Apply filter, if any
-            if (options.filter && !(*options.filter)(*feature)) {
-                continue;
+    if (auto data = getData()) {
+        if (auto layer = getData()->getLayer({})) {
+            auto featureCount = layer->featureCount();
+            for (std::size_t i = 0; i < featureCount; i++) {
+                auto feature = layer->getFeature(i);
+
+                // Apply filter, if any
+                if (options.filter && !(*options.filter)(*feature)) {
+                    continue;
+                }
+
+                result.push_back(convertFeature(*feature, id.canonical));
             }
-            
-            result.push_back(convertFeature(*feature, id.canonical));
         }
     }
 }
