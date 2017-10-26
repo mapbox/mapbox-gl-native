823a6a0b5f77a70c816434c5acbe75524c94f92d
Antonio Zugaldia <antonio@mapbox.com>
2017-10-26 12:57:46 -0400

    [android] - lower amount of external dependencies (#10268)

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/camera/CameraPositionActivity.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/camera/CameraPositionActivity.java
@@ -91,9 +91,9 @@ public class CameraPositionActivity extends AppCompatActivity implements OnMapRe
       @Override
       public void onClick(View view) {
         Context context = view.getContext();
+
         final View dialogContent = LayoutInflater.from(context).inflate(R.layout.dialog_camera_position, null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(
-          context, com.mapbox.mapboxsdk.R.style.mapbox_AlertDialogStyle);
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(R.string.dialog_camera_position);
         builder.setView(onInflateDialogContent(dialogContent));
         builder.setPositiveButton("Animate", new DialogInterface.OnClickListener() {
