24de48b7724cb359b3044cd691588a221dd2b550
tobrun <tobrun.van.nuland@gmail.com>
2017-11-06 14:36:13 +0100

    [android] - update android unit tests, update test make target to SDK

--- platform/android/MapboxGLAndroidSDK/src/test/java/com/mapbox/mapboxsdk/maps/AnnotationManagerTest.java
+++ platform/android/MapboxGLAndroidSDK/src/test/java/com/mapbox/mapboxsdk/maps/AnnotationManagerTest.java
@@ -10,6 +10,7 @@ import com.mapbox.mapboxsdk.annotations.MarkerViewManager;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,8 +33,9 @@ public class AnnotationManagerTest {
     Markers markers = new MarkerContainer(aNativeMapView, aMapView, annotationsArray, aIconManager, aMarkerViewManager);
     Polygons polygons = new PolygonContainer(aNativeMapView, annotationsArray);
     Polylines polylines = new PolylineContainer(aNativeMapView, annotationsArray);
+    ShapeAnnotations shapeAnnotations = new ShapeAnnotationContainer(aNativeMapView, annotationsArray);
     AnnotationManager annotationManager = new AnnotationManager(aNativeMapView, aMapView, annotationsArray,
-      aMarkerViewManager, aIconManager, annotations, markers, polygons, polylines);
+      aMarkerViewManager, aIconManager, annotations, markers, polygons, polylines, shapeAnnotations);
     Marker aMarker = mock(Marker.class);
     long aId = 5L;
     when(aNativeMapView.addMarker(aMarker)).thenReturn(aId);
@@ -58,17 +60,22 @@ public class AnnotationManagerTest {
     Markers markers = new MarkerContainer(aNativeMapView, aMapView, annotationsArray, aIconManager, aMarkerViewManager);
     Polygons polygons = new PolygonContainer(aNativeMapView, annotationsArray);
     Polylines polylines = new PolylineContainer(aNativeMapView, annotationsArray);
+    ShapeAnnotations shapeAnnotations = new ShapeAnnotationContainer(aNativeMapView, annotationsArray);
     AnnotationManager annotationManager = new AnnotationManager(aNativeMapView, aMapView, annotationsArray,
-      aMarkerViewManager, aIconManager, annotations, markers, polygons, polylines);
+      aMarkerViewManager, aIconManager, annotations, markers, polygons, polylines, shapeAnnotations);
     long firstId = 1L;
     long secondId = 2L;
     List<BaseMarkerOptions> markerList = new ArrayList<>();
     MarkerOptions firstMarkerOption = new MarkerOptions().position(new LatLng()).title("first");
     MarkerOptions secondMarkerOption = new MarkerOptions().position(new LatLng()).title("second");
+
     markerList.add(firstMarkerOption);
     markerList.add(secondMarkerOption);
     MapboxMap aMapboxMap = mock(MapboxMap.class);
     when(aNativeMapView.addMarker(any(Marker.class))).thenReturn(firstId, secondId);
+
+    when(aNativeMapView.addMarkers(ArgumentMatchers.<Marker>anyList()))
+            .thenReturn(new long[]{firstId, secondId});
 
     annotationManager.addMarkers(markerList, aMapboxMap);
 
