a845a55943e349ed74c4f99e7cfc960ca2a56d22
Chris Loer <chris.loer@gmail.com>
2017-11-09 13:24:43 -0800

    [core] Update shaders/tests from GL JS.

--- src/mbgl/programs/symbol_program.hpp
+++ src/mbgl/programs/symbol_program.hpp
@@ -75,15 +75,21 @@ struct SymbolLayoutAttributes : gl::Attributes<
 };
 
 struct SymbolDynamicLayoutAttributes : gl::Attributes<attributes::a_projected_pos> {
-    static Vertex vertex(Point<float> anchorPoint, float labelAngle, float labelminzoom) {
+    static Vertex vertex(Point<float> anchorPoint, float labelAngle) {
         return Vertex {
             {{
                  anchorPoint.x,
                  anchorPoint.y,
-                 static_cast<float>(mbgl::attributes::packUint8Pair(
-                         static_cast<uint8_t>(std::fmod(labelAngle + 2 * M_PI, 2 * M_PI) / (2 * M_PI) * 255),
-                         static_cast<uint8_t>(labelminzoom * 10)))
+                 labelAngle
              }}
+        };
+    }
+};
+
+struct SymbolOpacityAttributes : gl::Attributes<attributes::a_fade_opacity> {
+    static Vertex vertex(bool placed, float opacity) {
+        return Vertex {
+            {{ static_cast<uint8_t>((static_cast<uint8_t>(opacity * 127) << 1) | static_cast<uint8_t>(placed)) }}
         };
     }
 };
@@ -247,7 +253,7 @@ public:
     using LayoutAttributes = LayoutAttrs;
     using LayoutVertex = typename LayoutAttributes::Vertex;
     
-    using LayoutAndSizeAttributes = gl::ConcatenateAttributes<LayoutAttributes, SymbolDynamicLayoutAttributes>;
+    using LayoutAndSizeAttributes = gl::ConcatenateAttributes<LayoutAttributes, gl::ConcatenateAttributes<SymbolDynamicLayoutAttributes, SymbolOpacityAttributes>>;
 
     using PaintProperties = PaintProps;
     using PaintPropertyBinders = typename PaintProperties::Binders;
@@ -281,6 +287,7 @@ public:
               const UniformValues& uniformValues,
               const gl::VertexBuffer<LayoutVertex>& layoutVertexBuffer,
               const gl::VertexBuffer<SymbolDynamicLayoutAttributes::Vertex>& dynamicLayoutVertexBuffer,
+              const gl::VertexBuffer<SymbolOpacityAttributes::Vertex>& opacityVertexBuffer,
               const SymbolSizeBinder& symbolSizeBinder,
               const gl::IndexBuffer<DrawMode>& indexBuffer,
               const SegmentVector<Attributes>& segments,
@@ -294,7 +301,11 @@ public:
 
         typename Attributes::Bindings allAttributeBindings = LayoutAttributes::bindings(layoutVertexBuffer)
             .concat(SymbolDynamicLayoutAttributes::bindings(dynamicLayoutVertexBuffer))
+            .concat(SymbolOpacityAttributes::bindings(opacityVertexBuffer))
             .concat(paintPropertyBinders.attributeBindings(currentProperties));
+
+        assert(layoutVertexBuffer.vertexCount == dynamicLayoutVertexBuffer.vertexCount &&
+                layoutVertexBuffer.vertexCount == opacityVertexBuffer.vertexCount);
 
         for (auto& segment : segments) {
             auto vertexArrayIt = segment.vertexArrays.find(layerID);
@@ -330,9 +341,8 @@ class SymbolIconProgram : public SymbolProgram<
         uniforms::u_extrude_scale,
         uniforms::u_texsize,
         uniforms::u_texture,
-        uniforms::u_fadetexture,
+        uniforms::u_fade_change,
         uniforms::u_is_text,
-        uniforms::u_collision_y_stretch,
         uniforms::u_camera_to_center_distance,
         uniforms::u_pitch,
         uniforms::u_pitch_with_map,
@@ -350,7 +360,8 @@ public:
                                        const std::array<float, 2>& pixelsToGLUnits,
                                        const bool alongLine,
                                        const RenderTile&,
-                                       const TransformState&);
+                                       const TransformState&,
+                                       const float symbolFadeChange);
 };
 
 enum class SymbolSDFPart {
@@ -370,9 +381,8 @@ class SymbolSDFProgram : public SymbolProgram<
         uniforms::u_extrude_scale,
         uniforms::u_texsize,
         uniforms::u_texture,
-        uniforms::u_fadetexture,
+        uniforms::u_fade_change,
         uniforms::u_is_text,
-        uniforms::u_collision_y_stretch,
         uniforms::u_camera_to_center_distance,
         uniforms::u_pitch,
         uniforms::u_pitch_with_map,
@@ -394,9 +404,8 @@ public:
             uniforms::u_extrude_scale,
             uniforms::u_texsize,
             uniforms::u_texture,
-            uniforms::u_fadetexture,
+            uniforms::u_fade_change,
             uniforms::u_is_text,
-            uniforms::u_collision_y_stretch,
             uniforms::u_camera_to_center_distance,
             uniforms::u_pitch,
             uniforms::u_pitch_with_map,            
@@ -420,6 +429,7 @@ public:
                                        const bool alongLine,
                                        const RenderTile&,
                                        const TransformState&,
+                                       const float SymbolFadeChange,
                                        const SymbolSDFPart);
 };
 
