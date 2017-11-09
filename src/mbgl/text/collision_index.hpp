2f0f9c8c25184d3d557106a3d56590ef0e6c4882
Chris Loer <chris.loer@gmail.com>
2017-11-09 13:24:43 -0800

    [core] Add global CollisionIndex to replace CollisionTile.  - Switches from tile to viewport coordinates  - Represents line labels with circle geometries  - Projects line labels at collision detection time to improve accuracy  - Adapts tile-based symbol queries to viewport coordinates

---
+++ src/mbgl/text/collision_index.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <mbgl/geometry/feature_index.hpp>
+#include <mbgl/text/collision_feature.hpp>
+#include <mbgl/util/grid_index.hpp>
+#include <mbgl/map/transform_state.hpp>
+
+namespace mbgl {
+
+class PlacedSymbol;
+
+struct TileDistance;
+
+class CollisionIndex {
+public:
+    using CollisionGrid = GridIndex<IndexedSubfeature>;
+
+    explicit CollisionIndex(const TransformState&);
+
+    bool placeFeature(CollisionFeature& feature,
+                                      const mat4& posMatrix,
+                                      const mat4& labelPlaneMatrix,
+                                      const float textPixelRatio,
+                                      PlacedSymbol& symbol,
+                                      const float scale,
+                                      const float fontSize,
+                                      const bool allowOverlap,
+                                      const bool pitchWithMap,
+                                      const bool collisionDebug);
+
+    void insertFeature(CollisionFeature& feature, bool ignorePlacement);
+
+    std::vector<IndexedSubfeature> queryRenderedSymbols(const GeometryCoordinates&, const UnwrappedTileID& tileID, const std::string& sourceID) const;
+
+    
+private:
+    bool placeLineFeature(CollisionFeature& feature,
+                                  const mat4& posMatrix,
+                                  const mat4& labelPlaneMatrix,
+                                  const float textPixelRatio,
+                                  PlacedSymbol& symbol,
+                                  const float scale,
+                                  const float fontSize,
+                                  const bool allowOverlap,
+                                  const bool pitchWithMap,
+                                  const bool collisionDebug);
+    
+    float approximateTileDistance(const TileDistance& tileDistance, const float lastSegmentAngle, const float pixelsToTileUnits, const float cameraToAnchorDistance, const bool pitchWithMap);
+    
+    std::pair<float,float> projectAnchor(const mat4& posMatrix, const Point<float>& point) const;
+    std::pair<Point<float>,float> projectAndGetPerspectiveRatio(const mat4& posMatrix, const Point<float>& point) const;
+    Point<float> projectPoint(const mat4& posMatrix, const Point<float>& point) const;
+
+    TransformState transformState;
+    float pitchFactor;
+
+    CollisionGrid collisionGrid;
+    CollisionGrid ignoredGrid;
+};
+
+} // namespace mbgl
