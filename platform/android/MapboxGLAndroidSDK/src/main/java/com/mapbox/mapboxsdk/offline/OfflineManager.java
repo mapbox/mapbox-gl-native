e57127746ce022dbf2ca67eeaffd1bf9cb26eab6
Tobrun <tobrun@mapbox.com>
2017-11-22 17:08:23 +0100

    [android] - activate filesource to list offline regions (#10531)

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/offline/OfflineManager.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/offline/OfflineManager.java
@@ -149,8 +149,8 @@ public class OfflineManager {
    *
    * @param callback the callback to be invoked
    */
-  public void listOfflineRegions(@NonNull
-                                 final ListOfflineRegionsCallback callback) {
+  public void listOfflineRegions(@NonNull final ListOfflineRegionsCallback callback) {
+    fileSource.activate();
     listOfflineRegions(fileSource, new ListOfflineRegionsCallback() {
 
       @Override
@@ -158,6 +158,7 @@ public class OfflineManager {
         getHandler().post(new Runnable() {
           @Override
           public void run() {
+            fileSource.deactivate();
             callback.onList(offlineRegions);
           }
         });
@@ -168,6 +169,7 @@ public class OfflineManager {
         getHandler().post(new Runnable() {
           @Override
           public void run() {
+            fileSource.deactivate();
             callback.onError(error);
           }
         });
@@ -241,6 +243,7 @@ public class OfflineManager {
   /**
    * Changing or bypassing this limit without permission from Mapbox is prohibited
    * by the Mapbox Terms of Service.
+   *
    * @param limit the new tile count limit.
    */
   public native void setOfflineMapboxTileCountLimit(long limit);
