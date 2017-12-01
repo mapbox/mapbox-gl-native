7d9e9dcf4a785a4c570320acc4c6d0ad2afd25af
Chris Loer <chris.loer@gmail.com>
2017-12-01 13:24:30 -0800

    Android indentation fix

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/text/LocalGlyphRasterizer.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/text/LocalGlyphRasterizer.java
@@ -7,18 +7,18 @@ import android.graphics.Typeface;
 
 public class LocalGlyphRasterizer {
 
-    public static Bitmap drawGlyphBitmap(String fontFamily, boolean bold, char glyphID) {
-        Bitmap bitmap = Bitmap.createBitmap(35, 35, Bitmap.Config.ARGB_8888);
+  public static Bitmap drawGlyphBitmap(String fontFamily, boolean bold, char glyphID) {
+    Bitmap bitmap = Bitmap.createBitmap(35, 35, Bitmap.Config.ARGB_8888);
 
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setTextSize(24);
-        paint.setTypeface(Typeface.create(fontFamily, bold ? Typeface.BOLD : Typeface.NORMAL));
+    Paint paint = new Paint();
+    paint.setAntiAlias(true);
+    paint.setTextSize(24);
+    paint.setTypeface(Typeface.create(fontFamily, bold ? Typeface.BOLD : Typeface.NORMAL));
 
-        Canvas canvas = new Canvas();
-        canvas.setBitmap(bitmap);
-        canvas.drawText(String.valueOf(glyphID), 0, 20, paint);
+    Canvas canvas = new Canvas();
+    canvas.setBitmap(bitmap);
+    canvas.drawText(String.valueOf(glyphID), 0, 20, paint);
 
-        return bitmap;
-    }
+    return bitmap;
+  }
 }
