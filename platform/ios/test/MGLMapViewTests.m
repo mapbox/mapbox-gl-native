8b7413330a26f354ba6d775957d75d9d37ff0301
Andrew Kitchen <andrew.kitchen@mapbox.com>
2018-01-04 16:54:43 -0800

    WIP adding tests for layout of map view attribution logo and other ornaments

---
+++ platform/ios/test/MGLMapViewTests.m
@@ -0,0 +1,68 @@
+#import <XCTest/XCTest.h>
+#import "MGLMapView.h"
+#import "MGLMapViewDelegate.h"
+#import "MGLAccountManager.h"
+
+
+@interface MGLMapViewTests : XCTestCase <MGLMapViewDelegate>
+
+@property (nonatomic) MGLMapView *mapView;
+
+@end
+
+@implementation MGLMapViewTests {
+    XCTestExpectation *_styleLoadingExpectation;
+}
+
+- (void)setUp {
+    [super setUp];
+
+    [MGLAccountManager setAccessToken:@"pk.feedcafedeadbeefbadebede"];
+    NSURL *styleURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"one-liner" withExtension:@"json"];
+    self.mapView = [[MGLMapView alloc] initWithFrame:UIScreen.mainScreen.bounds styleURL:styleURL];
+    self.mapView.delegate = self;
+    if (!self.mapView.style) {
+        _styleLoadingExpectation = [self expectationWithDescription:@"Map view should finish loading style."];
+        [self waitForExpectationsWithTimeout:1 handler:nil];
+    }
+}
+
+- (void)mapView:(MGLMapView *)mapView didFinishLoadingStyle:(MGLStyle *)style {
+    XCTAssertNotNil(mapView.style);
+    XCTAssertEqual(mapView.style, style);
+
+    [_styleLoadingExpectation fulfill];
+}
+
+- (void)tearDown {
+    _styleLoadingExpectation = nil;
+    self.mapView = nil;
+
+    [super tearDown];
+}
+
+- (void)testOrnamentPlacement {
+
+    // TODO: check constraining to superview vs. constraining to safeAreaLayoutGuide.bottomAnchor
+    // what about iOS 8??
+
+    [self.mapView setZoomLevel:4.5 animated:NO];
+    [self.mapView.camera setHeading:12.0];
+    
+    [self.mapView setNeedsLayout];
+    [self.mapView layoutIfNeeded];
+
+    //compass
+    NSLog(@"================> %@", self.mapView.compassView);
+
+    //scale bar
+    NSLog(@"================> %@", self.mapView.scaleBar);
+
+    //info button
+    NSLog(@"================> %@", self.mapView.attributionButton);
+
+    //mapbox logo
+    NSLog(@"================> %@", self.mapView.logoView);
+}
+
+@end
