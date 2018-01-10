c7db016a60b27821a3335db72c8b761a87c59379
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2018-01-10 13:12:24 +0200

    [build] Make the appveyor build less verbose

--- scripts/check-sha256.ps1
+++ scripts/check-sha256.ps1
@@ -1,7 +1,6 @@
 param([string]$file, [string]$hash);
 
 $hashFromFile = Get-FileHash -Path $file -Algorithm SHA256
-$hashFromFile | Format-List
 
 if ($hashFromFile.Hash -ne $hash) {
     Throw 'Hash mismatch!'
