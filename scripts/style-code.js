ecfb77c637b4b572114a61fe4f6887edd34b474b
Konstantin Käfer <mail@kkaefer.com>
2018-01-10 16:04:06 -0800

    [build] Write out a list of files generated by our code generator scripts
    This will help us tracking deleted/modified/added files

--- scripts/style-code.js
+++ scripts/style-code.js
@@ -1,6 +1,7 @@
 // Global functions //
 
 const fs = require('fs');
+const path = require('path');
 
 global.iff = function (condition, val) {
   return condition() ? val : "";
@@ -26,7 +27,15 @@ global.unhyphenate = function (str) {
  return str.replace(/-/g, " ");
 };
 
+// Write out a list of files that this script is modifying so that we can check
+var files = [];
+process.on('exit', function() {
+  const list = path.join(path.dirname(process.argv[1]), path.basename(process.argv[1], '.js') + '.list');
+  fs.writeFileSync(list, files.join('\n'));
+});
+
 global.writeIfModified = function(filename, newContent) {
+  files.push(filename);
   try {
     const oldContent = fs.readFileSync(filename, 'utf8');
     if (oldContent == newContent) {
