e7e6cffcf354cc3d2427103b628f924bb39d7fb0
Konstantin Käfer <mail@kkaefer.com>
2018-01-10 16:04:06 -0800

    [build] convert CMake file list generation code to JavaScript
    Bash sorting is weird...

--- cmake/benchmark-files.cmake
+++ cmake/benchmark-files.cmake
@@ -1,30 +1,27 @@
-# Do not edit. Regenerate this with ./scripts/generate-benchmark-files.sh
+# This file is generated. Do not edit. Regenerate this with scripts/generate-cmake-files.js
 
 set(MBGL_BENCHMARK_FILES
     # api
     benchmark/api/query.benchmark.cpp
     benchmark/api/render.benchmark.cpp
 
+    # benchmark
+    benchmark/include/mbgl/benchmark.hpp
+    benchmark/src/main.cpp
+    benchmark/src/mbgl/benchmark/benchmark.cpp
+    benchmark/src/mbgl/benchmark/stub_geometry_tile_feature.hpp
+
     # function
     benchmark/function/camera_function.benchmark.cpp
     benchmark/function/composite_function.benchmark.cpp
     benchmark/function/source_function.benchmark.cpp
-
-    # include/mbgl
-    benchmark/include/mbgl/benchmark.hpp
 
     # parse
     benchmark/parse/filter.benchmark.cpp
     benchmark/parse/tile_mask.benchmark.cpp
     benchmark/parse/vector_tile.benchmark.cpp
 
-    # src
-    benchmark/src/main.cpp
-
-    # src/mbgl/benchmark
-    benchmark/src/mbgl/benchmark/benchmark.cpp
-    benchmark/src/mbgl/benchmark/stub_geometry_tile_feature.hpp
-
     # util
     benchmark/util/dtoa.benchmark.cpp
+
 )
