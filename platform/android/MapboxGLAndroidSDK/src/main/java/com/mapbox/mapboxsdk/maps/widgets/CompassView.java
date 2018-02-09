6afff00ed41e0a143b5eacc2a71a9bd70878da5c
Pablo Guardiola <guardiola31337@gmail.com>
2018-02-09 16:46:41 +0100

    [android] remove unnecessary @jar and @aar dependencies suffixes (#11161)
    * [android] remove unnecessary @jar and @aar dependencies suffixes
    
    * [android] remove lint baseline file from mapbox gl android sdk module and fix lint errors

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/widgets/CompassView.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/widgets/CompassView.java
@@ -6,10 +6,10 @@ import android.support.annotation.NonNull;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorCompat;
 import android.support.v4.view.ViewPropertyAnimatorListenerAdapter;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 
@@ -22,7 +22,7 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
  * use {@link com.mapbox.mapboxsdk.maps.UiSettings}.
  * </p>
  */
-public final class CompassView extends ImageView implements Runnable {
+public final class CompassView extends AppCompatImageView implements Runnable {
 
   public static final long TIME_WAIT_IDLE = 500;
   public static final long TIME_MAP_NORTH_ANIMATION = 150;
