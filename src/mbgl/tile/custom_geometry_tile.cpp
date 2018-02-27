777dd63a08f17832cc340e30f8629a3a7a000af8
Lucas Wojciechowski <lucas@mapbox.com>
2018-02-27 10:42:15 -0800

    Ensure the map zoom is passed into filtering operations wherever applicable

--- src/mbgl/tile/custom_geometry_tile.cpp
+++ src/mbgl/tile/custom_geometry_tile.cpp
@@ -79,7 +79,7 @@ void CustomGeometryTile::querySourceFeatures(
             auto feature = layer->getFeature(i);
             
             // Apply filter, if any
-            if (queryOptions.filter && !(*queryOptions.filter)(style::expression::EvaluationContext { feature.get() })) {
+            if (queryOptions.filter && !(*queryOptions.filter)(style::expression::EvaluationContext { static_cast<float>(id.overscaledZ), feature.get() })) {
                 continue;
             }
 
