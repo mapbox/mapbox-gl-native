c468df72078dcc50ee39ce9b61634a9885241966
Cameron Mace <cameron@mapbox.com>
2018-02-28 10:01:59 -0500

    use telem util in test

--- platform/android/MapboxGLAndroidSDK/src/test/java/com/mapbox/mapboxsdk/telemetry/HttpTransportTest.java
+++ platform/android/MapboxGLAndroidSDK/src/test/java/com/mapbox/mapboxsdk/telemetry/HttpTransportTest.java
@@ -2,8 +2,7 @@ package com.mapbox.mapboxsdk.telemetry;
 
 import org.junit.Test;
 
-import okhttp3.internal.Util;
-
+import static com.mapbox.services.android.telemetry.utils.TelemetryUtils.toHumanReadableAscii;
 import static junit.framework.Assert.assertEquals;
 
 public class HttpTransportTest {
@@ -15,6 +14,6 @@ public class HttpTransportTest {
     final String asciiVersion = "Sveriges Fj?ll/1.0/1 MapboxEventsAndroid/4.0.0-SNAPSHOT";
 
     assertEquals("asciiVersion and swedishUserAgent should match", asciiVersion,
-      Util.toHumanReadableAscii(swedishUserAgent));
+      toHumanReadableAscii(swedishUserAgent));
   }
 }
