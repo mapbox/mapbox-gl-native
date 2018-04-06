038e5cbcdc12c7b5ceaaf5139085bd2d050fd536
Fabian Guerra <fabian.guerra@mapbox.com>
2018-04-06 18:13:05 -0400

    [ios, macos] Add Expression filter support.

--- platform/darwin/src/NSComparisonPredicate+MGLAdditions.mm
+++ platform/darwin/src/NSComparisonPredicate+MGLAdditions.mm
@@ -1,218 +1,36 @@
 #import "NSComparisonPredicate+MGLAdditions.h"
 
+#import "MGLStyleValue_Private.h"
+
 #import "NSPredicate+MGLAdditions.h"
 #import "NSExpression+MGLPrivateAdditions.h"
 
 @implementation NSComparisonPredicate (MGLAdditions)
 
 - (mbgl::style::Filter)mgl_filter {
-    NSExpression *leftExpression = self.leftExpression;
-    NSExpression *rightExpression = self.rightExpression;
-    NSExpressionType leftType = leftExpression.expressionType;
-    NSExpressionType rightType = rightExpression.expressionType;
-    BOOL isReversed = ((leftType == NSConstantValueExpressionType || leftType == NSAggregateExpressionType)
-                       && rightType == NSKeyPathExpressionType);
+
     switch (self.predicateOperatorType) {
-        case NSEqualToPredicateOperatorType: {
-            mbgl::style::EqualsFilter eqFilter;
-            eqFilter.key = self.mgl_keyPath.UTF8String;
-            eqFilter.value = self.mgl_constantValue;
-            
-            // Convert $type == to TypeEqualsFilter.
-            if (eqFilter.key == "$type") {
-                mbgl::style::TypeEqualsFilter typeEqFilter;
-                typeEqFilter.value = self.mgl_featureType;
-                return typeEqFilter;
-            }
-            
-            // Convert $id == to IdentifierEqualsFilter.
-            if (eqFilter.key == "$id") {
-                // Convert $id == nil to NotHasIdentifierFilter.
-                if (eqFilter.value.is<mbgl::NullValue>()) {
-                    return mbgl::style::NotHasIdentifierFilter();
-                }
-                
-                mbgl::style::IdentifierEqualsFilter idEqFilter;
-                idEqFilter.value = self.mgl_featureIdentifier;
-                return idEqFilter;
-            }
-
-            // Convert == nil to NotHasFilter.
-            if (eqFilter.value.is<mbgl::NullValue>()) {
-                mbgl::style::NotHasFilter notHasFilter;
-                notHasFilter.key = eqFilter.key;
-                return notHasFilter;
-            }
-
-            return eqFilter;
-        }
-        case NSNotEqualToPredicateOperatorType: {
-            mbgl::style::NotEqualsFilter neFilter;
-            neFilter.key = self.mgl_keyPath.UTF8String;
-            neFilter.value = self.mgl_constantValue;
-            
-            // Convert $type != to TypeNotEqualsFilter.
-            if (neFilter.key == "$type") {
-                mbgl::style::TypeNotEqualsFilter typeNeFilter;
-                typeNeFilter.value = self.mgl_featureType;
-                return typeNeFilter;
-            }
-            
-            // Convert $id != to IdentifierNotEqualsFilter.
-            if (neFilter.key == "$id") {
-                // Convert $id != nil to HasIdentifierFilter.
-                if (neFilter.value.is<mbgl::NullValue>()) {
-                    return mbgl::style::HasIdentifierFilter();
-                }
-                
-                mbgl::style::IdentifierNotEqualsFilter idNeFilter;
-                idNeFilter.value = self.mgl_featureIdentifier;
-                return idNeFilter;
-            }
-
-            // Convert != nil to HasFilter.
-            if (neFilter.value.is<mbgl::NullValue>()) {
-                mbgl::style::HasFilter hasFilter;
-                hasFilter.key = neFilter.key;
-                return hasFilter;
-            }
-
-            return neFilter;
-        }
-        case NSGreaterThanPredicateOperatorType: {
-            if (isReversed) {
-                mbgl::style::LessThanFilter ltFilter;
-                ltFilter.key = self.mgl_keyPath.UTF8String;
-                ltFilter.value = self.mgl_constantValue;
-                return ltFilter;
-            } else {
-                mbgl::style::GreaterThanFilter gtFilter;
-                gtFilter.key = self.mgl_keyPath.UTF8String;
-                gtFilter.value = self.mgl_constantValue;
-                return gtFilter;
-            }
-        }
-        case NSGreaterThanOrEqualToPredicateOperatorType: {
-            if (isReversed) {
-                mbgl::style::LessThanEqualsFilter lteFilter;
-                lteFilter.key = self.mgl_keyPath.UTF8String;
-                lteFilter.value = self.mgl_constantValue;
-                return lteFilter;
-            } else {
-                mbgl::style::GreaterThanEqualsFilter gteFilter;
-                gteFilter.key = self.mgl_keyPath.UTF8String;
-                gteFilter.value = self.mgl_constantValue;
-                return gteFilter;
-            }
-        }
-        case NSLessThanPredicateOperatorType: {
-            if (isReversed) {
-                mbgl::style::GreaterThanFilter gtFilter;
-                gtFilter.key = self.mgl_keyPath.UTF8String;
-                gtFilter.value = self.mgl_constantValue;
-                return gtFilter;
-            } else {
-                mbgl::style::LessThanFilter ltFilter;
-                ltFilter.key = self.mgl_keyPath.UTF8String;
-                ltFilter.value = self.mgl_constantValue;
-                return ltFilter;
-            }
-        }
-        case NSLessThanOrEqualToPredicateOperatorType: {
-            if (isReversed) {
-                mbgl::style::GreaterThanEqualsFilter gteFilter;
-                gteFilter.key = self.mgl_keyPath.UTF8String;
-                gteFilter.value = self.mgl_constantValue;
-                return gteFilter;
-            } else {
-                mbgl::style::LessThanEqualsFilter lteFilter;
-                lteFilter.key = self.mgl_keyPath.UTF8String;
-                lteFilter.value = self.mgl_constantValue;
-                return lteFilter;
-            }
-        }
-        case NSInPredicateOperatorType: {
-            if (isReversed) {
-                if (leftType == NSConstantValueExpressionType && [leftExpression.constantValue isKindOfClass:[NSString class]]) {
-                    [NSException raise:NSInvalidArgumentException
-                                format:@"CONTAINS not supported for string comparison."];
-                }
-                [NSException raise:NSInvalidArgumentException
-                            format:@"Predicate cannot compare values IN attribute."];
-            }
-            
-            // Convert $type IN to TypeInFilter.
-            if ([leftExpression.keyPath isEqualToString:@"$type"]) {
-                mbgl::style::TypeInFilter typeInFilter;
-                typeInFilter.values = rightExpression.mgl_aggregateFeatureType;
-                return typeInFilter;
-            }
-            
-            // Convert $id IN to IdentifierInFilter.
-            if ([leftExpression.keyPath isEqualToString:@"$id"]) {
-                mbgl::style::IdentifierInFilter idInFilter;
-                idInFilter.values = rightExpression.mgl_aggregateFeatureIdentifier;
-                return idInFilter;
-            }
-            
-            mbgl::style::InFilter inFilter;
-            inFilter.key = leftExpression.keyPath.UTF8String;
-            inFilter.values = rightExpression.mgl_aggregateMBGLValue;
-            return inFilter;
-        }
-        case NSContainsPredicateOperatorType: {
-            if (!isReversed) {
-                if (rightType == NSConstantValueExpressionType && [rightExpression.constantValue isKindOfClass:[NSString class]]) {
-                    [NSException raise:NSInvalidArgumentException
-                                format:@"IN not supported for string comparison."];
-                }
-                [NSException raise:NSInvalidArgumentException
-                            format:@"Predicate cannot compare attribute CONTAINS values."];
-            }
-            
-            // Convert CONTAINS $type to TypeInFilter.
-            if ([rightExpression.keyPath isEqualToString:@"$type"]) {
-                mbgl::style::TypeInFilter typeInFilter;
-                typeInFilter.values = leftExpression.mgl_aggregateFeatureType;
-                return typeInFilter;
-            }
-            
-            // Convert CONTAINS $id to IdentifierInFilter.
-            if ([rightExpression.keyPath isEqualToString:@"$id"]) {
-                mbgl::style::IdentifierInFilter idInFilter;
-                idInFilter.values = leftExpression.mgl_aggregateFeatureIdentifier;
-                return idInFilter;
-            }
-            
-            mbgl::style::InFilter inFilter;
-            inFilter.key = rightExpression.keyPath.UTF8String;
-            inFilter.values = leftExpression.mgl_aggregateMBGLValue;
-            return inFilter;
-        }
+        case NSEqualToPredicateOperatorType:
+        case NSNotEqualToPredicateOperatorType:
+        case NSGreaterThanPredicateOperatorType:
+        case NSGreaterThanOrEqualToPredicateOperatorType:
+        case NSLessThanPredicateOperatorType:
+        case NSLessThanOrEqualToPredicateOperatorType:
+        case NSInPredicateOperatorType:
+        case NSContainsPredicateOperatorType:
         case NSBetweenPredicateOperatorType: {
-            if (isReversed) {
+            mbgl::style::conversion::Error valueError;
+            NSArray *jsonObject = self.mgl_jsonExpressionObject;
+            auto value = mbgl::style::conversion::convert<std::unique_ptr<mbgl::style::expression::Expression>>(mbgl::style::conversion::makeConvertible(jsonObject), valueError, mbgl::style::expression::type::Boolean);
+            mbgl::style::ExpressionFilter filter;
+            if (!value) {
                 [NSException raise:NSInvalidArgumentException
-                            format:@"Predicate cannot compare bounds BETWEEN attribute."];
+                            format:@"Invalid property value: %@", @(valueError.message.c_str())];
+                return {};
             }
-            if (![rightExpression.constantValue isKindOfClass:[NSArray class]]) {
-                [NSException raise:NSInvalidArgumentException
-                            format:@"Right side of BETWEEN predicate must be an array."]; // not NSSet
-            }
-            auto values = rightExpression.mgl_aggregateMBGLValue;
-            if (values.size() != 2) {
-                [NSException raise:NSInvalidArgumentException
-                            format:@"Right side of BETWEEN predicate must have two items."];
-            }
-            mbgl::style::AllFilter allFilter;
-            mbgl::style::GreaterThanEqualsFilter gteFilter;
-            gteFilter.key = leftExpression.keyPath.UTF8String;
-            gteFilter.value = values[0];
-            allFilter.filters.push_back(gteFilter);
-            mbgl::style::LessThanEqualsFilter lteFilter;
-            lteFilter.key = leftExpression.keyPath.UTF8String;
-            lteFilter.value = values[1];
-            allFilter.filters.push_back(lteFilter);
-            return allFilter;
+            filter.expression = std::move(*value);
+            
+            return filter;
         }
         case NSMatchesPredicateOperatorType:
         case NSLikePredicateOperatorType:
@@ -369,17 +187,9 @@
             case NSLessThanOrEqualToPredicateOperatorType:
             case NSGreaterThanPredicateOperatorType:
             case NSGreaterThanOrEqualToPredicateOperatorType: {
-                if ([self.rightExpression.constantValue isKindOfClass:NSNumber.class]) {
-                    NSString *type;
-                    NSNumber *number = (NSNumber *)self.rightExpression.constantValue;
-                    if ((strcmp([number objCType], @encode(char)) == 0) ||
-                        (strcmp([number objCType], @encode(BOOL)) == 0)) {
-                        type = @"boolean";
-                    } else {
-                        type = @"number";
-                    }
-                    leftExpression = @[type, leftExpression];
-                }
+                leftExpression = [self mgl_expressionWithExpressionType:self.leftExpression fallbackExpression:self.rightExpression];
+                rightExpression = [self mgl_expressionWithExpressionType:self.rightExpression fallbackExpression:self.leftExpression];
+                break;
             }
  
             default:
@@ -389,6 +199,28 @@
         return @[op, leftExpression, rightExpression];
     }
     return nil;
+}
+
+- (id)mgl_expressionWithExpressionType:(NSExpression *)expressionElement fallbackExpression:(NSExpression *)fallbackExpression {
+    NSExpression *expression = expressionElement.expressionType == NSConstantValueExpressionType ? expressionElement : fallbackExpression;
+    NSString *type;
+    if (expression.expressionType == NSConstantValueExpressionType && [expression.constantValue isKindOfClass:NSNumber.class]) {
+        NSNumber *number = (NSNumber *)expression.constantValue;
+        if ((strcmp([number objCType], @encode(char)) == 0) ||
+            (strcmp([number objCType], @encode(BOOL)) == 0)) {
+            type = @"boolean";
+        } else {
+            type = @"number";
+        }
+    } else if (expression.expressionType == NSConstantValueExpressionType && [expression.constantValue isKindOfClass:NSString.class]) {
+        type = @"string";
+    }
+    
+    if (type) {
+        return @[type, expressionElement.mgl_jsonExpressionObject];
+    }
+    
+    return expressionElement.mgl_jsonExpressionObject;
 }
 
 @end
--- platform/darwin/src/NSCompoundPredicate+MGLAdditions.mm
+++ platform/darwin/src/NSCompoundPredicate+MGLAdditions.mm
@@ -21,42 +21,8 @@
 - (mbgl::style::Filter)mgl_filter
 {
     switch (self.compoundPredicateType) {
-        case NSNotPredicateType: {
-            NSAssert(self.subpredicates.count <= 1, @"NOT predicate cannot have multiple subpredicates.");
-            NSPredicate *subpredicate = self.subpredicates.firstObject;
-            mbgl::style::Filter subfilter = subpredicate.mgl_filter;
-
-            // Convert NOT(!= nil) to NotHasFilter.
-            if (subfilter.is<mbgl::style::HasFilter>()) {
-                auto hasFilter = subfilter.get<mbgl::style::HasFilter>();
-                return mbgl::style::NotHasFilter { .key = hasFilter.key };
-            }
-
-            // Convert NOT(== nil) to HasFilter.
-            if (subfilter.is<mbgl::style::NotHasFilter>()) {
-                auto hasFilter = subfilter.get<mbgl::style::NotHasFilter>();
-                return mbgl::style::HasFilter { .key = hasFilter.key };
-            }
-
-            // Convert NOT(IN) or NOT(CONTAINS) to NotInFilter.
-            if (subfilter.is<mbgl::style::InFilter>()) {
-                auto inFilter = subfilter.get<mbgl::style::InFilter>();
-                mbgl::style::NotInFilter notInFilter;
-                notInFilter.key = inFilter.key;
-                notInFilter.values = inFilter.values;
-                return notInFilter;
-            }
-
-            // Convert NOT(), NOT(AND), NOT(NOT), NOT(==), etc. into NoneFilter.
-            mbgl::style::NoneFilter noneFilter;
-            if (subfilter.is<mbgl::style::AnyFilter>()) {
-                // Flatten NOT(OR).
-                noneFilter.filters = subfilter.get<mbgl::style::AnyFilter>().filters;
-            } else if (subpredicate) {
-                noneFilter.filters = { subfilter };
-            }
-            return noneFilter;
-        }
+        case NSNotPredicateType:
+        case NSOrPredicateType:
         case NSAndPredicateType: {
             mbgl::style::conversion::Error valueError;
             NSArray *jsonObject = self.mgl_jsonExpressionObject;
@@ -71,19 +37,12 @@
             
             return filter;
         }
-        case NSOrPredicateType: {
-            mbgl::style::AnyFilter filter;
-            filter.filters = self.mgl_subfilters;
-            return filter;
-        }
     }
 
     [NSException raise:@"Compound predicate type not handled"
                 format:@""];
     return {};
 }
-
-//- (mbgl::style::expression::Expression)
 
 @end
 
