5bdf9bdd2b08f1c48fc28d8d66bd4f0862c08a9c
Julian Rex <julian.rex@mapbox.com>
2018-04-05 22:57:27 -0400

    Failing test for #11180

---
+++ platform/ios/Integration Tests/MBShapeSourceTests.m
@@ -0,0 +1,74 @@
+//
+//  MBShapeSourceTests.m
+//  integration
+//
+//  Created by Julian Rex on 4/5/18.
+//  Copyright © 2018 Mapbox. All rights reserved.
+//
+
+#import "MGLMapViewIntegrationTest.h"
+
+@interface MBShapeSourceTests : MGLMapViewIntegrationTest
+@end
+
+@implementation MBShapeSourceTests
+
+- (void)testRepeatedlyChangingShapeSourceToNil {
+
+    NSMutableArray *features = [[NSMutableArray alloc] init];
+
+    for (NSUInteger i = 0; i <= 180; i+=5) {
+        CLLocationCoordinate2D coord[4] = {
+            CLLocationCoordinate2DMake(round(0), round(i)),
+            CLLocationCoordinate2DMake(round(20), round(i)),
+            CLLocationCoordinate2DMake(round(0), round(i / 2 )),
+            CLLocationCoordinate2DMake(round(20), round(i / 2))};
+
+        MGLPolygonFeature *feature = [MGLPolygonFeature polygonWithCoordinates:coord count:4];
+        [features addObject:feature];
+    }
+
+    MGLShapeSource *shapeSource = [[MGLShapeSource alloc] initWithIdentifier:@"source" features:features options:nil];
+    [self.style addSource:shapeSource];
+
+    MGLFillStyleLayer *layer = [[MGLFillStyleLayer alloc] initWithIdentifier:@"layer" source:shapeSource];
+    layer.fillOpacity = [NSExpression expressionForConstantValue:@0.5];
+    [self.style addLayer:layer];
+
+    XCTestExpectation *expectation = [self expectationWithDescription:@"regionDidChange expectation"];
+
+    __weak typeof(self) weakself = self;
+    __block NSInteger delegateCallCount;
+
+    self.regionDidChange = ^(MGLMapView *mapView, BOOL animated) {
+
+        MBShapeSourceTests *strongSelf = weakself;
+
+        if (!strongSelf)
+            return;
+
+        delegateCallCount++;
+
+        // Setting the shapeSource.shape = nil, was causing an infinite loop, so here
+        // we check for a runaway call. 10 here is arbitrary. We could argue that this
+        // should check that the call count is only 1, however in this case we particularly
+        // want to check for the infinite loop.
+        // See https://github.com/mapbox/mapbox-gl-native/issues/11180
+
+        if (delegateCallCount > 10) {
+            XCTFail();
+        }
+        else {
+            shapeSource.shape = nil;
+        }
+
+        [NSObject cancelPreviousPerformRequestsWithTarget:expectation selector:@selector(fulfill) object:nil];
+        [expectation performSelector:@selector(fulfill) withObject:nil afterDelay:5.0];
+    };
+
+    [self.mapView setCenterCoordinate:CLLocationCoordinate2DMake(10.0, 10.0)];
+    [self waitForExpectations:@[expectation] timeout:30.0];
+}
+
+
+@end
