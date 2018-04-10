2b190157083dd37d48cfc2010035cb63ccf46539
Julian Rex <julian.rex@mapbox.com>
2018-04-10 12:32:13 -0400

    Refactor integrations tests (extracted from branch 11180-nil-shape-source)

---
+++ platform/ios/Integration Tests/MGLMapViewIntegrationTest.h
@@ -0,0 +1,28 @@
+//
+//  MGLMapViewIntegrationTest.h
+//  ios
+//
+//  Created by Julian Rex on 4/5/18.
+//  Copyright © 2018 Mapbox. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <Mapbox/Mapbox.h>
+
+#define TestFailWithSelf(myself, ...) \
+    _XCTPrimitiveFail(myself, __VA_ARGS__)
+
+@interface MGLMapViewIntegrationTest : XCTestCase <MGLMapViewDelegate>
+@property (nonatomic) MGLMapView *mapView;
+@property (nonatomic) MGLStyle *style;
+@property (nonatomic) XCTestExpectation *styleLoadingExpectation;
+@property (nonatomic) XCTestExpectation *renderFinishedExpectation;
+@property (nonatomic) void (^regionDidChange)(MGLMapView *mapView, BOOL animated);
+@property (nonatomic) void (^regionIsChanging)(MGLMapView *mapView);
+
+
+
+// Utility methods
+- (void)waitForMapViewToFinishLoadingStyleWithTimeout:(NSTimeInterval)timeout;
+- (void)waitForMapViewToBeRenderedWithTimeout:(NSTimeInterval)timeout;
+@end
