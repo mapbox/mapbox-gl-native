4242ef68474bc8d84ee0be1403b0efad3a301ea5
Lucas Wojciechowski <lucas@mapbox.com>
2018-04-11 17:36:39 -0700

    Remove filter types other than `ExpressionFilter`

--- include/mbgl/style/filter_evaluator.hpp
+++ include/mbgl/style/filter_evaluator.hpp
@@ -23,30 +23,6 @@ class FilterEvaluator {
 public:
     const expression::EvaluationContext context;
 
-    bool operator()(const NullFilter&) const;
-    bool operator()(const EqualsFilter& filter) const;
-    bool operator()(const NotEqualsFilter& filter) const;
-    bool operator()(const LessThanFilter& filter) const;
-    bool operator()(const LessThanEqualsFilter& filter) const;
-    bool operator()(const GreaterThanFilter& filter) const;
-    bool operator()(const GreaterThanEqualsFilter& filter) const;
-    bool operator()(const InFilter& filter) const;
-    bool operator()(const NotInFilter& filter) const;
-    bool operator()(const AnyFilter& filter) const;
-    bool operator()(const AllFilter& filter) const;
-    bool operator()(const NoneFilter& filter) const;
-    bool operator()(const HasFilter& filter) const;
-    bool operator()(const NotHasFilter& filter) const;
-    bool operator()(const TypeEqualsFilter& filter) const;
-    bool operator()(const TypeNotEqualsFilter& filter) const;
-    bool operator()(const TypeInFilter& filter) const;
-    bool operator()(const TypeNotInFilter& filter) const;
-    bool operator()(const IdentifierEqualsFilter& filter) const;
-    bool operator()(const IdentifierNotEqualsFilter& filter) const;
-    bool operator()(const IdentifierInFilter& filter) const;
-    bool operator()(const IdentifierNotInFilter& filter) const;
-    bool operator()(const HasIdentifierFilter&) const;
-    bool operator()(const NotHasIdentifierFilter&) const;
     bool operator()(const ExpressionFilter&) const;
 
 };
