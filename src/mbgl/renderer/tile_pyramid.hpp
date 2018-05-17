ba07c873aa2c4bd8ef1b185660f6feeb4b12483e
Ansis Brammanis <ansis@mapbox.com>
2018-05-17 14:29:00 -0400

    [core] re-assign ids when lng jumps to avoid flicker

--- src/mbgl/renderer/tile_pyramid.hpp
+++ src/mbgl/renderer/tile_pyramid.hpp
@@ -49,6 +49,8 @@ public:
     std::vector<std::reference_wrapper<RenderTile>> getRenderTiles();
     Tile* getTile(const OverscaledTileID&);
 
+    void handleWrapJump(float lng);
+
     std::unordered_map<std::string, std::vector<Feature>>
     queryRenderedFeatures(const ScreenLineString& geometry,
                           const TransformState& transformState,
@@ -72,6 +74,8 @@ public:
     std::vector<RenderTile> renderTiles;
 
     TileObserver* observer = nullptr;
+
+    float prevLng = 0;
 };
 
 } // namespace mbgl
