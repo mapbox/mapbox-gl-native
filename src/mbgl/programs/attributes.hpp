b802cc141caa950f56fe928e5ca57aa3a80d93bd
Konstantin Käfer <mail@kkaefer.com>
2018-05-31 15:15:12 +0200

    [core] rename gl::Attribute to gl::Attribute to remove overloaded naming

--- src/mbgl/programs/attributes.hpp
+++ src/mbgl/programs/attributes.hpp
@@ -35,116 +35,116 @@ MBGL_DEFINE_ATTRIBUTE(uint8_t, 2, a_placed);
 template <typename T, std::size_t N>
 struct a_data {
     static auto name() { return "a_data"; }
-    using Type = gl::Attribute<T, N>;
+    using Type = gl::AttributeType<T, N>;
 };
 
 struct a_size {
     static auto name() { return "a_size"; }
-    using Type = gl::Attribute<uint16_t, 3>;
+    using Type = gl::AttributeType<uint16_t, 3>;
 };
 
 template <std::size_t N>
 struct a_offset {
     static auto name() { return "a_offset"; }
-    using Type = gl::Attribute<int16_t, N>;
+    using Type = gl::AttributeType<int16_t, N>;
 };
 
 // Paint attributes
 
 struct a_color {
     static auto name() { return "a_color"; }
-    using Type = gl::Attribute<float, 2>;
+    using Type = gl::AttributeType<float, 2>;
 };
 
 struct a_fill_color {
     static auto name() { return "a_fill_color"; }
-    using Type = gl::Attribute<float, 2>;
+    using Type = gl::AttributeType<float, 2>;
 };
 
 struct a_halo_color {
     static auto name() { return "a_halo_color"; }
-    using Type = gl::Attribute<float, 2>;
+    using Type = gl::AttributeType<float, 2>;
 };
 
 struct a_stroke_color {
     static auto name() { return "a_stroke_color"; }
-    using Type = gl::Attribute<float, 2>;
+    using Type = gl::AttributeType<float, 2>;
 };
 
 struct a_outline_color {
     static auto name() { return "a_outline_color"; }
-    using Type = gl::Attribute<float, 2>;
+    using Type = gl::AttributeType<float, 2>;
 };
 
 struct a_opacity {
     static auto name() { return "a_opacity"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_stroke_opacity {
     static auto name() { return "a_stroke_opacity"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_blur {
     static auto name() { return "a_blur"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_radius {
     static auto name() { return "a_radius"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_width {
     static auto name() { return "a_width"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_floorwidth {
     static auto name() { return "a_floorwidth"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_height {
     static auto name() { return "a_height"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_base {
     static auto name() { return "a_base"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_gapwidth {
     static auto name() { return "a_gapwidth"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_stroke_width {
     static auto name() { return "a_stroke_width"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 template <>
 struct a_offset<1> {
     static auto name() { return "a_offset"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_halo_width {
     static auto name() { return "a_halo_width"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_halo_blur {
     static auto name() { return "a_halo_blur"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 struct a_weight {
     static auto name() { return "a_weight"; }
-    using Type = gl::Attribute<float, 1>;
+    using Type = gl::AttributeType<float, 1>;
 };
 
 } // namespace attributes
