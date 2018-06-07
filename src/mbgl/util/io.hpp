1fbf3f2d48df27d028d76fa4ff2c199da347f52c
Konstantin Käfer <mail@kkaefer.com>
2018-06-07 13:01:38 +0200

    [test] standardize on database file name and move I/O functions to util

--- src/mbgl/util/io.hpp
+++ src/mbgl/util/io.hpp
@@ -9,8 +9,7 @@ namespace mbgl {
 namespace util {
 
 struct IOException : std::runtime_error {
-    IOException(int err, const char* msg) : std::runtime_error(msg), code(err) {
-    }
+    IOException(int err, const std::string& msg);
     const int code = 0;
 };
 
@@ -19,6 +18,7 @@ std::string read_file(const std::string &filename);
 
 optional<std::string> readFile(const std::string &filename);
 void deleteFile(const std::string& filename);
+void copyFile(const std::string& destination, const std::string& source);
 
 } // namespace util
 } // namespace mbgl
