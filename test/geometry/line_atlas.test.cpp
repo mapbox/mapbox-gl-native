e63a76f895bcd75d7cd7c7c8bcf07649c9c9a3f1
Konstantin Käfer <mail@kkaefer.com>
2018-06-11 14:02:15 +0200

    [core] fix undefined behavior in LineAtlas::addDash

---
+++ test/geometry/line_atlas.test.cpp
@@ -0,0 +1,31 @@
+#include <mbgl/test/util.hpp>
+
+#include <mbgl/geometry/line_atlas.hpp>
+
+#include <random>
+
+using namespace mbgl;
+
+TEST(LineAtlas, Random) {
+    std::mt19937 generator(42); // Use the same seed for reproducible tests.
+    std::uniform_int_distribution<size_t> countDistribution(0, 12);
+    std::uniform_int_distribution<size_t> capStyleDistribution(0, 1);
+    std::normal_distribution<float> lengthDistribution(3, 5);
+
+    for (size_t it = 0; it < 100; it++) {
+        LineAtlas atlas{ Size{ 128, 1024 } };
+        std::vector<float> dasharray;
+        dasharray.reserve(8);
+        for (size_t j = 0; j < 100; j++) {
+            dasharray.resize(0);
+            const size_t count = countDistribution(generator);
+            for (size_t i = 0; i < count; i++) {
+                dasharray.push_back(lengthDistribution(generator));
+            }
+            const LinePatternCap patternCap =
+                capStyleDistribution(generator) > 0 ? LinePatternCap::Round : LinePatternCap::Square;
+
+            atlas.addDash(dasharray, patternCap);
+        }
+    }
+}
