3c273bc176669f86dfce0c838e93b8895fd07c54
Bobby Sudekum <bobby@mapbox.com>
2018-06-27 11:08:37 -0700

    use rad

--- src/mbgl/map/map.cpp
+++ src/mbgl/map/map.cpp
@@ -439,7 +439,8 @@ CameraOptions Map::cameraForLatLngs(const std::vector<LatLng>& latLngs, const Ed
         transform.setAngle(angle);
     }
     if (pitch) {
-        transform.setPitch(*pitch);
+        double pichAsRadian = *pitch * util::DEG2RAD; // Convert to radians
+        transform.setPitch(pichAsRadian);
     }
     
     CameraOptions options = mbgl::cameraForLatLngs(latLngs, transform, padding);
