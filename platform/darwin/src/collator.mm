66f1d183c88133d1cad6bb6ef23b1c3e44bf0590
Chris Loer <chris.loer@gmail.com>
2018-06-28 12:57:24 -0700

    [ios, macos] Try to make darwin "resolvedLocale" BCP 47 compliant

--- platform/darwin/src/collator.mm
+++ platform/darwin/src/collator.mm
@@ -11,11 +11,11 @@ namespace expression {
 class Collator::Impl {
 public:
     Impl(bool caseSensitive, bool diacriticSensitive, optional<std::string> locale_)
-    : options((caseSensitive ? 0 : NSCaseInsensitiveSearch) |
-              (diacriticSensitive ? 0 : NSDiacriticInsensitiveSearch))
-    , locale(locale_ ?
-                [[NSLocale alloc] initWithLocaleIdentifier:@((*locale_).c_str())] :
-                [NSLocale currentLocale])
+        : options((caseSensitive ? 0 : NSCaseInsensitiveSearch) |
+                  (diacriticSensitive ? 0 : NSDiacriticInsensitiveSearch))
+        , locale(locale_ ?
+                    [[NSLocale alloc] initWithLocaleIdentifier:@((*locale_).c_str())] :
+                    [NSLocale currentLocale])
     {}
 
     bool operator==(const Impl& other) const {
@@ -35,7 +35,13 @@ public:
     }
 
     std::string resolvedLocale() const {
-        return [locale localeIdentifier].UTF8String;
+        // Following documentation at:
+        // https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPInternational/LanguageandLocaleIDs/LanguageandLocaleIDs.html#//apple_ref/doc/uid/10000171i-CH15-SW9
+        // We expect NSLocale to accept BCP 47 tags as localeIdentifier inputs, but the output format
+        // may append the region tag with an "_". Changing that to a "-" makes the identifier BCP 47 compliant.
+        // Experimentally, "zh-Hans-HK" and "zh-Hans_HK" both round trip -- if the second is used by
+        //  `currentLocale`, we don't want to return the underscore.
+        return [[locale localeIdentifier] stringByReplacingOccurrencesOfString:@"_" withString:@"-"].UTF8String;
     }
 private:
     NSStringCompareOptions options;
