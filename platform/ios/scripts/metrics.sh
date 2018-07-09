37e680c66b99b28d9fae9e15a1690a94760cb3a9
Jason Wray <jason@mapbox.com>
2018-07-09 12:05:21 -0400

    [ios, build] Strip symbols before recording size metrics

--- platform/ios/scripts/metrics.sh
+++ platform/ios/scripts/metrics.sh
@@ -4,7 +4,8 @@ set -e
 set -o pipefail
 
 # Generate stripped versions for every architecture
-xcrun bitcode_strip build/ios/pkg/dynamic/Mapbox.framework/Mapbox -r -o  build/ios/pkg/dynamic/Mapbox-stripped
+xcrun bitcode_strip build/ios/pkg/dynamic/Mapbox.framework/Mapbox -r -o build/ios/pkg/dynamic/Mapbox-stripped
+strip -Sx build/ios/pkg/dynamic/Mapbox-stripped
 lipo build/ios/pkg/dynamic/Mapbox-stripped -extract armv7 -output build/ios/pkg/dynamic/Mapbox-stripped-armv7
 lipo build/ios/pkg/dynamic/Mapbox-stripped -extract arm64 -output build/ios/pkg/dynamic/Mapbox-stripped-arm64
 lipo build/ios/pkg/dynamic/Mapbox-stripped -extract x86_64 -output build/ios/pkg/dynamic/Mapbox-stripped-x86_64
