8ca419fb2ebbba427f2f27ba3d794fb3d6cc5797
John Firebaugh <john.firebaugh@gmail.com>
2018-07-17 15:33:50 -0700

    [core] Evict unused font stacks from GlyphManager

--- include/mbgl/util/font_stack.hpp
+++ include/mbgl/util/font_stack.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <mbgl/util/immutable.hpp>
+#include <mbgl/style/layer.hpp>
+
 #include <string>
 #include <vector>
+#include <set>
 
 namespace mbgl {
 
@@ -13,5 +17,8 @@ std::string fontStackToString(const FontStack&);
 struct FontStackHash {
     std::size_t operator()(const FontStack&) const;
 };
+
+// Statically evaluate layer properties to determine what font stacks are used.
+std::set<FontStack> fontStacks(const std::vector<Immutable<style::Layer::Impl>>&);
 
 } // namespace mbgl
