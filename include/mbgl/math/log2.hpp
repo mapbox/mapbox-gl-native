fc0745a699a0ef60c080e9c2ff5eaecde9cb5306
Sudarsana Babu Nagineni <sudarsana.babu@mapbox.com>
2018-08-09 17:13:05 +0300

    [core] Fix build when building Qt Location plugin for Android
    - log2 is not available on Android before API 18.
    - Android doesn't have 'round' on the std:: namespace when using g++.
    
    Co-authored-by: Thiago Marcos P. Santos <thiago@mapbox.com>

--- include/mbgl/math/log2.hpp
+++ include/mbgl/math/log2.hpp
@@ -2,6 +2,11 @@
 
 #include <cmath>
 #include <cstdint>
+#include <type_traits>
+
+#if defined(__ANDROID__)
+#include <android/api-level.h>
+#endif
 
 namespace mbgl {
 namespace util {
@@ -12,3 +17,14 @@ uint32_t ceil_log2(uint64_t x);
 
 } // namespace util
 } // namespace mbgl
+
+// log2 is not available on Android before API 18.
+#if defined(__ANDROID__) && defined(__GNUC__) && \
+    defined(__ANDROID_API__) && __ANDROID_API__ < 18
+
+template <typename T>
+typename std::enable_if_t<std::is_floating_point<T>::value, T> log2(T x) {
+    return ::log(x) / M_LN2;
+}
+
+#endif
