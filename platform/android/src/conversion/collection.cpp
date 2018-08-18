14698650151d7d95d64c426f7e055f94f9cb2d40
John Firebaugh <john.firebaugh@gmail.com>
2018-08-17 17:04:20 -0700

    [android] Remove unused

--- platform/android/src/conversion/collection.cpp
+++ platform/android/src/conversion/collection.cpp
@@ -5,20 +5,6 @@ namespace mbgl {
 namespace android {
 namespace conversion {
 
-std::vector<std::string> toVector(JNIEnv& env, jni::jarray<jni::jobject>& array) {
-    std::vector<std::string> vector;
-    std::size_t len = jni::GetArrayLength(env, array);
-    vector.reserve(len);
-
-    for (std::size_t i = 0; i < len; i++) {
-        jni::jstring* jstr = reinterpret_cast<jni::jstring*>(jni::GetObjectArrayElement(env, array, i));
-        vector.push_back(*convert<std::string, jni::String>(env, jni::String(jstr)));
-        jni::DeleteLocalRef(env, jstr);
-    }
-
-    return vector;
-}
-
 std::vector<std::string> toVector(JNIEnv& env, jni::Array<jni::String> array) {
     std::size_t len = array.Length(env);
     std::vector<std::string> vector;
