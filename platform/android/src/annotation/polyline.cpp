5911e3b13f2f2b6741e26db3e41513ed21cc95b4
John Firebaugh <john.firebaugh@gmail.com>
2018-08-22 11:44:07 -0700

    [android] Simplify annotation conversion

--- platform/android/src/annotation/polyline.cpp
+++ platform/android/src/annotation/polyline.cpp
@@ -6,38 +6,21 @@ namespace mbgl {
 namespace android {
 
 mbgl::LineAnnotation Polyline::toAnnotation(jni::JNIEnv& env, jni::Object<Polyline> polyline) {
-    auto points = jni::SeizeLocal(env, Polyline::getPoints(env, polyline));
+    static auto javaClass = jni::Class<Polyline>::Singleton(env);
+    static auto points = javaClass.GetField<jni::Object<java::util::List>>(env, "points");
+    static auto alpha = javaClass.GetField<float>(env, "alpha");
+    static auto color = javaClass.GetField<int>(env, "color");
+    static auto width = javaClass.GetField<float>(env, "width");
 
-    mbgl::LineAnnotation annotation { MultiPoint::toGeometry<mbgl::LineString<double>>(env, *points) };
-    annotation.opacity = { Polyline::getOpacity(env, polyline) };
-    annotation.color = { Polyline::getColor(env, polyline) };
-    annotation.width = { Polyline::getWidth(env, polyline) };
+    mbgl::LineAnnotation annotation {
+        MultiPoint::toGeometry<mbgl::LineString<double>>(env, *jni::SeizeLocal(env, polyline.Get(env, points)))
+    };
+
+    annotation.opacity = polyline.Get(env, alpha);
+    annotation.color = *conversion::convert<mbgl::Color>(env, polyline.Get(env, color));
+    annotation.width = polyline.Get(env, width);
 
     return annotation;
-}
-
-jni::Object<java::util::List> Polyline::getPoints(jni::JNIEnv& env, jni::Object<Polyline> polyline) {
-    static auto javaClass = jni::Class<Polyline>::Singleton(env);
-    static auto field = javaClass.GetField<jni::Object<java::util::List>>(env, "points");
-    return polyline.Get(env, field);
-}
-
-float Polyline::getOpacity(jni::JNIEnv& env, jni::Object<Polyline> polyline) {
-    static auto javaClass = jni::Class<Polyline>::Singleton(env);
-    static auto field = javaClass.GetField<float>(env, "alpha");
-    return polyline.Get(env, field);
-}
-
-mbgl::Color Polyline::getColor(jni::JNIEnv& env, jni::Object<Polyline> polyline) {
-    static auto javaClass = jni::Class<Polyline>::Singleton(env);
-    static auto field = javaClass.GetField<int>(env, "color");
-    return *conversion::convert<mbgl::Color, int>(env, polyline.Get(env, field));
-}
-
-float Polyline::getWidth(jni::JNIEnv& env, jni::Object<Polyline> polyline) {
-    static auto javaClass = jni::Class<Polyline>::Singleton(env);
-    static auto field = javaClass.GetField<float>(env, "width");
-    return polyline.Get(env, field);
 }
 
 void Polyline::registerNative(jni::JNIEnv& env) {
