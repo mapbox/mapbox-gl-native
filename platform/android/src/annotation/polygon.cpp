70a204924552463eaeca975a5dd28c00b21aa53d
John Firebaugh <john.firebaugh@gmail.com>
2018-08-22 11:44:07 -0700

    [android] Simplify annotation conversion

--- platform/android/src/annotation/polygon.cpp
+++ platform/android/src/annotation/polygon.cpp
@@ -6,12 +6,21 @@ namespace mbgl {
 namespace android {
 
 mbgl::FillAnnotation Polygon::toAnnotation(jni::JNIEnv& env, jni::Object<Polygon> polygon) {
-    auto points = jni::SeizeLocal(env, Polygon::getPoints(env, polygon));
-    auto holes = jni::SeizeLocal(env, Polygon::getHoles(env, polygon));
+    static auto javaClass = jni::Class<Polygon>::Singleton(env);
+    static auto points = javaClass.GetField<jni::Object<java::util::List>>(env, "points");
+    static auto holes = javaClass.GetField<jni::Object<java::util::List>>(env, "holes");
+    static auto alpha = javaClass.GetField<float>(env, "alpha");
+    static auto fillColor = javaClass.GetField<int>(env, "fillColor");
+    static auto strokeColor = javaClass.GetField<int>(env, "strokeColor");
 
-    mbgl::Polygon<double> geometry { MultiPoint::toGeometry<mbgl::LinearRing<double>>(env, *points) };
+    mbgl::Polygon<double> geometry {
+        MultiPoint::toGeometry<mbgl::LinearRing<double>>(env, *jni::SeizeLocal(env, polygon.Get(env, points)))
+    };
 
-    auto jHoleListsArray = jni::SeizeLocal(env, java::util::List::toArray<java::util::List>(env, *holes));
+    auto jHoleListsArray = jni::SeizeLocal(env,
+        java::util::List::toArray<java::util::List>(env,
+            *jni::SeizeLocal(env, polygon.Get(env, holes))));
+
     std::size_t jHoleListsSize = jHoleListsArray->Length(env);
     for (std::size_t i = 0; i < jHoleListsSize; i++) {
         geometry.push_back(MultiPoint::toGeometry<mbgl::LinearRing<double>>(env,
@@ -19,41 +28,11 @@ mbgl::FillAnnotation Polygon::toAnnotation(jni::JNIEnv& env, jni::Object<Polygon
     }
 
     mbgl::FillAnnotation annotation { geometry };
-    annotation.opacity = { Polygon::getOpacity(env, polygon) };
-    annotation.color = { Polygon::getFillColor(env, polygon) };
-    annotation.outlineColor = { Polygon::getOutlineColor(env, polygon) };
+    annotation.opacity = polygon.Get(env, alpha);
+    annotation.color = *conversion::convert<mbgl::Color>(env, polygon.Get(env, fillColor));
+    annotation.outlineColor = *conversion::convert<mbgl::Color>(env, polygon.Get(env, strokeColor));
 
     return annotation;
-}
-
-jni::Object<java::util::List> Polygon::getPoints(jni::JNIEnv& env, jni::Object<Polygon> polygon) {
-    static auto javaClass = jni::Class<Polygon>::Singleton(env);
-    static auto field = javaClass.GetField<jni::Object<java::util::List>>(env, "points");
-    return polygon.Get(env, field);
-}
-
-jni::Object<java::util::List> Polygon::getHoles(jni::JNIEnv& env, jni::Object<Polygon> polygon) {
-    static auto javaClass = jni::Class<Polygon>::Singleton(env);
-    static auto field = javaClass.GetField<jni::Object<java::util::List>>(env, "holes");
-    return polygon.Get(env, field);
-}
-
-float Polygon::getOpacity(jni::JNIEnv& env, jni::Object<Polygon> polygon) {
-    static auto javaClass = jni::Class<Polygon>::Singleton(env);
-    static auto field = javaClass.GetField<float>(env, "alpha");
-    return polygon.Get(env, field);
-}
-
-mbgl::Color Polygon::getFillColor(jni::JNIEnv& env, jni::Object<Polygon> polygon) {
-    static auto javaClass = jni::Class<Polygon>::Singleton(env);
-    static auto field = javaClass.GetField<int>(env, "fillColor");
-    return *conversion::convert<mbgl::Color, int>(env, polygon.Get(env, field));
-}
-
-mbgl::Color Polygon::getOutlineColor(jni::JNIEnv& env, jni::Object<Polygon> polygon) {
-    static auto javaClass = jni::Class<Polygon>::Singleton(env);
-    static auto field = javaClass.GetField<int>(env, "strokeColor");
-    return *conversion::convert<mbgl::Color, int>(env, polygon.Get(env, field));
 }
 
 void Polygon::registerNative(jni::JNIEnv& env) {
