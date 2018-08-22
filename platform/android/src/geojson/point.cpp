ffda3694d465056d107a3eebca909eabd7e5454f
John Firebaugh <john.firebaugh@gmail.com>
2018-08-22 10:48:21 -0700

    [android] Simplify Point conversion

--- platform/android/src/geojson/point.cpp
+++ platform/android/src/geojson/point.cpp
@@ -1,7 +1,4 @@
 #include "point.hpp"
-#include "../java/util.hpp"
-#include "../java_types.hpp"
-#include "../style/value.hpp"
 
 namespace mbgl {
 namespace android {
@@ -13,37 +10,19 @@ jni::Object<Point> Point::New(jni::JNIEnv& env, const mbgl::Point<double>& point
     return javaClass.Call(env, method, point.x, point.y);
 }
 
-mapbox::geojson::point Point::convert(jni::JNIEnv &env, jni::Object<Point> jPoint) {
-    mapbox::geojson::point point;
-
-    if (jPoint) {
-        point = Point::convert(env, *jni::SeizeLocal(env, Point::coordinates(env, jPoint)));
-    }
-
-    return point;
-}
-
-mapbox::geojson::point Point::convert(jni::JNIEnv &env, jni::Object<java::util::List/*<Double>*/> jDoubleList) {
-    mapbox::geojson::point point;
-
-    if (jDoubleList) {
-        auto jDoubleArray = jni::SeizeLocal(env, java::util::List::toArray<jobject>(env, jDoubleList));
-
-        auto lonObject = jni::SeizeLocal(env, jDoubleArray->Get(env, 0));
-        auto latObject = jni::SeizeLocal(env, jDoubleArray->Get(env, 1));
-
-        point.x = jni::CallMethod<jni::jdouble>(env, *lonObject,
-                                                *java::Number::doubleValueMethodId);
-        point.y = jni::CallMethod<jni::jdouble>(env, *latObject,
-                                                *java::Number::doubleValueMethodId);
-    }
-    return point;
-}
-
-jni::Object<java::util::List> Point::coordinates(jni::JNIEnv &env, jni::Object<Point> jPoint) {
+mbgl::Point<double> Point::convert(jni::JNIEnv &env, jni::Object<Point> jPoint) {
     static auto javaClass = jni::Class<Point>::Singleton(env);
-    static auto method = javaClass.GetMethod<jni::Object<java::util::List> ()>(env, "coordinates");
-    return jPoint.Call(env, method);
+    static auto longitude = javaClass.GetMethod<jni::jdouble ()>(env, "longitude");
+    static auto latitude = javaClass.GetMethod<jni::jdouble ()>(env, "latitude");
+
+    if (!jPoint) {
+        return {};
+    }
+
+    return {
+        jPoint.Call(env, longitude),
+        jPoint.Call(env, latitude)
+    };
 }
 
 void Point::registerNative(jni::JNIEnv &env) {
--- platform/android/src/geojson/point.hpp
+++ platform/android/src/geojson/point.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <mbgl/util/geojson.hpp>
-#include <mbgl/util/noncopyable.hpp>
+#include <mbgl/util/geometry.hpp>
 
-#include "../java/util.hpp"
 #include "geometry.hpp"
 
 #include <jni/jni.hpp>
@@ -15,16 +13,10 @@ namespace geojson {
 class Point : public Geometry {
 public:
     static constexpr auto Name() { return "com/mapbox/geojson/Point"; };
-
     static constexpr auto Type() { return "Point"; };
 
     static jni::Object<Point> New(jni::JNIEnv&, const mbgl::Point<double>&);
-
-    static mapbox::geojson::point convert(jni::JNIEnv&, jni::Object<Point>);
-
-    static mapbox::geojson::point convert(jni::JNIEnv&, jni::Object<java::util::List/*<Double>*/>);
-
-    static jni::Object<java::util::List> coordinates(JNIEnv&, jni::Object<Point>);
+    static mbgl::Point<double> convert(jni::JNIEnv&, jni::Object<Point>);
 
     static void registerNative(jni::JNIEnv&);
 };
