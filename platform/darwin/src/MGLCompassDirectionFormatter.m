ff9472f55eaca04f71518e4ddf58b13296bf3a5c
jmkiley <jordan.kiley@mapbox.com>
2018-09-11 18:25:32 -0700

    [ios, macos] started to restore iOS 8 support

--- platform/darwin/src/MGLCompassDirectionFormatter.m
+++ platform/darwin/src/MGLCompassDirectionFormatter.m
@@ -15,8 +15,8 @@
 }
 
 - (NSString *)stringFromDirection:(CLLocationDirection)direction {
-    static NSArray<NSString *> *shortStrings;
-    static NSArray<NSString *> *longStrings;
+    static NS_ARRAY_OF(NSString *) *shortStrings;
+    static NS_ARRAY_OF(NSString *) *longStrings;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         shortStrings = @[
