2f63b7e6e3fe13d9acb36afa0fd07b9b6802ba6d
Chris Loer <chris.loer@gmail.com>
2018-09-19 15:20:03 -0700

    [core] Bidi support for styled text. Remove use of QString from non-ICU Qt stub bidi implementation since we weren't making use of it.

--- src/mbgl/text/bidi.hpp
+++ src/mbgl/text/bidi.hpp
@@ -14,17 +14,21 @@ class BiDiImpl;
 
 std::u16string applyArabicShaping(const std::u16string&);
 
+using StyledText = std::pair<std::u16string, std::vector<uint8_t>>;
+    
 class BiDi : private util::noncopyable {
 public:
     BiDi();
     ~BiDi();
 
     std::vector<std::u16string> processText(const std::u16string&, std::set<std::size_t>);
+    std::vector<StyledText> processStyledText(const StyledText&, std::set<std::size_t>);
 
 private:
     void mergeParagraphLineBreaks(std::set<std::size_t>&);
     std::vector<std::u16string> applyLineBreaking(std::set<std::size_t>);
     std::u16string getLine(std::size_t start, std::size_t end);
+    std::u16string writeReverse(const std::u16string&, std::size_t, std::size_t);
 
     std::unique_ptr<BiDiImpl> impl;
 };
