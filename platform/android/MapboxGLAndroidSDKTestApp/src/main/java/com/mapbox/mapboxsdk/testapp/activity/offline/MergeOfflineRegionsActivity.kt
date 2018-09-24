1c6c7d5238c3359ca426a9203f3246f202254dd4
Łukasz Paczos <lukas.paczos@gmail.com>
2018-09-24 12:32:28 +0200

    [android] copy files from assets task util

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/offline/MergeOfflineRegionsActivity.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/offline/MergeOfflineRegionsActivity.kt
@@ -1,10 +1,6 @@
 package com.mapbox.mapboxsdk.testapp.activity.offline
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.mapbox.mapboxsdk.Mapbox
@@ -12,15 +8,15 @@ import com.mapbox.mapboxsdk.constants.Style
 import com.mapbox.mapboxsdk.log.Logger
 import com.mapbox.mapboxsdk.offline.OfflineManager
 import com.mapbox.mapboxsdk.offline.OfflineRegion
+import com.mapbox.mapboxsdk.storage.FileSource
 import com.mapbox.mapboxsdk.testapp.R
+import com.mapbox.mapboxsdk.testapp.utils.FileUtils
 import kotlinx.android.synthetic.main.activity_merge_offline_regions.*
-import java.io.File
-import java.io.FileOutputStream
 
 class MergeOfflineRegionsActivity : AppCompatActivity() {
   companion object {
+    private const val LOG_TAG = "Mbgl-MergeOfflineRegionsActivity"
     private const val TEST_DB_FILE_NAME = "offline.db"
-    private const val PERMISSIONS_REQUEST_CODE = 11
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,34 +30,29 @@ class MergeOfflineRegionsActivity : AppCompatActivity() {
 
     mapView.onCreate(savedInstanceState)
     load_region_btn.setOnClickListener {
-      if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        != PackageManager.PERMISSION_GRANTED) {
-        ActivityCompat.requestPermissions(this,
-          arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
-      } else {
-        mergeDb()
-      }
+      copyAsset()
     }
   }
 
-  override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    if (requestCode == PERMISSIONS_REQUEST_CODE) {
-      for (result in grantResults) {
-        if (result != PackageManager.PERMISSION_GRANTED) {
-          finish()
-        }
+  private fun copyAsset() {
+    // copy db asset to internal memory
+    FileUtils.CopyFileFromAssetsTask(this, object : FileUtils.OnFileCopiedFromAssetsListener {
+      override fun onFileCopiedFromAssets() {
+        mergeDb()
       }
-      mergeDb()
-    }
+
+      override fun onError() {
+        Toast.makeText(
+          this@MergeOfflineRegionsActivity,
+          String.format("Error copying DB file."),
+          Toast.LENGTH_LONG).show()
+      }
+    }).execute(TEST_DB_FILE_NAME, FileSource.getResourcesCachePath(this))
   }
 
   private fun mergeDb() {
-    // copy db asset to internal memory
-    copyAsset()
-
     OfflineManager.getInstance(this).mergeOfflineRegions(
-      this.filesDir.absolutePath + "/" + TEST_DB_FILE_NAME,
+      FileSource.getResourcesCachePath(this) + "/" + TEST_DB_FILE_NAME,
       object : OfflineManager.MergeOfflineRegionsCallback {
         override fun onMerge(offlineRegions: Array<OfflineRegion>) {
           mapView.setStyleUrl(Style.SATELLITE)
@@ -72,24 +63,13 @@ class MergeOfflineRegionsActivity : AppCompatActivity() {
         }
 
         override fun onError(error: String) {
-          Logger.e("MBGL_OFFLINE_DB_MERGE", error)
+          Toast.makeText(
+            this@MergeOfflineRegionsActivity,
+            String.format("Offline DB merge error."),
+            Toast.LENGTH_LONG).show()
+          Logger.e(LOG_TAG, error)
         }
       })
-  }
-
-  private fun copyAsset() {
-    val bufferSize = 1024
-    val assetManager = this.assets
-    val inputStream = assetManager.open(TEST_DB_FILE_NAME)
-    val outputStream = FileOutputStream(File(this.filesDir, TEST_DB_FILE_NAME))
-
-    try {
-      inputStream.copyTo(outputStream, bufferSize)
-    } finally {
-      inputStream.close()
-      outputStream.flush()
-      outputStream.close()
-    }
   }
 
   override fun onStart() {
@@ -120,6 +100,9 @@ class MergeOfflineRegionsActivity : AppCompatActivity() {
   override fun onDestroy() {
     super.onDestroy()
     mapView.onDestroy()
+
+    // restoring connectivity state
+    Mapbox.setConnected(null)
   }
 
   override fun onSaveInstanceState(outState: Bundle) {
