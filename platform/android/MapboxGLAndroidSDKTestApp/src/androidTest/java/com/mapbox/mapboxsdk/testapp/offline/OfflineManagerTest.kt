a008c1efd9f08a65eaf5fa4bda8fd6ea26ed872a
Łukasz Paczos <lukas.paczos@gmail.com>
2018-09-24 12:32:28 +0200

    [android] copy files from assets task util

--- platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/testapp/offline/OfflineManagerTest.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/testapp/offline/OfflineManagerTest.kt
@@ -1,25 +1,22 @@
 package com.mapbox.mapboxsdk.testapp.offline
 
-import android.R
 import android.content.Context
-import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso
 import android.support.test.espresso.IdlingRegistry
 import android.support.test.espresso.UiController
-import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.idling.CountingIdlingResource
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
-import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.runner.AndroidJUnit4
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.offline.OfflineManager
 import com.mapbox.mapboxsdk.offline.OfflineRegion
+import com.mapbox.mapboxsdk.storage.FileSource
 import com.mapbox.mapboxsdk.testapp.action.MapboxMapAction.invoke
 import com.mapbox.mapboxsdk.testapp.activity.BaseActivityTest
 import com.mapbox.mapboxsdk.testapp.activity.espresso.EspressoTestActivity
+import com.mapbox.mapboxsdk.testapp.utils.FileUtils
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.File
-import java.io.FileOutputStream
+import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 class OfflineManagerTest : BaseActivityTest() {
@@ -48,19 +45,26 @@ class OfflineManagerTest : BaseActivityTest() {
 
     invoke(mapboxMap) { _: UiController, _: MapboxMap ->
       offlineIdlingResource.increment()
-      copyAsset(context)
-      OfflineManager.getInstance(context).mergeOfflineRegions(
-        context.filesDir.absolutePath + "/" + TEST_DB_FILE_NAME,
-        object : OfflineManager.MergeOfflineRegionsCallback {
-          override fun onMerge(offlineRegions: Array<out OfflineRegion>?) {
-            assert(offlineRegions?.size == 1)
-            offlineIdlingResource.decrement()
-          }
+      FileUtils.CopyFileFromAssetsTask(rule.activity, object : FileUtils.OnFileCopiedFromAssetsListener {
+        override fun onFileCopiedFromAssets() {
+          OfflineManager.getInstance(context).mergeOfflineRegions(
+            FileSource.getResourcesCachePath(rule.activity) + "/" + TEST_DB_FILE_NAME,
+            object : OfflineManager.MergeOfflineRegionsCallback {
+              override fun onMerge(offlineRegions: Array<out OfflineRegion>?) {
+                assert(offlineRegions?.size == 1)
+                offlineIdlingResource.decrement()
+              }
 
-          override fun onError(error: String?) {
-            throw RuntimeException("Unable to merge external offline database. $error")
-          }
-        })
+              override fun onError(error: String?) {
+                throw RuntimeException("Unable to merge external offline database. $error")
+              }
+            })
+        }
+
+        override fun onError() {
+          throw IOException("Unable to copy DB file.")
+        }
+      }).execute(TEST_DB_FILE_NAME, FileSource.getResourcesCachePath(rule.activity))
     }
 
     invoke(mapboxMap) { _: UiController, _: MapboxMap ->
@@ -94,26 +98,11 @@ class OfflineManagerTest : BaseActivityTest() {
     }
 
     // waiting for offline idling resource
-    onView(withId(R.id.content)).check(matches(isDisplayed()))
+    Espresso.onIdle()
   }
 
   override fun afterTest() {
     super.afterTest()
     IdlingRegistry.getInstance().unregister(offlineIdlingResource)
-  }
-
-  private fun copyAsset(context: Context) {
-    val bufferSize = 1024
-    val assetManager = context.assets
-    val inputStream = assetManager.open(TEST_DB_FILE_NAME)
-    val outputStream = FileOutputStream(File(context.filesDir.absoluteFile, TEST_DB_FILE_NAME))
-
-    try {
-      inputStream.copyTo(outputStream, bufferSize)
-    } finally {
-      inputStream.close()
-      outputStream.flush()
-      outputStream.close()
-    }
   }
 }
---
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/utils/FileUtils.kt
@@ -0,0 +1,65 @@
+package com.mapbox.mapboxsdk.testapp.utils
+
+import android.content.Context
+import android.os.AsyncTask
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.ref.WeakReference
+
+class FileUtils {
+
+  /**
+   * Task that copies a file from the assets directory to a provided directory.
+   * The asset's name is going to be kept in the new directory.
+   */
+  class CopyFileFromAssetsTask(context: Context, listener: OnFileCopiedFromAssetsListener) : AsyncTask<String, Void, Boolean>() {
+    private val contextWeakReference: WeakReference<Context> = WeakReference(context)
+    private val listenerWeakReference: WeakReference<OnFileCopiedFromAssetsListener> = WeakReference(listener)
+
+    override fun doInBackground(vararg strings: String): Boolean? {
+      val assetName = strings[0]
+      val destinationPath = strings[1]
+
+      contextWeakReference.get()?.let {
+        try {
+          copyAsset(it, assetName, destinationPath)
+        } catch (ex: Exception) {
+          return false
+        }
+      }
+
+      return true
+    }
+
+    override fun onCancelled() {
+      listenerWeakReference.get()?.onError()
+    }
+
+    override fun onPostExecute(result: Boolean) {
+      if (result) {
+        listenerWeakReference.get()?.onFileCopiedFromAssets()
+      } else {
+        listenerWeakReference.get()?.onError()
+      }
+    }
+
+    private fun copyAsset(context: Context, assetName: String, destinationPath: String) {
+      val bufferSize = 1024
+      val assetManager = context.assets
+      val inputStream = assetManager.open(assetName)
+      val outputStream = FileOutputStream(File(destinationPath, assetName))
+      try {
+        inputStream.copyTo(outputStream, bufferSize)
+      } finally {
+        inputStream.close()
+        outputStream.flush()
+        outputStream.close()
+      }
+    }
+  }
+
+  interface OnFileCopiedFromAssetsListener {
+    fun onFileCopiedFromAssets()
+    fun onError()
+  }
+}
