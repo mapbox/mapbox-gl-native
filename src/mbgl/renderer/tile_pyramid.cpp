fb75247ccf664f638575feebc3e800b100b836d8
Molly Lloyd <molly@mapbox.com>
2018-09-26 19:47:01 -0700

    [core] don't prefetch AnnotationTiles

--- src/mbgl/renderer/tile_pyramid.cpp
+++ src/mbgl/renderer/tile_pyramid.cpp
@@ -110,7 +110,7 @@ void TilePyramid::update(const std::vector<Immutable<style::Layer::Impl>>& layer
         }
 
         // Only attempt prefetching in continuous mode.
-        if (parameters.mode == MapMode::Continuous && type != style::SourceType::GeoJSON) {
+        if (parameters.mode == MapMode::Continuous && type != style::SourceType::GeoJSON && type != style::SourceType::Annotations) {
             // Request lower zoom level tiles (if configured to do so) in an attempt
             // to show something on the screen faster at the cost of a little of bandwidth.
             if (parameters.prefetchZoomDelta) {
