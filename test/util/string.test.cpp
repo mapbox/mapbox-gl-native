cb2397fd73e51c754d1e339892967bb504da840e
Konstantin Käfer <mail@kkaefer.com>
2018-10-17 15:45:35 +0200

    [core] deduplicate dtoa() from RapidJSON and add tests

---
+++ test/util/string.test.cpp
@@ -0,0 +1,13 @@
+#include <mbgl/test/util.hpp>
+
+#include <mbgl/util/string.hpp>
+
+using namespace mbgl;
+
+TEST(ToString, FloatingPoint) {
+    EXPECT_EQ("0", util::toString(0.0));
+    EXPECT_EQ("1.33", util::toString(1.33));
+    EXPECT_EQ("-1", util::toString(-1.0));
+    EXPECT_EQ("12340000000", util::toString(12340000000.0));
+    EXPECT_EQ("12340000000", util::toString(12340000000.0));
+}
