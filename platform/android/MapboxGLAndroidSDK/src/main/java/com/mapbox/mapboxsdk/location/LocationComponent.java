9b351565ebd223bb20d95a0f4726b1474b92bdd4
Tamás Szincsák <dontomika@gmail.com>
2018-10-22 14:38:30 +0200

    [android] Removed an unused import

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/location/LocationComponent.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/location/LocationComponent.java
@@ -8,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresPermission;
 import android.support.annotation.StyleRes;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.WindowManager;
 
 import com.mapbox.android.core.location.LocationEngine;
