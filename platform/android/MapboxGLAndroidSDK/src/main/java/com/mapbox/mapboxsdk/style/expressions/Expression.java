4b0d51e23f287a5d046d87b1d96ed597ac25fb1a
Łukasz Paczos <lukas.paczos@gmail.com>
2018-10-23 19:37:32 +0200

    [android] renamed format expression options, added format expression example

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/style/expressions/Expression.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/style/expressions/Expression.java
@@ -3246,10 +3246,13 @@ public class Expression {
    *     format(
    *       formatEntry(
    *         get("header_property"),
-   *         fontScale(2.0),
-   *         textFont(new String[] {"DIN Offc Pro Regular", "Arial Unicode MS Regular"})
+   *         formatFontScale(2.0),
+   *         formatTextFont(new String[] {"DIN Offc Pro Regular", "Arial Unicode MS Regular"})
    *       ),
-   *       formatEntry(concat(literal("\n"), get("description_property")), fontScale(1.5)),
+   *       formatEntry(concat(literal("\n"), get("description_property")), formatFontScale(1.5))
+   *     )
+   *   )
+   * );
    * }
    * </pre>
    *
@@ -4375,7 +4378,7 @@ public class Expression {
      * @return format option
      */
     @NonNull
-    public static FormatOption fontScale(@NonNull Expression expression) {
+    public static FormatOption formatFontScale(@NonNull Expression expression) {
       return new FormatOption("font-scale", expression);
     }
 
@@ -4389,7 +4392,7 @@ public class Expression {
      * @return format option
      */
     @NonNull
-    public static FormatOption fontScale(double scale) {
+    public static FormatOption formatFontScale(double scale) {
       return new FormatOption("font-scale", literal(scale));
     }
 
@@ -4405,7 +4408,7 @@ public class Expression {
      * @return format option
      */
     @NonNull
-    public static FormatOption textFont(@NonNull Expression expression) {
+    public static FormatOption formatTextFont(@NonNull Expression expression) {
       return new FormatOption("text-font", expression);
     }
 
@@ -4421,7 +4424,7 @@ public class Expression {
      * @return format option
      */
     @NonNull
-    public static FormatOption textFont(@NonNull String[] fontStack) {
+    public static FormatOption formatTextFont(@NonNull String[] fontStack) {
       return new FormatOption("text-font", literal(fontStack));
     }
   }
