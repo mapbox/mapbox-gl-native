30e3393be22da29ee708def4b1a2ab343423b20e
Tobrun <tobrun.van.nuland@gmail.com>
2018-10-31 10:03:15 +0100

    [android] - optimise imports, fixup small nitpicks

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/offline/OfflineRegionDefinition.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/offline/OfflineRegionDefinition.java
@@ -23,7 +23,6 @@ public interface OfflineRegionDefinition {
    * Returns the map style url of the OfflineRegion.
    *
    * @return The URL of the map style.
-   * @since 7.0.0
    */
   String getStyleURL();
 
@@ -31,7 +30,6 @@ public interface OfflineRegionDefinition {
    * Gets the minimum zoom level the OfflineRegion map can be displayed at.
    *
    * @return The minimum zoom level.
-   * @since 7.0.0
    */
   double getMinZoom();
 
@@ -39,7 +37,6 @@ public interface OfflineRegionDefinition {
    * Gets the maximum zoom level the OfflineRegion map can be displayed at.
    *
    * @return The maximum zoom level.
-   * @since 7.0.0
    */
   double getMaxZoom();
 
@@ -47,7 +44,6 @@ public interface OfflineRegionDefinition {
    * Gets the pixel ratio of the OfflineRegion map.
    *
    * @return The pixel ratio of the OfflineRegion map.
-   * @since 7.0.0
    */
   float getPixelRatio();
 
@@ -55,7 +51,6 @@ public interface OfflineRegionDefinition {
    * Gest the type of the OfflineRegionDefinition for telemetry ("tileregion", "shaperegion").
    *
    * @return The type of the OfflineRegionDefinition.
-   * @since 7.0.0
    */
   String getType();
 
