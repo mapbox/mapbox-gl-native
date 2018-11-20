a662de8bd8a6e4545f20addb61dd33e37ae1be92
Julian Rex <julian.rex@mapbox.com>
2018-11-20 12:25:34 -0500

    Move cast macro into private header.

--- platform/darwin/src/MGLFoundation_Private.h
+++ platform/darwin/src/MGLFoundation_Private.h
@@ -3,3 +3,11 @@
 #include <mbgl/util/run_loop.hpp>
 
 void MGLInitializeRunLoop();
+
+/* Using a compound statement (GNU Extension, supported by clang) */
+#define MGL_OBJC_DYNAMIC_CAST(object, type) \
+    ({ \
+        __typeof__( object ) temp##__LINE__ = (object); \
+        (type *)([temp##__LINE__ isKindOfClass:[type class]] ? temp##__LINE__ : nil); \
+    })
+
