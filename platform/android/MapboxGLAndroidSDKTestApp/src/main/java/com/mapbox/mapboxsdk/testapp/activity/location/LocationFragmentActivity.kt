4bc5595647f709b928bfa23c166261374a209be4
Łukasz Paczos <lukas.paczos@gmail.com>
2018-11-27 12:20:41 +0100

    [android] local db test

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/location/LocationFragmentActivity.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/location/LocationFragmentActivity.kt
@@ -2,24 +2,21 @@ package com.mapbox.mapboxsdk.testapp.activity.location
 
 import android.annotation.SuppressLint
 import android.app.Fragment
-import android.location.Location
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
-import com.mapbox.android.core.location.LocationEngineListener
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.log.Logger
 import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.offline.OfflineManager
+import com.mapbox.mapboxsdk.offline.OfflineRegion
 import com.mapbox.mapboxsdk.testapp.R
-import kotlinx.android.synthetic.main.activity_location_layer_fragment.*
 
 class LocationFragmentActivity : AppCompatActivity() {
   private lateinit var permissionsManager: PermissionsManager
@@ -27,50 +24,36 @@ class LocationFragmentActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_location_layer_fragment)
-
-    fab.setOnClickListener {
-      val fragment = fragmentManager.findFragmentByTag(EmptyFragment.TAG)
-      if (fragment == null) {
-        fragmentManager
-          .beginTransaction()
-          .replace(R.id.container, EmptyFragment.newInstance(), EmptyFragment.TAG)
-          .addToBackStack("transaction2")
-          .commit()
-      } else {
-        this.onBackPressed()
-      }
-    }
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-    if (PermissionsManager.areLocationPermissionsGranted(this)) {
-      if (savedInstanceState == null) {
-        fragmentManager
-          .beginTransaction()
-          .replace(R.id.container, LocationFragment.newInstance(), LocationFragment.TAG)
-          .commit()
-      }
-    } else {
-      permissionsManager = PermissionsManager(object : PermissionsListener {
-        override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
-          Toast.makeText(this@LocationFragmentActivity, "You need to accept location permissions.",
-            Toast.LENGTH_SHORT).show()
-        }
-
-        override fun onPermissionResult(granted: Boolean) {
-          if (granted) {
-            if (savedInstanceState == null) {
-              fragmentManager
-                .beginTransaction()
-                .replace(R.id.container, LocationFragment.newInstance(), LocationFragment.TAG)
-                .commit()
-            }
-          } else {
-            finish()
+    if (savedInstanceState == null) {
+      if (PermissionsManager.areLocationPermissionsGranted(this)) {
+        addLocationFragment()
+      } else {
+        permissionsManager = PermissionsManager(object : PermissionsListener {
+          override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+            Toast.makeText(this@LocationFragmentActivity, "You need to accept location permissions.",
+              Toast.LENGTH_SHORT).show()
           }
-        }
-      })
-      permissionsManager.requestLocationPermissions(this)
+
+          override fun onPermissionResult(granted: Boolean) {
+            if (granted) {
+              addLocationFragment()
+            } else {
+              finish()
+            }
+          }
+        })
+        permissionsManager.requestLocationPermissions(this)
+      }
     }
+  }
+
+  private fun addLocationFragment() {
+    fragmentManager
+      .beginTransaction()
+      .replace(R.id.container, LocationFragment.newInstance(), LocationFragment.FRAGMENT_TAG)
+      .commit()
   }
 
   override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -78,17 +61,19 @@ class LocationFragmentActivity : AppCompatActivity() {
     permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
   }
 
-  class LocationFragment : Fragment(), LocationEngineListener {
+  class LocationFragment : Fragment() {
     companion object {
-      const val TAG = "LFragment"
+      const val FRAGMENT_TAG = "LFragment"
+      private const val LOG_TAG = "Mbgl-LocationFragment"
+      private const val TEST_DB_FILE_PATH = "/storage/emulated/0/Download/local_db.db"
+      private const val TEST_STYLE_FILE_PATH = "file:///storage/emulated/0/Download/local_style.json"
+
       fun newInstance(): LocationFragment {
         return LocationFragment()
       }
     }
 
     private lateinit var mapView: MapView
-    private lateinit var mapboxMap: MapboxMap
-    private var component: LocationComponent? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
       mapView = MapView(inflater.context)
@@ -98,24 +83,42 @@ class LocationFragmentActivity : AppCompatActivity() {
     @SuppressLint("MissingPermission")
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
       mapView.onCreate(savedInstanceState)
-      mapView.getMapAsync {
-        mapboxMap = it
-        component = mapboxMap.locationComponent
-        component?.activateLocationComponent(activity)
-        component?.isLocationComponentEnabled = true
-        component?.locationEngine?.addLocationEngineListener(this)
+
+      mapView.setStyleUrl(TEST_STYLE_FILE_PATH)
+
+      mapView.getMapAsync { mapboxMap ->
+        mapboxMap.locationComponent.activateLocationComponent(activity)
+        mapboxMap.locationComponent.isLocationComponentEnabled = true
+
+        mapboxMap.addOnCameraMoveListener {
+          Logger.d(LOG_TAG, String.format("camera: %s", mapboxMap.cameraPosition))
+        }
+        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(48.11, 11.5), 9.0))
       }
+
+      mergeDb()
     }
 
-    override fun onLocationChanged(location: Location?) {
-      if (location != null) {
-        mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location), 12.0))
-        component?.locationEngine?.removeLocationEngineListener(this)
-      }
-    }
+    private fun mergeDb() {
+      OfflineManager.getInstance(activity).mergeOfflineRegions(TEST_DB_FILE_PATH,
+        object : OfflineManager.MergeOfflineRegionsCallback {
+          override fun onMerge(offlineRegions: Array<OfflineRegion>) {
+            mapView.setStyleUrl(TEST_STYLE_FILE_PATH)
+            Toast.makeText(
+              activity,
+              String.format("Merged %d regions.", offlineRegions.size),
+              Toast.LENGTH_LONG).show()
+            Logger.e(LOG_TAG, "success")
+          }
 
-    override fun onConnected() {
-      // no impl
+          override fun onError(error: String) {
+            Toast.makeText(
+              activity,
+              String.format("Offline DB merge error."),
+              Toast.LENGTH_LONG).show()
+            Logger.e(LOG_TAG, error)
+          }
+        })
     }
 
     override fun onStart() {
@@ -151,22 +154,6 @@ class LocationFragmentActivity : AppCompatActivity() {
     override fun onDestroyView() {
       super.onDestroyView()
       mapView.onDestroy()
-      component?.locationEngine?.removeLocationEngineListener(this)
-    }
-  }
-
-  class EmptyFragment : Fragment() {
-    companion object {
-      const val TAG = "EmptyFragment"
-      fun newInstance(): EmptyFragment {
-        return EmptyFragment()
-      }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-      val textView = TextView(inflater?.context)
-      textView.text = "This is an empty Fragment"
-      return textView
     }
   }
 }
