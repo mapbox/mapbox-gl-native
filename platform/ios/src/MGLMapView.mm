720676f61ed01c1f248736859fac62b289cc3af1
Lloyd Sheng <i@lloydsheng.com>
2018-12-21 15:53:18 +0800

    add negative check

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -818,6 +818,7 @@ public:
 }
 
 - (void)setScaleBarOffset:(CGPoint)scaleBarOffset {
+    NSAssert(scaleBarOffset.x >= 0 && scaleBarOffset.y >= 0, @"The position offset of the scale bar should not be negative.");
     MGLLogDebug(@"Setting scaleBarOffset: %@", scaleBarOffset);
     _scaleBarOffset = scaleBarOffset;
     [self installScaleBarConstraints];
@@ -830,6 +831,7 @@ public:
 }
 
 - (void)setCompassViewOffset:(CGPoint)compassViewOffset {
+    NSAssert(compassViewOffset.x >= 0 && compassViewOffset.y >= 0, @"The position offset of the compass should not be negative.");
     MGLLogDebug(@"Setting compassViewOffset: %@", compassViewOffset);
     _compassViewOffset = compassViewOffset;
     [self installCompressViewConstraints];
@@ -842,6 +844,7 @@ public:
 }
 
 - (void)setLogoViewOffset:(CGPoint)logoViewOffset {
+    NSAssert(logoViewOffset.x >= 0 && logoViewOffset.y >= 0, @"The position offset of the logo should not be negative.");
     MGLLogDebug(@"Setting logoViewOffset: %@", logoViewOffset);
     _logoViewOffset = logoViewOffset;
     [self installLogoViewConstraints];
@@ -854,6 +857,7 @@ public:
 }
 
 - (void)setAttributionButtonOffset:(CGPoint)attributionButtonOffset {
+    NSAssert(attributionButtonOffset.x >= 0 && attributionButtonOffset.y >= 0, @"The position offset of the attribution should not be negative.");
     MGLLogDebug(@"Setting attributionButtonOffset: %@", attributionButtonOffset);
     _attributionButtonOffset = attributionButtonOffset;
     [self installAttributionButtonConstraints];
