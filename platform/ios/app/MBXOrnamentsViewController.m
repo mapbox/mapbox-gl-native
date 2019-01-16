7eaa9474f77053d9e90f728d84a762494f9c99e5
Lloyd Sheng <i@lloydsheng.com>
2019-01-16 18:52:25 +0800

    Add ornaments manual test demo

---
+++ platform/ios/app/MBXOrnamentsViewController.m
@@ -0,0 +1,92 @@
+#import "MBXOrnamentsViewController.h"
+
+@import Mapbox;
+
+@interface MBXOrnamentsViewController ()<MGLMapViewDelegate>
+
+@property (nonatomic) MGLMapView *mapView;
+@property (nonatomic) NSTimer *timer;
+@property (nonatomic) NSInteger currentPositionIndex;
+
+@end
+
+@implementation MBXOrnamentsViewController
+
+- (void)dealloc {
+    [self.timer invalidate];
+}
+
+- (void)setCurrentPositionIndex:(NSInteger)currentPositionIndex {
+    NSArray *ornamentPositions = @[@[
+                                       @(MGLOrnamentPositionTopLeft),
+                                       @(MGLOrnamentPositionTopRight),
+                                       @(MGLOrnamentPositionBottomRight),
+                                       @(MGLOrnamentPositionBottomLeft)
+                                       ],
+                                   @[
+                                       @(MGLOrnamentPositionTopRight),
+                                       @(MGLOrnamentPositionBottomRight),
+                                       @(MGLOrnamentPositionBottomLeft),
+                                       @(MGLOrnamentPositionTopLeft)
+                                       ],
+                                   @[
+                                       @(MGLOrnamentPositionBottomRight),
+                                       @(MGLOrnamentPositionBottomLeft),
+                                       @(MGLOrnamentPositionTopLeft),
+                                       @(MGLOrnamentPositionTopRight)
+                                       ],
+                                   @[
+                                       @(MGLOrnamentPositionBottomLeft),
+                                       @(MGLOrnamentPositionTopLeft),
+                                       @(MGLOrnamentPositionTopRight),
+                                       @(MGLOrnamentPositionBottomRight)
+                                       ],
+                                   @[
+                                       @(MGLOrnamentPositionTopLeft),
+                                       @(MGLOrnamentPositionTopRight),
+                                       @(MGLOrnamentPositionBottomRight),
+                                       @(MGLOrnamentPositionBottomLeft)
+                                       ],
+                                   ];
+    NSArray *currentPosition = ornamentPositions[currentPositionIndex];
+    self.mapView.scaleBarPosition = [currentPosition[0] integerValue];
+    self.mapView.compassViewPosition = [currentPosition[1] integerValue];
+    self.mapView.logoViewPosition = [currentPosition[2] integerValue];
+    self.mapView.attributionButtonPosition = [currentPosition[3] integerValue];
+    
+    _currentPositionIndex = currentPositionIndex;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Ornaments";
+
+    MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.frame];
+    mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [mapView setCenterCoordinate:CLLocationCoordinate2DMake(39.915143, 116.404053)
+                       zoomLevel:16
+                       direction:30
+                        animated:NO];
+    mapView.delegate = self;
+    mapView.showsScale = YES;
+    [self.view addSubview:mapView];
+
+    self.mapView = mapView;
+}
+
+- (void)tick {
+    self.currentPositionIndex ++;
+    if (self.currentPositionIndex >= 4) {
+        self.currentPositionIndex = 0;
+    }
+}
+
+- (void)mapViewDidFinishRenderingMap:(MGLMapView *)mapView fullyRendered:(BOOL)fullyRendered {
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1
+                                                  target:self
+                                                selector:@selector(tick)
+                                                userInfo:nil
+                                                 repeats:YES];
+}
+
+@end
