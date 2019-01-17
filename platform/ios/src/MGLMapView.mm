92352aea8c3bbad429947686e6e4a61fb71893da
Lloyd Sheng <i@lloydsheng.com>
2019-01-17 16:18:39 +0800

    Correct format specifier

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -812,7 +812,7 @@ public:
 }
 
 - (void)setScaleBarPosition:(MGLOrnamentPosition)scaleBarPosition {
-    MGLLogDebug(@"Setting scaleBarPosition: %d", scaleBarPosition);
+    MGLLogDebug(@"Setting scaleBarPosition: %lu", scaleBarPosition);
     _scaleBarPosition = scaleBarPosition;
     [self installScaleBarConstraints];
 }
@@ -825,7 +825,7 @@ public:
 }
 
 - (void)setCompassViewPosition:(MGLOrnamentPosition)compassViewPosition {
-    MGLLogDebug(@"Setting compassViewPosition: %d", compassViewPosition);
+    MGLLogDebug(@"Setting compassViewPosition: %lu", compassViewPosition);
     _compassViewPosition = compassViewPosition;
     [self installCompassViewConstraints];
 }
@@ -838,7 +838,7 @@ public:
 }
 
 - (void)setLogoViewPosition:(MGLOrnamentPosition)logoViewPosition {
-    MGLLogDebug(@"Setting logoViewPosition: %d", logoViewPosition);
+    MGLLogDebug(@"Setting logoViewPosition: %lu", logoViewPosition);
     _logoViewPosition = logoViewPosition;
     [self installLogoViewConstraints];
 }
@@ -851,7 +851,7 @@ public:
 }
 
 - (void)setAttributionButtonPosition:(MGLOrnamentPosition)attributionButtonPosition {
-    MGLLogDebug(@"Setting attributionButtonPosition: %d", attributionButtonPosition);
+    MGLLogDebug(@"Setting attributionButtonPosition: %lu", attributionButtonPosition);
     _attributionButtonPosition = attributionButtonPosition;
     [self installAttributionButtonConstraints];
 }
