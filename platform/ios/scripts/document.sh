b175a60cbf7843a024298d51a75f1c003cca86c3
Jason Wray <jason@mapbox.com>
2019-01-28 15:55:15 -0800

    [build, ios] Update jazzy to 0.9.5

--- platform/ios/scripts/document.sh
+++ platform/ios/scripts/document.sh
@@ -1,27 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
-set -u
+set -euo pipefail
 
 function step { >&2 echo -e "\033[1m\033[36m* $@\033[0m"; }
 function finish { >&2 echo -en "\033[0m"; }
 trap finish EXIT
 
 if [ -z `which jazzy` ]; then
-    step "Installing jazzy…"
-
-    CIRCLECI=${CIRCLECI:-false}
-    if [[ "${CIRCLECI}" == true ]]; then
-        sudo gem install jazzy -v 0.9.4 --no-document
-    else
-        gem install jazzy -v 0.9.4 --no-document
-    fi
-
-    if [ -z `which jazzy` ]; then
-        echo "Unable to install jazzy. See https://github.com/mapbox/mapbox-gl-native/blob/master/platform/ios/INSTALL.md"
-        exit 1
-    fi
+    ./platform/ios/scripts/install-packaging-dependencies.sh
 fi
 
 OUTPUT=${OUTPUT:-documentation}
