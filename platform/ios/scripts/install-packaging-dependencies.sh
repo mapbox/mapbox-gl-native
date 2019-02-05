28bdf1d542a99b8c65f71daf57d3cc1d714db5cc
Julian Rex <julian.rex@mapbox.com>
2019-02-05 07:41:23 -0500

    Specify sqlite3 version

--- platform/ios/scripts/install-packaging-dependencies.sh
+++ platform/ios/scripts/install-packaging-dependencies.sh
@@ -16,7 +16,7 @@ if [ -z `which jazzy` ]; then
 
     CIRCLECI=${CIRCLECI:-false}
     if [[ "${CIRCLECI}" == true ]]; then
-        sudo gem install sqlite3 -- --with-sqlite3-lib=/usr/lib
+        sudo gem install sqlite3 -v 1.3.13 -- --with-sqlite3-lib=/usr/lib
         sudo gem install jazzy -v $JAZZY_VERSION --no-document
     else
         gem install jazzy -v $JAZZY_VERSION --no-document
