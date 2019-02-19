d724176def4ed23cac954b674fb4bbd9df046f67
jmkiley <jordan.kiley@mapbox.com>
2019-02-18 17:59:05 -0800

    [ios] testing out coverage script

--- platform/ios/scripts/code-coverage.sh
+++ platform/ios/scripts/code-coverage.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 # Get code coverage 
 
-xcodebuild -workspace platform/ios/ios.xcworkspace -scheme CI -enableCodeCoverage YES build -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+# xcodebuild -workspace platform/ios/ios.xcworkspace -scheme CI -enableCodeCoverage YES build test -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 
 # echo build/Logs/Test/*.xcresult
 # result=
@@ -13,5 +13,10 @@ xcodebuild -workspace platform/ios/ios.xcworkspace -scheme CI -enableCodeCoverag
 cov_result=build/ios/ios/Logs/Test/*.xcresult/*_Test/*.xccovreport
 echo $cov_result
 xcrun xccov view $cov_result --json > output.json
+
+# json
+percentage=`node -e "console.log(require('./output.json').lineCoverage)"`
+
+echo "$(($percentage * 100))"
 
 rm -rf build/ios/ios/Logs/Test/*.xcresult/
