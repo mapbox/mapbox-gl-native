bde9d02fafb923524db4ed3221bb128a51009095
tobrun <tobrun.van.nuland@gmail.com>
2019-02-22 10:26:14 +0100

    [android] - add missing package-info.java files to packages for javadoc generation

---
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/module/http/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the Mapbox Maps Android Modular Http API classes.
+ */
+package com.mapbox.mapboxsdk.module.http;
---
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/module/telemetry/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the Mapbox Maps Android Modular Telemetry API classes.
+ */
+package com.mapbox.mapboxsdk.module.telemetry;
