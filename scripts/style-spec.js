7f4cd536c4d7de7cc67bc3d6ac14dd3222806fb4
Mikhail Pozdnyakov <mikhail.pozdnyakov@mapbox.com>
2019-03-04 22:38:53 +0200

    [scripts] Disable unsupported style spec features

--- scripts/style-spec.js
+++ scripts/style-spec.js
@@ -1,3 +1,6 @@
 var spec = module.exports = require('../mapbox-gl-js/src/style-spec/reference/v8');
 
 // Make temporary modifications here when Native doesn't have all features that JS has.
+delete spec.layout_symbol['symbol-sort-key'];
+delete spec.layout_symbol['symbol-z-order'].values['auto'];
+spec.layout_symbol['symbol-z-order'].default = 'viewport-y';
