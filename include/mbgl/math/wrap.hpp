b2f0608284c860dbf89ada16990073b30eb83d65
Bruno de Oliveira Abinader <bruno@mapbox.com>
2019-03-04 09:29:30 +0200

    [core] util::wrap(): std::fmod is not lossless

--- include/mbgl/math/wrap.hpp
+++ include/mbgl/math/wrap.hpp
@@ -9,6 +9,11 @@ namespace util {
 // arithmetic.
 template <typename T>
 T wrap(T value, T min, T max) {
+    if (value >= min && value < max) {
+        return value;
+    } else if (value == max) {
+        return min;
+    }
     T d = max - min;
     return std::fmod((std::fmod((value - min), d) + d), d) + min;
 }
