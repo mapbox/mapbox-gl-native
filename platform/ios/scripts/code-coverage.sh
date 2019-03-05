0a8c6b0d9765d48bf5658068cdd6e3e7bc6808db
jmkiley <jordan.kiley@mapbox.com>
2019-03-05 15:13:48 -0800

    [ios] rename S3 folder

--- platform/ios/scripts/code-coverage.sh
+++ platform/ios/scripts/code-coverage.sh
@@ -43,4 +43,4 @@ gzip -f $file_name
 if [ -z `which aws` ]; then	
     brew install awscli	
 fi
-aws s3 cp $file_name.gz s3://mapbox-loading-dock/raw/mobile.coverage/$current_date/
+aws s3 cp $file_name.gz s3://mapbox-loading-dock/raw/mobile.code_coverage/$current_date/
