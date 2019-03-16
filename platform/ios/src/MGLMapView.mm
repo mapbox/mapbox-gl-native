14a9eba16a29190725a5ab384737fe02961e8a04
Tim Watson <tewatson89@gmail.com>
2019-03-15 18:30:21 -0700

    Use contentInset for visibleCoordinateBounds

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -3272,7 +3272,7 @@ public:
 
 - (MGLCoordinateBounds)visibleCoordinateBounds
 {
-    return [self convertRect:self.bounds toCoordinateBoundsFromView:self];
+    return [self convertRect:self.bounds.inset(by: self.contentInset) toCoordinateBoundsFromView:self];
 }
 
 - (void)setVisibleCoordinateBounds:(MGLCoordinateBounds)bounds
