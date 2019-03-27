43f27e837cb444ba4263405aa4c39a651b222067
tobrun <tobrun.van.nuland@gmail.com>
2019-03-27 16:52:29 +0100

    [android] - use different Activity launchmode setup. This faills on pre 6.0 and post 9.0 Androdid OS versions. Stabilize tests. Ignore telemetry dialog test.

--- platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/integration/BaseIntegrationTest.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/integration/BaseIntegrationTest.kt
@@ -2,10 +2,12 @@ package com.mapbox.mapboxsdk.integration
 
 import android.content.Context
 import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.support.test.InstrumentationRegistry
 import android.support.test.uiautomator.*
 import org.junit.Before
+
+const val TIMEOUT_UI_SEARCH_WAIT = 5000L
 
 abstract class BaseIntegrationTest {
 
@@ -17,12 +19,18 @@ abstract class BaseIntegrationTest {
   }
 }
 
+/**
+ * Launches an activity with FLAG_ACTIVITY_NEW_TASK.
+ * <p>
+ * To resume an activity, you need to add a single instance launchmode to your manifest configuration.
+ * <p>
+ */
 fun UiDevice.launchActivity(context: Context, clazz: Class<*>) {
   val applicationPackage = InstrumentationRegistry.getTargetContext().packageName
   val intent = Intent(context, clazz)
-  intent.addFlags(FLAG_ACTIVITY_SINGLE_TOP)
+  intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
   InstrumentationRegistry.getContext().startActivity(intent)
-  wait(Until.hasObject(By.pkg(applicationPackage).depth(0)), 5000)
+  wait(Until.hasObject(By.pkg(applicationPackage).depth(0)), TIMEOUT_UI_SEARCH_WAIT)
 }
 
 fun UiDevice.scrollRecyclerViewTo(recycleItem: String) {
