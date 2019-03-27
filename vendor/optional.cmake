c7d53676d0dcbc72d71d921e385c656087b788a5
Julian Rex <julian.rex@gmail.com>
2019-03-27 09:58:41 -0400

    [ios, core] Updates project for iOS SDK 12.2 / Always use vendored optional (#14241)

--- vendor/optional.cmake
+++ vendor/optional.cmake
@@ -1,9 +1,5 @@
 add_library(optional INTERFACE)
 
-# This polyfill is needed for Windows and Android since these standard libraries don't ship with
-# usable versions of <experimental/optional>
-if(WIN32 OR MBGL_PLATFORM STREQUAL "android")
-    target_include_directories(optional SYSTEM INTERFACE
-        ${CMAKE_SOURCE_DIR}/vendor/optional/include
-    )
-endif()
+target_include_directories(optional SYSTEM INTERFACE
+    ${CMAKE_SOURCE_DIR}/vendor/optional/include
+)
