ef65313d2d5767e891decd30f70f4adde403737e
Nadia Barbosa <nadiabarbosa@me.com>
2019-03-29 16:20:24 -0700

    Fire timer after 5 seconds

--- platform/ios/app/MBXTabBarViewController.m
+++ platform/ios/app/MBXTabBarViewController.m
@@ -11,9 +11,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    _repeatingTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 repeats:YES block:^(NSTimer * _Nonnull timer) {
-        self.selectedIndex = self.selectedIndex == 1 ? 0 : 1;
-    }];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        self->_repeatingTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+            self.selectedIndex = self.selectedIndex == 1 ? 0 : 1;
+        }];
+    });
 }
 
 @end
