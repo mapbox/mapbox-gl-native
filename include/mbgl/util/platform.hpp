494fa1acb7aacebce967c83c1bfa3e7652c7b3de
Konstantin Käfer <mail@kkaefer.com>
2019-04-02 14:11:37 +0200

    [core] remove platform::show(Color)?DebugImage

--- include/mbgl/util/platform.hpp
+++ include/mbgl/util/platform.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <memory>
 #include <string>
 
 namespace mbgl {
@@ -23,10 +22,5 @@ void setCurrentThreadName(const std::string& name);
 // Makes the current thread low priority.
 void makeThreadLowPriority();
 
-// Shows an alpha image with the specified dimensions in a named window.
-void showDebugImage(std::string name, const char *data, size_t width, size_t height);
-
-// Shows an alpha image with the specified dimensions in a named window.
-void showColorDebugImage(std::string name, const char *data, size_t logical_width, size_t logical_height, size_t width, size_t height);
 } // namespace platform
 } // namespace mbgl
