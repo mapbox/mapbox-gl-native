c17b3255b32fd631a2821a0ce4c20978b11a6652
Tim Watson <tewatson89@gmail.com>
2019-04-04 12:43:55 -0700

    Use NSEdgeInsets instead of UIEdgeInsets

--- platform/macos/src/MGLMapView.mm
+++ platform/macos/src/MGLMapView.mm
@@ -1290,7 +1290,19 @@ public:
 }
 
 - (MGLCoordinateBounds)visibleCoordinateBounds {
-    return [self convertRect:UIEdgeInsetsInsetRect(self.bounds, self.contentInset) toCoordinateBoundsFromView:self];
+    return [self convertRect:NSEdgeInsetsInsetRect(self.bounds, self.contentInset) toCoordinateBoundsFromView:self];
+}
+
+- (CGRect)NSEdgeInsetsInsetRect(CGRect rect, NSEdgeInsets insets) {
+	if (((insets.top + insets.bottom) > rect.size.height) || ((insets.left + insets.right) > rect.size.width)) {
+		return CGRectNull;
+	} else {
+		rect.origin.x += insets.left;
+		rect.origin.y += insets.bottom;
+		rect.size.height -= (insets.top + insets.bottom);
+		rect.size.width -= (insets.left + insets.right);
+		return rect;
+	}
 }
 
 - (void)setVisibleCoordinateBounds:(MGLCoordinateBounds)bounds {
