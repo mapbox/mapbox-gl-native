cc1b6a91c8675bee788d5e5712268dc897c2aeef
Mikhail Pozdnyakov <mikhail.pozdnyakov@mapbox.com>
2019-04-10 11:13:18 +0300

    [core] Add `FadingTiles` layer type property

--- src/mbgl/style/layers/custom_layer.cpp
+++ src/mbgl/style/layers/custom_layer.cpp
@@ -13,7 +13,8 @@ namespace {
       LayerTypeInfo::Source::NotRequired,
       LayerTypeInfo::Pass3D::NotRequired,
       LayerTypeInfo::Layout::NotRequired,
-      LayerTypeInfo::Clipping::NotRequired };
+      LayerTypeInfo::Clipping::NotRequired,
+      LayerTypeInfo::FadingTiles::NotRequired };
 }  // namespace
 
 CustomLayer::CustomLayer(const std::string& layerID,
--- src/mbgl/style/layers/raster_layer.cpp
+++ src/mbgl/style/layers/raster_layer.cpp
@@ -23,7 +23,8 @@ const LayerTypeInfo* RasterLayer::Impl::staticTypeInfo() noexcept {
           LayerTypeInfo::Source::Required,
           LayerTypeInfo::Pass3D::NotRequired,
           LayerTypeInfo::Layout::NotRequired,
-          LayerTypeInfo::Clipping::NotRequired
+          LayerTypeInfo::Clipping::NotRequired,
+          LayerTypeInfo::FadingTiles::NotRequired
         };
     return &typeInfo;
 }
