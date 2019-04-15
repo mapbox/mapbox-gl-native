57512a9100b2b4333389f6f4a312e3c771b4c598
tobrun <tobrun.van.nuland@gmail.com>
2019-04-15 13:12:10 +0200

    [android] - provide default navigation experience for singleInstance activities

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/textureview/TextureViewDebugModeActivity.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/textureview/TextureViewDebugModeActivity.java
@@ -1,8 +1,10 @@
 package com.mapbox.mapboxsdk.testapp.activity.textureview;
 
+import android.view.MenuItem;
 import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.testapp.activity.maplayout.DebugModeActivity;
+import com.mapbox.mapboxsdk.testapp.utils.NavUtils;
 
 /**
  * Test activity showcasing the different debug modes and allows to cycle between the default map styles.
@@ -14,5 +16,25 @@ public class TextureViewDebugModeActivity extends DebugModeActivity implements O
     MapboxMapOptions mapboxMapOptions = super.setupMapboxMapOptions();
     mapboxMapOptions.textureMode(true);
     return mapboxMapOptions;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        // activity uses singleInstance for testing purposes
+        // code below provides a default navigation when using the app
+        onBackPressed();
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+
+  @Override
+  public void onBackPressed() {
+    // activity uses singleInstance for testing purposes
+    // code below provides a default navigation when using the app
+    NavUtils.navigateHome(this);
   }
 }
