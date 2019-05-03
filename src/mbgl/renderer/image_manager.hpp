d24524440c949e02dddfbd9e4b23eb4e23ea9e69
Alexander Shalamov <alexander.shalamov@mapbox.com>
2019-05-03 16:11:41 +0300

    [core] Add onRemoveUnusedStyleImages observer API

--- src/mbgl/renderer/image_manager.hpp
+++ src/mbgl/renderer/image_manager.hpp
@@ -56,6 +56,7 @@ public:
     void removeRequestor(ImageRequestor&);
     void notifyIfMissingImageAdded();
     void reduceMemoryUse();
+    void reduceMemoryUseIfCacheSizeExceedsLimit();
 
     ImageVersionMap updatedImageVersions;
 
@@ -75,6 +76,7 @@ private:
     };
     std::map<ImageRequestor*, MissingImageRequestPair> missingImageRequestors;
     std::map<std::string, std::set<ImageRequestor*>> requestedImages;
+    std::size_t requestedImagesCacheSize = 0ul;
     ImageMap images;
 
     ImageManagerObserver* observer = nullptr;
