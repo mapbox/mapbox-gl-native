f3dcf7937bca4ae6abb4245c1f1489635761408a
Jason Wray <jason@mapbox.com>
2019-05-14 15:38:51 -0700

    Revert "[ios] Remove events=true from new SKU requests"
    This reverts commit a51e3856d7d12576df2d0584a9b8a52b0241cb8f.

--- platform/darwin/src/http_file_source.mm
+++ platform/darwin/src/http_file_source.mm
@@ -208,17 +208,20 @@ std::unique_ptr<AsyncRequest> HTTPFileSource::request(const Resource& resource, 
         if (impl->accountType == 0 &&
             ([url.host isEqualToString:@"mapbox.com"] || [url.host hasSuffix:@".mapbox.com"])) {
             NSURLComponents *components = [NSURLComponents componentsWithURL:url resolvingAgainstBaseURL:NO];
-            NSURLQueryItem *accountsQueryItem = nil;
+            
+            NSMutableArray *newQueryItems = [NSMutableArray arrayWithArray:@[
+                [NSURLQueryItem queryItemWithName:@"events" value:@"true"]
+                ]];
 
             // Only add the token if we have enabled the accounts SDK
-            if (MGLAccountManager.isAccountsSDKEnabled) {
+            if (MGLAccountManager.isAccountsSDKEnabled)
+            {
                 NSCAssert(MGLAccountManager.skuToken, @"skuToken should be non-nil if the accounts SDK is enabled");
-                accountsQueryItem = [NSURLQueryItem queryItemWithName:@"sku" value:MGLAccountManager.skuToken];
-            } else {
-                accountsQueryItem = [NSURLQueryItem queryItemWithName:@"events" value:@"true"];
+                [newQueryItems addObject:[NSURLQueryItem queryItemWithName:@"sku" value:MGLAccountManager.skuToken]];
             }
 
-            components.queryItems = components.queryItems ? [components.queryItems arrayByAddingObject:accountsQueryItem] : @[accountsQueryItem];
+            components.queryItems = components.queryItems ? [components.queryItems arrayByAddingObjectsFromArray:newQueryItems] : newQueryItems;
+
             url = components.URL;
         }
 #endif
