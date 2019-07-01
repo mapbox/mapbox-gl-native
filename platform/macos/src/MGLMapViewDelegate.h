c24d67b38c1846f32069b68123bac100473504fe
Fabian Guerra <fabian.guerra@mapbox.com>
2019-07-01 15:59:22 -0700

    [ios, macos] Update shouldRemoveStyle method documentation.

--- platform/macos/src/MGLMapViewDelegate.h
+++ platform/macos/src/MGLMapViewDelegate.h
@@ -183,6 +183,20 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (nullable NSImage *)mapView:(MGLMapView *)mapView didFailToLoadImage:(NSString *)imageName;
 
+/**
+ Asks the delegate whether the map view should evict cached images.
+ 
+ This method is called in two scenarios: when the cumulative size of unused images
+ exceeds the cache size or when the last tile that includes the image is removed from
+ memory.
+ 
+ @param mapView The map view that is evicting the image.
+ @param imageName The image name that is going to be removed.
+ @return A Boolean value indicating whether the map view should evict
+ the cached image.
+ */
+- (BOOL)mapView:(MGLMapView *)mapView shouldRemoveStyleImage:(NSString *)imageName;
+
 #pragma mark Managing the Appearance of Annotations
 
 /**
@@ -332,17 +346,6 @@ NS_ASSUME_NONNULL_BEGIN
  @return A view controller for the given annotation.
  */
 - (nullable NSViewController *)mapView:(MGLMapView *)mapView calloutViewControllerForAnnotation:(id <MGLAnnotation>)annotation;
-
-/**
- Asks the delegate whether the map view should evict cached images.
- 
- This method is called in two scenarios: when the cumulative size of unused images
- goes over the cache size or when the image's requesting tile is destroyed.
- 
- @param mapView The map view that is evicting the image.
- @param imageName The image name that is going to be removed.
- */
-- (BOOL)mapView:(MGLMapView *)mapView shouldRemoveStyleImage:(NSString *)imageName;
 
 @end
 
