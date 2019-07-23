6d1c8b0846d1195b3570391b9586a88b9df0917f
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2019-07-23 17:40:30 +0300

    [build] Rewrite our CMake buildsystem
    - Try as much as possible not to use explicit compiler flags,
      relying on CMake to do the job in a portable away.
    - Build mbgl-core as a single static library and leverage lto as
      much as possible.
    - Support a MinSizRel build for other platforms.
    - Make GLFW and Node a client of mbgl-core public interface.
    - Same for the offline tool and static render tool.
    - Use only target settings (do not pollute the global scope)
    - Confine core code in a single file.
    - Confine platform code inside a single include (exception being Qt
      that needs AUTOMOC before adding the targets)
    - Align the Android build with the Android toolchain file.

---
+++ next/platform/ios/ios.cmake
@@ -0,0 +1,70 @@
+target_compile_definitions(
+    mbgl-core
+    PUBLIC MBGL_USE_GLES2 GLES_SILENCE_DEPRECATION
+)
+
+target_sources(
+    mbgl-core
+    PRIVATE
+        ${MBGL_ROOT}/platform/darwin/src/async_task.cpp
+        ${MBGL_ROOT}/platform/darwin/src/collator.mm
+        ${MBGL_ROOT}/platform/darwin/src/gl_functions.cpp
+        ${MBGL_ROOT}/platform/darwin/src/headless_backend_eagl.mm
+        ${MBGL_ROOT}/platform/darwin/src/http_file_source.mm
+        ${MBGL_ROOT}/platform/darwin/src/image.mm
+        ${MBGL_ROOT}/platform/darwin/src/local_glyph_rasterizer.mm
+        ${MBGL_ROOT}/platform/darwin/src/logging_nslog.mm
+        ${MBGL_ROOT}/platform/darwin/src/nsthread.mm
+        ${MBGL_ROOT}/platform/darwin/src/reachability.m
+        ${MBGL_ROOT}/platform/darwin/src/run_loop.cpp
+        ${MBGL_ROOT}/platform/darwin/src/string_nsstring.mm
+        ${MBGL_ROOT}/platform/darwin/src/timer.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/gfx/headless_backend.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/gfx/headless_frontend.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/gl/headless_backend.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/map/map_snapshotter.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/asset_file_source.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/default_file_source.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/file_source.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/file_source_request.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/local_file_request.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/local_file_source.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/offline.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/offline_database.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/offline_download.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/online_file_source.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/storage/sqlite3.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/text/bidi.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/util/png_writer.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/util/thread_local.cpp
+        ${MBGL_ROOT}/platform/default/src/mbgl/util/utf.cpp
+)
+
+target_include_directories(
+    mbgl-core
+    PRIVATE ${MBGL_ROOT}/platform/darwin/include ${MBGL_ROOT}/platform/default/include
+)
+
+include(${PROJECT_SOURCE_DIR}/vendor/icu.cmake)
+
+target_link_libraries(
+    mbgl-core
+    PRIVATE
+        "-framework CoreGraphics"
+        "-framework CoreImage"
+        "-framework CoreLocation"
+        "-framework CoreServices"
+        "-framework CoreText"
+        "-framework Foundation"
+        "-framework GLKit"
+        "-framework ImageIO"
+        "-framework OpenGLES"
+        "-framework QuartzCore"
+        "-framework Security"
+        "-framework SystemConfiguration"
+        "-framework UIKit"
+        "-framework WebKit"
+        mbgl-vendor-icu
+        sqlite3
+        z
+)
