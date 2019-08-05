bcdb1cd6e44f7cdff7ef004044ff7cf312088e18
Aleksandar Stojiljkovic <aleksandar.stojiljkovic@mapbox.com>
2019-08-05 22:13:59 +0300

    Ignore flaky render tests only on linux.
    Related to: #14423, #14870, #14768, #14859

--- platform/node/test/render.test.js
+++ platform/node/test/render.test.js
@@ -2,4 +2,11 @@ import {run} from '../../../mapbox-gl-js/test/integration/lib/render';
 import implementation from './suite_implementation';
 import ignores from './ignores.json';
 
-run('native', ignores, implementation);
+let platformIgnore = {};
+try {
+    platformIgnore = require('./ignores-' + process.platform + '.json');
+} catch (ex) {
+    console.error(ex);
+}
+
+run('native', Object.assign({}, ignores, platformIgnore), implementation);
