93b5d2c5e5d2dcd701d5aa87f6cb4e6039385a9b
Konstantin Käfer <mail@kkaefer.com>
2019-08-16 15:15:28 +0200

    [core] also remove version info in ImageManager when removing an image

--- src/mbgl/renderer/image_manager.cpp
+++ src/mbgl/renderer/image_manager.cpp
@@ -81,6 +81,7 @@ void ImageManager::removeImage(const std::string& id) {
         requestedImages.erase(requestedIt);
     }
     images.erase(it);
+    updatedImageVersions.erase(id);
 }
 
 const style::Image::Impl* ImageManager::getImage(const std::string& id) const {
