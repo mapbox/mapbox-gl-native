a1da06cb68ebfe98601390349d5d914a9c939c70
Mikhail Pozdnyakov <mikhail.pozdnyakov@mapbox.com>
2019-08-21 15:41:09 +0300

    [core] Fix GeoJSON tiles update
    Before this change, GeoJSON tiles data were updated before the
    corresponding layers were applied, therefore `GeometryTileWorker`
    parsed new data with the outdated layers.
    
    It caused the following render test failure:
    
    `mbgl-render-test text-max-width/unlimited regressions/mapbox-gl-native#9976 --recycle-map`
    
    Now a GeoJSON tile, which needs relayout, is not parsed until the valid layers
    are set.

--- src/mbgl/tile/geometry_tile_worker.hpp
+++ src/mbgl/tile/geometry_tile_worker.hpp
@@ -39,7 +39,7 @@ public:
     ~GeometryTileWorker();
 
     void setLayers(std::vector<Immutable<style::LayerProperties>>, uint64_t correlationID);
-    void setData(std::unique_ptr<const GeometryTileData>, uint64_t correlationID);
+    void setData(std::unique_ptr<const GeometryTileData>, bool resetLayers, uint64_t correlationID);
     void setShowCollisionBoxes(bool showCollisionBoxes_, uint64_t correlationID_);
     
     void onGlyphsAvailable(GlyphMap glyphs);
