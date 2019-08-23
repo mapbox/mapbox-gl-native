9eff849c743c6e861404a2c39afa53c4eecdca3d
Aleksandar Stojiljkovic <aleksandar.stojiljkovic@mapbox.com>
2019-08-23 12:04:32 +0300

    [core] Fix tileCover / TileCoordinate fromScreenCoordinate(#15442)
    Remove unecessary tile coordinate unproject -> project  step.
    Unproject+project isn't always a noop and this was the issue leading to wrong cover calculation in high pitch values.
    
    Fixes: https://github.com/mapbox/mapbox-gl-native/issues/15442

--- src/mbgl/map/transform_state.hpp
+++ src/mbgl/map/transform_state.hpp
@@ -17,6 +17,7 @@
 namespace mbgl {
 
 class UnwrappedTileID;
+class TileCoordinate;
 
 class TransformState {
     friend class Transform;
@@ -81,6 +82,8 @@ public:
     // Conversion
     ScreenCoordinate latLngToScreenCoordinate(const LatLng&) const;
     LatLng screenCoordinateToLatLng(const ScreenCoordinate&, LatLng::WrapMode = LatLng::Unwrapped) const;
+    // Implements mapbox-gl-js pointCoordinate() : MercatorCoordinate.
+    TileCoordinate screenCoordinateToTileCoordinate(const ScreenCoordinate&, uint8_t atZoom) const;
 
     double zoomScale(double zoom) const;
     double scaleZoom(double scale) const;
