d38237e0cb1f8e47eded53809a84455f6e0f9dff
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2019-09-06 14:52:37 +0300

    [core] Fix build with old ICU versions
    Use the old API for number formatting.

--- platform/default/src/mbgl/util/format_number.cpp
+++ platform/default/src/mbgl/util/format_number.cpp
@@ -23,7 +23,11 @@ std::string formatNumber(double number, const std::string& localeId, const std::
                 .toString();
     } else {
         ustr = icu::number::NumberFormatter::with()
+#if U_ICU_VERSION_MAJOR_NUM >= 62
                 .precision(icu::number::Precision::minMaxFraction(minFractionDigits, maxFractionDigits))
+#else
+                .rounding(icu::number::Rounder::minMaxFraction(minFractionDigits, maxFractionDigits))
+#endif
                 .locale(locale)
                 .formatDouble(number, status)
                 .toString();
