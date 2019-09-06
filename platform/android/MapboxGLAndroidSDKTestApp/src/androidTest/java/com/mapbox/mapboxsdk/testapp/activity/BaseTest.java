3605f31a126eade8d1565e902cc4f7e826b9d45a
Peng Liu <peng.liu@mapbox.com>
2019-09-06 12:23:47 +0300

    [android] Include more generated sanity tests for Android test app.

--- platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/testapp/activity/BaseTest.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/testapp/activity/BaseTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.CallSuper;
 import android.support.annotation.UiThread;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
 
 import com.mapbox.mapboxsdk.AppCenter;
 import com.mapbox.mapboxsdk.Mapbox;
@@ -34,6 +35,9 @@ public abstract class BaseTest extends AppCenter {
 
   @Rule
   public TestName testName = new TestName();
+
+  @Rule
+  public GrantPermissionRule grantLocationPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
 
   protected MapboxMap mapboxMap;
   protected MapView mapView;
