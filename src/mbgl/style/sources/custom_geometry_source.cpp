5d1342ebcb3b6b216949a40d88cd0678db05426a
Mikhail Pozdnyakov <mikhail.pozdnyakov@mapbox.com>
2019-09-17 18:51:53 +0300

    [core] Check layer compatibility with source

--- src/mbgl/style/sources/custom_geometry_source.cpp
+++ src/mbgl/style/sources/custom_geometry_source.cpp
@@ -1,12 +1,14 @@
-#include <mbgl/style/sources/custom_geometry_source.hpp>
-#include <mbgl/style/custom_tile_loader.hpp>
-#include <mbgl/style/sources/custom_geometry_source_impl.hpp>
-#include <mbgl/style/source_observer.hpp>
+#include <cstring>
+#include <map>
 #include <mbgl/actor/actor.hpp>
 #include <mbgl/actor/scheduler.hpp>
+#include <mbgl/style/custom_tile_loader.hpp>
+#include <mbgl/style/layer.hpp>
+#include <mbgl/style/source_observer.hpp>
+#include <mbgl/style/sources/custom_geometry_source.hpp>
+#include <mbgl/style/sources/custom_geometry_source_impl.hpp>
 #include <mbgl/tile/tile_id.hpp>
 #include <tuple>
-#include <map>
 
 namespace mbgl {
 namespace style {
@@ -27,6 +29,12 @@ void CustomGeometrySource::loadDescription(FileSource&) {
     baseImpl = makeMutable<CustomGeometrySource::Impl>(impl(), loader->self());
     loaded = true;
     observer->onSourceLoaded(*this);
+}
+
+bool CustomGeometrySource::supportsLayerType(const mbgl::style::LayerTypeInfo* info) const {
+    return !std::strcmp(info->type, "line") || !std::strcmp(info->type, "symbol") ||
+           !std::strcmp(info->type, "circle") || !std::strcmp(info->type, "fill") ||
+           !std::strcmp(info->type, "fill-extrusion") || !std::strcmp(info->type, "heatmap");
 }
 
 void CustomGeometrySource::setTileData(const CanonicalTileID& tileID,
