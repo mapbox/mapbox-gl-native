198e3453394ccb2b1f7db72d1858cfd18e302a1e
Alexander Shalamov <alexander.shalamov@mapbox.com>
2019-10-04 11:56:26 +0300

    [android] Check if Mapbox instance exists before creating default file source
    This check is needed for mbgl-benchmark target, as it does not require Mapbox
    instance, nor Android asset manager.

--- platform/android/src/mapbox.hpp
+++ platform/android/src/mapbox.hpp
@@ -10,6 +10,7 @@ namespace android {
 class Mapbox {
 public:
     static constexpr auto Name() { return "com/mapbox/mapboxsdk/Mapbox"; };
+    static jboolean hasInstance(jni::JNIEnv&);
     static jni::Local<jni::Object<AssetManager>> getAssetManager(jni::JNIEnv&);
     static void registerNative(jni::JNIEnv&);
 };
