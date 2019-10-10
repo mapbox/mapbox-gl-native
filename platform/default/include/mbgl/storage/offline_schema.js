990fdc43facab2d296f35bc8670a926d05fbe864
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2019-10-10 19:33:15 +0300

    [build] Make sure offline_schema.hpp is up-to-date
     - Get the bots to verify that.
     - Fix the paths.

--- platform/default/include/mbgl/storage/offline_schema.js
+++ platform/default/include/mbgl/storage/offline_schema.js
@@ -1,13 +1,15 @@
+#!/usr/bin/env node
+
 var fs = require('fs');
-fs.writeFileSync('platform/default/mbgl/storage/offline_schema.hpp', `#pragma once
+fs.writeFileSync('platform/default/include/mbgl/storage/offline_schema.hpp', `#pragma once
 
 // THIS IS A GENERATED FILE; EDIT offline_schema.sql INSTEAD
-// To regenerate, run \`node platform/default/mbgl/storage/offline_schema.js\`
+// To regenerate, run \`node platform/default/include/mbgl/storage/offline_schema.js\`
 
 namespace mbgl {
 
 static constexpr const char* offlineDatabaseSchema =
-${fs.readFileSync('platform/default/mbgl/storage/offline_schema.sql', 'utf8')
+${fs.readFileSync('platform/default/include/mbgl/storage/offline_schema.sql', 'utf8')
     .replace(/ *--.*/g, '')
     .split('\n')
     .filter(a => a)
