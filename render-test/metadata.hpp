0e03995c937ee1b7aa34474df1dbb5ac8371d547
Mikhail Pozdnyakov <mikhail.pozdnyakov@mapbox.com>
2019-10-11 10:43:02 +0300

    [test runner] Threshold for memory metrics

--- render-test/metadata.hpp
+++ render-test/metadata.hpp
@@ -49,12 +49,19 @@ struct FileSizeProbe {
 
 struct MemoryProbe {
     MemoryProbe() = default;
-    MemoryProbe(size_t peak_, size_t allocations_)
-        : peak(peak_)
-        , allocations(allocations_) {}
+    MemoryProbe(size_t peak_, size_t allocations_) : peak(peak_), allocations(allocations_), tolerance(0.0f) {}
 
     size_t peak;
     size_t allocations;
+    float tolerance;
+
+    static std::tuple<bool, float> checkPeak(const MemoryProbe& expected, const MemoryProbe& actual) {
+        return checkValue(expected.peak, actual.peak, actual.tolerance);
+    }
+
+    static std::tuple<bool, float> checkAllocations(const MemoryProbe& expected, const MemoryProbe& actual) {
+        return checkValue(expected.allocations, actual.allocations, actual.tolerance);
+    }
 };
 
 struct NetworkProbe {
