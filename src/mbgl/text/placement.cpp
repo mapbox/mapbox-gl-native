fec7d3c17826c8e5525ed2925e20854af122aa54
Ansis Brammanis <ansis@mapbox.com>
2019-10-23 17:21:38 -0400

    [core] avoid edges for labels that use text-variable-anchors
    to prevent clipped labels in rendered image tiles.

--- src/mbgl/text/placement.cpp
+++ src/mbgl/text/placement.cpp
@@ -164,9 +164,9 @@ void Placement::placeBucket(
     auto partiallyEvaluatedIconSize = bucket.iconSizeBinder->evaluateForZoom(state.getZoom());
 
     optional<CollisionTileBoundaries> avoidEdges;
-    if (mapMode == MapMode::Tile &&
-        (layout.get<style::SymbolAvoidEdges>() ||
-         layout.get<style::SymbolPlacement>() == style::SymbolPlacementType::Line)) {
+    if (mapMode == MapMode::Tile && (layout.get<style::SymbolAvoidEdges>() ||
+                                     layout.get<style::SymbolPlacement>() == style::SymbolPlacementType::Line ||
+                                     !layout.get<style::TextVariableAnchor>().empty())) {
         avoidEdges = collisionIndex.projectTileBoundaries(posMatrix);
     }
     
