3d9130150f2b8d5ae77d5fb95420bc1e78016f53
M.Stephen <truestyle2005@163.com>
2019-12-09 23:49:53 +0800

    add source/header

---
+++ platform/darwin/include/mbgl/interface/native_apple_interface.h
@@ -0,0 +1,44 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol MGLNativeAppleInterfaceDelegate <NSObject>
+
+- (NSString *)nai_skuToken;
+
+- (NSURLSessionConfiguration *)nai_sessionConfiguration;
+
+- (NSString *)nai_accountType;
+
+- (void)nai_startDownloadEvent:(NSString *)event type:(NSString *)type;
+
+- (void)nai_cancelDownloadEventForResponse:(NSURLResponse *)response;
+
+- (void)nai_stopDownloadEventForResponse:(NSURLResponse *)response;
+
+@end
+
+#define MGL_APPLE_EXPORT __attribute__((visibility ("default")))
+
+@interface MGLNativeAppleInterfaceTransmitter: NSObject
+
++ (MGLNativeAppleInterfaceTransmitter *)shared;
+
+@property (nonatomic, weak) id<MGLNativeAppleInterfaceDelegate> delegate;
+
+@property (nonatomic, readonly) NSURLSessionConfiguration *sessionConfiguration;
+
+@property (nonatomic, readonly) NSString *accountType;
+
+@property (nonatomic, readonly) NSString *skuToken;
+
+- (void)startDownloadEvent:(NSString *)event type:(NSString *)type;
+
+- (void)cancelDownloadEventForResponse:(NSURLResponse *)response;
+
+- (void)stopDownloadEventForResponse:(NSURLResponse *)response;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ platform/darwin/src/native_apple_interface.m
@@ -0,0 +1,55 @@
+#import <Foundation/Foundation.h>
+#import <mbgl/interface/native_apple_interface.h>
+
+@implementation MGLNativeAppleInterfaceTransmitter
+
+static MGLNativeAppleInterfaceTransmitter *instance = nil;
+
++ (MGLNativeAppleInterfaceTransmitter *)shared {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[MGLNativeAppleInterfaceTransmitter alloc] init];
+    });
+    return instance;
+}
+
+- (NSString *)skuToken {
+    if(_delegate && [_delegate respondsToSelector:@selector(nai_skuToken)]) {
+        return [_delegate nai_skuToken];
+    }
+    return nil;
+}
+
+- (NSURLSessionConfiguration *)sessionConfiguration {
+    if (_delegate && [_delegate respondsToSelector:@selector(nai_sessionConfiguration)]) {
+        return [_delegate nai_sessionConfiguration];
+    }
+    return nil;
+}
+
+- (NSString *)accountType {
+    if (_delegate && [_delegate respondsToSelector:@selector(nai_accountType)]) {
+        return [_delegate nai_accountType];
+    }
+    return nil;
+}
+
+- (void)startDownloadEvent:(NSString *)event type:(NSString *)type {
+    if (_delegate && [_delegate respondsToSelector:@selector(nai_startDownloadEvent:type:)]) {
+        [_delegate nai_startDownloadEvent:event type:type];
+    }
+}
+
+- (void)cancelDownloadEventForResponse:(NSURLResponse *)response {
+    if (_delegate && [_delegate respondsToSelector:@selector(nai_cancelDownloadEventForResponse:)]) {
+        return [_delegate nai_cancelDownloadEventForResponse:response];
+    }
+}
+
+- (void)stopDownloadEventForResponse:(NSURLResponse *)response {
+    if (_delegate && [_delegate respondsToSelector:@selector(nai_stopDownloadEventForResponse:)]) {
+        return [_delegate nai_stopDownloadEventForResponse:response];
+    }
+}
+
+@end
