e4cab3f37fda236339d40a0de291cd75c9b1a51e
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-12-29 15:27:58 +0100

    [util] fix thread.cpp portability

--- platform/default/src/mbgl/util/thread.cpp
+++ platform/default/src/mbgl/util/thread.cpp
@@ -12,26 +12,32 @@ namespace platform {
 
 std::string getCurrentThreadName() {
     char name[32] = "unknown";
+#ifdef __linux__
     pthread_getname_np(pthread_self(), name, sizeof(name));
+#endif
 
     return name;
 }
 
 void setCurrentThreadName(const std::string& name) {
+#ifdef __linux__
     if (name.size() > 15) { // Linux hard limit (see manpages).
         pthread_setname_np(pthread_self(), name.substr(0, 15).c_str());
     } else {
         pthread_setname_np(pthread_self(), name.c_str());
     }
+#endif
 }
 
 void makeThreadLowPriority() {
+#ifdef SCHED_IDLE
     struct sched_param param;
     param.sched_priority = 0;
 
     if (sched_setscheduler(0, SCHED_IDLE, &param) != 0) {
         Log::Warning(Event::General, "Couldn't set thread scheduling policy");
     }
+#endif
 }
 
 void attachThread() {
