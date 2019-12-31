70a6a5200e102880a7eacc5194a3f3065ffeb1de
Alex Chugunov <alex.chugunov@wework.com>
2019-12-30 16:52:04 -0800

    Use copy attribute instead of assign on accessToken property

--- platform/darwin/src/MGLAccountManager.h
+++ platform/darwin/src/MGLAccountManager.h
@@ -34,7 +34,7 @@ MGL_EXPORT
     and the type `String`. Alternatively, you may call this method from your
     application delegate’s `-applicationDidFinishLaunching:` method.
  */
-@property (class, assign, nullable) NSString *accessToken;
+@property (class, copy, nullable) NSString *accessToken;
 
 @end
 
