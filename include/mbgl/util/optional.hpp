7652be37e8bf5f891975c2ffdd7d2c1de5a334e2
Alexander Shalamov <alexander.shalamov@mapbox.com>
2020-01-14 17:27:05 +0200

    temporary add flag for cpp 17

--- include/mbgl/util/optional.hpp
+++ include/mbgl/util/optional.hpp
@@ -1,5 +1,18 @@
 #pragma once
 
+#if __has_include(<optional>) && __cplusplus >= 201703L
+#include <optional>
+
+namespace mbgl {
+
+template <typename T>
+using optional = std::optional<T>;
+
+using nullopt_t = std::nullopt_t;
+constexpr nullopt_t nullopt = std::nullopt;
+
+} // namespace mbgl
+#else
 #include <optional.hpp>
 
 namespace mbgl {
@@ -11,3 +24,4 @@ using nullopt_t = std::experimental::nullopt_t;
 constexpr nullopt_t nullopt = std::experimental::nullopt;
 
 } // namespace mbgl
+#endif
