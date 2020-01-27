0de6ffc383022e3ebfb658a729a0c9d7d7b026c4
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2020-01-27 16:31:33 +0200

    [build] Cleanup CircleCI configuration
    - Remove the legacy build jobs
    - Remove the next- namespace.

--- render-test/ios/setup_test_data.sh
+++ render-test/ios/setup_test_data.sh
@@ -9,8 +9,8 @@ cp -r ../../mapbox-gl-js/test/integration/query-tests ../../test-data/integratio
 cp -r ../../metrics/expectations ../../test-data/
 cp -r ../../metrics/ignores ../../test-data/
 cp -r ../../metrics/tests ../../test-data/
-cp -r ../../metrics/next-ios-render-test-runner ../../test-data/
-cp -r ../../metrics/next-ios-render-test-runner-style.json ../../test-data/
-cp -r ../../metrics/next-ios-render-test-runner-metrics.json ../../test-data/
+cp -r ../../metrics/ios-render-test-runner ../../test-data/
+cp -r ../../metrics/ios-render-test-runner-style.json ../../test-data/
+cp -r ../../metrics/ios-render-test-runner-metrics.json ../../test-data/
 cp -r ../../metrics/cache-metrics.db ../../test-data/
 cp -r ../../metrics/cache-style.db ../../test-data/
