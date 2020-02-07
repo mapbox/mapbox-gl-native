c9bbb8699cf62c85e16e1ba721fd1e78203e0f4d
Alexander Shalamov <alexander.shalamov@mapbox.com>
2020-02-07 16:04:20 +0200

    [core] Make CustomGeometrySource::TileOptions immutable

--- src/mbgl/style/sources/custom_geometry_source_impl.cpp
+++ src/mbgl/style/sources/custom_geometry_source_impl.cpp
@@ -4,27 +4,20 @@
 namespace mbgl {
 namespace style {
 
-CustomGeometrySource::Impl::Impl(std::string id_,
-                               const CustomGeometrySource::Options options)
+CustomGeometrySource::Impl::Impl(std::string id_, CustomGeometrySource::Options options)
     : Source::Impl(SourceType::CustomVector, std::move(id_)),
-      tileOptions(options.tileOptions),
+      tileOptions(makeMutable<CustomGeometrySource::TileOptions>(options.tileOptions)),
       zoomRange(options.zoomRange),
-      loaderRef({}) {
-}
+      loaderRef({}) {}
 
 CustomGeometrySource::Impl::Impl(const Impl& impl, ActorRef<CustomTileLoader> loaderRef_)
-    : Source::Impl(impl),
-    tileOptions(impl.tileOptions),
-    zoomRange(impl.zoomRange),
-    loaderRef(loaderRef_){
-    
-}
+    : Source::Impl(impl), tileOptions(impl.tileOptions), zoomRange(impl.zoomRange), loaderRef(loaderRef_) {}
 
 optional<std::string> CustomGeometrySource::Impl::getAttribution() const {
     return {};
 }
 
-CustomGeometrySource::TileOptions CustomGeometrySource::Impl::getTileOptions() const {
+Immutable<CustomGeometrySource::TileOptions> CustomGeometrySource::Impl::getTileOptions() const {
     return tileOptions;
 }
 
