46d705c7d73f249d6a3aa36dde6e05c56fb6eb92
Juha Alanen <juha.alanen@mapbox.com>
2020-02-12 15:50:39 +0200

    [test-runner] Share common code between test runners
    Also remove some unnecessary files.

--- test/ios/iosTestRunner.h
+++ test/ios/iosTestRunner.h
@@ -1,5 +1,8 @@
 #import <Foundation/Foundation.h>
 
+NSString *dataDir = @"test-data";
+NSString *resultFilePath = @"test/results.xml";
+
 __attribute__((visibility ("default")))
 @interface IosTestRunner : NSObject
 
