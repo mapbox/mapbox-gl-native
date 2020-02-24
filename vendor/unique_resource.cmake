8f39f5d9e6409eba2cb2055fbe2517088bad10a9
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2020-02-24 15:49:11 +0200

    [build] Generate the LICENSE.md for mbgl-core
    Add a script that for everything we link against
    mbgl-core, it will check the license and generate
    a file all the dependencies listed with the respective
    authors, project urls and license.

--- vendor/unique_resource.cmake
+++ vendor/unique_resource.cmake
@@ -10,3 +10,12 @@ target_include_directories(
     mbgl-vendor-unique_resource SYSTEM
     INTERFACE ${CMAKE_CURRENT_LIST_DIR}/unique_resource
 )
+
+set_target_properties(
+    mbgl-vendor-unique_resource
+    PROPERTIES
+        INTERFACE_MAPBOX_NAME "unique_resource"
+        INTERFACE_MAPBOX_URL "https://github.com/okdshin/unique_resource"
+        INTERFACE_MAPBOX_AUTHOR "Shintarou Okada"
+        INTERFACE_MAPBOX_LICENSE ${CMAKE_CURRENT_LIST_DIR}/unique_resource/LICENSE.txt
+)
