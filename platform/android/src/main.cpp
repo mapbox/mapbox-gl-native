338cfd1b33dfe26b9402d79cc64609b944a9f1d1
Igor Tarasov <igor.tarasov@mapbox.com>
2020-04-02 18:36:55 +0300

    [android] [build] [ci] Move Android JNI bindings code to mapbox-gl-native-android repo. (#16356)

--- platform/android/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "jni.hpp"
-#include "jni_native.hpp"
-#include <jni/jni.hpp>
-
-extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *) {
-    assert(vm != nullptr);
-    mbgl::android::registerNatives(vm);
-    return JNI_VERSION_1_6;
-}
