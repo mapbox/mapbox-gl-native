d1aff1c73f542f1ae17b76555b6343da4f1ae474
Alexander Shalamov <alexander.shalamov@mapbox.com>
2020-04-06 16:17:39 +0300

    Add rotation.cpp / hpp files

---
+++ include/mbgl/style/rotation.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace mbgl {
+namespace style {
+
+// Could be made a template class if needed
+// template <size_t Period>
+// size_t period() const noexcept { return Period; }
+class Rotation {
+public:
+   Rotation() = default;
+   Rotation(double angle_) : angle(angle_) {}
+   double period() const noexcept { return 360.0; }
+   double getAngle() const noexcept { return angle; }
+
+    friend bool operator==(const Rotation& lhs, const Rotation& rhs) {
+        return lhs.angle == rhs.angle;
+    }
+
+    friend bool operator!=(const Rotation& lhs, const Rotation& rhs) {
+        return !(lhs == rhs);
+    }
+
+private:
+    double angle;
+};
+
+} // namespace style
+} // namespace mbgl
