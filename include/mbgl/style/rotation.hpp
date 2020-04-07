a2b3ed1371ec4beebd79dc6b782d515c296429a7
Gali Nelle <galinelle.mapbox@gmail.com>
2020-04-07 15:06:55 +0300

    Add unit tests for Rotation interpolator

--- include/mbgl/style/rotation.hpp
+++ include/mbgl/style/rotation.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <mbgl/math/wrap.hpp>
 namespace mbgl {
 namespace style {
 
@@ -9,8 +9,8 @@ namespace style {
 class Rotation {
 public:
     Rotation() = default;
-    Rotation(double angle_) : angle(angle_) {}
-    double period() const noexcept { return 360.0; }
+    Rotation(double angle_) : angle(mbgl::util::wrap<double>(angle_, 0, period())) {}
+    constexpr double period() const noexcept { return 360.0; }
     double getAngle() const noexcept { return angle; }
 
     friend bool operator==(const Rotation& lhs, const Rotation& rhs) { return lhs.angle == rhs.angle; }
