3cd5ec2aed3a9d549dee8ddf5a7ff21a913ec4c7
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2020-04-24 16:44:17 +0300

    [build] Make the output directory consistent
    Instead of placing binaries and libraries
    scattered inside the build directory.

--- render-test/ios/codesigning/codesign_all.sh
+++ render-test/ios/codesigning/codesign_all.sh
@@ -3,9 +3,9 @@ identity=$(/usr/bin/env xcrun security find-identity -v -p codesigning)
 var=${identity#*1)\ }
 CODE_SIGN_IDENTITY=${var%\ \"Apple\ Development*}
 
-codesign --force --sign $CODE_SIGN_IDENTITY --entitlements codesigning/RenderTestApp.app.xcent --timestamp=none ./Release-iphoneos/RenderTestApp.app
-codesign --force --sign $CODE_SIGN_IDENTITY --deep --preserve-metadata=identifier,entitlements,flags --timestamp=none ./Release-iphoneos/RenderTestApp.app/Frameworks/libXCTestSwiftSupport.dylib
-codesign --force --sign $CODE_SIGN_IDENTITY --deep --preserve-metadata=identifier,entitlements,flags --timestamp=none ./Release-iphoneos/RenderTestApp.app/Frameworks/libXCTestBundleInject.dylib
-codesign --force --sign $CODE_SIGN_IDENTITY --deep --preserve-metadata=identifier,entitlements,flags --timestamp=none ./Release-iphoneos/RenderTestApp.app/Frameworks/XCTAutomationSupport.framework
-codesign --force --sign $CODE_SIGN_IDENTITY --deep --preserve-metadata=identifier,entitlements,flags --timestamp=none ./Release-iphoneos/RenderTestApp.app/Frameworks/XCTest.framework
-codesign --force --sign $CODE_SIGN_IDENTITY --deep --entitlements codesigning/RenderTestAppTests.xctest.xcent --timestamp=none ./Release-iphoneos/RenderTestApp.app/PlugIns/RenderTestAppTests.xctest
+codesign --force --sign $CODE_SIGN_IDENTITY --entitlements codesigning/RenderTestApp.app.xcent --timestamp=none ./Release/RenderTestApp.app
+codesign --force --sign $CODE_SIGN_IDENTITY --deep --preserve-metadata=identifier,entitlements,flags --timestamp=none ./Release/RenderTestApp.app/Frameworks/libXCTestSwiftSupport.dylib
+codesign --force --sign $CODE_SIGN_IDENTITY --deep --preserve-metadata=identifier,entitlements,flags --timestamp=none ./Release/RenderTestApp.app/Frameworks/libXCTestBundleInject.dylib
+codesign --force --sign $CODE_SIGN_IDENTITY --deep --preserve-metadata=identifier,entitlements,flags --timestamp=none ./Release/RenderTestApp.app/Frameworks/XCTAutomationSupport.framework
+codesign --force --sign $CODE_SIGN_IDENTITY --deep --preserve-metadata=identifier,entitlements,flags --timestamp=none ./Release/RenderTestApp.app/Frameworks/XCTest.framework
+codesign --force --sign $CODE_SIGN_IDENTITY --deep --entitlements codesigning/RenderTestAppTests.xctest.xcent --timestamp=none ./Release/RenderTestApp.app/PlugIns/RenderTestAppTests.xctest
