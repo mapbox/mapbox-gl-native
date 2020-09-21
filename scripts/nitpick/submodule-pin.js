b8edc2399b9640498ccbbbb5b8f058c63d070933
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2020-09-21 13:55:26 +0300

    [build] s/master/main on GL JS
    Check if pinned against main.

--- scripts/nitpick/submodule-pin.js
+++ scripts/nitpick/submodule-pin.js
@@ -6,8 +6,8 @@ const child_process = require('child_process');
 const head = child_process.execSync('git -C mapbox-gl-js rev-parse HEAD').toString().trim();
 const revs = child_process.execSync(`git -C mapbox-gl-js branch -a --contains ${head}`).toString().split('\n');
 
-if (revs.indexOf('  remotes/origin/master') >= 0) {
-    nitpick.ok(`mapbox-gl-js submodule pin is merged to master`);
+if (revs.indexOf('  remotes/origin/main') >= 0) {
+    nitpick.ok(`mapbox-gl-js submodule pin is merged to main`);
 } else {
-    nitpick.fail(`mapbox-gl-js submodule is pinned to ${head}, which isn't merged to master`);
+    nitpick.fail(`mapbox-gl-js submodule is pinned to ${head}, which isn't merged to main`);
 }
